2fdbea4418b8bf34d119286202f32f338081e0d3
johnghattas <john.backend@utdsoftware.com>
2024-08-18 14:33:46 +0300

    update repository

--- vendor/imdhemy/laravel-purchases/routes/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-use Illuminate\Support\Facades\Route;
-use Imdhemy\Purchases\Http\Controllers\ServerNotificationController;
-
-Route::post('/liap/notifications', ServerNotificationController::class)
-    ->name('liap.serverNotifications');
