8ea06dba0a071b664dff05eda18ef91b5d58a5ca
Erinyelkess <ariny.backend@utdsoftware.com>
2024-08-20 10:38:33 +0300

    ovip

--- app/Http/Resources/Api/V1/OVipResource.php
+++ app/Http/Resources/Api/V1/OVipResource.php
@@ -10,6 +10,12 @@ class OVipResource extends JsonResource
         return [
             'id' => $this->id,
             'level' => $this->level,
+            "sort"=> $this->sort,
+            "name"=> $this->name,
+            "img"=> $this->img,
+            "price"=> $this->price,
+            "expire"=> $this->expire,
+            "exp"=> $this->exp,
             'privilegs' => VipPrivilegeResource::collection(
                 $this->privilegs->map(function ($priv) {
                     $priv->oVipPrivilegIds = $this->privilegs->pluck('id')->toArray();
