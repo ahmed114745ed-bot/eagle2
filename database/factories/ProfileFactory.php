ea1fafe132d7d6a39a00593cf62eaa9fd2300063
johnghattas <john.backend@utdsoftware.com>
2024-08-25 17:33:11 +0300

    create user factory

--- database/factories/ProfileFactory.php
+++ database/factories/ProfileFactory.php
@@ -2,11 +2,14 @@
 
 namespace Database\Factories;
 
+use App\Models\Profile;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
 class ProfileFactory extends Factory
 {
+
+    protected $model = Profile::class;
     /**
      * Define the model's default state.
      *
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,11 +2,15 @@
 
 namespace Database\Factories;
 
+use App\Models\Profile;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
 {
+
+    protected $model = User::class;
     /**
      * Define the model's default state.
      *
@@ -18,9 +22,18 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
+            'phone' => $this->faker->phoneNumber,
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'lat' => $this->faker->latitude,
+            'long' => $this->faker->longitude
         ];
+    }
+    public function configure()
+    {
+        return $this->afterCreating(function (User $user) {
+            $user->profile()->save(Profile::factory()->make());
+        });
     }
 
     /**
