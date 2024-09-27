a559c94064a0950813d94190a1b027537dd7ff73
Erinyelkess <ariny.backend@utdsoftware.com>
2024-09-27 17:53:52 +0300

    file

--- app/Http/Middleware/DecryptDataMiddleware.php
+++ app/Http/Middleware/DecryptDataMiddleware.php
@@ -12,7 +12,7 @@ class DecryptDataMiddleware
     protected $privateKey;
     public function __construct()
     {
-        $this->privateKey = file_get_contents(storage_path('app/public/PrivetKey.txt'));
+        $this->privateKey = file_get_contents(public_path('PrivetKey.txt'));
     }
     /**
      * Handle an incoming request.
