69a40423d7b333cd70aebcdcb66f91192802bacb
Erinyelkess <ariny.backend@utdsoftware.com>
2024-10-02 18:15:53 +0300

    test

--- app/Admin/Controllers/DedicateWareController.php
+++ app/Admin/Controllers/DedicateWareController.php
@@ -18,83 +18,85 @@ use Encore\Admin\Controllers\HasResourceActions;
 class DedicateWareController extends MainController
 {
     use HasResourceActions;
-    public $permission_name = 'vips-dedicate';    /**
+    public $permission_name = 'vips-dedicate';
+    /**
      * Make a grid builder.
      *
      * @return Grid
      */
     protected function grid()
     {
-        $typeSpecial=false;
-        if (request('type') == 25){
-            $typeSpecial=true;
+        $typeSpecial = false;
+        $request = request('type');
+        if ($request !=null && ($request == 25)) {
+            $typeSpecial = true;
         }
         $grid = new Grid(new Ware);
-        $grid->model ()->orderByDesc('created_at');
-        if ($typeSpecial){
+        $grid->model()->orderByDesc('created_at');
+        if ($typeSpecial) {
             $grid->model()->where('type', '=', 25);
-        }else{
+        } else {
             $grid->model()->where('type', '!=', 25);
         }
         $grid->id('ID');
-        $grid->column('get_type',__ ('get_type'))->select (
+        $grid->column('get_type', __('get_type'))->select(
             [
-                1=>trans ('vip level automatic acquisition'),
-            //    2=>trans ('activity'),
-            //    3=>trans ('treasure box'),
-                4=>trans ('purchase'),
-            // 5=>trans ('background modification'),
-                6=>trans ('limited time purchase'),
-            // 7=>trans ('treasure box point exchange'),
-            // 8=>trans ('cp level unlock'),
+                1 => trans('vip level automatic acquisition'),
+                //    2=>trans ('activity'),
+                //    3=>trans ('treasure box'),
+                4 => trans('purchase'),
+                // 5=>trans ('background modification'),
+                6 => trans('limited time purchase'),
+                // 7=>trans ('treasure box point exchange'),
+                // 8=>trans ('cp level unlock'),
             ]
         );
         if (!$typeSpecial) {
-            $grid->column('type',__ ('type'))->select (
+            $grid->column('type', __('type'))->select(
                 [
-                    1=>trans ('Gemstone'),
-                    3=>trans ('Card Scroll'),
-                    4=>trans ('Avatar Frame'),
-                    5=>trans ('Bubble Frame'),
-                    6=>trans ('Entering Special Effects'),
-                    7=>trans ('Microphone Aperture'),
-                    8=>trans ('Badge'),
-                    9=>trans ('NoKick'),
-                    10=>trans ('Icon'),
-                    11=>trans ('intro animation'),
-                    12=>trans ('wapel'),
-                    13=>trans ('hide country and last login'),
-                    14=>trans ('vip gifts'),
-                    15=>trans ('no pan'),
-                    16=>trans ('hidden room'),
-                    17=>trans ('anonymous man'),
-                    18=>trans ('colored name'),
-                    19=>trans ('profile visitors hide in'),
-                    25=>trans ('Special Id'),
+                    1 => trans('Gemstone'),
+                    3 => trans('Card Scroll'),
+                    4 => trans('Avatar Frame'),
+                    5 => trans('Bubble Frame'),
+                    6 => trans('Entering Special Effects'),
+                    7 => trans('Microphone Aperture'),
+                    8 => trans('Badge'),
+                    9 => trans('NoKick'),
+                    10 => trans('Icon'),
+                    11 => trans('intro animation'),
+                    12 => trans('wapel'),
+                    13 => trans('hide country and last login'),
+                    14 => trans('vip gifts'),
+                    15 => trans('no pan'),
+                    16 => trans('hidden room'),
+                    17 => trans('anonymous man'),
+                    18 => trans('colored name'),
+                    19 => trans('profile visitors hide in'),
+                    25 => trans('Special Id'),
                     21 => trans('sound effect'),
                     22 => trans('upload GIF image')
                 ]
             );
         }
-        $grid->column('name',__ ('name'))->editable ();
+        $grid->column('name', __('name'))->editable();
         if (!$typeSpecial) {
             $grid->title(__('title'));
-        }else{
+        } else {
             $grid->value(__('value'));
         }
-        $grid->column('price',__ ('price'))->currency ();
+        $grid->column('price', __('price'))->currency();
         $grid->level(__('level'));
-        $grid->column('show_img',__ ('show_img'))->image ('',30);
+        $grid->column('show_img', __('show_img'))->image('', 30);
 
-        $grid->column('color',__('color'));
+        $grid->column('color', __('color'));
         $grid->expire(__('expire'));
         if (\Encore\Admin\Facades\Admin::user()->can('*')) {
             $grid->column('enable', __('enable'))->switch(Common::getSwitchStates());
         }
-        $grid->sort(__('sort'),__ ('sort'));
+        $grid->sort(__('sort'), __('sort'));
         $grid->disableExport();
 
-        $grid->actions (function ($actions){
+        $grid->actions(function ($actions) {
             $actions->disableDelete();
             $actions->disableEdit();
             $actions->disableView();
@@ -102,12 +104,12 @@ class DedicateWareController extends MainController
         });
         $grid->disableCreateButton();
 
-        $grid->tools(function (Grid\Tools $tools) use ($typeSpecial){
+        $grid->tools(function (Grid\Tools $tools) use ($typeSpecial) {
             $url = '/admin/wares/create';
-            if ($typeSpecial){
+            if ($typeSpecial) {
                 $url = "/admin/special-wares/create";
             }
-            $button = '<a href="'.$url.'" class="btn btn-sm btn-success"><i class="fa fa-plus"></i>&nbsp;&nbsp;Create New</a>';
+            $button = '<a href="' . $url . '" class="btn btn-sm btn-success"><i class="fa fa-plus"></i>&nbsp;&nbsp;Create New</a>';
             $tools->append($button);
         });
         return $grid;
@@ -124,43 +126,43 @@ class DedicateWareController extends MainController
         $form = new Form(new Ware());
 
         $form->display('ID');
-        $form->select('get_type', trans('get_type'))->options (
-           [
-               1=>trans ('vip level automatic acquisition'),
-        //               2=>trans ('activity'),
-        //               3=>trans ('treasure box'),
-               4=>__ ('purchase'),
-        //               5=>trans ('background modification'),
-                    //    6=>trans ('limited time purchase'),
-        //               7=>trans ('treasure box point exchange'),
-        //               8=>trans ('cp level unlock'),
-           ]
-        )->default (4);
-        $form->select('type', trans('type'))->options (
+        $form->select('get_type', trans('get_type'))->options(
             [
-                1=>trans ('Gemstone'),
-                3=>trans ('Card Scroll'),
-                4=>trans ('Avatar Frame'),
-                5=>trans ('Bubble Frame'),
-                6=>trans ('Entering Special Effects'),
-                7=>trans ('Microphone Aperture'),
-                8=>trans ('Badge'),
-                9=>trans ('NoKick'),
-                10=>trans ('Icon'),
-                11=>trans ('intro animation'),
-                12=>trans ('wapel'),
-                13=>trans ('hide country'),
-                14=>trans ('vip gifts'),
-                15=>trans ('no pan'),
-                16=>trans ('hidden room'),
-                17=>trans ('anonymous man'),
-                18=>trans ('colored name'),
-                19=>trans ('profile visitors hide in'),
-                20=>trans ('hide last active'),
+                1 => trans('vip level automatic acquisition'),
+                //               2=>trans ('activity'),
+                //               3=>trans ('treasure box'),
+                4 => __('purchase'),
+                //               5=>trans ('background modification'),
+                //    6=>trans ('limited time purchase'),
+                //               7=>trans ('treasure box point exchange'),
+                //               8=>trans ('cp level unlock'),
+            ]
+        )->default(4);
+        $form->select('type', trans('type'))->options(
+            [
+                1 => trans('Gemstone'),
+                3 => trans('Card Scroll'),
+                4 => trans('Avatar Frame'),
+                5 => trans('Bubble Frame'),
+                6 => trans('Entering Special Effects'),
+                7 => trans('Microphone Aperture'),
+                8 => trans('Badge'),
+                9 => trans('NoKick'),
+                10 => trans('Icon'),
+                11 => trans('intro animation'),
+                12 => trans('wapel'),
+                13 => trans('hide country'),
+                14 => trans('vip gifts'),
+                15 => trans('no pan'),
+                16 => trans('hidden room'),
+                17 => trans('anonymous man'),
+                18 => trans('colored name'),
+                19 => trans('profile visitors hide in'),
+                20 => trans('hide last active'),
                 21 => trans('sound effect'),
                 22 => trans('upload GIF image')
             ]
-        )->rules ('required');
+        )->rules('required');
         //        ->rules (function ($form){
         //            if (!$id = $form->model()->id) {
         //                return 'required';
@@ -168,17 +170,17 @@ class DedicateWareController extends MainController
         //        });
         $form->text('name', trans('name'));
         $form->text('title', trans('title'));
-        $form->currency('price', trans('price'))->symbol ('💰');
+        $form->currency('price', trans('price'))->symbol('💰');
         //        $form->number('score', trans('score'));
         $form->number('level', trans('level'));
-        $form->image('show_img', trans('img'))->default ('1.png')->rules ('required');
+        $form->image('show_img', trans('img'))->default('1.png')->rules('required');
 
         //        $form->image('img1', trans('img'));
         $form->file('img2', trans('svg'));
         //        $form->file('img3', trans('video'));
         $form->color('color', trans('color'));
-        $form->number('expire', trans('expire(in days)'))->placeholder (trans ('0 if permanent'));
-        $form->switch('enable', trans('enable'))->states (Common::getSwitchStates ());
+        $form->number('expire', trans('expire(in days)'))->placeholder(trans('0 if permanent'));
+        $form->switch('enable', trans('enable'))->states(Common::getSwitchStates());
         //        $form->number('sort', 'sort');
         $form->number('num', __('num'));
 
@@ -190,5 +192,4 @@ class DedicateWareController extends MainController
 
         return $form;
     }
-
 }
