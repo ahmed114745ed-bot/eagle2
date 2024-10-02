079375832aca62747ae424cfb08c7e881fb74b4a
Erinyelkess <ariny.backend@utdsoftware.com>
2024-10-02 18:29:09 +0300

    test

--- app/Admin/Controllers/DedicateWareController.php
+++ app/Admin/Controllers/DedicateWareController.php
@@ -26,64 +26,64 @@ class DedicateWareController extends MainController
      */
     protected function grid()
     {
-        $typeSpecial = false;
-        $request = request('type');
-        if ($request !=null && ($request == 25)) {
-            $typeSpecial = true;
-        }
-        $grid = new Grid(new Ware);
-        $grid->model()->orderByDesc('created_at');
-        if ($typeSpecial) {
-            $grid->model()->where('type', '=', 25);
-        } else {
-            $grid->model()->where('type', '!=', 25);
-        }
-        $grid->id('ID');
-        $grid->column('get_type', __('get_type'))->select(
-            [
-                1 => trans('vip level automatic acquisition'),
-                //    2=>trans ('activity'),
-                //    3=>trans ('treasure box'),
-                4 => trans('purchase'),
-                // 5=>trans ('background modification'),
-                6 => trans('limited time purchase'),
-                // 7=>trans ('treasure box point exchange'),
-                // 8=>trans ('cp level unlock'),
-            ]
-        );
-        if (!$typeSpecial) {
-            $grid->column('type', __('type'))->select(
-                [
-                    1 => trans('Gemstone'),
-                    3 => trans('Card Scroll'),
-                    4 => trans('Avatar Frame'),
-                    5 => trans('Bubble Frame'),
-                    6 => trans('Entering Special Effects'),
-                    7 => trans('Microphone Aperture'),
-                    8 => trans('Badge'),
-                    9 => trans('NoKick'),
-                    10 => trans('Icon'),
-                    11 => trans('intro animation'),
-                    12 => trans('wapel'),
-                    13 => trans('hide country and last login'),
-                    14 => trans('vip gifts'),
-                    15 => trans('no pan'),
-                    16 => trans('hidden room'),
-                    17 => trans('anonymous man'),
-                    18 => trans('colored name'),
-                    19 => trans('profile visitors hide in'),
-                    25 => trans('Special Id'),
-                    21 => trans('sound effect'),
-                    22 => trans('upload GIF image')
-                ]
-            );
-        }
-        $grid->column('name', __('name'))->editable();
-        if (!$typeSpecial) {
-            $grid->title(__('title'));
-        } else {
-            $grid->value(__('value'));
-        }
+        // $typeSpecial = false;
+        // $request = request('type');
+        // if ($request !=null && ($request == 25)) {
+        //     $typeSpecial = true;
+        // }
+        // $grid = new Grid(new Ware);
+        // $grid->model()->orderByDesc('created_at');
+        // if ($typeSpecial) {
+        //     $grid->model()->where('type', '=', 25);
+        // } else {
+        //     $grid->model()->where('type', '!=', 25);
+        // }
+        // $grid->id('ID');
+        // $grid->column('get_type', __('get_type'))->select(
+        //     [
+        //         1 => trans('vip level automatic acquisition'),
+        //         //    2=>trans ('activity'),
+        //         //    3=>trans ('treasure box'),
+        //         4 => trans('purchase'),
+        //         // 5=>trans ('background modification'),
+        //         6 => trans('limited time purchase'),
+        //         // 7=>trans ('treasure box point exchange'),
+        //         // 8=>trans ('cp level unlock'),
+        //     ]
+        // );
+        // if (!$typeSpecial) {
+        //     $grid->column('type', __('type'))->select(
+        //         [
+        //             1 => trans('Gemstone'),
+        //             3 => trans('Card Scroll'),
+        //             4 => trans('Avatar Frame'),
+        //             5 => trans('Bubble Frame'),
+        //             6 => trans('Entering Special Effects'),
+        //             7 => trans('Microphone Aperture'),
+        //             8 => trans('Badge'),
+        //             9 => trans('NoKick'),
+        //             10 => trans('Icon'),
+        //             11 => trans('intro animation'),
+        //             12 => trans('wapel'),
+        //             13 => trans('hide country and last login'),
+        //             14 => trans('vip gifts'),
+        //             15 => trans('no pan'),
+        //             16 => trans('hidden room'),
+        //             17 => trans('anonymous man'),
+        //             18 => trans('colored name'),
+        //             19 => trans('profile visitors hide in'),
+        //             25 => trans('Special Id'),
+        //             21 => trans('sound effect'),
+        //             22 => trans('upload GIF image')
+        //         ]
+        //     );
+        // }
+        // $grid->column('name', __('name'))->editable();
+        // if (!$typeSpecial) {
+        //     $grid->title(__('title'));
+        // } else {
+        //     $grid->value(__('value'));
+        // }
         $grid->column('price', __('price'))->currency();
         $grid->level(__('level'));
         $grid->column('show_img', __('show_img'))->image('', 30);
@@ -100,7 +100,7 @@ class DedicateWareController extends MainController
             $actions->disableDelete();
             $actions->disableEdit();
             $actions->disableView();
-          //  $actions->add(new DedicateAction());
+            $actions->add(new DedicateAction());
         });
         $grid->disableCreateButton();
 
