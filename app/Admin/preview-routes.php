4746a4ea597e5540187914b2ffe12472e5a1e6a8
makled98 <mkled.backend@utdsoftware.com>
2024-10-07 15:17:12 +0300

    update

--- app/Admin/preview-routes.php
+++ app/Admin/preview-routes.php
@@ -47,6 +47,9 @@ use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
 use KevinSoft\MultiLanguage\MultiLanguage;
 
+use Modules\Achievement\Http\Controllers\web\GiftAchievemntController;
+use Modules\Achievement\Http\Controllers\web\UserAchievementLevelController;
+use Modules\Achievement\Http\Controllers\web\AchievementLevelsModuleController;
 Admin::routes();
 
 
@@ -251,6 +254,21 @@ Route::group(['prefix' => config('admin.route.prefix'), 'namespace' => config('a
 
     $router->resource('banners', BannerController::class);
 
+    // module achievement
+    
+    $router->resource('achievements', AchievementsController::class);
+    $router->post('/store-user-achievement', [AchievementLevelsModuleController::class, 'store'])->name('store-user-achievement');
+    $router->get('/get-achievement-levels/{achievementId}', [AchievementLevelsModuleController::class,'getAchievementLevels'])->name('get-achievement-levels');
+    $router->get('/get-view-page', [AchievementLevelsModuleController::class,'viewPage'])->name('get-view-page');
+   $router->resource('user-achievement-levels', UserAchievementLevelController::class);
+  // $router->post('postAddGiftAchievementLevel', [GiftAchievemntController::class,'postAddGiftAchievementLevel'])->name('postAddGiftAchievementLevel');
+   $router->get('achievement-levels/create/{id}', 'AchievementsLevelsController@create')->name('achievement-levels.create');
+    $router->resource('gift-achievements','UserGiftAchController');
+    $router->resource('gift-achievment', 'GiftAchiementController');
+    $router->post('postAddGiftAchievement', [GiftAchievemntController::class,'postAddGiftAchievemnt'])->name('postAddGiftAchievement');
+    $router->post('postAddGiftAchievementLevel', [GiftAchievemntController::class,'postAddGiftAchievementLevel'])->name('postAddGiftAchievementLevel');
+    $router->post('posteditGiftAchievementLevel', [GiftAchievemntController::class,'posteditGiftAchievementLevel'])->name('posteditGiftAchievementLevel');
+    $router->resource('achievement-levels', AchievementsLevelsController::class,['name'=>['create'=>'create2']]);
 
 });
 
