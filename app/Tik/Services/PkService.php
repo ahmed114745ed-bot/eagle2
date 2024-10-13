14b7af6dd3a30a435c2b751bb15eb225384671c4
johnghattas <john.backend@utdsoftware.com>
2024-10-13 11:57:31 +0300

    Update PkService.php

--- app/Tik/Services/PkService.php
+++ app/Tik/Services/PkService.php
@@ -18,7 +18,7 @@ class PkService
 
     public function create($request, $userId)
     {
-        $room =  $this->roomRepository->findRoomUser($userId)->where('room_status', 1)->first();
+        $room =  $this->roomRepository->findRoomUser($request->owner_id)->where('room_status', 1)->first();
         if (!$room) throw new \Exception('not found');
         if ($userId != $room->uid  && $room->room_visitor = '')  throw new \Exception('room closed');
         $ex =  $this->pkRepository->getPk($room->id);
