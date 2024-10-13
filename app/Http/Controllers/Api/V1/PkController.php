8cd31205aadffa2c06c2ca7638e81a6780cb374a
johnghattas <john.backend@utdsoftware.com>
2024-10-13 12:20:20 +0300

    Update PkController.php

--- app/Http/Controllers/Api/V1/PkController.php
+++ app/Http/Controllers/Api/V1/PkController.php
@@ -91,7 +91,7 @@ class PkController extends Controller
         if (!$request->owner_id) return Common::apiResponse(0, __('api_responses.missing_params'), null, 422);
 
         try {
-            $room = $this->pkService->showPkOrHide($request->owner_id,1);
+            $room = $this->pkService->showPkOrHide($request->owner_id,0);
         } catch (Exception $e) {
             return Common::apiResponse(false, $e->getMessage(), null, 407);
         }
