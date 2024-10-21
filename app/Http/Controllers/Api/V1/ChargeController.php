878224ac65b9863409ff3adb413cf9e4fd2f9e52
johnghattas <john.backend@utdsoftware.com>
2024-10-21 16:41:56 +0300

    Update ChargeController.php

--- app/Http/Controllers/Api/V1/ChargeController.php
+++ app/Http/Controllers/Api/V1/ChargeController.php
@@ -56,7 +56,7 @@ class ChargeController extends Controller
 
     public function chargeTo(Request $request)
     {
-
+        dd($to);
         $stop_all_charge = settings()->get("stop_charge") ? settings()->get("stop_charge") : 0;
         if ($stop_all_charge == 1) {
             return Common::apiResponse(0, __('api_responses.freez_charge'), 404);
@@ -66,7 +66,7 @@ class ChargeController extends Controller
         $isRoomTarget = false;
         $to   = User::withoutAppends()->searchByUuid($toId)->first();
 
-        dd($to);
+
 
         if ($from->charge_status == 0) {
             return Common::apiResponse(0, __('api.freez_charge'), 404);
