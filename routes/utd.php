5ffd8d21ca1db1fbc3fe2a534bab1e65e74c0983
Erinyelkess <ariny.backend@utdsoftware.com>
2024-10-27 16:16:56 +0300

    edit

--- routes/utd.php
+++ routes/utd.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 
 
 // utd apis
-Route::middleware([])->prefix('utd')->group(function () {
+Route::middleware([])->group(function () {
     //configs
     Route::prefix('configs')->group(function () {
         Route::get('/all', [ConfigController::class, 'index']);
