9a5bf422cc7a7ee49d522ce39be4992c7dbc7d0c
johnghattas <john.backend@utdsoftware.com>
2024-10-27 18:45:18 +0300

    Update FamilyController.php

--- app/Http/Controllers/Api/V1/FamilyController.php
+++ app/Http/Controllers/Api/V1/FamilyController.php
@@ -258,11 +258,11 @@ class FamilyController extends Controller
             return Common::apiResponse(0, $exception->getMessage(), null, 400);
         }
         request()->family_status = 2;
-        $membersUserResource = (new MembersUserResource($owner))->toJson();
+        $membersUserResource = (new MembersUserResource($owner))->toArray();
         request()->family_status = 1;
-        $anonymousResourceCollection = MembersUserResource::collection($admins)->toJson();
+        $anonymousResourceCollection = MembersUserResource::collection($admins)->toArray();
         request()->family_status = 0;
-        $anonymousResourceCollection1 = MembersUserResource::collection($members)->toJson();
+        $anonymousResourceCollection1 = MembersUserResource::collection($members)->toArray();
         $data = [
             'owner' => $membersUserResource,
             'admins' => $anonymousResourceCollection,
