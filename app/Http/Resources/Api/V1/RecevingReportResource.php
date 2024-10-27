b6ae4e4f8268a772828c91c0f982719ebb86b5df
makled98 <mkled.backend@utdsoftware.com>
2024-10-27 20:17:57 +0300

    reports

---
+++ app/Http/Resources/Api/V1/RecevingReportResource.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Resources\Api\V1;
+
+use App\Helpers\Common;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RecevingReportResource extends JsonResource
+{
+   
+    public function toArray($request)
+    {
+        return [
+            'id'          => $this->user_id, 
+            'diamonds'    => $this->amount,
+            'operation_no'=> (int)$this->id,
+            'created_at'  => $this->created_at->format('Y-m-d h:i:s A')
+        ];
+    }
+}
