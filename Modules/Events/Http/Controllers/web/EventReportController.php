438328ea52efd67312b528a760197609729bb4c3
Erinyelkess <ariny.backend@utdsoftware.com>
2024-10-27 15:21:35 +0300

    test

--- Modules/Events/Http/Controllers/web/EventReportController.php
+++ Modules/Events/Http/Controllers/web/EventReportController.php
@@ -1,5 +1,9 @@
 <?php
+
 namespace Modules\Events\Http\Controllers\web;
+
+use App\Models\OVip;
+use App\Models\Ware;
 use Encore\Admin\Grid;
 use Encore\Admin\Layout\Content;
 use App\Services\AppFeatureService;
@@ -7,122 +11,178 @@ use App\Admin\Controllers\MainController;
 use Modules\Events\Entities\WinnerReward;
 use Modules\Events\Entities\RewardWinnerPk;
 
-class EventReportController extends MainController {
+class EventReportController extends MainController
+{
     public $permission_name = 'event_report';
     public function __construct()
     {
         (new AppFeatureService)->validateStatusEnable("event_report");
     }
-     
-    public function index ( Content $content )
+
+    public function index(Content $content)
     {
         return $content
             ->title(trans('reports'))
-            ->description(__(request ('desc')?:'users'))
-            ->row(function($row) {
+            ->description(__(request('desc') ?: 'users'))
+            ->row(function ($row) {
                 $row->column(2, view('admin.grid.common.event-reports'));
                 $row->column(10, $this->grid());
             });
     }
 
-    protected function grid(){
-        $name = request ('name')?:'weekly_star';
+    protected function grid()
+    {
+        $name = request('name') ?: 'weekly_star';
         if (request("name") == "pk_event") {
-            $name="pk_event";
+            $name = "pk_event";
         }
         if (request("name") == "event_period") {
-            $name="event_period";
+            $name = "event_period";
         }
         $grid = $name;
         $grid = $this->{$grid}();
         $grid->disableexport();
-        $grid->disableActions ();
-        $grid->disableCreateButton ();
+        $grid->disableActions();
+        $grid->disableCreateButton();
         return $grid;
     }
 
-    protected function weekly_star(){
+    protected function weekly_star()
+    {
         $grid = new Grid(new WinnerReward());
-        $grid->model()->where('type', 'weekly_star')->orWhere('type',null);
-        $grid->column ('id',__ ('ID'));
-        $grid->column ('winner.uuid',__ ('uuid'));
-        $grid->column ('winner.name',__ ('name'));
-        $grid->column ('reward.level',__ ('level'));
-        $grid->column ('reward.type',__ ('type'));
-        $grid->column (__ ('الهديه'))->display(function (){
-            if ($this->reward != null){
-                $target='';
-                if ($this->reward->type == 'coins'){
-                    $target=$this->reward->target;
-                }elseif ($this->reward->type == 'vip'){
-                    $target=$this->reward->vip->name;
-                }elseif ($this->reward->type == 'ware'){
-                    $target=$this->reward->ware->name;
+        $grid->model()->where('type', 'weekly_star')->orWhere('type', null);
+        $grid->column('id', __('ID'));
+        $grid->column('winner.uuid', __('uuid'));
+        $grid->column('winner.name', __('name'));
+        $grid->column('reward.level', __('level'));
+        $grid->column('reward.type', __('type'));
+        $grid->column(__('الهديه'))->display(function () {
+            if ($this->reward != null) {
+                $target = '';
+                if ($this->reward->type == 'coins') {
+                    $target = $this->reward->target;
+                } elseif ($this->reward->type == 'vip') {
+                    $target = $this->reward->vip->name;
+                } elseif ($this->reward->type == 'ware') {
+                    $target = $this->reward->ware->name;
                 }
                 return $target;
             }
         });
-        $grid->column (__ ('return'))->display (function (){
-            $options = ['user'=>__('user')];
-            return (new \Modules\Events\Http\Actions\EventReportAction($this->id, 'weekly'))->render () ;
+        $grid->column('image', __('image'))->display(function ($path) {
+            if ($this->reward->type == 'ware') {
+                $ware = Ware::find($this->reward->target);
+                $path = $ware->img2 ?? $ware->show_img;
+            } elseif ($this->reward->type == 'vip') {
+                $vips = OVip::find($this->reward->target);
+                $path = $vips->img;
+            } elseif ($this->reward->type == 'achievement') {
+                $path = $this->reward->target;
+            } else {
+                $path = 'cion.png';
+            }
+
+            /** @var Gift $this */
+            $url = getImagePath($path);
+            return handleShowImageWithTypes($this->id, $url, 50, 50);
+        });
+        $grid->column(__('return'))->display(function () {
+            $options = ['user' => __('user')];
+            return (new \Modules\Events\Http\Actions\EventReportAction($this->id, 'weekly'))->render();
         });
 
         return $grid;
     }
 
-    protected function pk_event(){
+    protected function pk_event()
+    {
         $grid = new Grid(new RewardWinnerPk());
 
-        $grid->column ('id',__ ('ID'));
-        $grid->column ('winner.uuid',__ ('uuid'));
-        $grid->column ('winner.name',__ ('name'));
-        $grid->column ('reward.level',__ ('level'));
-        $grid->column ('reward.type',__ ('type'));
-        $grid->column (__ ('الهديه'))->display(function (){
-            if ($this->reward != null){
-                $target='';
-                if ($this->reward->type == 'coins'){
-                    $target=$this->reward->target;
-                }elseif ($this->reward->type == 'vip'){
-                    $target=$this->reward->vip->name;
-                }elseif ($this->reward->type == 'ware'){
-                    $target=$this->reward->ware->name;
+        $grid->column('id', __('ID'));
+        $grid->column('winner.uuid', __('uuid'));
+        $grid->column('winner.name', __('name'));
+        $grid->column('reward.level', __('level'));
+        $grid->column('reward.type', __('type'));
+        $grid->column(__('الهديه'))->display(function () {
+            if ($this->reward != null) {
+                $target = '';
+                if ($this->reward->type == 'coins') {
+                    $target = $this->reward->target;
+                } elseif ($this->reward->type == 'vip') {
+                    $target = $this->reward->vip->name;
+                } elseif ($this->reward->type == 'ware') {
+                    $target = $this->reward->ware->name;
                 }
                 return $target;
             }
         });
-        $grid->column (__ ('return'))->display (function (){
-            $options = ['user'=>__('user')];
-            return (new \Modules\Events\Http\Actions\EventReportAction($this->id, 'pk'))->render () ;
+        $grid->column('image', __('image'))->display(function ($path) {
+            if ($this->reward->type == 'ware') {
+                $ware = Ware::find($this->reward->target);
+                $path = $ware->img2 ?? $ware->show_img;
+            } elseif ($this->reward->type == 'vip') {
+                $vips = OVip::find($this->reward->target);
+                $path = $vips->img;
+            } elseif ($this->reward->type == 'achievement') {
+                $path = $this->reward->target;
+            } else {
+                $path = 'cion.png';
+            }
+
+            /** @var Gift $this */
+            $url = getImagePath($path);
+            return handleShowImageWithTypes($this->id, $url, 50, 50);
+        });
+        $grid->column(__('return'))->display(function () {
+            $options = ['user' => __('user')];
+            return (new \Modules\Events\Http\Actions\EventReportAction($this->id, 'pk'))->render();
         });
 
         return $grid;
     }
 
-    protected function event_period(){
+    protected function event_period()
+    {
         $grid = new Grid(new WinnerReward());
         $grid->model()->where('type', 'event_period');
-        $grid->column ('id',__ ('ID'));
-        $grid->column ('winner.uuid',__ ('uuid'));
-        $grid->column ('winner.name',__ ('name'));
-        $grid->column ('reward.level',__ ('level'));
-        $grid->column ('reward.type',__ ('type'));
-        $grid->column (__ ('الهديه'))->display(function (){
-            if ($this->reward != null){
-                $target='';
-                if ($this->reward->type == 'coins'){
-                    $target=$this->reward->target;
-                }elseif ($this->reward->type == 'vip'){
-                    $target=$this->reward->vip->name;
-                }elseif ($this->reward->type == 'ware'){
-                    $target=$this->reward->ware->name;
+        $grid->column('id', __('ID'));
+        $grid->column('winner.uuid', __('uuid'));
+        $grid->column('winner.name', __('name'));
+        $grid->column('reward.level', __('level'));
+        $grid->column('reward.type', __('type'));
+        $grid->column(__('الهديه'))->display(function () {
+            if ($this->reward != null) {
+                $target = '';
+                if ($this->reward->type == 'coins') {
+                    $target = $this->reward->target;
+                } elseif ($this->reward->type == 'vip') {
+                    $target = $this->reward->vip->name;
+                } elseif ($this->reward->type == 'ware') {
+                    $target = $this->reward->ware->name;
                 }
                 return $target;
             }
         });
-        $grid->column (__ ('return'))->display (function (){
-            $options = ['user'=>__('user')];
-            return (new \Modules\Events\Http\Actions\EventReportAction($this->id, 'weekly'))->render () ;
+        $grid->column('image', __('image'))->display(function ($path) {
+            if ($this->reward->type == 'ware') {
+                $ware = Ware::find($this->reward->target);
+                $path = $ware->img2 ?? $ware->show_img;
+            } elseif ($this->reward->type == 'vip') {
+                $vips = OVip::find($this->reward->target);
+                $path = $vips->img;
+            } elseif ($this->reward->type == 'achievement') {
+                $path = $this->reward->target;
+            } else {
+                $path = 'cion.png';
+            }
+
+            /** @var Gift $this */
+            $url = getImagePath($path);
+            return handleShowImageWithTypes($this->id, $url, 50, 50);
+        });
+        $grid->column(__('return'))->display(function () {
+            $options = ['user' => __('user')];
+            return (new \Modules\Events\Http\Actions\EventReportAction($this->id, 'weekly'))->render();
         });
 
         return $grid;
