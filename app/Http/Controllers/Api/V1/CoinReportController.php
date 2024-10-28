9d2f65439f4bfb5bc6eed9cde529c23c6b10ba8c
makled98 <mkled.backend@utdsoftware.com>
2024-10-28 11:57:46 +0300

    update report

--- app/Http/Controllers/Api/V1/CoinReportController.php
+++ app/Http/Controllers/Api/V1/CoinReportController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\V1;
 use App\Helpers\Common;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\V1\EventCoinsReportResource;
 use App\Http\Resources\Api\V1\ExchangeCoinsReportResource;
 use App\Tik\Services\PaymentGatewayService;
 use App\Http\Resources\Api\V1\PaymentResource;
@@ -13,6 +14,8 @@ use App\Http\Resources\Api\V1\RechargeCoinsReportResource;
 use App\Models\Charge;
 use App\Models\CoinLog;
 use App\Models\ExchangeLog;
+use Modules\DailyPrize\Entities\DailyUserGift;
+use Modules\Events\Entities\WinnerReward;
 
 class CoinReportController extends Controller
 {
@@ -67,5 +70,49 @@ class CoinReportController extends Controller
         ->get();
         return RechargeCoinsReportResource::collection($data);
     }
+
+    public function eventCoins()
+    {
+        $user = auth()->user();
+    
+        $result1 = $this->getCoinsData(
+            DailyUserGift::class,
+            ['user_id' => $user->id, 'gift_type' => 'coins'],
+            'daily_prize'
+        );
+    
+        $result2 = $this->getCoinsData(
+            WinnerReward::class,
+            ['winner_id' => $user->id],
+            'events',
+            function ($query) {
+                $query->whereHas('reward', function ($q) {
+                    $q->where("type", "coins");
+                });
+            }
+        );
+    
+        $result = array_merge($result1, $result2);
+        return Common::apiResponse(1, '', $result, 200);
+    }
+    
+    private function getCoinsData($model, array $conditions, $type, $additionalQuery = null)
+    {
+        $query = $model::where($conditions);
+    
+        if ($additionalQuery) {
+            $additionalQuery($query);
+        }
+    
+        $query->when(request("start_date") && request("end_date"), function ($q) {
+            $q->whereDate("created_at", ">=", request("start_date"))
+              ->whereDate("created_at", "<=", request("end_date"));
+        });
+    
+        return $query->get()->map(function ($item) use ($type) {
+            return new EventCoinsReportResource($item, $type);
+        })->toArray();
+    }
+    
 
 }
