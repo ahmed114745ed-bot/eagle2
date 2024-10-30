551f3f502a94c8f512cc63f402f13967d66230fb
johnghattas <john.backend@utdsoftware.com>
2024-10-30 16:34:29 +0300

    Update AuthService.php

--- app/Tik/Services/AuthService.php
+++ app/Tik/Services/AuthService.php
@@ -37,7 +37,7 @@ class AuthService
 
         if (!$request->country_id) {
             $country = $this->countryRepository->findByPhoneCode('101');
-            $user->country_id = @$country->id ?: 0;
+            $user->country_id = @$country->id;
         }
         $user->is_points_first = 1;
         $user->save();
@@ -172,8 +172,8 @@ class AuthService
         return [$user, $token];
     }
 
-    
- 
+
+
 
     public function logoutAsConfiguration($user)
     {
