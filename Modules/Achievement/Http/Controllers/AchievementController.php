177ace83f804c88d7ae86bff7800a2d37955bb95
johnghattas <john.backend@utdsoftware.com>
2024-11-04 15:47:20 +0300

    Update AchievementController.php

--- Modules/Achievement/Http/Controllers/AchievementController.php
+++ Modules/Achievement/Http/Controllers/AchievementController.php
@@ -152,7 +152,12 @@ class AchievementController extends Controller
                 ];
                 $pkArray = ($pkEvent?->rewards->map(fn($e) =>/** @var PkReward $e*/ collect(['valid_image' => $e->target, 'target' => __($e->pk_type) . ' top ' . $e->level,
                 ])->merge($append))->toArray()) ?? [];
-                $chargeArray = $chargeEvent?->pluck('rewards')->map(fn($e) =>/** @var RewardTarget $e*/ collect(['valid_image' => @$e->getAttribute('target'), 'target' => __('target-events') ])->merge($append))->toArray() ?? [];
+                $chargeArray = $chargeEvent?->pluck('rewards')->map(function ($e) use ($append) {
+                        /** @var RewardTarget $e */ ;
+
+                        \Log::info(json_encode($e));
+                    return collect(['valid_image' => @$e->getAttribute('target'), 'target' => __('target-events') ])->merge($append);
+                } ) ?? [];
                 $weeklyArray = $weeklyStar?->pluck('rewards')->map(fn($e) =>/** @var Reward $e*/ collect(['valid_image' => $e->target, 'target' => __('weekly Star') . ' top ' . $e->level])->merge($append))->toArray() ?? [];
 
 
