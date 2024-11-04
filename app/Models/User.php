24ebbb22989de274cad23f59e60bf32b983f94d3
johnghattas <john.backend@utdsoftware.com>
2024-11-04 18:03:59 +0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -185,7 +185,7 @@ class User extends Authenticatable
         $userSallary = UserSallary::query()
                                   ->selectRaw('sum(sallary) as total_salary, sum(cut_amount) as total_cut_amount')
                                   ->where(function ($query) use ($year, $month) {
-                                      $query->where(DB::raw('year'), '<=', $year )->where('month', '<=', $month);
+                                      $query->where('year', '<=', $year )->where('month', '<=', $month);
                                   })
                                   ->where('user_id', $this->id)
                                   ->groupBy('user_id')
@@ -939,7 +939,7 @@ class User extends Authenticatable
         }
         if ($this->agency_id) {
             $userSallary = UserSallary::query()->where(function ($query) use ($year, $month) {
-                $query->where(DB::raw('concat(year,"-", month)'), '<=', $year . '-' . $month);
+                $query->where('year', '<=', $year )->where('month', '<=', $month);
             })->where('user_id', $this->id)
                 ->where('is_paid', 0)
                 ->where('user_agency_id', $this->agency_id)
