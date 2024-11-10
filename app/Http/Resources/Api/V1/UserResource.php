9efeb66709df96b6d87a638c5925a16650e37584
Erinyelkess <ariny.backend@utdsoftware.com>
2024-11-10 17:47:25 +0200

    remove some data

--- app/Http/Resources/Api/V1/UserResource.php
+++ app/Http/Resources/Api/V1/UserResource.php
@@ -107,7 +107,6 @@ class UserResource extends JsonResource
         $data      = [
             'id'      => @$this->id, // both
             'uuid'    => @$this->uuid, // both
-            'uuid'                 => @$this->uuid, // both
             'id_image'             => @$this->specialId?->ware?->show_img ?? '',
             'special_id'          =>  @$this->specialId?->ware?->id ?? 0,
             'chat_id' => @$this->chat_id ?: "", // both                     ///////
