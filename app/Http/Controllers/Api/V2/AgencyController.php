1508a7cf71168174079ec75fa8c5230cd4f84e4e
makled98 <mkled.backend@utdsoftware.com>
2024-11-13 15:29:18 +0200

    Update AgencyController.php

--- app/Http/Controllers/Api/V2/AgencyController.php
+++ app/Http/Controllers/Api/V2/AgencyController.php
@@ -168,15 +168,15 @@ class AgencyController extends Controller
         $list_req = AgencyJoinRequest::where('agency_id', $agency_id);
 
         if ($type == "application") {
-            $list_req1 = $list_req->where('status', 0)->with('user')->paginate(10);
+            $list_req1 = $list_req->where('status', 0)->with('user')->get(10);
             $list_req = MyDataForAgencyNewResource::collection($list_req1, 'application');
         } elseif ($type == "record") {
-            $list_req = $list_req->where('status', '!=', 0)->with('user','admin')->paginate(10);
+            $list_req = $list_req->where('status', '!=', 0)->with('user','admin')->get(10);
             $list_req = MyDataForAgencyNewResource::collection($list_req, 'record');
         }
 
         if ($list_req) {
-            return Common::apiResponse(1, '', $list_req,200 , Common::getPaginates($list_req));
+            return Common::apiResponse(1, '', $list_req,200,Common::getPaginates($list_req));
         }
         return Common::apiResponse(0, 'لا يوجد بيانات', []);
     }
