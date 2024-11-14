fc7a004f32b411d29a39a556c9342146826f6a69
makled98 <mkled.backend@utdsoftware.com>
2024-11-14 13:03:20 +0200

    update config

--- app/Enums/ConfigCategory.php
+++ app/Enums/ConfigCategory.php
@@ -25,4 +25,20 @@ enum ConfigCategory : string
     {
         return translateCategory(self::getOptions());
     }
+
+    public static function getLinkedStringsByValue(string $value): ?array
+    {
+        return match ($value) {
+            self::SYSTEM_SETTINGS->value => ['setting1', 'setting2', 'setting3'],
+            self::TARGET_SETTINGS->value => ['target1', 'target2', 'target3'],
+            self::ZEGO_SETTINGS->value => ['zego1', 'zego2', 'zego3'],
+            self::SMS_SETTINGS->value => ['sms1', 'sms2', 'sms3'],
+            self::FAMILY_SETTINGS->value => ['family1', 'family2', 'family3'],
+            self::PAYMENT_SETTINGS->value => ['payment1', 'payment2', 'payment3'],
+            self::ROOM_SETTINGS->value => ['room1', 'room2', 'room3'],
+            self::AGENCY_SETTINGS->value => ['agency1', 'agency2', 'agency3'],
+            self::LEVEL_SETTINGS->value => ['level1', 'level2', 'level3'],
+            default => null,
+        };
+    }
 }
--- app/Http/Controllers/Api/V1/ConfigController.php
+++ app/Http/Controllers/Api/V1/ConfigController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Enums\ConfigCategory;
 use App\Http\Requests\Api\ConfigValuesRequest;
 use App\Models\Config;
 use App\Helpers\Common;
@@ -74,7 +75,8 @@ class ConfigController extends Controller
         $formattedConfigs = [];
 
         foreach ($configs as $category => $items) {
-            $formattedConfigs[__($category)] = ConfigResource::collection($items);
+            $formattedConfigs[__($category)]['sub_categoory'] =  ConfigCategory::getLinkedStringsByValue($category);
+            $formattedConfigs[__($category)]['data'] = ConfigResource::collection($items);
         }
 
         return Common::apiResponse(1, '', $formattedConfigs);
