966a16cb47f51b6720e8fd3ce8d41ac2c101f7b6
makled98 <mkled.backend@utdsoftware.com>
2024-11-19 17:23:08 +0200

    Update AchievementDetailResource.php

--- Modules/Achievement/Transformers/AchievementDetailResource.php
+++ Modules/Achievement/Transformers/AchievementDetailResource.php
@@ -22,7 +22,7 @@ class AchievementDetailResource extends JsonResource
         $achievementLevel = $this->achievementLevel;
         return [
             'id' => $this->id,
-            'name' => $achievementLevel?->achievement_levels .' ' .$this->user?->name ?? '',
+            'name' =>'قام ' .$this->user?->name .' بشحن قيمه'. $achievementLevel?->achievement_levels?->target ?? '',
             'type' => $achievementLevel?->achievement?->type ?? 'no achievement',
             'image' => $achievementLevel?->valid_image ?? $this->custom_image,
             'description' =>    $achievementLevel ?  $achievementLevel?->ar_description  : __('get it by admin'),
