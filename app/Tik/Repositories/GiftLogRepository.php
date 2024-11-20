c795f25d111c965bffc553481ef91f914018bd20
makled98 <mkled.backend@utdsoftware.com>
2024-11-20 16:25:09 +0200

    Update GiftLogRepository.php

--- app/Tik/Repositories/GiftLogRepository.php
+++ app/Tik/Repositories/GiftLogRepository.php
@@ -67,17 +67,16 @@ class GiftLogRepository extends AbstractRepository
     public function getByUserId($userId)
     {
         return $this->model->query()
-        ->has('sender')
-        ->with('sender', 'receiver')
-        ->select('sender_id', \DB::raw('ANY_VALUE(receiver_id) AS receiver_id'))
-        ->selectRaw('SUM(giftNum * giftPrice) AS total')
-        ->selectRaw('CAST(SUM(giftNum * giftPrice) AS DECIMAL(10, 2)) AS total_decimal')
-        ->where('receiver_id', $userId)
-        ->groupBy('sender_id')
-        ->orderByDesc('total')
-        ->take(20)
-        ->get();
-
+            ->has('sender')
+            ->with('sender', 'receiver')
+            ->select('sender_id', 'receiver_id')
+            ->selectRaw('SUM(giftNum * giftPrice) AS total')
+            ->selectRaw('CAST(SUM(giftNum * giftPrice) AS DECIMAL(10, 2)) AS total_decimal')
+            ->where('receiver_id', $userId)
+            ->groupBy('sender_id', 'receiver_id')
+            ->orderByDesc('total')
+            ->take(20)
+            ->get();
     }
 
 }
