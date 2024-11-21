b242e904eadc7f382fe85f9a32cdc9023ec1c351
johnghattas <john.backend@utdsoftware.com>
2024-11-21 20:06:14 +0300

    Update EnteranceRoomServices.php

--- app/Tik/Services/EnteranceRoomServices.php
+++ app/Tik/Services/EnteranceRoomServices.php
@@ -369,8 +369,8 @@ class EnteranceRoomServices
         }
         $room_info = (new EnterRoomCollection($room,$user->id));
 
-        $keys = Common::getConfFromKey(['app_sign', 'zego_app_id']);
-        $room_info = $room_info->toArray($request);
+//        $keys = Common::getConfFromKey(['app_sign', 'zego_app_id']);
+//        $room_info = $room_info->toArray($request);
         // $room_info['zego_keys'] = $keys->mapWithKeys(function ($item){
         //     return [$item['name'] => (($item['name'] == 'zego_app_id') ? (integer)$item['value'] :$item['value'])];
         // });
