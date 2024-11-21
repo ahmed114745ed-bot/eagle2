440b34f6685f651d278f6a512e206ae1669b0c2e
mido203020 <Mohamed.gamal@utdsoftware.com>
2024-11-21 13:28:11 +0200

    converting expire time to days & add is_use boolean value

--- app/Http/Resources/Api/V1/MyPacksVipResource.php
+++ app/Http/Resources/Api/V1/MyPacksVipResource.php
@@ -13,16 +13,19 @@ class MyPacksVipResource extends JsonResource
     {
 
         /** @var Pack $this */
+        $expiration_in_seconds = ($this->expire - time());
+        $expiration_in_days = floor($expiration_in_seconds / (24 * 60 * 60));
         return [
             'id' => $this->id,
             'target_id' => $this->id,
             'user_id' => $this->user_id,
             'type' => "vip bag",
             'num' => $this->qty,
-            'expire' => $this->expire,
+            'expire' => $expiration_in_days,
             'created_at' => Carbon::parse($this->created_at)->setTimezone($request->hasHeader('tz') ? $request->header()['tz'][0] : 'UTC')->format('Y-m-d H:i:s') ??'',
             'updated_at' => $this->updated_at,
             'sender_id' => $this->sender_id,
+            'use' => $this->is_used == 1? true : false,
             'is_used' => $this->num_used > 0,
             'num_used' => $this->num_used > 0,
             'name' => $this->OVip?->name,
