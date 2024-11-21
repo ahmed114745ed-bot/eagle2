0ba1cbddb80058f504b971c9451287b1b06fc508
johnghattas <john.backend@utdsoftware.com>
2024-11-21 21:13:12 +0300

    Update Handler.php

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -42,7 +42,8 @@ class Handler extends ExceptionHandler
                 return Common::apiResponse (false,'Unauthenticated',[],401);
             }
 
-            \Log::error($e->getMessage() . $request->url());
+            \Log::error($e->getMessage() );
+            \Log::info($request->fullUrl() );
             return Common::apiResponse(0, $e->getMessage(), null, 500);
 
         }
