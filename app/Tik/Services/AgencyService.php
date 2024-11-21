50abe2760a82e6286d0164f2b074e5a33f4a919f
Erinyelkess <ariny.backend@utdsoftware.com>
2024-11-21 17:46:26 +0200

    edit numToString

--- app/Tik/Services/AgencyService.php
+++ app/Tik/Services/AgencyService.php
@@ -515,8 +515,8 @@ class AgencyService
         $minutes = $hours * 60;
         $data = [
             'user_salary' => [
-                'cut_amount' => $totalCutAmount,
-                'salary' => $totalSalary
+                'cut_amount' => numToString($totalCutAmount),
+                'salary' => numToString($totalSalary),
             ],
             'request_leave_agency' => $this->leaveAgencyRequestRepository->getRequest($user->id, $user->agency_id),
             'diamonds' => numToStringNew($dailyDiamonds->sum('diamonds')),
