de8a463c4b789bd5f49207e265f6646aa012d4cf
Erinyelkess <ariny.backend@utdsoftware.com>
2024-11-24 14:18:27 +0200

    test user

--- app/Tik/Services/AuthService.php
+++ app/Tik/Services/AuthService.php
@@ -104,6 +104,7 @@ class AuthService
     public function loginWithGoogle($request)
     {
         $user = User::where('google_id', $request['google_id'])->first();
+        dd($user);
         if (!$user) {
             if ($this->userRepository->checkTrashedEmail($request['email'], $request['google_id'])) {
                 $resource = [
