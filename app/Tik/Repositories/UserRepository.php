55a20e63e9891d016e99be6099a5fbdbefd31e47
Erinyelkess <ariny.backend@utdsoftware.com>
2024-11-24 14:41:54 +0200

    edit

--- app/Tik/Repositories/UserRepository.php
+++ app/Tik/Repositories/UserRepository.php
@@ -158,7 +158,7 @@ class UserRepository extends AbstractRepository
 
     public function findByGoogleId($googleId)
     {
-        return $this->model->whereNotNull(['google_id'])->where('google_id', $googleId)->first();
+        return $this->model->whereNotNull('google_id')->where('google_id', $googleId)->first();
     }
     public function checkTrashedEmail($email, $googleId)
     {
