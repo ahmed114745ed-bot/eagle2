d53ae594a0606bf972eccd9081b803b686a7fdcf
Erinyelkess <ariny.backend@utdsoftware.com>
2024-12-08 18:57:19 +0200

    edit

--- app/Admin/Controllers/DeleteAccountController.php
+++ app/Admin/Controllers/DeleteAccountController.php
@@ -71,7 +71,7 @@ class DeleteAccountController extends AdminController
 
         $grid->column('id', __('Id'));
         $grid->column('title', __('Title'));
-        $grid->column('image', __('Image'));
+        $grid->column('image', __('Image'))->image('', 50);
         return $grid;
     }
 
