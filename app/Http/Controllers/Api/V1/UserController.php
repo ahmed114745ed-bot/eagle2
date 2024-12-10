55455d25f1cbef231030e47fbbf480e8a459b35b
johnghattas <john.backend@utdsoftware.com>
2024-12-10 16:59:45 +0300

    Update UserController.php

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -161,7 +161,7 @@ class UserController extends Controller
         $user = $request->user();
         try {
 
-            \Log::info('This is the device token '. json_encode($request->headers->all()));
+            \Log::info('This is the device token '. $request->get('device_token'));
             $userWithMedals = $this->userService->processUserData($user, $request->header('device_token'), $request->header('lat'), $request->header('long'));
         } catch (\Exception $exception) {
 
