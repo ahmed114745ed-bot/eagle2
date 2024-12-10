0723ac8df7ba977a6d6f4360fd7ff30f9b45eb50
makled98 <mkled.backend@utdsoftware.com>
2024-12-10 14:24:20 +0200

    update pusher

--- Modules/Chat/Http/Controllers/PusherController.php
+++ Modules/Chat/Http/Controllers/PusherController.php
@@ -26,11 +26,13 @@ class PusherController extends Controller
         }
 
         // Extract relevant data from the request
-        $channel = $request->events[0]['channel'];
-        $eventName = $request->events[0]['name'];
-
-        // Delegate handling user status to the service
-        $this->pusherService->handleUserStatusChange($channel, $eventName);
+        foreach ($request->events as $event) {
+            $channel = $event['channel'];
+            $eventName = $event['name'];
+    
+            // Delegate handling user status to the service
+            $this->pusherService->handleUserStatusChange($channel, $eventName);
+        }
 
         return response()->json(['status' => 'Webhook received']);
     }
