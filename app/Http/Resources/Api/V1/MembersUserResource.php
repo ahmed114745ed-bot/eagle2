dffcb030c190e0150d70160620ca7ffc513caa97
mido203020 <Mohamed.gamal@utdsoftware.com>
2024-12-16 16:50:41 +0200

    userVip and monthly_diamond_received

--- app/Http/Resources/Api/V1/MembersUserResource.php
+++ app/Http/Resources/Api/V1/MembersUserResource.php
@@ -11,6 +11,7 @@ use App\Models\Family;
 use App\Models\FamilyUser;
 use App\Models\Pack;
 use App\Models\Room;
+use App\Models\Vip;
 use App\Models\Ware;
 use Carbon\Carbon;
 use http\Client\Curl\User;
@@ -26,30 +27,30 @@ class MembersUserResource extends JsonResource
      */
     public function toArray($request)
     {
-
-
         $data = [
-            'id'=>@$this->id,
-            'name'=>@$this->name?:'',
-            'profile'=> [
+            'id' => @$this->id,
+            'name' => @$this->name ?: '',
+            'profile' => [
                 'image' => @$this->profile->avatar,
-                'age' => Carbon::parse (@$this->profile->birthday)->age,
-                'gender'=> (integer)(@$this->profile->gender ?? 1),
+                'age' => Carbon::parse(@$this->profile->birthday)->age,
+                'gender' => (int)(@$this->profile->gender ?? 1),
             ],
             'type_user'            => intval(@$this->type_user) ?: 0, // both
-            "manger_type"          =>new MangerTypeResource(@$this->mangerType),
+            'vip_level' => $this->userVip->level ?? 0,
+            "manger_type"          => new MangerTypeResource(@$this->mangerType),
             'uuid'                 => @$this->uuid, // both
+            'monthly_diamond_received' => $this->monthly_diamond_received,
             'id_image'             => @$this->specialId?->ware?->show_img ?? '',
             'special_id'          =>  @$this->specialId?->ware?->id ?? 0,
-            $this->mergeWhen(isset(request()->family_status) , [
+            $this->mergeWhen(isset(request()->family_status), [
                 'family_status' => request()->family_status
             ]),
-           /* 'level'=> [
+            /* 'level'=> [
                 'receiver_img' => @$this->getImageReceiverOrSender('receiver_id',1)->img,
                 'sender_img' => @$this->getImageReceiverOrSender('sender_id',2)->img,
             ],*/
-            'frame_id'=>@$this->dress_1,
-            'frame'=>Common::getUserDress(@$this->id,@$this->dress_1,4,'img2', true)?:Common::getUserDress(@$this->id,@$this->dress_1,4,'img1', true),
+            'frame_id' => @$this->dress_1,
+            'frame' => Common::getUserDress(@$this->id, @$this->dress_1, 4, 'img2', true) ?: Common::getUserDress(@$this->id, @$this->dress_1, 4, 'img1', true),
         ];
 
         return $data;
