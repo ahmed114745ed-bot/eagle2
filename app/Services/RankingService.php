10521f6a72d540a4fd41a71ad3ed7196ee8865bf
shami <ahmed.shehata@utdsoftware.com>
2024-12-18 13:11:14 +0200

    Update RankingService.php

--- app/Services/RankingService.php
+++ app/Services/RankingService.php
@@ -186,7 +186,7 @@ class RankingService
         $total_received_level_img = Common::getImageTotalReceiverOrSender(4);
         $total_sender_level_img = Common::getImageTotalReceiverOrSender($user->total_sender_level);
         $vip_level  = Common::ovip_center_rank($arr['user']['user_id']);
-        $vip_level_img  = Common::ovip_center_rank_img($arr['user']['user_id']);
+        // $vip_level_img  = Common::ovip_center_rank_img($arr['user']['user_id']);
         // $levels =Common::getSenderAndReceiverLevels($user->id);
         if (gettype($vip_level) != 'integer') {
             $vip_level = 0;
@@ -196,7 +196,7 @@ class RankingService
         $arr['user']['vip_level']  = $vip_level;
         $arr['user']['sender_level']  = $user->total_sender_level;
         $arr['user']['reciver_level']  = $user->total_received_level;
-        $arr['user']['vip_level_img']  = $vip_level_img->image ?? '';
+        // $arr['user']['vip_level_img']  = $vip_level_img->image ?? '';
         $arr['user']['sender_level_img']  = $total_sender_level_img->image ?? '';
         $arr['user']['reciver_level_img']  = $total_received_level_img->image ?? '';
         $arr['user']['type_user'] =  intval(@$user->type_user) ?: 0;
