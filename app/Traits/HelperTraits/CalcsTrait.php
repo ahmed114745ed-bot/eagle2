09c816e92aee67d273e707ed3856312fcb7592f3
shami <ahmed.shehata@utdsoftware.com>
2024-12-18 13:01:08 +0200

    Update CalcsTrait.php

--- app/Traits/HelperTraits/CalcsTrait.php
+++ app/Traits/HelperTraits/CalcsTrait.php
@@ -729,6 +729,7 @@ trait CalcsTrait
         } else {
             $user = $user_id;
         }
+        dd($user);
         if (!isset($user->UserVip)) return 0;
         $uvip = $user?->UserVip;
         if (!$uvip) return new \stdClass();
