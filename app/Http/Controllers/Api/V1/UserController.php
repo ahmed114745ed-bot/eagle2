6be05b2f0f516807ff4908390d58bbb0b0c79c59
mido203020 <Mohamed.gamal@utdsoftware.com>
2024-12-23 18:05:51 +0200

    first problem

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -177,7 +177,7 @@ class UserController extends Controller
             return Common::apiResponse(0, $exception->getMessage(), null, 400);
         }
         $this->userService->unlockDressHand($user->id);
-        request()->default_background = \DB::table('backgrounds')->where('enable', 1)->orderBy('id', 'asc')->limit(1)->first()->img;
+        request()->default_background = \DB::table('backgrounds')->where('enable', 1)->orderBy('id', 'asc')->first()?->img;
 
         $data = new MyDataResource($userWithMedals);
 
