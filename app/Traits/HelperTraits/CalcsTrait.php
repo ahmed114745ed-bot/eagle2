4e54c08e3815cc7fcc6c938e59fe4bb55b1733b4
makled98 <mkled.backend@utdsoftware.com>
2024-12-24 13:36:58 +0200

    updates

--- app/Traits/HelperTraits/CalcsTrait.php
+++ app/Traits/HelperTraits/CalcsTrait.php
@@ -770,7 +770,7 @@ trait CalcsTrait
 
         // return $vip->level;
         // [
-        return $vip;
+        return $vipIcon->show_img;
         // [
         // 'id'        => 1,
         // 'level'     => $vip->level?? 0,
