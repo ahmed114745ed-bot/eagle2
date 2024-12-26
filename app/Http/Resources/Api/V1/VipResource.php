ab1669d631288f23a13e7fa85d852fa1536dfa2d
mido203020 <Mohamed.gamal@utdsoftware.com>
2024-12-26 12:36:56 +0200

    adding sender & receiver to vipResource

--- app/Http/Resources/Api/V1/VipResource.php
+++ app/Http/Resources/Api/V1/VipResource.php
@@ -29,17 +29,18 @@ class VipResource extends JsonResource
 
 private function getLevelGroups()
 {
-
     // Use Cache::remember to cache the results for 1 hour
     return Cache::remember('levels_chunks', 3600, function () {
         // Fetch all rows for the given type and order by level
-        $vips = Vip::all();
+        $sender_vips = Vip::where('type',2)->get();
+        $receiver_vips = Vip::where('type',1)->get();
 
         // Group rows into chunks of 9
-        $chunks = $vips->chunk(10);
+        $sender_chunks = $sender_vips->chunk(10);
+        $receiver_chunks = $receiver_vips->chunk(10);
 
         // Transform each chunk into the desired structure
-        $levelGroups = $chunks->map(function ($chunk) {
+        $sender_levelGroups = $sender_chunks->map(function ($chunk) {
             return [
                 'minlevel' => $chunk->first()->level, // Minimum level in the chunk
                 'maxlevel' => $chunk->last()->level,  // Maximum level in the chunk
@@ -47,7 +48,16 @@ private function getLevelGroups()
             ];
         });
 
-        return $levelGroups->toArray(); // Convert collection to array
+
+        $receiver_levelGroups = $receiver_chunks->map(function ($chunk) {
+            return [
+                'minlevel' => $chunk->first()->level, // Minimum level in the chunk
+                'maxlevel' => $chunk->last()->level,  // Maximum level in the chunk
+                'badge' => $chunk->last()->img,
+            ];
+        });
+
+        return ['sender' => $sender_levelGroups->toArray(), 'receiver' => $receiver_levelGroups->toArray()]; // Convert collection to array
     });
 }
 
