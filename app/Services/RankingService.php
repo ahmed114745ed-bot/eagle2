beaca40ef86467279f8295e16c9f8ba908045e4e
shami2024 <ahmed.shehata@utdsoftware.com>
2024-12-29 13:38:25 +0200

    Update RankingService.php

--- app/Services/RankingService.php
+++ app/Services/RankingService.php
@@ -217,12 +217,12 @@ class RankingService
         $arr['user']['reciver_level']  = $user->total_received_level ?? '';
         $arr['user']['vip_level_img']  = $vip_level_img == 0 ? "" : $vip_level_img;
         $arr['user']['vip_level_img_test']  = $vipIcon->show_img ?? '';
-        $arr['user']['sender_level_img']  = $total_sender_level_img->img ?? '';
-        $arr['user']['reciver_level_img']  = $total_received_level_img->img ?? '';
-        $arr['user']['type_user'] =  intval(@$user->type_user) ?: 0;
-        $arr['user']['country'] =  @$user->country;
-        $arr['user']['manger_type'] = !$user->mangerType ? null : new MangerTypeResource(@$user->mangerType);
-        $arr['user']['age'] = $user->profile->age;
+        // $arr['user']['sender_level_img']  = $total_sender_level_img->img ?? '';
+        // $arr['user']['reciver_level_img']  = $total_received_level_img->img ?? '';
+        // $arr['user']['type_user'] =  intval(@$user->type_user) ?: 0;
+        // $arr['user']['country'] =  @$user->country;
+        // $arr['user']['manger_type'] = !$user->mangerType ? null : new MangerTypeResource(@$user->mangerType);
+        // $arr['user']['age'] = $user->profile->age;
 
 
         $toArray = $data->toArray();
