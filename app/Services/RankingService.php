740ce6bc8dfecf3ecedcce2ca98d10541419d533
shami2024 <ahmed.shehata@utdsoftware.com>
2024-12-29 13:22:17 +0200

    Update RankingService.php

--- app/Services/RankingService.php
+++ app/Services/RankingService.php
@@ -214,8 +214,8 @@ class RankingService
         $arr['user']['vip_level']  = $vip_level ?? 0;
         $arr['user']['sender_level']  = $user->total_sender_level ?? '';
         $arr['user']['reciver_level']  = $user->total_received_level ?? '';
-        // $arr['user']['vip_level_img']  = $vip_level_img == 0 ? "" : $vip_level_img;
-        $arr['user']['vip_level_img']  = $vipIcon->show_img ?? '';
+        $arr['user']['vip_level_img']  = $vip_level_img == 0 ? "" : $vip_level_img;
+        // $arr['user']['vip_level_img']  = $vipIcon->show_img ?? '';
         $arr['user']['sender_level_img']  = $total_sender_level_img->img ?? '';
         $arr['user']['reciver_level_img']  = $total_received_level_img->img ?? '';
         $arr['user']['type_user'] =  intval(@$user->type_user) ?: 0;
