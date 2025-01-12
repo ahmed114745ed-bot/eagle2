b2ad42390cd1b620ddbbc960452cca2cce2c0675
shami2024 <ahmed.shehata@utdsoftware.com>
2025-01-12 17:38:55 +0200

    Update CoinReportController.php

--- app/Http/Controllers/Api/V1/CoinReportController.php
+++ app/Http/Controllers/Api/V1/CoinReportController.php
@@ -66,7 +66,7 @@ class CoinReportController extends Controller
     {
         $user = auth()->user();
         $data = CoinLog::where("user_id", $user->id)
-        ->whereIn('method', ['huawei_pay', 'google_pay', 'apple_pay'])
+        ->whereIn('method', ['huawei_pay', 'google_pay', 'apple_pay','fawry'])
         ->when(request("start_date") && request("end_date"), function ($q) {
             $q->whereDate("created_at", ">=", request("start_date"))
               ->whereDate("created_at", "<=", request("end_date"));
