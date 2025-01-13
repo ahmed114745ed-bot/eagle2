7c0a1f8ebbf497b9e83729f6a35802bb093df5f9
makled98 <mkled.backend@utdsoftware.com>
2025-01-13 13:05:57 +0200

    Update MyDataResource.php

--- app/Http/Resources/Api/V1/MyDataResource.php
+++ app/Http/Resources/Api/V1/MyDataResource.php
@@ -174,6 +174,7 @@ class MyDataResource extends JsonResource
             ]),
             'profile_frame' =>Common::hasProfileFramePack($this->id, 28, true),
             'company_number' => Common::getConfig('company_number'),
+            'special_id'          =>  @$this->specialId?->ware?->id ?? 0,
         ];
 
         $data['auth_token'] = $this->auth_token;
