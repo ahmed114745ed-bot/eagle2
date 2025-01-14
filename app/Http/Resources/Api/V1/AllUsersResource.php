455636d97717fee4b166d91ea93e34209872199e
Erinyelkess <ariny.backend@utdsoftware.com>
2025-01-14 19:54:21 +0200

    edit

--- app/Http/Resources/Api/V1/AllUsersResource.php
+++ app/Http/Resources/Api/V1/AllUsersResource.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Resources\Api\V1;
+
 use App\Models\User;
 use App\Facades\UserHandling;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -16,14 +17,14 @@ class AllUsersResource extends JsonResource
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
     public function toArray($request)
-    { 
+    {
         $achievement      = new UserAchievementService();
         $data_achivement = $achievement->getUserAchievement($this->resource);
         return [
             'id' => $this->id,
             'coins' => number_format($this->di),
             'uuid' => $this->uuid . ' ' . $this->original_uuid,
-            'name' => $this->name,
+            'name' => $this->name ?? '',
             'nickname' => $this->nickname,
             'charge_status' => $this->charge_status,
             'hide_chat' => $this->userSetting->hide_chat,
@@ -31,63 +32,59 @@ class AllUsersResource extends JsonResource
             'moment_count' => count($this->moments),
             'total_days' => $this->total_days,
             'total_hours' => $this->liveTime->sum("hours"),
-            'image' => $this->profile->avatar,
+            'image' => $this->profile->avatar ?? '',
+            'image_id' => $this->profile->image_id ?? '',
             'can_play' => UserHandling::chickLevelToPlay($this->resource),
-            'phone' => $this->phone,
-            'agency_id' => $this->agency_id,
-            'family_id' => $this->family_id,
+            'phone' => $this->phone ?? '',
+            'agency_id' => $this->agency_id ?? 0,
+            'family_id' => $this->family_id ?? 0,
             'agency' => [
-                'id' => $this->agency->id,
-                'name' => $this->agency->name,
-                'notice' => $this->agency->notice,
-                'phone' =>$this->agency->phone,
-                'url' => $this->agency->url,
-                'image'=> $this->agency->img,
-                'contents' => $this->agency->contents,
+                'id' => @$this?->agency?->id ?? 0,
+                'name' => @$this?->agency?->name ?? '',
+                'notice' => @$this->agency->notice ?? '',
+                'phone' => @$this?->agency?->phone ?? '',
+                'url' => @$this->agency?->url ?? '',
+                'image' => @$this?->agency?->img ?? '',
+                'contents' => @$this?->agency?->contents ?? '',
             ],
             'target' => $this->targets()->orderBy('created_at', 'desc')->get()->map(function ($target) {
-                $target = $target->only(
-                    [
-                        'id',
-                        'add_month',
-                        'add_year',
-                        'target_usd',
-                        'target_hours',
-                        'target_days',
-                        'target_agency_share',
-                        'user_diamonds',
-                        'user_hours',
-                        'user_days',
-                        'user_obtain',
-                        'agency_obtain',
-                        'updated_at'
-                    ]
-                );
-
-
-                return $target;
+                return [
+                    'id'                  => $target->id,
+                    'add_month'           => $target->add_month,
+                    'add_year'            => $target->add_year,
+                    'target_usd'          => $target->target_usd,
+                    'target_hours'        => $target->target_hours,
+                    'target_days'         => $target->target_days,
+                    'target_agency_share' => $target->target_agency_share,
+                    'user_diamonds'       => $target->user_diamonds,
+                    'user_hours'          => $target->user_hours,
+                    'user_days'           => $target->user_days,
+                    'user_obtain'         => $target->user_obtain,
+                    'agency_obtain'       => $target->agency_obtain,
+                    'updated_at'          => $target->updated_at->toDateTimeString(), // Format date
+                ];
             }),
             'device-token' => [
-                'count' =>User::where('device_token', $this->device_token)->count(),
-                'users' => User::select("name", 'uuid', 'phone')->where('device_token', $this->device_token)->where('device_token', '!=', null)->map(function ($user) {
-                    return [  'name'     => $user['name'],
-                    'uuid' => $user['uuid'],
-                    'phone'  => $user['phone']];
-    
+                'count' => User::where('device_token', $this->device_token)->count(),
+                'users' => User::select("name", 'uuid', 'phone')->where('device_token', $this->device_token)->where('device_token', '!=', null)->get()->map(function ($user) {
+                    return [
+                        'name'     => $user['name'] ?? '',
+                        'uuid' => $user['uuid'] ?? 0,
+                        'phone'  => $user['phone'] ?? 0,
+                    ];
                 }),
             ],
             'achievements' => $data_achivement->map(function ($user) {
 
                 $img = $user["valid_image"] ?? $user["custom_image"];
                 return [
-                    'id'     => $user['id'],
-                    'target' => $user['target'],
-                    'image'  => $img,
+                    'id'     => $user['id'] ?? 0,
+                    'target' => $user['target'] ?? '',
+                    'image'  => $img ?? '',
                 ];
             }),
 
 
         ];
     }
-
 }
