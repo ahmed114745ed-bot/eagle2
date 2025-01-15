5ab03ec0b86cdc0427d50b9e93a9c8fb3ea4906f
johnghattas <john.backend@utdsoftware.com>
2025-01-15 22:59:35 +0200

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,7 @@ return [
     'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Define methods explicitly
     'allowed_origins' => ['https://event.utdsoftware.com'], // Specify frontend origin
     'allowed_origins_patterns' => [],
-    'allowed_headers' => ['Content-Type', 'X-Requested-With', 'Authorization'], // Limit headers
+    'allowed_headers' => ['Content-Type', 'X-Requested-With', 'Authorization', 'X-Localization', '*'], // Limit headers
     'exposed_headers' => [],
     'max_age' => 3600, // Cache the preflight response
     'supports_credentials' => true, // Allow credentials
