6ec08d1f13066cb6be30db9d9799a1e46dfe1ee5
makled98 <mkled.backend@utdsoftware.com>
2025-01-16 14:28:45 +0200

    Update api.php

--- Modules/Reals/Routes/api.php
+++ Modules/Reals/Routes/api.php
@@ -20,7 +20,7 @@ Route::middleware(['auth:sanctum', 'checkLatestToken', 'generalBan','userBan'])-
         Route::prefix('/')->middleware("appFeatureEnable:reel")->group(function (){
             Route::get('reals/user/{user_id?}', 'RealsController@getUserReals');
             Route::get('reals/user-followers', 'RealsController@getUserFollowersReals');
-            Route::apiResource('/reals', 'RealsController');
+            // Route::apiResource('/reals', 'RealsController');
             Route::apiResource('reals/{real_id}/comment', 'RealsUserCommentController', [
                 'names' => [
                     'index' => 'reals.comment.index',
