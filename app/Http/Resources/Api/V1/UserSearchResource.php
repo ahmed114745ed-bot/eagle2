d834e7e010e3605cd36438f5929432d3ff3c9468
shami2024 <ahmed.shehata@utdsoftware.com>
2025-01-19 10:24:19 +0200

    uuid color

--- app/Http/Resources/Api/V1/UserSearchResource.php
+++ app/Http/Resources/Api/V1/UserSearchResource.php
@@ -37,6 +37,7 @@ class UserSearchResource extends JsonResource
         $data = [
             'id'=>@$this->id,
             'uuid'=>@$this->uuid,
+            'color'    => @$this->color_id ??'',
             'name'=>@$this->name?:'',
             'visit_time' => '',
             'profile'=> [
