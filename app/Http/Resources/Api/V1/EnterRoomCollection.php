f296e8387458b9117150101dea88a9684fd3b239
Erinyelkess <ariny.backend@utdsoftware.com>
2025-01-19 12:26:47 +0200

    ad vip in enter room

--- app/Http/Resources/Api/V1/EnterRoomCollection.php
+++ app/Http/Resources/Api/V1/EnterRoomCollection.php
@@ -103,6 +103,7 @@ class EnterRoomCollection extends JsonResource
                 'exp' => @$this->exp ?? 0,
                 'level_num' => @$this->level->level ?? 0,
             ],
+            'vip' => Common::ovip_center(@$owner->id),
         ];
     }
 
