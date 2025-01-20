31d7c7521c524aa4ea3f88c2218d11290a59bfbf
johnghattas <john.backend@utdsoftware.com>
2025-01-20 19:28:34 +0200

    Update CalcsTrait.php

--- app/Traits/HelperTraits/CalcsTrait.php
+++ app/Traits/HelperTraits/CalcsTrait.php
@@ -720,6 +720,7 @@ trait CalcsTrait
             'image_from_wares'     => $vipIcon->show_img ?? '',
             'expire'    => $vip->expire ?? 0,
             'ware_id' => $vipIcon->id,
+            'color' => '',
             'colored_name' => $hasColor ? common::wareUserVip($user->id, 18, 'color') ?? '' : '',
         ];
     }
