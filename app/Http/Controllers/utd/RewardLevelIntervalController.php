19615bf7a3789a7481f7cf2c4ead78a8f38be799
Erinyelkess <ariny.backend@utdsoftware.com>
2025-01-21 15:32:01 +0200

    interval

--- app/Http/Controllers/utd/RewardLevelIntervalController.php
+++ app/Http/Controllers/utd/RewardLevelIntervalController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\utd;
 
+use App\Models\OVip;
+use App\Models\Ware;
 use App\Helpers\Common;
 use App\Enums\IntervalLevel;
 use Illuminate\Http\Request;
@@ -70,8 +72,39 @@ class RewardLevelIntervalController extends Controller
 
     public function allType()
     {
-       
+
         $data = IntervalLevel::getTranslatedOptions();
+        return Common::apiResponse(1, 'success', $data, 200);
+    }
+
+    public function wareInterval()
+    {
+        $ops = [];
+        $wares = Ware::query()->select(['id', 'name', 'type'])->whereIn('type', [4, 5, 6])->get();
+        foreach ($wares as  $ware) {
+            $ops[$ware->id] = $ware->name . '_' . $ware->id;
+
+            if ($ware->type == 4) {
+                $ops[$ware->id] .= '_' . 'frame';
+            } elseif ($ware->type == 5) {
+                $ops[$ware->id] .= '_' . 'bubble';
+            } elseif ($ware->type == 6) {
+                $ops[$ware->id] .= '_' . 'intro';
+            }
+        }
+
+        $data = $ops;
+        return Common::apiResponse(1, 'success', $data, 200);
+    }
+
+    public function vipInterval()
+    {
+        $vips = OVip::query()->select('id', 'name')->get();
+        foreach ($vips as  $vip) {
+            $ops[$vip->id] = $vip->name;
+        }
+        
+        $data = $ops;
         return Common::apiResponse(1, 'success', $data, 200);
     }
 }
