b91577b399a3cf32d8eaafab00e591f732713bf9
shami2024 <67282324+ahmedmakled517@users.noreply.github.com>
2025-01-23 17:11:40 +0200

    Update RankingRepository.php

--- app/Repositories/RankingRepository.php
+++ app/Repositories/RankingRepository.php
@@ -67,14 +67,40 @@ class RankingRepository
 
     protected function applyDateFilters(&$query, $type)
     {
+       
+        // if ($type == 0) {
+        //     $query->whereBetween('created_at', [Carbon::now()->startOfHour(), Carbon::now()->endOfHour()]);
+        // } elseif ($type == 1) {
+        //     $query->whereBetween('created_at', [Carbon::now()->startOfDay(), Carbon::now()->endOfDay()]);
+        // } elseif ($type == 2) {
+        //     $query->whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()]);
+        // } elseif ($type == 3) {
+        //     $query->whereMonth('created_at', Carbon::now()->month)->whereYear('created_at', Carbon::now()->year);
+        // }
+        $currentTime = now()->subHours(6);
+
         if ($type == 0) {
-            $query->whereBetween('created_at', [Carbon::now()->startOfHour(), Carbon::now()->endOfHour()]);
+
+            $startHour = $currentTime->startOfHour();
+            $endHour = $currentTime->endOfHour();
+            $query->whereBetween('created_at', [$startHour, $endHour]);
+
         } elseif ($type == 1) {
-            $query->whereBetween('created_at', [Carbon::now()->startOfDay(), Carbon::now()->endOfDay()]);
+
+            $startDay = $currentTime->copy()->subDay()->setTime(18, 0, 0);
+            $endDay = $currentTime->copy()->setTime(17, 59, 59);
+            $query->whereBetween('created_at', [$startDay, $endDay]);
+
+
         } elseif ($type == 2) {
-            $query->whereBetween('created_at', [Carbon::now()->startOfWeek(), Carbon::now()->endOfWeek()]);
+
+            $startWeek = $currentTime->copy()->startOfWeek()->subDay()->setTime(18, 0, 0); 
+            $endWeek = $currentTime->copy()->endOfWeek()->setTime(17, 59, 59); 
+            $query->whereBetween('created_at', [$startWeek, $endWeek]);
+
         } elseif ($type == 3) {
-            $query->whereMonth('created_at', Carbon::now()->month)->whereYear('created_at', Carbon::now()->year);
+
+            $query->whereMonth('created_at', $currentTime->month)->whereYear('created_at', $currentTime->year);
         }
     }
 }
