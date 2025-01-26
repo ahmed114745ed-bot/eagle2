41d9ed80b5321243fd3836938cb308f44d91af28
makled98 <mkled.backend@utdsoftware.com>
2025-01-26 14:41:46 +0200

    Update BannerController.php

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -83,7 +83,6 @@ class BannerController extends Controller
         $banners = $this->bannerServices->index2($ids);
         if (empty($banners)) {
             UserBannerShow::where("user_id", $user->id)->delete();
-            dd(1);
            return Common::apiResponse(true, 'successful', null);
         }
         // UserBannerShow::where("user_id", $user->id)->delete();
