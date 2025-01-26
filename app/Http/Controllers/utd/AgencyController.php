64bd6d21384cb8066eb4958f42d3859bf392c329
Erinyelkess <ariny.backend@utdsoftware.com>
2025-01-26 13:46:55 +0200

    agency

--- app/Http/Controllers/utd/AgencyController.php
+++ app/Http/Controllers/utd/AgencyController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use App\Tik\Services\AgencyService;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
+use App\Http\Resources\ActiveAgencyResource;
 use App\Http\Resources\AgencyRequestsResource;
 
 
@@ -44,6 +45,125 @@ class AgencyController extends Controller
             return Common::apiResponse(1, 'deleted request',  200);
         } else {
             return Common::apiResponse(1, 'accept request',  200);
+        }
+    }
+
+    public function activeAgencies(Request $request)
+    {
+        try {
+            $data = $this->agencyService->activeAgencies($request->id,  $request->per_page,  $request->page);
+            return Common::apiResponse(true, 'success', ActiveAgencyResource::collection($data));
+        } catch (Exception $exception) {
+
+            return Common::apiResponse(0, $exception->getMessage(), null, 400);
+        }
+    }
+
+    public function delete(Request $request)
+    {
+        try {
+            $data = $this->agencyService->deleteAgency($request->id);
+            return Common::apiResponse(true, 'deleted successfully');
+        } catch (Exception $exception) {
+
+            return Common::apiResponse(0, $exception->getMessage(), null, 400);
+        }
+    }
+
+    public function changeAgencyMembers(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'old_agency_id' => 'required|integer|exists:agencies,id',
+            'new_agency_id' => 'required|integer|exists:agencies,id',
+        ]);
+
+        if ($validator->fails()) {
+            return Common::apiResponse(0, __('api_responses.validation_error'), $validator->errors());
+        }
+        try {
+            $data = $this->agencyService->changeAgencyMembers($request->old_agency_id, $request->new_agency_id);
+            return Common::apiResponse(true, 'changes successfully');
+        } catch (Exception $exception) {
+
+            return Common::apiResponse(0, $exception->getMessage(), null, 400);
+        }
+    }
+
+    public function allAgenciesExceptOld(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'old_agency_id' => 'required|integer|exists:agencies,id',
+        ]);
+
+        if ($validator->fails()) {
+            return Common::apiResponse(0, __('api_responses.validation_error'), $validator->errors());
+        }
+
+        try {
+            $data = $this->agencyService->AllAgencyExceptOld($request->old_agency_id, $request->per_page, $request->page);
+            return Common::apiResponse(true, 'success', AgencyRequestsResource::collection($data));
+        } catch (Exception $exception) {
+
+            return Common::apiResponse(0, $exception->getMessage(), null, 400);
+        }
+    }
+
+    public function create(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'app_owner_id' => 'required|integer|exists:users,id',
+            'name' => 'required|string',
+            'phone' => 'required|string',
+            'notice' => 'required',
+            'url' => 'nullable',
+            'img' => 'required|mimes:jpeg,png,jpg',
+            'contents' => 'nullable',
+            'Host_agency' => 'required|boolean',
+            'Shipping_agency' => 'required|boolean',
+        ]);
+        if ($validator->fails()) {
+            return Common::apiResponse(0, __('api_responses.validation_error'), $validator->errors());
+        }
+        try {
+            $this->agencyService->createAgencyUtd($request);
+            return Common::apiResponse(1, 'created successfully',  200);
+        } catch (Exception $e) {
+            return Common::apiResponse(false, $e->getMessage(), null, 407);
+        }
+    }
+
+    public function update($id, Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'app_owner_id' => 'required|integer|exists:users,id',
+            'name' => 'required|string',
+            'phone' => 'required|string',
+            'notice' => 'required',
+            'url' => 'nullable',
+            'img' => 'required|mimes:jpeg,png,jpg',
+            'contents' => 'nullable',
+            'Host_agency' => 'required|boolean',
+            'Shipping_agency' => 'required|boolean',
+        ]);
+        if ($validator->fails()) {
+            return Common::apiResponse(0, __('api_responses.validation_error'), $validator->errors());
+        }
+        try {
+            $this->agencyService->updateAgencyUtd($id, $request);
+            return Common::apiResponse(1, 'updated successfully',  200);
+        } catch (Exception $e) {
+            return Common::apiResponse(false, $e->getMessage(), null, 407);
+        }
+    }
+
+    public function show($id)
+    {
+        try {
+            $data = $this->agencyService->show($id);
+            return Common::apiResponse(true, 'success', $data);
+        } catch (Exception $exception) {
+
+            return Common::apiResponse(0, $exception->getMessage(), null, 400);
         }
     }
 }
