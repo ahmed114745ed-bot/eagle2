8581738bc6d7a9b778c497329d4d222be420e81a
johnghattas <john.backend@utdsoftware.com>
2025-01-26 17:34:51 +0200

    update exchange diamond

--- app/Classes/Gifts/UpdateUserWhenSendGift.php
+++ app/Classes/Gifts/UpdateUserWhenSendGift.php
@@ -35,6 +35,9 @@ class UpdateUserWhenSendGift
         $receivedUser->monthly_diamond_received += $totalCoins;
         $receivedUser->total_diamond_received   += $totalCoins;
         // update levels
+        if ($receivedUser->user_type == 0 && $receivedUser->agency_id == 0) {
+            $receivedUser->exchange_diamonds += $totalCoins;
+        }
 
         $lastReceivedLevel = $receivedUser->total_received_level;
 
