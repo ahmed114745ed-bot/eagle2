1cf05080fa092bb065ecc67d9da2e0e21ba4848b
Erinyelkess <ariny.backend@utdsoftware.com>
2025-01-27 16:04:57 +0200

    Merge branch 'test' of https://github.com/UTD-Backend/Eagle into test

--- routes/utd.php
+++ routes/utd.php
@@ -513,17 +513,17 @@ Route::middleware([])->group(function () {
 
     Route::prefix('countries')->group(function () {
         Route::get('/', [CountryController::class, 'index']);
-       
+
     });
 
     Route::prefix('colors')->group(function () {
         Route::get('/', [CountryController::class, 'index']);
-       
+
     });
 
     Route::prefix('colors')->group(function () {
         Route::get('/', [ColorController::class, 'index']);
-       
+
     });
 
 });
