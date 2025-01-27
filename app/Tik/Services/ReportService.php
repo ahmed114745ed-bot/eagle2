718e4af445c417973900f3a1386543e0d0289a9c
Erinyelkess <ariny.backend@utdsoftware.com>
2025-01-27 12:39:33 +0200

    edit

--- app/Tik/Services/ReportService.php
+++ app/Tik/Services/ReportService.php
@@ -20,10 +20,11 @@ class ReportService
     public function report($request)
     {
         if ($request->type == 'users') {
-            $data = $this->userRepository->report($request->uuid, $request->agencyId, $request->month, $request->year, $request->per_page, $request->page);
+            $data = $this->userRepository->report($request->uuid, $request->agency_id, $request->month, $request->year, $request->per_page, $request->page);
             return UserReportResource::collection($data);
         } elseif ($request->type == 'agencies') {
-            $data = $this->agencyRepository->report($request->id, $request->month, $request->year, $request->per_page, $request->page);
+            $data = $this->agencyRepository->report($request->id, $request->month , $request->year, $request->per_page, $request->page);
+            
             return AgencyReportResource::collection($data);
         } elseif ($request->type == 'agencies_manger') {
             $data = $this->adminUsersRepository->report($request->id, $request->per_page, $request->page);
