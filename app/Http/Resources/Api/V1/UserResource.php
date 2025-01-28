c3d4b1b814ed8a63870b708ef02dc4dd17f658ef
mido203020 <Mohamed.gamal@utdsoftware.com>
2025-01-28 14:28:06 +0200

    Test

--- app/Http/Resources/Api/V1/UserResource.php
+++ app/Http/Resources/Api/V1/UserResource.php
@@ -67,7 +67,7 @@ class UserResource extends JsonResource
             $timeDifferenceFormatted = "In the future";
         }
 
-        $frame  = !empty($this->getUserDress(4, $this->dress_1, 'img1')) ?  $this->getUserDress(4, $this->dress_1, 'img1') : $this->getUserDress(4, $this->dress_1, 'img2');
+        $frame  = $this->getUserDress(4, $this->dress_1, 'img2') == '' ? $this->getUserDress(4, $this->dress_1, 'img1') : $this->getUserDress(4, $this->dress_1, 'img2');
         // Common::getUserDress($this->id, $this->dress_1, 4, 'img2', true) ?: Common::getUserDress($this->id, $this->dress_1, 4, 'img1', true);
         $bubble = $this->getUserDress(5, $this->dress_2, 'show_img');
         //  Common::getUserDress($this->id, $this->dress_2, 5, 'show_img', true);
