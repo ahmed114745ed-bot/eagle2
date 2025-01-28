6daa20c80e50770c6f43ccabd806d47a39024356
mido203020 <Mohamed.gamal@utdsoftware.com>
2025-01-28 15:10:19 +0200

    test

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -409,10 +409,9 @@ class UserService
 
     public function showUser($userId, $auth, $request, $isVisit)
     {
-        $user = $this->userRepository->findOrFail($userId, ['packs' => function ($q) {
-            /** @var Builder $q*/
+        $user = $this->userRepository->findOrFail($userId, ['packs' /* => function ($q) {
             $q->whereIn('type', [20, 18, 17, 20, 19, 16, 13, 3, 4, 5])->where('is_used', 1)->with('ware');
-        }, 'profile', 'room', 'family']);
+        } */, 'profile', 'room', 'family']);
         if (!$user) throw new \Exception('not found');
         if (in_array($user->id, Common::getUserBlackList($auth->id))) throw new \Exception('in black list');
         $request['user_id'] = $userId;
