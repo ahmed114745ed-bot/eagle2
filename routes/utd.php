5a4d167476300ff776ad271f8c4199e2135472d5
mido203020 <Mohamed.gamal@utdsoftware.com>
2025-01-28 16:45:40 +0200

    adding frame into user and adding home-carousels apis

--- routes/utd.php
+++ routes/utd.php
@@ -60,6 +60,7 @@ use App\Http\Controllers\Api\V1\PaymentMethodController;
 use App\Http\Controllers\utd\PercentageTargetController;
 use App\Http\Controllers\Api\V1\AgencyStatisticController;
 use App\Http\Controllers\utd\AppearChargerAgencyController;
+use App\Http\Controllers\utd\HomeCarouselController;
 use App\Http\Controllers\utd\RewardLevelIntervalController;
 use App\Http\Controllers\utd\RequestBackgroundImageController;
 use Modules\Achievement\Http\Controllers\UtdAchievementController;
@@ -223,6 +224,16 @@ Route::middleware([])->group(function () {
         Route::get('/{id}', [BannerController::class, 'show']);
     });
 
+    Route::prefix('home-carousels')->group(function () {
+        Route::get('/', [HomeCarouselController::class, 'index']);
+        Route::post('/create', [HomeCarouselController::class, 'store']);
+        Route::post('/delete-all', [HomeCarouselController::class, 'delete_all']);
+        Route::post('update/{id}', [HomeCarouselController::class, 'update']);
+        Route::post('delete/{id}', [HomeCarouselController::class, 'delete']);
+        Route::post('update-enable/{id}', [HomeCarouselController::class, 'update_is_active']);
+        Route::get('/{id}', [HomeCarouselController::class, 'show']);
+
+    });
 
     Route::prefix('daily-gift-types')->group(function () {
         Route::get('/', [DailyGiftTypesController::class, 'index']);
@@ -528,12 +539,12 @@ Route::middleware([])->group(function () {
 
     Route::prefix('countries')->group(function () {
         Route::get('/', [CountryController::class, 'index']);
-       
+
     });
 
     Route::prefix('colors')->group(function () {
         Route::get('/', [ColorController::class, 'index']);
-       
+
     });
 
     Route::prefix('target-events')->group(function () {
