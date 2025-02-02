36b61f2a33e33ef9ee82ed52bf76c2c3ee41e3cf
shami2024 <>
2025-02-02 10:56:55 +0200

    Update UserAchievementLevel.blade.php

--- resources/views/admin/grid/users/UserAchievementLevel.blade.php
+++ resources/views/admin/grid/users/UserAchievementLevel.blade.php
@@ -2,7 +2,7 @@
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
 <div class="box box-solid">
-    <div style="height: 417px;     height: 417px;
+    <div style="height: 417px;     height: 517px;
     margin: auto;
     width: 46%;
     top:50px;
