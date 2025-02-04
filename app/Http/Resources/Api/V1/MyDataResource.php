0ebfd3427d0bc0754c8b0cb397dfa06dd81563d3
shami2024 <>
2025-02-04 11:43:10 +0200

    test my data

--- app/Http/Resources/Api/V1/MyDataResource.php
+++ app/Http/Resources/Api/V1/MyDataResource.php
@@ -163,7 +163,7 @@ class MyDataResource extends JsonResource
 
             ],
             'phone_bind' => (bool)@$this->phone,
-            'vip' => Common::ovip_center($this->id),
+            'vip' => Common::ovip_center($this),
             'family_id' => @$this->family_id,
             'uuid' => @$this->uuid,
             'special_color'    => @$this->color_id ?? '',
