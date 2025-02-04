a14731ddc3d05d94a8fce5bb0a27018d722c0523
Erinyelkess <ariny.backend@utdsoftware.com>
2025-02-04 11:30:46 +0200

    room

--- app/Services/RoomService.php
+++ app/Services/RoomService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use Exception;
 use App\Repositories\Room\RoomRepo;
 use App\Repositories\Room\RoomRepository;
 
@@ -10,8 +11,10 @@ class RoomService
     protected $roomRepo;
     protected $roomRepository;
 
-    public function __construct(RoomRepo $roomRepo,RoomRepository $roomRepository)
-    {
+    public function __construct(
+        RoomRepo $roomRepo,
+        RoomRepository $roomRepository,
+    ) {
         $this->roomRepo = $roomRepo;
         $this->roomRepository = $roomRepository;
     }
@@ -21,9 +24,10 @@ class RoomService
         return $this->roomRepo->all($request);
     }
 
-
-    
-
-
-
+    public function roomDetails($userId)
+    {
+        $room = $this->roomRepository->FindByUserId($userId);
+        if (!$room) throw new Exception('This user don\'t have room');
+        return $room;
+    }
 }
