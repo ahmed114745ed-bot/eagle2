32492b35f081138d4628a386c2901e1f1c146a51
shami2024 <>
2025-02-04 10:24:42 +0200

    Update MyDataResource.php

--- app/Http/Resources/Api/V1/MyDataResource.php
+++ app/Http/Resources/Api/V1/MyDataResource.php
@@ -117,7 +117,7 @@ class MyDataResource extends JsonResource
         $pks = !is_null($ownerRoom?->id) ? $this->getRoomTwoLastPk($ownerRoom->id) : null;
         /**@var User $this
          * @var Room $ownerRoom*/
-        dd('k');
+      
         $data = [
             'id' => @$this->id,
             'notification_id' => @$this->notification_id ?: "",
@@ -202,7 +202,7 @@ class MyDataResource extends JsonResource
         if ($pass_status) {
             $data['pass_status'] = $pass_status;
         }
-
+        dd('data');
         return $data;
     }
     private function getRoomTwoLastPk(int $roomId)
