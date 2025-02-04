4f037be7c16b5d387ceb647142694c03a9c541cc
shami2024 <>
2025-02-04 10:21:22 +0200

    Update UserController.php

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -200,9 +200,10 @@ class UserController extends Controller
 
             return Common::apiResponse(0, $exception->getMessage(), null, 400);
         }
-        dd($userWithMedals);
+        
         $this->userService->unlockDressHand($user->id);
         request()->default_background = \DB::table('backgrounds')->where('enable', 1)->orderBy('id', 'asc')->first()?->img;
+        dd($userWithMedals);
         $data = new MyDataResource($userWithMedals);
 
         return Common::apiResponse(true, '', $data, 200);
