7c75963d733f39575969874568b5e685483c9f00
shami2024 <>
2025-02-04 10:25:41 +0200

    Update MyDataResource.php

--- app/Http/Resources/Api/V1/MyDataResource.php
+++ app/Http/Resources/Api/V1/MyDataResource.php
@@ -202,7 +202,6 @@ class MyDataResource extends JsonResource
         if ($pass_status) {
             $data['pass_status'] = $pass_status;
         }
-        dd($data);
         return $data;
     }
     private function getRoomTwoLastPk(int $roomId)
