89c4bce8ad338f9b3379f30406da55c70e549118
makled98 <mkled.backend@utdsoftware.com>
2025-02-09 15:58:24 +0200

    Update MusicController.php

--- app/Http/Controllers/Api/V1/MusicController.php
+++ app/Http/Controllers/Api/V1/MusicController.php
@@ -18,7 +18,6 @@ class MusicController extends Controller
     public function index()
     {
         $data = $this->musicService->all();
-        dd($data);
         return Common::apiResponse(1, '', MusicResource::collect($data));
     }
 
