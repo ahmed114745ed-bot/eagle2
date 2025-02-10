651668892288cba26f3c9865fdd4548fc197e024
mido203020 <Mohamed.gamal@utdsoftware.com>
2025-02-10 15:29:17 +0200

    test

---
+++ database/seeders/RoomSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Pk;
+use App\Models\Room;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoomSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        for($i=0;$i<10;$i++){
+
+            $room = Room::create([
+                'uid' => 828,
+                'room_name' => fake()->name(),
+                'numid' => rand(10000,20000)
+            ]);
+            $team1 = User::inRandomOrder()->take(4)->get();
+            $team2 = User::inRandomOrder()->take(4)->get();
+            Pk::create([
+                'room_id' => $room->id,
+                'team_1' => $team1[0]->id . ',' . $team1[1]->id . ',' .$team1[2]->id . ',' . $team1[3]->id,
+                'team_2' => $team2[0]->id . ',' . $team2[1]->id . ',' .$team2[2]->id . ',' . $team2[3]->id,
+                't1_score' => rand(10,100),
+                't2_score' => rand(10,100),
+                'winner' => 1,
+                'start_at' => date('Y-m-d'),
+                'end_at' => date('Y-m-d')
+            ]);
+        }
+    }
+}
