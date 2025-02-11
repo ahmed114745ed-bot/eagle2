fad18c402ce678f07c2e81c6d8c2653540509b69
makled98 <mkled.backend@utdsoftware.com>
2025-02-11 12:00:58 +0200

    Update AgoraController.php

--- app/Http/Controllers/Api/V1/AgoraController.php
+++ app/Http/Controllers/Api/V1/AgoraController.php
@@ -30,8 +30,28 @@ class AgoraController extends Controller
     }
 
     public function webhook(Request $request){
-        Log::info('agora webhook triggered', [
-            $request->all()
-        ]);
+        // Log::info('agora webhook triggered', [
+        //     $request->all()
+        // ]);
+        $agoraSignature = $request->header('Agora-Signature');
+        Log::info("Agora-Signature: " . $agoraSignature);
+
+        // التحقق من أن الطلب يحتوي على JSON صحيح
+        $data = $request->json()->all();
+
+        // التحقق من صحة البيانات المطلوبة
+        if (!isset($data['eventType'], $data['payload'])) {
+            return response()->json(['error' => 'Invalid JSON structure'], 400);
+        }
+
+        // استخراج البيانات
+        $eventType = $data['eventType'];
+        $uid = $data['payload']['uid'] ?? null;
+        $channelName = $data['payload']['channelName'] ?? '';
+        $clientSeq = $data['payload']['clientSeq'] ?? '';
+
+        // تسجيل البيانات
+        Log::info("Event code: $eventType, UID: $uid, Channel: $channelName, ClientSeq: $clientSeq");
+
     }
 }
