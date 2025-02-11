be9fffa3d9dc2ed5d3d32c6b7e56a2fbea38d6e1
Erinyelkess <ariny.backend@utdsoftware.com>
2025-02-11 16:13:36 +0200

    edit

--- routes/api.php
+++ routes/api.php
@@ -92,7 +92,7 @@ Route::prefix(config('app.api_prefix'))->group(function () {
         Route::post('app-check', [VersionController::class, 'versionAndCache']);
     });
 
-    Route::get('/image-intro/{id}',[ UserController::class, 'image_intro']);
+    Route::get('/image-intro/{id}', [UserController::class, 'image_intro']);
     Route::get('colors', [ColorController::class, 'index']);
     Route::get('all-servers', [RegisterController::class, 'all_servers']);
 
@@ -492,11 +492,11 @@ Route::prefix(config('app.api_prefix'))->group(function () {
                 Route::get('/user', [MusicController::class, 'userMusic']);
                 Route::post('/create', [MusicController::class, 'store']);
             });
-
-            Route::get('/privacy-policy', function () {
-                $Page=\App\Models\Page::where("name","privacy-policy")->first();
-                return response()->json(['html' => $Page]);
-            });
         }
     );
+
+    Route::get('/privacy-policy', function () {
+        $Page = \App\Models\Page::where("name", "privacy-policy")->first();
+        return response()->json(['html' => $Page]);
+    });
 });
