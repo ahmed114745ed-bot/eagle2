b3b4c19be6f50063315fdda6c0032811c8d110ff
mido203020 <Mohamed.gamal@utdsoftware.com>
2025-02-12 11:56:27 +0200

    adding gifts end point

--- app/Http/Controllers/Api/V1/RoomController.php
+++ app/Http/Controllers/Api/V1/RoomController.php
@@ -1752,7 +1752,7 @@ class RoomController extends Controller
 
     public function roomGifts($id){
         $perPage = request('per_page',10);
-        $result = Room::where('uid',$id)->gifts()->paginate($perPage);
+        $result = Room::where('uid',$id)->first()->gifts()->paginate($perPage);
         return Common::apiResponse(true, 'done', $result);
     }
 }
