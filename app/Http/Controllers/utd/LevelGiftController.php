6f986c07b68ca26d57e2089dd122739f489c2af5
makled98 <mkled.backend@utdsoftware.com>
2025-02-13 11:20:23 +0200

    fd

--- app/Http/Controllers/utd/LevelGiftController.php
+++ app/Http/Controllers/utd/LevelGiftController.php
@@ -90,7 +90,7 @@ class LevelGiftController extends Controller
         } elseif ($request->type === 'vip') {
             $data['item_id'] = $request->item_id;
         } elseif ($request->type === 'coins') {
-            $data['item_id'] = $request->coins;
+            $data['item_id'] = $request->item_id;
         } elseif ($request->type === 'achievement' && $request->hasFile('item_id')) {
             $data['item_id'] = Common::upload('achievements', $request->item_id);
         }
