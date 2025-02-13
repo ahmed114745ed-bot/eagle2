bae3b233f98cc35a044b004e7f777b302edd8476
Erinyelkess <ariny.backend@utdsoftware.com>
2025-02-13 17:32:39 +0200

    enum home carousel

--- routes/utd.php
+++ routes/utd.php
@@ -403,12 +403,16 @@ Route::middleware([])->group(function () {
 
     Route::prefix('home-carousels')->group(function () {
         Route::get('/', [HomeCarouselController::class, 'index']);
+        Route::get('/type', [HomeCarouselController::class, 'type']);
+        Route::get('/event-type', [HomeCarouselController::class, 'eventType']);
+        Route::get('/form', [HomeCarouselController::class, 'form']);
         Route::post('/create', [HomeCarouselController::class, 'store']);
         Route::post('/delete-all', [HomeCarouselController::class, 'delete_all']);
         Route::post('update/{id}', [HomeCarouselController::class, 'update']);
         Route::post('delete/{id}', [HomeCarouselController::class, 'delete']);
         Route::post('update-enable/{id}', [HomeCarouselController::class, 'update_is_active']);
         Route::get('/{id}', [HomeCarouselController::class, 'show']);
+       
     });
 
     Route::prefix('official-msgs')->group(function () {
