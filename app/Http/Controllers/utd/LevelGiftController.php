de4119490e08f266680ef78d0ff26d49dddfa5a2
Erinyelkess <ariny.backend@utdsoftware.com>
2025-02-13 13:10:13 +0200

    edit

--- app/Http/Controllers/utd/LevelGiftController.php
+++ app/Http/Controllers/utd/LevelGiftController.php
@@ -120,7 +120,7 @@ class LevelGiftController extends Controller
 
         $request->validate([
             'type' => 'required|in:ware,vip,coins,achievement',
-            'item_id' => 'nullable|integer',
+            'item_id' => 'required',
             'coins' => 'nullable|integer|min:1',
             'achievement' => 'nullable|image',
             'expire' => 'nullable|integer|min:1',
