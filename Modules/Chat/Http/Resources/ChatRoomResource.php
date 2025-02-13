3a2482b1a7d3c59aca23019ccb49570baaebbe12
Erinyelkess <ariny.backend@utdsoftware.com>
2025-02-13 16:20:11 +0200

    edit

--- Modules/Chat/Http/Resources/ChatRoomResource.php
+++ Modules/Chat/Http/Resources/ChatRoomResource.php
@@ -13,24 +13,23 @@ class ChatRoomResource extends JsonResource
     public function toArray(Request $request)
     {
 
-        if($this->user_id !== $request->user()->id)
-        {
+        if ($this->user_id !== $request->user()->id) {
             $user = User::find($this->user_id2);
             $user2 = User::find($this->user_id);
-        }
-        else{
+        } else {
             $user = User::find($this->user_id);
             $user2 = User::find($this->user_id2);
         }
 
-        $total_undread_message = ChatMessage::where('chat_room_id',$this->id)->where('user_id','not Like',$user->id)->where('status','not Like' ,'seen')->count();
+        $total_undread_message = ChatMessage::where('chat_room_id', $this->id)->where('user_id', 'not Like', $user->id)->where('status', 'not Like', 'seen')->count();
         return [
             'user_id'             => @$user2->id,
             'name'                => @$user2->name,
             'img'                 => @$user2->profile->avatar,
+            'in_room'             => @$user2->now_room_uid ? true : false,
             'chat_id'             => $this->id,
             'unread_message'      => $total_undread_message,
-            'last_message'        => @ new ChatMessageResource( $this->messages[0]),
+            'last_message'        => @new ChatMessageResource($this->messages[0]),
         ];
     }
 }
