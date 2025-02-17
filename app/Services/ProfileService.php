4b02add60410aa2d0e7958d4a704f10e459e4a20
Erinyelkess <ariny.backend@utdsoftware.com>
2025-02-17 16:39:39 +0200

    edit profile update

--- app/Services/ProfileService.php
+++ app/Services/ProfileService.php
@@ -58,7 +58,7 @@ class ProfileService
         //     }
         // }
         if ($request->has('old_multi_image')) {
-            $newImages = $request->multi_image; 
+            $newImages = $request->old_multi_image; 
         
             $existingImages = $user->images()->pluck('img')->toArray();
         
