4dcc287845243733e464f4c8d3c9b74f977e127a
Erinyelkess <ariny.backend@utdsoftware.com>
2025-02-17 12:34:24 +0200

    edit

--- Modules/SalaryTransaction/Entities/ChargeCountry.php
+++ Modules/SalaryTransaction/Entities/ChargeCountry.php
@@ -11,7 +11,7 @@ use Modules\SalaryTransaction\Database\factories\ChargeCountryFactory;
 class ChargeCountry extends Model
 {
     use HasFactory;
-
+    protected $guarded = ['id'];
     public function getCreatedAtAttribute($value)
     {
         $timeZone = request()->header('tz') ?? 'UTC';
