9674d9d3d4631d2e23695d64529486c0b5276f3b
Erinyelkess <ariny.backend@utdsoftware.com>
2025-02-17 17:41:46 +0200

    edit

--- app/Repositories/ProfileRepository.php
+++ app/Repositories/ProfileRepository.php
@@ -20,9 +20,11 @@ class ProfileRepository
 
     public function updateUser($user, $data)
     {
-        $user->fill($data);
-        $user->is_points_first = 0;
-        $user->save();
+        $data['is_points_first'] = 0;
+        $user->update($data);
+        // $user->fill($data);
+        // $user->is_points_first = 0;
+        // $user->save();
         return $user;
     }
 
@@ -39,21 +41,23 @@ class ProfileRepository
         $profile->save();
         return $profile;
     }
-    
-    public function getProfileVisits(User $user,$keyword)
+
+    public function getProfileVisits(User $user, $keyword)
     {
         return $user->profileVisits()->with([
             'room' => function ($query) {
                 return $query->withoutAppends()->select(['id', 'room_pass', 'uid']);
             },
-            'followPacks', 'profile', 'ware', 'UserVip'
+            'followPacks',
+            'profile',
+            'ware',
+            'UserVip'
         ])->fitterByUuid($keyword)->paginate(15);
-    } 
+    }
 
     public function getRandomUsers($limit = 10)
     {
         $user = User::find(auth()->user()->id);
-        return User::has("images")->whereNotIn("id",$user?->followeds?->pluck("followed_user_id")->toArray())->inRandomOrder()->where("online",1)->limit($limit)->get();
+        return User::has("images")->whereNotIn("id", $user?->followeds?->pluck("followed_user_id")->toArray())->inRandomOrder()->where("online", 1)->limit($limit)->get();
     }
-   
 }
