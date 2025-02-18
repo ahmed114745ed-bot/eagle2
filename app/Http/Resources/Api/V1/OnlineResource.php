26925828d4bcdab37f1612b5c7332ae0bd2e42c5
Erinyelkess <ariny.backend@utdsoftware.com>
2025-02-18 18:56:52 +0200

    edit

--- app/Http/Resources/Api/V1/OnlineResource.php
+++ app/Http/Resources/Api/V1/OnlineResource.php
@@ -14,7 +14,7 @@ class OnlineResource extends JsonResource
             'id' => $this->id ?? 0,
             'uuid' => $this->uuid ?? 0,
             'image' => $this->profile?->avatar ?? '',
-            'country' => $this->country ?? (object) [],
+            'country' => @$this->country,
             'is_followed'            => $this->is_followed,
             'is_follow'            => $this->is_follow, // user data  ----
             'is_friend'            => $this->isFriends(),
