f51b5826830f9044853fbcf6d0a06314ed536fa7
Erinyelkess <ariny.backend@utdsoftware.com>
2025-02-19 13:57:45 +0200

    edit

--- Modules/Events/Entities/GeneralRole.php
+++ Modules/Events/Entities/GeneralRole.php
@@ -9,6 +9,8 @@ use Illuminate\Database\Eloquent\Model;
 class GeneralRole extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
+    
     public function getCreatedAtAttribute($value)
     {
         $timeZone = request()->header('tz') ?? 'UTC';
