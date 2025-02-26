72000a3901c1532de47baa11204218e9067dfce6
Erinyelkess <ariny.backend@utdsoftware.com>
2025-02-26 13:34:05 +0200

    edit

--- app/Http/Resources/Api/V1/RecevingReportResource.php
+++ app/Http/Resources/Api/V1/RecevingReportResource.php
@@ -11,11 +11,23 @@ class RecevingReportResource extends JsonResource
    
     public function toArray($request)
     {
+        $name = '';
+        $image = '';
+        if($this->charger_type == 'dash'){
+            // admin
+            $name = $this->admin?->name;
+            $image = $this->admin?->avatar;
+        } else if($this->charger_type == 'agency'){
+            $name = $this->senderAgency?->name;
+            $image = $this->senderAgency?->img;
+        }
         return [
-            'id'          => $this->user_id, 
+            'id'          => $this->user_id,
             'diamonds'    => numToStringNew($this->amount),
             'operation_no'=> (int)$this->id,
-            'created_at'  =>Carbon::parse( $this->created_at)->format('Y-m-d h:i:s A'),
+            'created_at'  => Carbon::parse( $this->created_at)->format('Y-m-d h:i:s A'),
+            'name' => $name,
+            'image' => $image,
         ];
     }
 }
