6534bba0160adac4558da3b45769e09d01c941ed
Erinyelkess <ariny.backend@utdsoftware.com>
2025-02-26 16:32:54 +0200

    edit

--- app/Http/Resources/Api/V1/MangerTypeResource.php
+++ app/Http/Resources/Api/V1/MangerTypeResource.php
@@ -15,10 +15,10 @@ class MangerTypeResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id'=>$this->id ?? 0,
-            'name'=>app()->getLocale() == "ar" ? $this->name_ar ?? '' : $this->name_en ?? '',
-            'img'=>$this?->img ?? '',
-            'description'=>app()->getLocale() == "ar" ? $this->description_ar ?? '' : $this->description_en ?? ''
+            'id'=> @$this->id ?? 0,
+            'name'=>app()->getLocale() == "ar" ? @$this->name_ar ?? '' : @$this->name_en ?? '',
+            'img'=>@$this?->img ?? '',
+            'description'=>app()->getLocale() == "ar" ? @$this->description_ar ?? '' : @$this->description_en ?? ''
         ];
     }
 }
