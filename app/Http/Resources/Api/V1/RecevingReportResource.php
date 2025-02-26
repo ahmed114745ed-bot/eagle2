2eb60e9ab18a5a238f68f7bc1be3b6cc874da764
Erinyelkess <ariny.backend@utdsoftware.com>
2025-02-26 13:43:12 +0200

    edit

--- app/Http/Resources/Api/V1/RecevingReportResource.php
+++ app/Http/Resources/Api/V1/RecevingReportResource.php
@@ -8,26 +8,17 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class RecevingReportResource extends JsonResource
 {
-   
+
     public function toArray($request)
     {
-        $name = '';
-        $image = '';
-        if($this->charger_type == 'dash'){
-            // admin
-            $name = $this->admin?->name;
-            $image = $this->admin?->avatar;
-        } else if($this->charger_type == 'agency'){
-            $name = $this->senderAgency?->name;
-            $image = $this->senderAgency?->img;
-        }
+
         return [
             'id'          => $this->user_id,
             'diamonds'    => numToStringNew($this->amount),
-            'operation_no'=> (int)$this->id,
-            'created_at'  => Carbon::parse( $this->created_at)->format('Y-m-d h:i:s A'),
-            'name' => $name,
-            'image' => $image,
+            'operation_no' => (int)$this->id,
+            'created_at'  => Carbon::parse($this->created_at)->format('Y-m-d h:i:s A'),
+            'name' => $this->sender->name ?? '',
+            'image' => $this->sender->profile->avatar ?? '',
         ];
     }
 }
