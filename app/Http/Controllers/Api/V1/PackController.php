ea2a83af7cf1a6db31b055c7494187cd87c4d8a6
Erinyelkess <ariny.backend@utdsoftware.com>
2025-02-27 18:27:15 +0200

    validation

--- app/Http/Controllers/Api/V1/PackController.php
+++ app/Http/Controllers/Api/V1/PackController.php
@@ -118,7 +118,7 @@ class PackController extends Controller
         $user = $request->user();
         $type = $request->type;
         $item = $request->item_id;
-        if (!$type) return Common::apiResponse(0, 'missing params', null, 422);
+        if (!$type && !$item) return Common::apiResponse(0, 'missing params', null, 422);
         if (!in_array($type, [1, 2, 3, 4])) return Common::apiResponse(0, 'type invalid', null, 403);
 
         $this->packService->updateDress($user, $type,$item);
