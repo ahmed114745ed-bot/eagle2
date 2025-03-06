1ba984d3d81b966042adf21fb3077a4322b809db
Erinyelkess <ariny.backend@utdsoftware.com>
2025-03-06 15:22:46 +0200

    edit

--- app/Http/Resources/HomeCarouselResource.php
+++ app/Http/Resources/HomeCarouselResource.php
@@ -42,7 +42,7 @@ class HomeCarouselResource extends JsonResource
 
         if($this->type == 'room'){
             $data += ['room' => [
-                "id" => @$ownerRoom->id ?? 0,
+                "id" => @$this->user->id ?? 0,
                 "owner_uuid" => @$this->user->uuid ?? 0,
                 "room_name" => @$ownerRoom->room_name ?? '',
                 "room_cover" => @$ownerRoom->room_cover ?? '',
