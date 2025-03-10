fdfd7ca56447ccecef3e4eb540077ff13796c9e7
Erinyelkess <ariny.backend@utdsoftware.com>
2025-03-10 11:06:09 +0200

    edit

--- app/Http/Resources/Api/V1/RoomSearchResource.php
+++ app/Http/Resources/Api/V1/RoomSearchResource.php
@@ -22,27 +22,28 @@ class RoomSearchResource extends JsonResource
         $room = Room::find(@$this->id);
         return [
             'id' => $this->id ?? 0,
+            'room_id' => $this->id ?? 0,
             "room_name" => $this->room_name ?? '',
-
-
             "numid" => $this->numid ?? 0,
             "hot" => $this->hot ?? '',
             "room_cover" => $this->room_cover ?? '',
             "room_intro" => $this->room_intro ?? '',
-            "room_background" => @ $room->final_room_image ?? '',
+            "room_background" => @$room->final_room_image ?? '',
             "room_welcome" => $this->room_welcome ?? '',
             "mode" => @$this->mode ?? 0,
             'giftPrice' => @$this->session_string ?? "0",
             "show_pk"             => @$this->is_show_pk ?? 0,
             'password_status'     => !(@$this->room_pass == ""),
             'type-number'                => @$this->room_type ?? 0,
-            'type' => @ $room->myType ?: new \stdClass(),
+            'type' => @$room->myType ?: new \stdClass(),
             "is_pk"               => (@$pks[0]) && @$pks[0]->end_at >= now() ? @$pks[0]->status : 0,
 
             "room_pass" => $this->room_pass ?? '',
             "uid" => $this->uid ?? 0,
+            'owner_id' =>  $this->uid ?? 0,
+            'owner_uuid' => $this->uuid ?? '',
             "name" => $this->name ?? '',
-            'uuid' => $this->uuid ?? '',
+
             "nickname" => $this->nickname ?? '',
 
 
