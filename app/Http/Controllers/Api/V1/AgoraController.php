cd3448b795451be217ac7fd60e0ecd3ec0b48b79
makled98 <mkled.backend@utdsoftware.com>
2025-03-10 10:51:05 +0200

    Update AgoraController.php

--- app/Http/Controllers/Api/V1/AgoraController.php
+++ app/Http/Controllers/Api/V1/AgoraController.php
@@ -37,9 +37,8 @@ class AgoraController extends Controller
     }
 
     public function webhook(Request $request){
-        Log::info('agora webhook triggered', [
-            $request->all()
-        ]);
+        Log::info(['agora webhook triggered' =>  $request->all() ]);
+        
         return ;
         $agoraSignature = $request->header('Agora-Signature');
         Log::info("Agora-Signature: " . $agoraSignature);
