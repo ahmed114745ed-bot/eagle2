89a3e040e461da32230043d4087e675deecc9fc7
shami2024 <>
2025-03-13 13:45:51 +0200

    test

--- app/Http/Controllers/Api/V1/AgoraController.php
+++ app/Http/Controllers/Api/V1/AgoraController.php
@@ -43,9 +43,9 @@ class AgoraController extends Controller
 
     public function webhook(Request $request){
 
-        // Log::info('agora webhook triggered', [
-        //     $request->all()
-        // ]);
+        Log::info('agora webhook triggered enter room ', [
+            $request->all()
+        ]);
         // return ;
         // $agoraSignature = $request->header('Agora-Signature');
         // Log::info("Agora-Signature: " . $agoraSignature);
--- app/Tik/Services/EnteranceRoomServices.php
+++ app/Tik/Services/EnteranceRoomServices.php
@@ -192,7 +192,7 @@ class EnteranceRoomServices
     {
     
         $data = $request->all(); 
-      
+        Log::info('Agora data ',[$data ]);
         if (!isset($data[0]['eventType'], $data[0]['payload']['channelName'], $data[0]['payload']['lastUid'])) {
             return response()->json(['status' => 'Invalid Webhook Data'], 400);
         }
@@ -201,11 +201,11 @@ class EnteranceRoomServices
         $roomId = $data[0]['payload']['channelName'];
         $userId = $data[0]['payload']['lastUid'];
     
-        // Log::info('Agora data received', [
-        //     'event_type' => $eventType,
-        //     'user_id' => $userId,
-        //     'room_id' => $roomId,
-        // ]);
+        Log::info('Agora data received', [
+            'event_type' => $eventType,
+            'user_id' => $userId,
+            'room_id' => $roomId,
+        ]);
 
         $room = Room::select(['id', 'uid', 'count_room_socket', 'room_visitor', 'charizma_status', 'microphone'])
                     ->find($roomId);
@@ -219,6 +219,11 @@ class EnteranceRoomServices
         $this->updateRoomVisitorsBasedOnEvent($eventType, $room, $user->id);
     
         if (in_array($eventType, [101, 103])) {
+            Log::info('enter rooom 101,102', [
+                'event_type' => $eventType,
+          
+            ]);
+    
             $this->addUserToVisitors($room->id, $user->id);
             $user->now_room_uid = $room->uid;
         
