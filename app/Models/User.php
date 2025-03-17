2a61da23a1b6c1dabab08c203358fa523e97ae8a
makled98 <mkled.backend@utdsoftware.com>
2025-03-17 13:52:02 +0200

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -665,7 +665,7 @@ class User extends Authenticatable
         })
             ->orderByDesc('id')
             ->sum(DB::raw('salary - cut_amount'));
-        \Log::info([$userSallary,$roomSalary]);
+        // \Log::info([$userSallary,$roomSalary]);
         return (floor($userSallary + (int)$roomSalary));
         //        } else {
         //            return 0;
