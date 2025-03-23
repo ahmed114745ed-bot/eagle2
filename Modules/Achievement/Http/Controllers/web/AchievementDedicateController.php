860455460f4071bc27da9ccd92d37c998813c2ac
Erinyelkess <ariny.backend@utdsoftware.com>
2025-03-23 15:57:11 +0200

    achievement

--- Modules/Achievement/Http/Controllers/web/AchievementDedicateController.php
+++ Modules/Achievement/Http/Controllers/web/AchievementDedicateController.php
@@ -52,15 +52,45 @@ class AchievementDedicateController extends MainController
      */
     protected function grid()
     {
-        $grid = new Grid(new AchievementValidImage());
+        $grid = new Grid(new UserAchievementLevel());
 
         $grid->filter(function (Grid\Filter $filter) {
             $filter->expand();
+           
+            $filter->column(1 / 2, function ($filter) {
+                $filter->equal('user.uuid', __('uuid'));
+            });
         });
+        $grid->model()->whereNotNull('custom_image')->orWhereNotNull('file');
         $grid->column('id', __('Id'));
-        $grid->column( 'file',__('image'))->display(function ($img) {
+        $grid->column('user.name', __('user'))
+        ->display (function ($recever){
+            $name =  $this->user?->name ?? '';
+             $uid = @$this->user?->uuid ?? 0;
+             $path = @$this->user?->profile?->avatar;
+             $defaultImage = asset("images/businessman-icon.jpg");
+             $url = getImagePath($path) ?? $defaultImage;
+
+             // Check if the image exists
+             if (!isImageExists($url)) {
+                 $url = $defaultImage;
+             }
+             $image = handleShowImageWithTypes($this->id, $url, 40, 40);
+
+             return "
+             <div style='display: flex; align-items: center; gap: 10px;'>
+                 $image
+                 <div>
+                     <strong>$name</strong><br>
+                     <span style='color: #aaa; font-size: smaller;'>UID: $uid</span>
+                 </div>
+             </div>
+         ";
+
+         });
+        $grid->column('file',__('image'))->display(function ($img) {
             $defaultImage = asset("images/background_room.jpg");
-            $path = getImagePath($img);
+            $path = getImagePath($img ?? $this->custom_image);
             if (!isImageExists($path)) {
                 $path = $defaultImage;
             }
@@ -97,13 +127,6 @@ class AchievementDedicateController extends MainController
                 ";
         });
 
-
-
-
-        $grid->column ('return',__ ('dedicate'))->display (function (){
-           
-            return (new AchievementDedicate($this->id))->render ();
-          });
           $grid->disableActions();
         $grid->actions(function (Grid\Displayers\Actions $actions) {
             $actions->disableView();
