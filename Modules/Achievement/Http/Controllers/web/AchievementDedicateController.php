9afa63d51a83f39c052a21f7bb6f81aacd6b1895
Erinyelkess <ariny.backend@utdsoftware.com>
2025-03-23 14:49:14 +0200

    image achievement

--- Modules/Achievement/Http/Controllers/web/AchievementDedicateController.php
+++ Modules/Achievement/Http/Controllers/web/AchievementDedicateController.php
@@ -2,19 +2,20 @@
 
 namespace Modules\Achievement\Http\Controllers\web;
 
-use App\Admin\Actions\AchievementDedicateAction;
-use App\Models\AchievementValidImage;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
 use App\Helpers\Common;
-
-
 use Encore\Admin\Layout\Content;
+use Illuminate\Support\Facades\Auth;
+
+
+use App\Models\AchievementValidImage;
 
 use App\Admin\Controllers\MainController;
-use Illuminate\Support\Facades\Auth;
+use App\Admin\Actions\AchievementDedicate;
 use Modules\Achievement\Entities\Achievement;
+use App\Admin\Actions\AchievementDedicateAction;
 use Modules\Achievement\Entities\AchievementLevel;
 use Modules\Achievement\Entities\UserAchievementLevel;
 
@@ -51,32 +52,59 @@ class AchievementDedicateController extends MainController
      */
     protected function grid()
     {
-        $grid = new Grid(new AchievementLevel());
+        $grid = new Grid(new AchievementValidImage());
 
         $grid->filter(function (Grid\Filter $filter) {
             $filter->expand();
         });
         $grid->column('id', __('Id'));
-        $grid->column('achievement_id', __('Achievement Id'));
-        $grid->column('target', __('Target'));
-        $grid->column('valid_image', __('Valid image'))->display(function($value){
-            $value = getDriverUrl() .'/'. $value;
-            return "<img src='$value' width='80' height='80'>";
+        $grid->column( 'file',__('image'))->display(function ($img) {
+            $defaultImage = asset("images/background_room.jpg");
+            $path = getImagePath($img);
+            if (!isImageExists($path)) {
+                $path = $defaultImage;
+            }
+            $parsedUrl = parse_url($path);
+            $correctUrl = isset($parsedUrl['host']) ? $path : url("/$path");
+
+            return "
+                    <img src='$correctUrl' style='width: 50px; height: 50px; border-radius: 5px; cursor: pointer;' onclick='openModal(\"$correctUrl\")' />
+
+                    <div id='imageModal' class='modal' style='display:none; position:fixed; z-index:1000; left:0; top:0; width:100%; height:100%; background:rgba(0,0,0,0.7); text-align:center;'>
+                        <span onclick='closeModal()' style='position:absolute; top:10px; right:20px; font-size:30px; color:white; cursor:pointer;'>&times;</span>
+                        <img id='modalImage' style='display:block; margin:auto; max-width:90%; max-height:90%; margin-top:50px; border-radius:5px;' />
+                    </div>
+
+                    <script>
+                        function openModal(src) {
+                            let modal = document.getElementById('imageModal');
+                            let modalImage = document.getElementById('modalImage');
+                            modal.style.display = 'block';
+                            modalImage.src = src;
+                        }
+
+                        function closeModal() {
+                            document.getElementById('imageModal').style.display = 'none';
+                        }
+
+                        // Close modal when clicking outside the image
+                        document.getElementById('imageModal').addEventListener('click', function(event) {
+                            if (event.target === this) {
+                                closeModal();
+                            }
+                        });
+                    </script>
+                ";
         });
 
-        $grid->column('invalid_image', __('Invalid image'))->display(function($value){
-            $value = getDriverUrl() .'/'. $value;
-            return "<img src='$value' width='80' height='80'>";
-        });
-        $grid->column('ar_description', __('Description Ar'));
-        $grid->column('en_description', __('Description En'));
 
 
 
         $grid->column ('return',__ ('dedicate'))->display (function (){
            
-            return (new AchievementDedicateAction($this->id))->render ();
+            return (new AchievementDedicate($this->id))->render ();
           });
+          $grid->disableActions();
         $grid->actions(function (Grid\Displayers\Actions $actions) {
             $actions->disableView();
             $actions->disableEdit();
