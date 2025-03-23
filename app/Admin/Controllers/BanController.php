52d4c11899d32447c42aabd1c79e354da300ea6f
shami2024 <>
2025-03-23 14:36:07 +0200

    BanRoomsController

--- app/Admin/Controllers/BanController.php
+++ app/Admin/Controllers/BanController.php
@@ -91,8 +91,8 @@ class BanController extends MainController
         $grid = new Grid(new Ban);
         $grid->model()->whereHas('user')
             ->whereRaw("DATE_ADD(created_at, INTERVAL duration HOUR) > '$now'")
-            ->select('uid', 'duration', 'type', 'device_number', 'staff_id',  'description_ar', 'img', DB::raw('(SELECT created_at FROM bans AS b WHERE b.uid = bans.uid AND b.type = bans.type ORDER BY b.id DESC LIMIT 1) AS created_at'), 'ban_type_id')
-            ->groupBy(['uid', 'type', 'duration', 'device_number', 'description_ar', 'img', 'staff_id',  'ban_type_id'])->orderByDesc('created_at');
+            ->select('uid', 'duration', 'type', 'device_number', 'staff_id',   DB::raw('(SELECT created_at FROM bans AS b WHERE b.uid = bans.uid AND b.type = bans.type ORDER BY b.id DESC LIMIT 1) AS created_at'), 'ban_type_id')
+            ->groupBy(['uid', 'type', 'duration', 'device_number',  'staff_id',  'ban_type_id'])->orderByDesc('created_at');
         //    $grid->id(__ ('ID'));
         // $grid->uid(__('uuid'));
         //        $grid->user_type(__('user_type'));
---
+++ app/Models/BanRoom.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+
+class BanRoom extends Model
+{
+    protected $table='bans_rooms';
+    protected $guarded = ['id'];
+
+
+    public function getCreatedAtAttribute($value)
+    {
+        $timeZone = request()->header('tz') ?? 'UTC';
+        //$timeZone = 'Asia/Dhaka'; // Get the user's time zone from the session
+        return Carbon::parse($value)->setTimezone($timeZone)->format('Y-m-d H:i:s');
+    }
+
+    // Convert updated_at to the user's local time zone
+    public function getUpdatedAtAttribute($value)
+    {
+        $timeZone = request()->header('tz') ?? 'UTC';
+        //$timeZone = 'Asia/Dhaka'; // Get the user's time zone from the session
+        return Carbon::parse($value)->setTimezone($timeZone)->format('Y-m-d H:i:s');
+    }
+    public function room()
+    {
+        return $this->hasOne(Room::class, 'id', 'room_id');
+    }
+
+    // public function banType()
+    // {
+    //     return $this->belongsTo(BanType::class,);
+    // }
+
+        public function staff()
+    {
+        return $this->belongsTo(Admin::class, 'staff_id');
+    }
+
+
+}
