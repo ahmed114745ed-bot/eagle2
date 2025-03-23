d79dee53b943932f9bebd84109792de01a3339a0
Erinyelkess <ariny.backend@utdsoftware.com>
2025-03-23 11:53:15 +0200

    fix enter room error

--- app/Http/Resources/Api/V1/EnterRoomCollection.php
+++ app/Http/Resources/Api/V1/EnterRoomCollection.php
@@ -75,7 +75,7 @@ class EnterRoomCollection extends JsonResource
             'owner_name'          => @$owner->name ?? '',
             'owner_avatar'        => @$owner->profile->avatar ?? '',
             'owner_vip_level'     => (int) ($owner->UserVip->level ?? 0),
-            'owner_vip_img'     => $vip_level_img == 0 ? "" : $vip_level_img,
+            'owner_vip_img'     => $vip_level_img  ,
             'vip' => Common::ovip_center(@$owner->id),
             'owner_country'        =>        $owner && $owner->country
                 ? [
