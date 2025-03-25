90a8321772d22e3b1da87fd0c070d7985996befc
makled98 <mkled.backend@utdsoftware.com>
2025-03-25 12:08:37 +0200

    Update RecevingReportResource.php

--- app/Http/Resources/Api/V1/RecevingReportResource.php
+++ app/Http/Resources/Api/V1/RecevingReportResource.php
@@ -25,8 +25,7 @@ class RecevingReportResource extends JsonResource
         return [
             'id'            => $this->user_id,
             'uuid'          => $uuid,
-            'diamonds'      => 0,
-            'coins'         => numToStringNew($this->amount),
+            'diamonds'      => numToStringNew($this->amount),
             'operation_no'  => (int)$this->id,
             'created_at'    => Carbon::parse($this->created_at)->format('Y-m-d h:i:s A'),
             'name'          => $name ?? '',
