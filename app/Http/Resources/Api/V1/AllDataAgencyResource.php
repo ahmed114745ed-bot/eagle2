4b0817608e5f43df4bc13e6b48a4f021950f7ff9
Erinyelkess <ariny.backend@utdsoftware.com>
2025-03-26 11:11:32 +0200

    user_agency_status show agency

--- app/Http/Resources/Api/V1/AllDataAgencyResource.php
+++ app/Http/Resources/Api/V1/AllDataAgencyResource.php
@@ -6,6 +6,7 @@ use App\Models\Agency;
 use App\Models\Target;
 use App\Models\UserSallary;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
 
 class AllDataAgencyResource extends JsonResource
 {
@@ -16,6 +17,7 @@ class AllDataAgencyResource extends JsonResource
      * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
      */
 
+   
 
     public function toArray($request)
     {
@@ -28,6 +30,11 @@ class AllDataAgencyResource extends JsonResource
             ->first();
 
         $result = (@$minValue->agency_share / 100) * @$target;
+       
+        
+         $authUser = Auth::user();
+        $owner = @$authUser->ownAgency;
+        $admin = @$authUser->agencyUserJob;
 
         // $target =Target::where('level',$this->id)->first();
         // // $target_usd =Agency::where('id',$this->id)->sum('target_usd');
@@ -55,7 +62,10 @@ class AllDataAgencyResource extends JsonResource
                 "image" => ''
                 ]
             ],
-            // 'members'=>MyDataForAgancyResource::collection($members),
+            'mempers_count'=>$this->mempers_count,
+            // 'mempers'=>$this->mempers ?? (object)[], 
+            'members'=>MyDataForAgancyNewResource::collection($this->mempers),
+            'user_agency_status' => $owner ? 2 : ($admin ? 1 : 3),
         ];
     }
 }
