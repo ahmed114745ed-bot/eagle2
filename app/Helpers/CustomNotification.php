38ff6daae607ba2fbe353ccb8811d74dfa70c9c1
shami2024 <>
2025-03-27 14:20:04 +0200

    Update CustomNotification.php

--- app/Helpers/CustomNotification.php
+++ app/Helpers/CustomNotification.php
@@ -172,8 +172,7 @@ class CustomNotification
         $firebaseBody = ($receiver->lan === 'ar') ? $body_ar : $body_en;
         $data['image'] = getImagePath($user->profile->avatar);
         $icon = $data['image'];
-        $response = Common::send_firebase_notification($tokens_notfacion, $this->appName($user->lan), $firebaseBody, $icon, $data, 'follow');
-        dd($response);
+   
         Common::send_firebase_notification($tokens_notfacion, $this->appName($user->lan), $firebaseBody, icon: $icon, data: $data, messageType: 'follow');
         Common::sendOfficialMessage($receiver->id, image: $user->profile->avatar, title: $body_en, content: $user->name, titleAr: $body_ar, fromUserId: $user->id);
         (new UserCounterServices)->eventUser($receiver, 'official-messages');
