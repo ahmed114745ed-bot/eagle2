5174ceca4aaf0241d8ae794d5da09185b200f1cb
Erinyelkess <ariny.backend@utdsoftware.com>
2025-03-27 14:45:47 +0200

    type room

--- app/Http/Resources/Api/V1/RoomResource.php
+++ app/Http/Resources/Api/V1/RoomResource.php
@@ -50,7 +50,7 @@ class RoomResource extends JsonResource
             'is_pk' => (bool) $pk,
             'is_party' => $isParty,
             'room_background' => $this->final_room_image,
-            'stream_type' => $this->type,
+            'stream_type' => $this->type ?? 'audio',
             'is_live' => (bool)$this->is_live,
             'country' => $this->country
                 ? new CountryResource($this->country)
