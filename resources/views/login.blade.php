649ee6b1576ad492123e3962eadb6de7afd9cde5
Erinyelkess <ariny.backend@utdsoftware.com>
2025-04-03 16:41:50 +0200

    edit

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -31,7 +31,7 @@
     <div class="login-logo">
         @php
         $logo = App\Models\Setting::where('key', 'app_logo')->first();
-        $logo_url = asset('uploads/settings/'. $logo?->value);
+        $logo_url = $logo?->value;
     @endphp
         <div><img src="{{ empty($logo)? asset('images/app-logo.png') : getImagePath($logo_url)}}" style="width: 150px;"></div>
         <div class="box-title">
