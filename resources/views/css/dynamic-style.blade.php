f4183a08934bc49d03e7574fd8836675bd7a932b
shami2024 <>
2025-04-06 13:18:46 +0200

    wallet_posation

--- resources/views/css/dynamic-style.blade.php
+++ resources/views/css/dynamic-style.blade.php
@@ -651,8 +651,12 @@
                 left: 107px;
 
             }
+            .rtl .wallet_posation{
+                position: absolute;
 
+            }
 
+            
 
     @media (max-width: 768px) {
         .rtl .main-sidebar {
