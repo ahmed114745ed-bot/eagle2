5bcb3590657c99d6e851305aa4e3e67208c656e9
Erinyelkess <ariny.backend@utdsoftware.com>
2025-04-06 16:56:52 +0200

    edit achievement name

--- Modules/Achievement/Transformers/AchievementOneLevelsResource.php
+++ Modules/Achievement/Transformers/AchievementOneLevelsResource.php
@@ -20,6 +20,12 @@ class AchievementOneLevelsResource extends JsonResource
     public function toArray($request)
     {
 
+        $targets = [
+            'recharge_target' => 'recharge-target',
+            'room_target' => 'room-target',
+        ];
+        
+        $target = $targets[$this->type->value] ?? 'gift-target';
         return [
             'id' => $this->id,
             'type' => $this->type,
@@ -27,8 +33,8 @@ class AchievementOneLevelsResource extends JsonResource
             'invalid_image' => $this->invalid_image,
             'target' => $this->target,
             'target_type' => $this->target_type,
-            'levels' => $this->whenLoaded('levels', function () {
-                return $this->levels->map(function ($level) {
+            'levels' => $this->whenLoaded('levels', function ()use($target) {
+                return $this->levels->map(function ($level) use($target) {
                     $img = $level->invalid_image;
                    
                         $valid = $level->valid_image;
@@ -38,7 +44,7 @@ class AchievementOneLevelsResource extends JsonResource
                         'id' => $level->id,
                         'achievement_id' => $level->achievement_id,
                         'gift_id' => $level->gift_id,
-                        'name' => $this->type->value . '_'. $level->target,
+                        'name' => $target . '-'. $level->target,
                         'target' => $level->target,
                         'target_type' => $level->target_type,
                         'image' => $img,
