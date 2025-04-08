7f3d73ade8289b66945372b10d8493b44cf860fd
Erinyelkess <ariny.backend@utdsoftware.com>
2025-04-08 13:19:37 +0200

    data

--- Modules/SpecialId/Http/Controllers/web/SpecialHistoryController.php
+++ Modules/SpecialId/Http/Controllers/web/SpecialHistoryController.php
@@ -73,7 +73,7 @@ class SpecialHistoryController extends AdminController
         $grid->column('user.name', __('User'))->display(function () {
             $defaultImage = asset("images/businessman-icon.jpg");
             $url = getImagePath($this->user?->profile?->avatar) ?? $defaultImage;
-
+            $name =$this->user?->name ?? '';
             if (!isImageExists($url)) {
                 $url = $defaultImage;
             }
@@ -82,7 +82,7 @@ class SpecialHistoryController extends AdminController
                 <div style="display: flex; align-items: center; gap: 10px;">
                     <img src="'.$url.'" alt="User Image" style="width: 40px; height: 40px;">
                     <div>
-                        <a href="/admin/users/'.$this->user_id.'" style="text-decoration: none; color:rgb(253, 253, 253); font-weight: bold;">'.$this->user?->name.'</a>
+                        <a href="/admin/users/'.$this->user_id.'" style="text-decoration: none; color:rgb(253, 253, 253); font-weight: bold;">'.$name.'</a>
                         <div style="font-size: 12px; color: #fff;">' .'Uuid: '.$this->user?->uuid.'</div>
                     </div>
                 </div>
