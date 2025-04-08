b52212303356001b535d7c665fb29a0035eddddc
shami2024 <>
2025-04-08 12:22:42 +0200

    Update UserController.php

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -129,7 +129,7 @@ class UserController extends MainController
         return (new Box(
             title: __('admin.Actions'),
             content: view('admin.grid.users.userChargeViewNew', compact(['stop_charge', 'make_rooms_top', 'stop_invite_code', 'transfer_salary',])),
-        ))->collapsable()->class('box collapsed-box');
+        ))->collapsable()->class('box ');
         
     }
     protected function grid()
