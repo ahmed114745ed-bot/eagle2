6e549d8d13cfc7684e21931ba57e7c4f933d68ff
shami2024 <>
2025-04-08 12:15:43 +0200

    Update dynamic-style.blade.php

--- resources/views/css/dynamic-style.blade.php
+++ resources/views/css/dynamic-style.blade.php
@@ -625,7 +625,7 @@
                 text-align: start;
             }
             .rtl .form-horizontal .row {
-                /* display: grid; */
+                display: ruby;
                 direction: rtl !important;
                 flex-direction: row-reverse !important;
 
