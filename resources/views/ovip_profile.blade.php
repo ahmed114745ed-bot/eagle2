14b0ad1da289f452333c3087b9cdb9dee33b3646
shami2024 <>
2025-04-09 12:32:33 +0200

    Update ovip_profile.blade.php

--- resources/views/ovip_profile.blade.php
+++ resources/views/ovip_profile.blade.php
@@ -37,7 +37,7 @@
             box-sizing: border-box;
         }
         .container {
-            background: #222;
+            /* background: #222; */
             padding: 20px;
             border-radius: 5px;
             width: 80%;
@@ -55,6 +55,12 @@
             margin: 0 auto 20px;
             text-align: center;
         }
+        .avatar  {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+        }
         .avatar img {
             width: 200px;
             height: 200px;
@@ -63,7 +69,7 @@
           
             margin-bottom: 15px;
         }
-   
+
         .details {
             text-align: left;
             margin-top: 10px;
@@ -105,6 +111,11 @@
         th {
             background-color: #333;
         }
+        .imageContainer{
+            border: 2px solid;
+            border-radius: 50%;
+            width: 50%;
+        }
         
         /* Responsive adjustments */
         @media (max-width: 768px) {
@@ -128,6 +139,11 @@
             th, td {
                 padding: 6px 4px;
             }
+            .imageContainer{
+            border: 2px solid;
+            border-radius: 50%;
+            width: 100%;
+             }
         }
         
         @media (max-width: 480px) {
@@ -140,6 +156,12 @@
             th, td {
                 padding: 4px 2px;
             }
+
+            .imageContainer{
+            border: 2px solid;
+            border-radius: 50%;
+            width: 100%;
+             }
         }
 
         /* Main Container */
@@ -259,7 +281,7 @@
     <div class="main-content">
         <div class="container">
             <div class="avatar text-center">
-                <div id="imageContainer{{ $oVip->id }}"></div>
+                <div class="imageContainer" id="imageContainer{{ $oVip->id }}"></div>
             </div>
 
             {{-- <button onclick="window.history.back()">{{ __("Go Back") }}</button> --}}
