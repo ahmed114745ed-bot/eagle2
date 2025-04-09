70d470929593d99a379d5ca67543ec1381b2b98a
shami2024 <>
2025-04-09 11:25:27 +0200

    Update js.blade.php

--- resources/views/admin/views/partials/js.blade.php
+++ resources/views/admin/views/partials/js.blade.php
@@ -4,11 +4,16 @@
 
 </script>
 @endforeach
+<script>
+
+</script>
 
 <script src="https://cdn.jsdelivr.net/npm/svgaplayerweb@2.3.1/build/svga.min.js"
         onerror="this.onerror=null; this.src='';"></script>
  <script src="https://cdnjs.cloudflare.com/ajax/libs/viewerjs/1.10.5/viewer.min.js"></script>
- 
+ <script src="https://cdn..net/npm/svgaplayerweb@2.3.1/build/svga.min.js"></script>
+ <script src="https://cdn.jsdelivr.net/npm/svgaplayerweb@2.3.1/build/svga.min.js"></script>
+
 <script>
     $(document).ready(function () {
         console.log('done');
