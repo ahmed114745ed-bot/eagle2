36f9f862719cd8e906e46ad7683b1374306f9b61
Erinyelkess <ariny.backend@utdsoftware.com>
2025-04-09 10:11:59 +0200

    test

--- resources/views/ovip_profile.blade.php
+++ resources/views/ovip_profile.blade.php
@@ -345,7 +345,7 @@
                     if (isSvga) {
                         // Render SVGA animation
                         showImageContent = `
-                <canvas id="${canvasId}"  width="2000" height="150"></canvas>
+                <canvas id="${canvasId}"  width="350" height="200"></canvas>
                 `;
                     } else if (show_img.endsWith('.mp4')) {
                         // Render MP4 video
