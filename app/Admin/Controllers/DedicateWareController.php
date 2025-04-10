c00506a4e639e70e6c336ec9302e7d69c392db48
Erinyelkess <ariny.backend@utdsoftware.com>
2025-04-10 16:30:53 +0200

    lang

--- app/Admin/Controllers/DedicateWareController.php
+++ app/Admin/Controllers/DedicateWareController.php
@@ -25,7 +25,7 @@ class DedicateWareController extends MainController
     public function index(Content $content)
     {
         return $content
-            ->title(trans('Gift Products'))
+            ->title(trans('Gift from the store'))
             ->body($this->grid());
     }
 
