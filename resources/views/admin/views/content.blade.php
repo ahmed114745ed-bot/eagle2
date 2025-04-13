2e2091103cc2616e0b3c72937bf758e596578f42
makled98 <mkled.backend@utdsoftware.com>
2025-04-13 19:34:53 +0200

    Update content.blade.php

--- resources/views/admin/views/content.blade.php
+++ resources/views/admin/views/content.blade.php
@@ -1,6 +1,8 @@
 @extends('admin::index', ['header' => strip_tags($header)])
 
 @section('content')
+@dd("dsdsds");
+
     <section class="content-header">
         <h1>
             {!! $header ?: trans('admin.title') !!}
@@ -51,7 +53,6 @@
             @endphp
             @for($i = 1; $i < count($segments); $i++)
                 @php
-                dd("dsdsds");
                     $segment = is_array($segments[$i]) ? implode('/', $segments[$i]) : $segments[$i];
                     $formatted = Str::of($segment)->replace('-', ' ')->ucfirst();
                 @endphp
