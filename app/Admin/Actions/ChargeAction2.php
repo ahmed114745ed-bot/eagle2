167c9eced45ad8d0db57958efe6dd4a36c36ceaa
makled98 <mkled.backend@utdsoftware.com>
2025-04-13 20:12:25 +0200

    update charge

---
+++ app/Admin/Actions/ChargeAction2.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace App\Admin\Actions;
+
+use App\Models\User;
+use App\Models\Agency;
+use App\Models\Charge;
+use Encore\Admin\Form;
+use App\Helpers\Common;
+use App\Helpers\UserCommon;
+use Illuminate\Http\Request;
+use Encore\Admin\Actions\Action;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Modules\Achievement\Http\Services\UserAchievementService;
+use App\Facades\CustomNotification;
+
+class ChargeAction2 extends Action
+{
+    public $name;
+    protected $selector = '.charge_action';
+
+    public function handle(Request $request)
+    {
+        if ($request->user_type != 'dash') {
+            $user = $this->getUser($request);
+            if (!$user) {
+                return $this->response()->error(__('user not found'))->refresh();
+            }
+
+            if ($this->isInvalidAmount($request->amount)) {
+                return $this->response()->error(__('amount must be more than 10'))->refresh();
+            }
+        }
+
+        if ($request->user_type == 'dash') {
+            $agency = $this->getAgency($request->user_id);
+            if (!$agency) {
+                return $this->response()->error(__('api_responses.agency'))->refresh();
+            }
+            if ($agency->is_frozen == 1) {
+                return $this->response()->error(__('api_responses.frozen'))->refresh();
+            }
+            $user = $agency->owner;
+            return $this->handleAgencyCharge($request, $agency, $user);
+        }
+
+        return $this->handleUserCharge($request, $user);
+    }
+
+    private function getUser(Request $request)
+    {
+        if ($request->user_type == 'dashdash') {
+            return $request->id_type == '1'
+                ? User::query()->searchByUuid($request->user_id)->first()
+                : User::query()->find($request->user_id);
+        }
+
+        return $request->id_type == '1'
+            ? User::query()->where('uuid', $request->user_id)->first()
+            : User::query()->find($request->user_id);
+    }
+
+    private function getAgency($agencyId)
+    {
+        return Agency::where("id", $agencyId)->first();
+    }
+
+    private function isInvalidAmount($amount)
+    {
+        return $amount < 10;
+    }
+
+    private function handleAgencyCharge(Request $request, Agency $agency, User $user)
+    {
+        $amount = $request->charge_type == 'increment' ? $request->amount : -$request->amount;
+        if ($amount < 0 && $agency->coins < abs($amount)) {
+            return $this->response()->error(__('Insufficient agency balance'))->refresh();
+        }
+
+        DB::transaction(function () use ($request, $agency, $user, $amount) {
+            $agency->coins += $amount;
+            $agency->save();
+
+            $this->createChargeRecord($request, $user, $agency, $amount);
+
+            if ($request->charge_type == "increment") {
+                CustomNotification::chargeAction($user, $request);
+            }
+        });
+
+        return $this->response()->success('Success')->refresh();
+    }
+
+    private function handleUserCharge(Request $request, User $user)
+    {
+        $percentage = Common::getConf("special_transfer_to_usd") ?? 1;
+        $usdAmount = $request->amount / $percentage;
+
+        DB::transaction(function () use ($request, $user, $usdAmount) {
+            $amount = $request->charge_type == 'increment' ? $request->amount : -$request->amount;
+            if ($amount < 0 && $user->di < abs($amount)) {
+                return $this->response()->error(__('Insufficient user balance'))->refresh();
+            }
+
+            $user->di += $amount;
+            $user->save();
+            if ($request->charge_type == "increment") {
+                CustomNotification::chargeAction($user, $request);
+            }
+            $this->createChargeRecord($request, $user, null, $amount, $usdAmount);
+            (new UserAchievementService())->insertCharging($user, $request->amount);
+        });
+
+        return $this->response()->success('Success')->refresh();
+    }
+
+    private function createChargeRecord(Request $request, User $user, ?Agency $agency, $amount, $usdAmount = 0)
+    {
+        $charge = new Charge();
+        $charge->charger_id = Auth::id();
+        $charge->charger_type = $request->user_type == 'dash' ? 'dash' : 'dash';
+        $charge->user_id = $user->id;
+        $charge->agency_id = $agency->id ?? null;
+        $charge->user_type = $request->user_type ?? 'app';
+        $charge->amount = $amount;
+        $charge->usd = $usdAmount;
+        $charge->balance_before = ($agency ? $agency->coins : $user->di) - $amount;
+        //dd($charge);
+        $charge->save();
+
+        UserCommon::UserEarnedInvitation($user->id, $amount);
+    }
+
+    public function form()
+    {
+        $this->name = __('Charge');
+        $this->hidden('charger_id')->value(Auth::id());
+        // $this->hidden('charger_type')->value('dash');
+        $this->text('user_id', __('User ID / Agency ID'));
+        $this->select('id_type', __('ID Type'))->options([0 => __('Normal'), 1 => __('Uuid')]);
+        $this->select('charge_type', __('Charge Type'))->options(['increment' => __('increment'), 'decrement' => __('decrement')])->default('increment');
+        $this->select('user_type', __('User Type'))->options(['dashdash' => __('App'), 'dash' => __('Agencies')])->default('dashdash'); //dashdash
+        $this->text('amount', __('Amount'));
+        $this->hidden('amount_type')->value(1);
+    }
+
+    public function html()
+    {
+        $title = __('dashboard.add_coins');
+        return <<<HTML
+            <a href="javascript:void(0);" class="charge_action btn btn-sm  text-white" 
+       style="background-color: #28a745; border-color: #28a745; color: white;">
+        {$title}
+    </a>
+HTML;
+    }
+}
--- app/Admin/Controllers/ChargeController.php
+++ app/Admin/Controllers/ChargeController.php
@@ -13,6 +13,7 @@ use Encore\Admin\Layout\Content;
 use Encore\Admin\Auth\Permission;
 use App\Admin\Actions\ChargeAction;
 use App\Http\Controllers\Controller;
+use App\Models\Agency;
 use Illuminate\Support\Facades\Request;
 use Encore\Admin\Controllers\HasResourceActions;
 
@@ -50,127 +51,187 @@ class ChargeController extends MainController
      *
      * @return Grid
      */
+    // protected function grid()
+    // {
+    //     $grid = new Grid(new Charge);
+    //     $grid->model()->orderByDesc('id');
+    //     $grid->id(__('ID'));
+    //     $grid->filter(function ($filter) {
+    //         // إلغاء الفلاتر الافتراضية
+    //         $filter->disableIdFilter();
+
+    //         // فلتر "من تاريخ إلى تاريخ" على عمود created_at
+    //         $filter->between('created_at', __('Filter by date'))->date();
+
+
+    //         $filter->column('1/2', function ($filter) {
+    //             $filter->where(function ($query) {
+    //                 $input = $this->input; // Retrieve the selected value
+    //                 $query->where('user_type', $input);
+    //             }, __('User Type'))->select([
+    //                 'dashdash' => __(trans('Users')),
+    //                 'dash' => __(trans('Agency')),
+    //             ]);
+    //         });
+
+
+
+    //         $filter->expand();
+    //     });
+
+
+    //     $grid->column('user_id', __('User'))->display(function ($userId) {
+    //         if ($this->user_type == "dash") {
+    //             $agency = \App\Models\Agency::find($this->agency_id);
+    //             $img = $agency ? getDriverUrl() . '/' . $agency->img : null;
+    //             $id = $agency->id ?? 0;
+    //             $type = "agency";
+    //         } else {
+    //             $user = \App\Models\User::find($userId);
+    //             $img = getDriverUrl() . '/' . @$user->profile?->avatar ?? '';
+    //             $id = @$user->uuid ?? 0;
+    //             $type = "user";
+    //         }
+    //         return "<img src='$img' style='width: 50px; height: 50px; border-radius: 50%;' /> <br> uid: #{$id} <br> type: #{$type}";
+    //     });
+    //     $grid->column('admin.name', __('creator'))
+    //         ->display(function ($name) {
+    //             if ($this->user_type != "dash") {
+    //                 $name = $this->admin->name ?? '';
+    //                 $path = $this->admin->avatar ?? null;
+    //                 $defaultImage = asset("images/businessman-icon.jpg");
+    //                 $url = getImagePath($path) ?? $defaultImage;
+
+    //                 // Check if the image exists
+    //                 if (!isImageExists($url)) {
+    //                     $url = $defaultImage;
+    //                 }
+
+    //                 $image = handleShowImageWithTypes($this->id, $url, 40, 40);
+
+    //                 // Validate admin existence before accessing id
+    //                 $showUrl = '#'; // Default to prevent broken links
+    //                 if ($this->admin && $this->admin->id) {
+    //                     $showUrl = url("admin/auth/users/{$this->admin->id}");
+    //                 }
+
+    //                 return "
+    //                  <div style='display: flex; align-items: center; gap: 10px;'>
+    //                      <a href='{$showUrl}' style='text-decoration: none; color: inherit; display: flex; align-items: center; gap: 10px;'>
+    //                          $image
+    //                          <span style='text-decoration: underline; cursor: pointer;'>$name</span>
+    //                      </a>
+    //                  </div>
+    //                  ";
+    //             } else {
+    //                 $user = $this->sender;
+    //                 $img = getDriverUrl() . '/' . @$user->profile?->avatar ?? '';
+    //                 $id = @$user->uuid ?? 0;
+    //                 $type = "user";
+    //                 return "<img src='$img' style='width: 50px; height: 50px; border-radius: 50%;' /> <br> uid: #{$id} <br> type: #{$type}";
+    //             }
+    //         });
+    //     // $grid->column('user_type', __('User Type'))->using([
+    //     //     'app' => __('app'),
+    //     //     'dash' => __('office')
+    //     // ]);
+
+    //     $grid->column('amount', __('Coins'))->display(function ($usd) {
+
+    //         $image = asset('images/coin.png'); // تأكد من أن الصورة موجودة
+
+    //         return "<div style='display: flex; align-items: center; gap: 5px;'>
+    //                     <span>{$usd}</span>
+    //                     <img src='{$image}' alt='USD' width='20' height='20'>
+    //                 </div>";
+    //     });
+
+    //     $grid->column('balance_before', __('balance before'))->display(function ($usd) {
+
+    //         $image = asset('images/coin.png'); // تأكد من أن الصورة موجودة
+
+    //         return "<div style='display: flex; align-items: center; gap: 5px;'>
+    //                     <span>{$usd}</span>
+    //                     <img src='{$image}' alt='USD' width='20' height='20'>
+    //                 </div>";
+    //     });
+
+    //     // $grid->amount(__('coins'));
+    //     $grid->column('created_at', trans('admin.created_at'));
+
+    //     $grid->tools(function (Grid\Tools $tools) {
+    //         $buttons = '<span style="display: inline-flex; gap: 10px;">'
+    //             . (new ChargeAction())->render()
+    //             . '</span>';
+
+    //         $tools->append($buttons);
+    //     });
+
+    //     $grid->disableActions();
+    //     $grid->disableCreateButton();
+    //     $grid->disableExport();
+
+    //     $this->extendGrid($grid);
+
+    //     return $grid;
+    // }
     protected function grid()
     {
-        $grid = new Grid(new Charge);
-        $grid->model()->orderByDesc('id');
+        $grid = new Grid(new Agency());
+        $grid->model()->where("shipping_agency", 1)->orderByDesc('id');
+        
         $grid->id(__('ID'));
-        $grid->filter(function ($filter) {
-            // إلغاء الفلاتر الافتراضية
-            $filter->disableIdFilter();
-
-            // فلتر "من تاريخ إلى تاريخ" على عمود created_at
-            $filter->between('created_at', __('Filter by date'))->date();
-
-
-            $filter->column('1/2', function ($filter) {
-                $filter->where(function ($query) {
-                    $input = $this->input; // Retrieve the selected value
-                    $query->where('user_type', $input);
-                }, __('User Type'))->select([
-                    'dashdash' => __(trans('Users')),
-                    'dash' => __(trans('Agency')),
-                ]);
-            });
-
-
-
-            $filter->expand();
-        });
-
-
-        $grid->column('user_id', __('User'))->display(function ($userId) {
-            if ($this->user_type == "dash") {
-                $agency = \App\Models\Agency::find($this->agency_id);
-                $img = $agency ? getDriverUrl() . '/' . $agency->img : null;
-                $id = $agency->id ?? 0;
-                $type = "agency";
-            } else {
-                $user = \App\Models\User::find($userId);
-                $img = getDriverUrl() . '/' . @$user->profile?->avatar ?? '';
-                $id = @$user->uuid ?? 0;
-                $type = "user";
-            }
-            return "<img src='$img' style='width: 50px; height: 50px; border-radius: 50%;' /> <br> uid: #{$id} <br> type: #{$type}";
-        });
-        $grid->column('admin.name', __('creator'))
+        
+        $grid->column('name', __('Agency'))
             ->display(function ($name) {
-                if ($this->user_type != "dash") {
-                    $name = $this->admin->name ?? '';
-                    $path = $this->admin->avatar ?? null;
-                    $defaultImage = asset("images/businessman-icon.jpg");
-                    $url = getImagePath($path) ?? $defaultImage;
-
-                    // Check if the image exists
-                    if (!isImageExists($url)) {
-                        $url = $defaultImage;
-                    }
-
-                    $image = handleShowImageWithTypes($this->id, $url, 40, 40);
-
-                    // Validate admin existence before accessing id
-                    $showUrl = '#'; // Default to prevent broken links
-                    if ($this->admin && $this->admin->id) {
-                        $showUrl = url("admin/auth/users/{$this->admin->id}");
-                    }
-
-                    return "
-                     <div style='display: flex; align-items: center; gap: 10px;'>
-                         <a href='{$showUrl}' style='text-decoration: none; color: inherit; display: flex; align-items: center; gap: 10px;'>
-                             $image
-                             <span style='text-decoration: underline; cursor: pointer;'>$name</span>
-                         </a>
-                     </div>
-                     ";
-                } else {
-                    $user = $this->sender;
-                    $img = getDriverUrl() . '/' . @$user->profile?->avatar ?? '';
-                    $id = @$user->uuid ?? 0;
-                    $type = "user";
-                    return "<img src='$img' style='width: 50px; height: 50px; border-radius: 50%;' /> <br> uid: #{$id} <br> type: #{$type}";
+                $path = @$this->img;
+                $defaultImage = asset("images/icon-agency.jpg");
+                $url = getImagePath($path) ?? $defaultImage;
+    
+                if (!isImageExists($url)) {
+                    $url = $defaultImage;
                 }
+                $image = handleShowImageWithTypes($this->id, $url, 40, 40);
+    
+                return "
+                <div style='display: flex; align-items: center; gap: 10px;'>
+                    $image
+                    <span>$name</span>
+                </div>";
             });
-        // $grid->column('user_type', __('User Type'))->using([
-        //     'app' => __('app'),
-        //     'dash' => __('office')
-        // ]);
-
-        $grid->column('amount', __('Coins'))->display(function ($usd) {
-
-            $image = asset('images/coin.png'); // تأكد من أن الصورة موجودة
-
-            return "<div style='display: flex; align-items: center; gap: 5px;'>
-                        <span>{$usd}</span>
-                        <img src='{$image}' alt='USD' width='20' height='20'>
+        
+        $grid->column('owner.name', trans('owner'))
+            ->display(function ($name) {
+                $uid = @$this->owner->uuid;
+                $path = @$this->owner->profile?->avatar;
+                $defaultImage = asset("images/businessman-icon.jpg");
+                $url = getImagePath($path) ?? $defaultImage;
+    
+                if (!isImageExists($url)) {
+                    $url = $defaultImage;
+                }
+    
+                $image = handleShowImageWithTypes($this->id, $url, 40, 40);
+                $showUrl = $this->owner ? url("admin/users/{$this->owner->id}") : 0;
+                return "
+                    <div style='display: flex; align-items: center; gap: 10px;'>
+                        $image
+                        <div>
+                           <a href='{$showUrl}' style='text-decoration: none; color: inherit; display: flex; align-items: center; gap: 10px;'>
+                             <span style='text-decoration: underline; cursor: pointer;'>$name</span>
+                            </a>
+                            <span style='color: #aaa; font-size: smaller;'>UUID: $uid</span>
+                        </div>
                     </div>";
-        });
-
-        $grid->column('balance_before', __('balance before'))->display(function ($usd) {
-
-            $image = asset('images/coin.png'); // تأكد من أن الصورة موجودة
-
-            return "<div style='display: flex; align-items: center; gap: 5px;'>
-                        <span>{$usd}</span>
-                        <img src='{$image}' alt='USD' width='20' height='20'>
-                    </div>";
-        });
-
-        // $grid->amount(__('coins'));
-        $grid->column('created_at', trans('admin.created_at'));
-
-        $grid->tools(function (Grid\Tools $tools) {
-            $buttons = '<span style="display: inline-flex; gap: 10px;">'
-                . (new ChargeAction())->render()
-                . '</span>';
-
-            $tools->append($buttons);
-        });
-
-        $grid->disableActions();
+            });
+            $grid->column('actions', __('Actions'))
+            ->display(function () {
+                return (new ChargeAction())->render();
+            })
+            ->style('white-space: nowrap; width: 100px;');
         $grid->disableCreateButton();
         $grid->disableExport();
-
-        $this->extendGrid($grid);
+        $grid->disableActions();
 
         return $grid;
     }
