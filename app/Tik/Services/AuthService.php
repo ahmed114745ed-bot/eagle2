9dfc3753694f27c3994ed5f7770b7ed5b9f61172
Erinyelkess <ariny.backend@utdsoftware.com>
2025-04-15 12:43:12 +0200

    validation id_token

--- app/Tik/Services/AuthService.php
+++ app/Tik/Services/AuthService.php
@@ -135,10 +135,13 @@ class AuthService
                 /*return  [[], '', $resource];
                 Common::apiResponse(false, 'email already taken', $resource, 405);*/
             } else {
-                $checkValidation = $this->verifyGoogleToken($request['id_token']);
-                if (!$checkValidation /*|| $checkValidation['sub'] != $request['google_id']*/) {
-                    throw new \App\Exceptions\CValidationException('Invalid Google ID Token');
+                if ($request['id_token']) {
+                    $checkValidation = $this->verifyGoogleToken($request['id_token']);
+                    if (!$checkValidation /*|| $checkValidation['sub'] != $request['google_id']*/) {
+                        throw new \App\Exceptions\CValidationException('Invalid Google ID Token');
+                    }
                 }
+
                 $country = $this->countryRepository->findByPhoneCode('101');
                 $data = [
                     'name' => $request['name'],
