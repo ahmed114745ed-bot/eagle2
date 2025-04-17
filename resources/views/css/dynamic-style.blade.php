c5b28c6c5fc10f732a3a0eade2015438067602ce
Bola-Philip <77309023+Bola-Philip@users.noreply.github.com>
2025-04-17 15:49:27 +0200

    room modes issues

--- resources/views/css/dynamic-style.blade.php
+++ resources/views/css/dynamic-style.blade.php
@@ -325,6 +325,10 @@
         border: 1px solid var(--second-alpha) !important;
     }
 
+    .rtl [class*="col-md-"] {
+        float: none !important;
+    }
+
     .pagination > li > a, .pagination > li > span {
         position: relative;
         float: left;
