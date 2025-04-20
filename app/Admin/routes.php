0651e1554c842d908949eed53c044f54f8567dce
mido203020 <Mohamed.gamal@utdsoftware.com>
2025-04-20 11:42:39 +0200

    test

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -333,7 +333,7 @@ Route::group(
 
 
         Route::get('ovip-gift/{ovip_id}/{type?}', [OvipGiftTapController::class, 'index']);
-        $router->get('ovip-gift/{id}/edit', [OvipGiftTapController::class, 'edit']);
+        Route::get('ovip-gift/{ovip_id}/{id}/edit', [OvipGiftTapController::class, 'edit']);
 
             Route::get('room-mic/{room_id}/', [RoomMicController::class, 'index']);
             Route::prefix('ware-gift/{level}')->group(function () {
