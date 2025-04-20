e5d72695f7d10f78e080b6c73fb7eb520624c5a0
mido203020 <Mohamed.gamal@utdsoftware.com>
2025-04-20 17:49:34 +0200

    test

--- app/Admin/Controllers/OvipGiftTapController.php
+++ app/Admin/Controllers/OvipGiftTapController.php
@@ -99,11 +99,11 @@ class OvipGiftTapController extends MainController
         $grid->expire(__('expire'));
 
 
-        $grid->column('actions', __('Actions'))->display(function () use ($level) {
+        $grid->column('actions', __('Actions'))->display(function () use ($type) {
             $id = $this->id;
 
-            $editUrl = url("admin/ware-gift/{$level}/{$id}/edit");
-            $deleteUrl = url("admin/ware-gift/{$level}/{$id}");
+            $editUrl = url("admin/ware-gift/{$type}/{$id}/edit");
+            $deleteUrl = url("admin/ware-gift/{$type}/{$id}");
             $csrf = csrf_token();
 
             $editText = __('admin.edit');
