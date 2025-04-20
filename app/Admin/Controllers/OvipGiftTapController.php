27cf949c055b71478555904f0791dce5284d4a67
mido203020 <Mohamed.gamal@utdsoftware.com>
2025-04-20 14:13:55 +0200

    test

--- app/Admin/Controllers/OvipGiftTapController.php
+++ app/Admin/Controllers/OvipGiftTapController.php
@@ -99,11 +99,35 @@ class OvipGiftTapController extends MainController
         $grid->expire(__('expire'));
 
 
-        $grid->actions(function ($actions) {
+        $grid->actions(function ($actions) use ($level) {
             $actions->disableView();
             $actions->disableEdit();
             $actions->disableDelete();
+
+            /** @var \App\Models\Ware $model */
+            $model = $actions->getKey();
+
+            // Example edit and delete URLs – replace with your actual routes
+            $editUrl = url("admin/ware-gift/{$level}/{$model}/edit");
+            $deleteUrl = url("admin/ware-gift/{$level}/{$model}");
+            $csrf = csrf_token();
+
+            $actions->append("
+                <a href='{$editUrl}' class='btn btn-xs btn-primary' style='margin-right: 5px'>
+                    <i class='fa fa-edit'></i> Edit
+                </a>
+
+            <form action='{$deleteUrl}' method='POST' style='display:inline-block;' onsubmit='return confirm(\"Are you sure?\")'>
+                <input type='hidden' name='_token' value='{$csrf}'>
+                <input type='hidden' name='_method' value='DELETE'>
+                <button type='submit' class='btn btn-xs btn-danger'>
+                    <i class='fa fa-trash'></i> Delete
+                </button>
+            </form>
+            ");
         });
+
+
         $grid->disableCreateButton();
         $this->extendGrid($grid);
         $grid->disableExport();
