a1622711a46c83a468c18d9e4cab29ab41c1a34d
shami2024 <>
2025-04-21 16:22:56 +0200

    Update bootstrap.php

--- app/Admin/bootstrap.php
+++ app/Admin/bootstrap.php
@@ -60,7 +60,4 @@ view()->composer('admin::partials.css', function (Illuminate\View\View $view) {
     $view->setPath(resource_path('views/admin/views/partials/css.blade.php'));
 });
 
-Admin::css('https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css');
-Admin::js('https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js');
-
 
