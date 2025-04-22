260fd3154a6671a4e3e557cdcc5095966c47101c
shami2024 <>
2025-04-22 13:51:51 +0200

    UserStatus event

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -221,7 +221,7 @@ class UserController extends MainController
         });
         $grid->column('name', __('Name'));
 
-            $grid->column('return', __('status user'))->display(function () {
+        $grid->column('return', __('status user'))->display(function () {
                 $userSetting = $this->userSetting ?? (object) ['show_invite_code' => 0, 'hide_chat' => 0];
                 return (new \App\Admin\Actions\UserAction(
                     $this->id,
