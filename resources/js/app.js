5522954ad7d3e6e18ef58bdf93e295acd3f0b370
Bola-Philip <77309023+Bola-Philip@users.noreply.github.com>
2025-04-22 09:32:18 +0200

    popup model issue

--- resources/js/app.js
+++ resources/js/app.js
@@ -63,3 +63,9 @@ document.addEventListener("DOMContentLoaded", function () {
         table.scrollLeft = 0;
     }
 });
+
+$(document).on('click', '[data-toggle="modal"]', function () {
+    setTimeout(function () {
+        $('html, body').animate({ scrollTop: 0 }, 300);
+    }, 200); // Small delay to allow modal DOM to render
+});
