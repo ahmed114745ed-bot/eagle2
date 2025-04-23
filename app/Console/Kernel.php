1a2f66e555920eba0c3006dc1299eba0e0ee2d34
Erinyelkess <ariny.backend@utdsoftware.com>
2025-04-23 09:58:20 +0200

    time

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -32,8 +32,8 @@ class Kernel extends ConsoleKernel
         $schedule->command('queue:work --queue=heavy1')->withoutOverlapping()->runInBackground();
         $schedule->command('queue:work --queue=heavy2')->withoutOverlapping()->runInBackground();
         $schedule->command('queue:work --queue=heavy3')->withoutOverlapping()->runInBackground();
-        $schedule->command('normal-lucy-box')->everyMinute();
-        $schedule->command('super-lucy-box')->everyMinute();
+        $schedule->command('normal-lucy-box')->everyMinute()->withoutOverlapping()->runInBackground();
+        $schedule->command('super-lucy-box')->everyMinute()->withoutOverlapping()->runInBackground();
     }
 
     /**
