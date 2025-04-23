bb2959464ffa9b36d92f27f549fe8ebf0ba79368
shami2024 <>
2025-04-23 14:33:23 +0200

    Update charges_settings.blade.php

--- resources/views/admin/charges_settings.blade.php
+++ resources/views/admin/charges_settings.blade.php
@@ -480,7 +480,18 @@
     @csrf
                     <div class="form row">
 
+                      
                         <div class="col-md-6">
+                            <div class="form-group">
+                                <label>{{ __('Zones Coins') }}</label>
+                                <input type="text" name="zones_coins" id="zones_coins"
+                                    value="{{ $settings['zones_coins'] ?? '' }}" class="form-control">
+                                    <small id="zones_coins_hint" class="form-text text-muted mt-1"
+                                data-template="{{ __('1 :dollar = :value :coins', ['dollar' => __('Dollar'), 'coins' => __('Coins')]) }}">
+                            </small>            </div>
+                         </div>
+
+                         <div class="col-md-6">
                             <div class="form-group">
                                 <label>{{ __('Super Admin Coins') }}</label>
                                 <input type="text" name="super_admin_coins" id="super_admin_coins"
@@ -492,25 +503,8 @@
                         </div>
                         </div>
 
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label>{{ __('Zones Coins') }}</label>
-                                <input type="text" name="zones_coins" id="zones_coins"
-                                    value="{{ $settings['zones_coins'] ?? '' }}" class="form-control">
-                                    <small id="zones_coins_hint" class="form-text text-muted mt-1"
-                                data-template="{{ __('1 :dollar = :value :coins', ['dollar' => __('Dollar'), 'coins' => __('Coins')]) }}">
-                            </small>            </div>
-                         </div>
 
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <label>{{ __('User Coins') }}</label>
-                                <input type="text" name="user_coins" id="user_coins"
-                                    value="{{ $user_coins }}" class="form-control">
-                                    <small id="user_coins_hint" class="form-text text-muted mt-1"
-                                data-template="{{ __('1 :dollar = :value :coins', ['dollar' => __('Dollar'), 'coins' => __('Coins')]) }}">
-                            </small>            </div>
-                        </div>
+                       
 
                         <div class="col-md-6">
                             <div class="form-group">
@@ -522,6 +516,16 @@
                             </small>     
                         
                         </div>
+                        </div>
+
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>{{ __('User Coins') }}</label>
+                                <input type="text" name="user_coins" id="user_coins"
+                                    value="{{ $user_coins }}" class="form-control">
+                                    <small id="user_coins_hint" class="form-text text-muted mt-1"
+                                data-template="{{ __('1 :dollar = :value :coins', ['dollar' => __('Dollar'), 'coins' => __('Coins')]) }}">
+                            </small>            </div>
                         </div>
 
                         <button type="button" onclick="showConfirmationModal()">{{ __('save') }}</button>
