4f6e161d966d55ef631d5e96babb832afeadcd34
Erinyelkess <ariny.backend@utdsoftware.com>
2025-04-23 10:17:40 +0200

    edit command

--- app/Console/Commands/SuperLuckyBoxCommend.php
+++ app/Console/Commands/SuperLuckyBoxCommend.php
@@ -30,6 +30,7 @@ class SuperLuckyBoxCommend extends Command
         foreach ($userBoxes as $userBox) {
             $keyBoxUse  = 'BoxUse_' . $userBox->bid;
             $pickerBoxIds =   PickBoxList::where('box_user_id', $userBox->id)->pluck('user_id')->toArray();
+            if (!$pickerBoxIds) return '';
             $users =  User::whereIn('id', $pickerBoxIds)->inRandomOrder()->get();
             foreach ($users as $user) {
 
