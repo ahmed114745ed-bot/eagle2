de40ade3a97a7ad244a913357b1a0c0c5c67deda
Erinyelkess <ariny.backend@utdsoftware.com>
2025-04-27 10:56:50 +0300

    edit

--- app/Traits/HelperTraits/CalcsTrait.php
+++ app/Traits/HelperTraits/CalcsTrait.php
@@ -807,6 +807,25 @@ trait CalcsTrait
         return optional($ware)->{$item} ?? '';
     }
 
+    public static function wareUserVipColor($user_id, $type)
+    {
+        if (gettype($user_id) == 'integer') {
+            $user = User::query()->find($user_id);
+            if (!$user) return new \stdClass();
+        } else {
+            $user = $user_id;
+        }
+        if (!isset($user->UserVip)) return '';
+        $uvip = $user?->UserVip;
+        if (!$uvip) return new \stdClass();
+
+        $vip = OVip::query()->find($uvip->vip_id);
+
+        if (!$vip) return new \stdClass();
+        $ware = Ware::where('level', $vip->level)->where('type', $type)->where('get_type', 1)->first();
+        return @$ware?->color ?? '';
+    }
+
 
     public static function ovip_centerforFaml($user_id)
     {
