94b9cfd8f4ebee769700976e26103d125576d4d0
Erinyelkess <ariny.backend@utdsoftware.com>
2025-04-28 14:04:48 +0300

    delete agency in table join

--- app/Observers/Api/V1/AgencyObserver.php
+++ app/Observers/Api/V1/AgencyObserver.php
@@ -7,6 +7,7 @@ use App\Models\Admin;
 use App\Models\Agency;
 use App\Facades\UserHandling;
 use App\Models\AgencyJoinRequest;
+use App\Models\UsersJoinedAgency;
 
 class AgencyObserver
 {
@@ -45,6 +46,8 @@ class AgencyObserver
         User::query()->where('agency_id', $agency->id)->update(['agency_id' => 0, 'type_user' => 0]);
         AgencyJoinRequest::query()->where('agency_id', $agency->id)->delete();
         UserHandling::kickOfAllUsersFromAgency($agency);
+        $joinedAgency = UsersJoinedAgency::where(['agency_id' =>   $agency->id])->get();
+        if ($joinedAgency) UsersJoinedAgency::where('agency_id',  $agency->id)->update(['leave_date' => now()]);
         $user = User::find($agency->app_owner_id);
         Admin::where('username', $user->uuid)->delete();
     }
