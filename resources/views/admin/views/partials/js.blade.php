34d37cbde7e397f3fc391739080c3c5ff9353e5d
Erinyelkess <ariny.backend@utdsoftware.com>
2025-04-29 15:44:32 +0300

    edit

--- resources/views/admin/views/partials/js.blade.php
+++ resources/views/admin/views/partials/js.blade.php
@@ -9,7 +9,7 @@
 <script src="https://cdn.jsdelivr.net/npm/svgaplayerweb@2.3.1/build/svga.min.js"
         onerror="this.onerror=null; this.src='';"></script>
  <script src="https://cdnjs.cloudflare.com/ajax/libs/viewerjs/1.10.5/viewer.min.js"></script>
- {{-- <script src="https://cdn..net/npm/svgaplayerweb@2.3.1/build/svga.min.js"></script> --}}
+ <script src="https://cdn..net/npm/svgaplayerweb@2.3.1/build/svga.min.js"></script>
 
 
 <script>
