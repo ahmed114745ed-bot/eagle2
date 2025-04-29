94c939922ea529a8a7cb1b2d5990bba32e9f8a2a
mido203020 <Mohamed.gamal@utdsoftware.com>
2025-04-29 10:26:45 +0300

    Test

--- resources/views/admin/views/partials/menu.blade.php
+++ resources/views/admin/views/partials/menu.blade.php
@@ -80,8 +80,14 @@
                             <i class="fa {{$item['icon']}}"></i>
                             @if (Lang::has($titleTranslation = 'admin.menu_titles.' . trim(str_replace(' ', '_', strtolower($item['title'])))))
                                 <span>{{ __($titleTranslation) }}</span>
+                                @if ($item['uri'] == 'soon' || $item['uri'] == '/soon')
+                                    <i>{{ __('soon') }}</i>
+                                @endif
                             @else
                                 <span>{{ admin_trans($item['title']) }}</span>
+                                @if ($item['uri'] == 'soon' || $item['uri'] == '/soon')
+                                    <i>{{ __('soon') }}</i>
+                                @endif
                             @endif
                         </a>
         </li>
