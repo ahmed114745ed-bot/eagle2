e550b254b4c28de9143714d90e8a403ad22ac362
shami2024 <>
2025-04-30 09:53:53 +0300

    admin img

--- resources/views/vendor/admin/partials/sidebar.blade.php
+++ resources/views/vendor/admin/partials/sidebar.blade.php
@@ -6,12 +6,16 @@
         <!-- Sidebar user panel (optional) -->
         <div class="user-panel">
             <div class="pull-left image">
-                <img src="{{ Admin::user()->avatar }}" class="img-circle" alt="User Image">
+                <img src="{{ Admin::user()->img }}" class="img-circle" alt="User Image">
             </div>
             <div class="pull-left info">
                 <p>{{ Admin::user()->name }}</p>
                 <!-- Status -->
                 <a href="#"><i class="fa fa-circle text-success"></i> {{ trans('admin.online') }}</a>
+            </div>
+            <div class="pull-role">
+            {{ Auth::user()->roles[0]->name }}
+            <!-- Status -->
             </div>
         </div>
 
