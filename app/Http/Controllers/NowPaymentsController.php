44640191c1763fc5f6121d28cde859e62a664eb6
mido203020 <Mohamed.gamal@utdsoftware.com>
2025-05-01 12:44:21 +0300

    Test

--- app/Http/Controllers/NowPaymentsController.php
+++ app/Http/Controllers/NowPaymentsController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Services\NowPaymentsService;
+use App\Models\NowpaymentOrder;
 use Database\Seeders\config;
 use Illuminate\Http\Request;
+use Log;
 
 class NowPaymentsController extends Controller
 {
@@ -21,7 +23,8 @@ class NowPaymentsController extends Controller
             'price_amount' => $request->amount, // Amount in fiat currency
             'price_currency' => 'usd',
             'pay_currency' => $request->currency, // Cryptocurrency to receive
-            'ipn_callback_url' => config('services.now_payments.callback_url'), // Callback URL for IPN
+            //'ipn_callback_url' => config('services.now_payments.callback_url'), // Callback URL for IPN
+            'ipn_callback_url' => 'https://eagle.utdsoftware.com/api/now-payments-callback', // Callback URL for IPN
             'order_id' => uniqid(), // Unique order ID
             'order_description' => 'Payment',
         ];
@@ -50,10 +53,17 @@ class NowPaymentsController extends Controller
     public function paymentCallback(Request $request)
     {
         // Handle IPN callback from Now Payments
+        Log::info('now payments'. $request->payment_id);
         $paymentId = $request->input('payment_id');
         $status = $this->nowPayments->getPaymentStatus($paymentId);
+        Log::info($status);
 
         // Update your database or trigger actions based on payment status
+        if($status['data']['payment_status'] == 'paid'){
+            NowpaymentOrder::where('payment_id', $paymentId)->update([
+                'payment_status' => 'paid'
+            ]);
+        }
         // Example: Mark order as paid
 
         return response()->json(['status' => 'success']);
