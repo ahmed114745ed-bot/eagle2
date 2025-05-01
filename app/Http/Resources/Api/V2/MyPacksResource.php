a79c5c95b995a446b72f039f4acd842ac9ff4ae0
Erinyelkess <ariny.backend@utdsoftware.com>
2025-05-01 15:54:54 +0300

    edit

--- app/Http/Resources/Api/V2/MyPacksResource.php
+++ app/Http/Resources/Api/V2/MyPacksResource.php
@@ -74,7 +74,7 @@ class MyPacksResource extends JsonResource
             'is_dress' => $is_dress ?? 0,
             'title' => $title,
             'color' => $color,
-            'using' => @(bool)$this->using,
+            'using' => $this->using,
         ];
     }
 }
