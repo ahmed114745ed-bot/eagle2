dc1d95ff9a0d9d3c02b7367ab83a584862a115bc
mido203020 <Mohamed.gamal@utdsoftware.com>
2025-05-04 12:21:29 +0300

    Test

--- resources/views/mangerSetting.blade.php
+++ resources/views/mangerSetting.blade.php
@@ -305,7 +305,7 @@
                     <a  href="{{ route('admin.create-payment-gateway') }}" class="btn btn-success">
                         {{ __('Add') }}
                     </a>
-                    <table class="table">
+                    <table class="table" style="background-color: var(--secondary-color);">
                         <thead>
                             <tr>
                                 <th>#</th>
