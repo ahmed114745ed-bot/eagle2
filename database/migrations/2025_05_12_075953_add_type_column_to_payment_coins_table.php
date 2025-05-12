8f191f29fa6f4405bdc6cbff46511cee8456753e
Bola-Philip <77309023+Bola-Philip@users.noreply.github.com>
2025-05-12 13:39:17 +0300

    get back the payment gateways to be static

--- database/migrations/2025_05_12_075953_add_type_column_to_payment_coins_table.php
+++ database/migrations/2025_05_12_075953_add_type_column_to_payment_coins_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('payment_coins', function (Blueprint $table) {
-            $table->string('type')->unique();
+            $table->string('type')->nullable()->unique();
         });
     }
 
