0f658cf089bdd39c303e02ebd6e524e88591d276
Bola-Philip <77309023+Bola-Philip@users.noreply.github.com>
2025-05-14 12:27:06 +0300

    register issue

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
+    protected function schedule(Schedule $schedule): void
     {
         $schedule->command('queue:work --queue=default')->withoutOverlapping()->runInBackground();
         $schedule->command('queue:work --queue=updatePkAndSendToZigo')->withoutOverlapping()->runInBackground();
@@ -42,7 +42,7 @@ class Kernel extends ConsoleKernel
      *
      * @return void
      */
-    protected function commands()
+    protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
 
