164b15c1ce1fd087f2f23f2137ccc9b14e47e5f6
Bola-Philip <77309023+Bola-Philip@users.noreply.github.com>
2025-05-15 15:19:30 +0300

    wallet transactions

--- Modules/Wallet/Services/WalletService.php
+++ Modules/Wallet/Services/WalletService.php
@@ -60,7 +60,7 @@ class WalletService
         return $this->startTransaction($userWallet, $receiver, $sender, $amount, $coins, $receiverType);
     }
 
-    public function startTransaction(UserWallet $userWallet, User $receiver,User $sender, int $amount, int $coins,string $receiverType): JsonResponse
+    public function startTransaction(UserWallet $userWallet, $receiver,User $sender, int $amount, int $coins,string $receiverType): JsonResponse
     {
         DB::beginTransaction();
         try {
