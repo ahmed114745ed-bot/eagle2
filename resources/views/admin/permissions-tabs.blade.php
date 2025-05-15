cdb54ce965ec4ebdf69e6846ba526c6b8db5b15d
Erinyelkess <ariny.backend@utdsoftware.com>
2025-05-15 15:39:17 +0300

    edit

--- resources/views/admin/permissions-tabs.blade.php
+++ resources/views/admin/permissions-tabs.blade.php
@@ -98,7 +98,7 @@
                                     id="perm-${perm.id}"
                                     ${checked}>
                              <label class="form-check-label" for="perm-${perm.id}">
-                                 ${ __(perm.name)}
+                                 ${ perm.name}
                              </label>
                          </div>
                      `;
