8f2e80f28681b5910c41c2c7603f55813cd1ced1
Erinyelkess <ariny.backend@utdsoftware.com>
2025-05-18 13:28:14 +0300

    edit

--- app/Http/Controllers/SettingsController.php
+++ app/Http/Controllers/SettingsController.php
@@ -139,6 +139,7 @@ class SettingsController extends Controller
             $data['app_background'] = $request->background_color;
             $data['background_color'] = $request->background_color;
         } elseif ($request->background_type === 'image' && $request->hasFile('app_background_image')) {
+            dd(123);
             $data['app_background'] = Common::upload('images', $request->file('app_background_image'));
             $data['images_background'] = Common::upload('images', $request->file('app_background_image'));
         } else if ($request->background_type == 'gradient') {
