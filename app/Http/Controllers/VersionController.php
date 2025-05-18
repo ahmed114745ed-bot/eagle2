164298e9fb30e6fe11d0e327c1bfe2ea2ad30a16
Bola-Philip <77309023+Bola-Philip@users.noreply.github.com>
2025-05-18 15:57:47 +0300

    app feature

--- app/Http/Controllers/VersionController.php
+++ app/Http/Controllers/VersionController.php
@@ -59,6 +59,7 @@ class VersionController extends Controller
                 'profile_frame_updated' => $ProfileFrameUpdated,
                 'colors' => settings()->get('colors_updated_at') ?? false,
                 'background' => settings()->get('ground_updated_at') ?? false,
+                'host_agency' => (bool)\Cache::get('host_agency'),
                 //intro - frames - extradata - emoji
             ], 'enable_chat'  => settings()->get('chat_status') == "on"
         ];
