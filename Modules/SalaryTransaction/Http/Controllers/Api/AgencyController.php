f96fbfa7ea1f47ec1b7a4b5e52411b0e15a53ff4
johnghattas <john.backend@utdsoftware.com>
2025-05-18 16:26:22 +0300

    Update AgencyController.php

--- Modules/SalaryTransaction/Http/Controllers/Api/AgencyController.php
+++ Modules/SalaryTransaction/Http/Controllers/Api/AgencyController.php
@@ -6,6 +6,7 @@ use App\Helpers\Common;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Api\V1\AgencyResource;
 use App\Models\Agency;
+use App\Models\ShippingAgency;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -17,7 +18,7 @@ class AgencyController extends Controller
     public function get_info()
     {
         $user = Auth::user();
-        $agency = Agency::query()->with("countries", 'AgencypaymentGateways')->whereAppOwnerId($user->id)->first();
+        $agency = ShippingAgency::query()->with("countries", 'AgencypaymentGateways')->whereAppOwnerId($user->id)->first();
         if (!$agency) {
             return Common::apiResponse(1, __("api_responses.agency"), []);
         }
