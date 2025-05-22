22ef18a3ec2dbae462bd7b6429bab3bc7d23cdc1
johnghattas <john.backend@utdsoftware.com>
2025-05-22 19:32:12 +0300

    Update AuthController.php

--- app/Admin/Controllers/AuthController.php
+++ app/Admin/Controllers/AuthController.php
@@ -125,7 +125,7 @@ class AuthController extends BaseAuthController
         $form->display('username', trans('admin.username'));
         $form->text('name', trans('admin.name'))->rules('required');
         $form->image('avatar', trans('admin.avatar'));
-       if(!(Auth::user()->id != 1))
+       if(!(Auth::user()->id == 1))
        {
         $form->password('password', trans('admin.password'))->rules('confirmed|required');
         $form->password('password_confirmation', trans('admin.password_confirmation'))->rules('required')
