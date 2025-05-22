8e8728febed83df1feb836dc59d37ad2cf5e5fd5
Bola-Philip <77309023+Bola-Philip@users.noreply.github.com>
2025-05-22 12:33:16 +0300

    fawry issue

--- app/Services/FawryPaymentServiceV2.php
+++ app/Services/FawryPaymentServiceV2.php
@@ -6,6 +6,7 @@ use App\Models\PaymentMethodHistory;
 use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
+use Mockery\Exception;
 
 class FawryPaymentServiceV2
 {
@@ -29,39 +30,55 @@ class FawryPaymentServiceV2
 
     public function makePayment($trx,$amount,$exterData): string
     {
+        info(config("services.fawry.fawry_merchant_code"));
+        info(config("services.fawry.fawry_secret"));
+        info($this->fawryUrl);
         $data = $this->getBodyForFawry($trx,$amount);
 
-        $response = Http::withHeaders([
-            'Content-Type' => 'application/json',
-        ])->post($this->fawryUrl, $data);
+        try {
+            $response = Http::withHeaders([
+                'Content-Type' => 'application/json',
+            ])->post($this->fawryUrl, $data);
 
+        }catch (\Exception $exception){
+            info($exception->getMessage());
+        }
+
+        info($response->body());
         return $response->body();
     }
 
     public function getBodyForFawry($trx,$amount): array
     {
-        $merchantCode = config("services.fawry.fawry_merchant_code");
-        $merchantRefNum = $trx;
-        $secure_key = config("services.fawry.fawry_secret");
-        $price = number_format($amount, 2, '.', '');
-        $qty = 1;
-        $syn = $merchantCode.$merchantRefNum."".self::redirect_if_payment_success ($trx).$trx.$qty.$price.$secure_key;
-        $signature = hash('sha256', $syn);
-        $data = [
-            "merchantCode"=> $merchantCode,
-            "merchantRefNum"=> $merchantRefNum,
-            "language" => "en-gb",
-            "chargeItems"=> [
-                [
-                    "itemId"=> $trx,
-                    "price"=> $price,
-                    "quantity"=> $qty,
-                ]
-            ],
-            "returnUrl"=> self::redirect_if_payment_success ($trx),
-            "signature"=> $signature
+        info('yes');
+        try {
+            $merchantCode = config("services.fawry.fawry_merchant_code");
+            $merchantRefNum = $trx;
+            $secure_key = config("services.fawry.fawry_secret");
+            $price = number_format($amount, 2, '.', '');
+            $qty = 1;
+            $syn = $merchantCode.$merchantRefNum."".self::redirect_if_payment_success ($trx).$trx.$qty.$price.$secure_key;
+            $signature = hash('sha256', $syn);
+            $data = [
+                "merchantCode"=> $merchantCode,
+                "merchantRefNum"=> $merchantRefNum,
+                "language" => "en-gb",
+                "chargeItems"=> [
+                    [
+                        "itemId"=> $trx,
+                        "price"=> $price,
+                        "quantity"=> $qty,
+                    ]
+                ],
+                "returnUrl"=> self::redirect_if_payment_success ($trx),
+                "signature"=> $signature
 
-        ];
+            ];
+        }catch (\Exception $e){
+            info($e->getMessage());
+        }
+
+        info('123');
         return $data;
     }
 
