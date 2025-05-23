efb04f3dc8fead36f661ec36d082d2cf3127e973
johnghattas <john.backend@utdsoftware.com>
2025-05-23 14:59:56 +0300

    update user types

--- app/Http/Resources/Api/V1/MyDataResource.php
+++ app/Http/Resources/Api/V1/MyDataResource.php
@@ -136,20 +136,7 @@ class MyDataResource extends JsonResource
         /**@var User $this
          * @var Room $ownerRoom*/
 
-        $userTypes = [];
 
-        if ($this->type_user == 2 || $this->type_user == 4) {
-            $userTypes = array_merge($userTypes, [1, 2]);
-        }elseif ($this->type_user == 1){
-            $userTypes[] = 1;
-        }
-        if ($this->hasShippingAgency()) {
-            $userTypes[] = 3;
-        }
-        $userTypes = array_unique($userTypes);
-
-        if (count($userTypes) == 0) $userTypes[] = 0;
-        
         $data = [
             'id' => @$this->id,
             'notification_id' => @$this->notification_id ?: "",
@@ -228,7 +215,7 @@ class MyDataResource extends JsonResource
             'new_gift'          => (bool)$this->new_gift,
             'show_invite_code' => (bool)$this->userSetting?->show_invite_code ?? false,
             'wallet' => $this->wallet?->value ?? 0,
-            'user_types' => $userTypes ?? [0],
+            'user_types' => $this->user_types,
             'wabble' => $this->getUserPack(12),
             "shipping-agency" => $this->shippingAgency ? [
                 "id" => $this->shippingAgency->id,
@@ -280,4 +267,6 @@ class MyDataResource extends JsonResource
             'image' => 'wappel.png',
         ];
     }
+
+
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -770,7 +770,6 @@ class User extends Authenticatable
     }
 
 
-
     public function setOldUsdAttribute()
     {
         $currentYear                 = date('Y');
@@ -1356,5 +1355,22 @@ class User extends Authenticatable
         }
 
         return false;
+    }
+
+    public function getUserTypesAttribute() : array
+    {
+        $userTypes = match (true) {
+            in_array($this->type_user, [2, 4]) => [1, 2],
+            $this->type_user == 1 => [1],
+            default => []
+        };
+
+        if ($this->hasShippingAgency()) {
+            $userTypes[] = 3;
+        }
+
+        $userTypes = array_unique($userTypes);
+
+        return empty($userTypes) ? [0] : $userTypes;
     }
 }
