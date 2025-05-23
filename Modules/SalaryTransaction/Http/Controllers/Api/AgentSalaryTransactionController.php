652b6ff46086ef97336bdb97cedb673c5ba4f124
johnghattas <john.backend@utdsoftware.com>
2025-05-23 16:30:05 +0300

    Update AgentSalaryTransactionController.php

--- Modules/SalaryTransaction/Http/Controllers/Api/AgentSalaryTransactionController.php
+++ Modules/SalaryTransaction/Http/Controllers/Api/AgentSalaryTransactionController.php
@@ -117,9 +117,9 @@ class AgentSalaryTransactionController extends Controller
             return Common::apiResponse(0, __('api_responses.this_user_not_found'));
         }
 
-        
+
         $user_id = $user_Resve->id;
-        $agency = $user->agency;
+        $agency = $user->shippingAgency;
         if (!$agency) {
             return Common::apiResponse(0, __('api_responses.agency'));
         }
