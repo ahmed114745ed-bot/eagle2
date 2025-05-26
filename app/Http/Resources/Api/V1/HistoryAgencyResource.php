2ad581495c46202fbc0549f8d49bdce42689210b
johnghattas <john.backend@utdsoftware.com>
2025-05-26 14:48:03 +0300

    update agency history

--- app/Http/Resources/Api/V1/HistoryAgencyResource.php
+++ app/Http/Resources/Api/V1/HistoryAgencyResource.php
@@ -37,7 +37,7 @@ class HistoryAgencyResource extends JsonResource
             ->with('receiver')->groupBy('receiver_id')->whereHas('receiver')->whereYear('created_at', $year)->whereMonth('created_at', $month)->orderByDesc('exp')->take(3)->get();
         $heroGiftLog = GiftLog::where('agency_id', $this->id)->selectRaw("SUM(giftPrice) as exp, sender_id")
             ->with('sender')->groupBy('sender_id')->whereHas('sender')->whereYear('created_at', $year)->whereMonth('created_at', $month)->orderByDesc('exp')->take(3)->get();
-        if ($request->is_onwer_agency) {
+        if (request()->is_onwer_agency) {
             $salary = AgencySallary::where('agency_id', $this->id)->where('year', $year)->where('month', $month)->sum('sallary');
             $target = $this->userSalaries()->where('year', $year)->where('month', $month)->sum('target_diamonds');
         }
@@ -47,7 +47,7 @@ class HistoryAgencyResource extends JsonResource
         return [
             'star' => ReceiverGiftLogResource::collection($giftLog),
             'heroes' => SenderGiftLogResource::collection($heroGiftLog),
-            $this->mergeWhen($request->is_onwer_agency,
+            $this->mergeWhen(request()->is_onwer_agency,
                 [
                     'salary' => @$salary ?? 0,
                     'target' =>  @$target ?? 0,
