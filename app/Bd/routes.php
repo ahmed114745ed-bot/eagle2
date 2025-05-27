c2e14b68be903aba49dbdc164c0ac1bbbbba206b
shami2024 <>
2025-05-27 09:25:31 +0300

    Update routes.php

--- app/Bd/routes.php
+++ app/Bd/routes.php
@@ -4,12 +4,12 @@ use KevinSoft\MultiLanguage\MultiLanguage;
 use Illuminate\Routing\Router;
 
 
-// use App\Bd\Controllers\HomeController;
+use App\Bd\Controllers\HomeController;
 // use App\Bd\Controllers\RequestAgencyController;
 // use App\Bd\Controllers\ChargeController;
 // use App\Bd\Controllers\BdSalariesController;
 // use App\Bd\Controllers\AgencyController;
-// use App\Bd\Controllers\WalletController;
+use App\Bd\Controllers\WalletController;
 
 
 
