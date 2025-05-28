425da6dc1e5d0d048fb43eda1f1b235e4c046b44
makled98 <mkled.backend@utdsoftware.com>
2025-05-28 20:13:44 +0300

    Update WalletController.php

--- app/Bd/Controllers/WalletController.php
+++ app/Bd/Controllers/WalletController.php
@@ -292,30 +292,30 @@ class WalletController extends MainController
     
     public function charge(Request $request)
     {
-        $request->validate([
-            'amount' => 'required|integer|min:1',
-            'target_id' => 'nullable',    
-            'target_type' => 'required|string',
-        ]);
-       
-
-        $types = [
-            'user' => [$this, 'chargeToUser'],
-            'agency' => [$this, 'chargeToAgency']
-        ];
-      
-        $type = $request->input('target_type');
-     
-        if (!array_key_exists($type, $types)) {
-            admin_toastr('نوع الوجهة غير موجود', 'error');
-            return back();
-        }
-       
         try {
-          
-            $data = call_user_func($types[$type], $request->all());
-            admin_toastr('تم الشحن بنجاح', 'success');
-            return back();
+            $request->validate([
+                'amount' => 'required|integer|min:1',
+                'target_id' => 'nullable',    
+                'target_type' => 'required|string',
+            ]);
+        
+
+            $types = [
+                'user' => [$this, 'chargeToUser'],
+                'agency' => [$this, 'chargeToAgency']
+            ];
+        
+            $type = $request->input('target_type');
+        
+            if (!array_key_exists($type, $types)) {
+                admin_toastr('نوع الوجهة غير موجود', 'error');
+                return back();
+            }
+                $data = call_user_func($types[$type], $request->all());
+                admin_toastr('تم الشحن بنجاح', 'success');
+                return back();
+        } catch (\Exception $e) {
+            return Common::apiResponse(false, $e->getMessage());
         } catch (\Throwable $e) {
             admin_toastr('حدث خطأ أثناء الشحن: ' . $e->getMessage(), 'error');
             return back();
