bfa892914009158c62b1e595784097096ef4944a
shami2024 <>
2025-05-28 17:07:17 +0300

    Update AgentSalaryTransactionController.php

--- Modules/SalaryTransaction/Http/Controllers/Api/AgentSalaryTransactionController.php
+++ Modules/SalaryTransaction/Http/Controllers/Api/AgentSalaryTransactionController.php
@@ -82,8 +82,8 @@ class AgentSalaryTransactionController extends Controller
                         ->where("charger_type", 'agency')
                         ->where("charger_id", $agency->id);
 
-        $data = $data->when($type == 'sent', function ($q) use ($search, $usrAuth) {
-            $q->where("charger_id", $usrAuth->agency_id)->where('charger_type', '=', 'agency')
+        $data = $data->when($type == 'sent', function ($q) use ($search, $agency) {
+            $q->where("charger_id", $agency->id)->where('charger_type',  'agency')
                 ->whereHas('receiver', function ($q2) use ($search) {
                     $q2->fitterByUuid($search);
                 });
