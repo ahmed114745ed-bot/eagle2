2d66234fcede44eb7c13e1a4fcef4c79c40eede2
shami2024 <>
2025-05-29 17:30:15 +0300

    Update AgencyUsersTargetResource.php

--- app/Http/Resources/Api/V1/AgencyUsersTargetResource.php
+++ app/Http/Resources/Api/V1/AgencyUsersTargetResource.php
@@ -68,6 +68,7 @@ class AgencyUsersTargetResource extends JsonResource
         //         $query->where(DB::raw('concat(year,"-", month)'), '=', $year . '-' . $month);
         //     })->sum(DB::raw('sallary - cut_amount'));
         $agencySallary = UserSallary::query()
+        ->where('user_id', $this->id)
         ->where('user_agency_id', $this->agency_id)
         ->where(function ($query) use ($year, $month) {
             $query->where(DB::raw('concat(year,"-", month)'), '=', $year . '-' . $month);
