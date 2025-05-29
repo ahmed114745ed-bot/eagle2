0e8544d99ce1f3b850ea3b90a99ff7497ea8b814
shami2024 <>
2025-05-29 17:31:02 +0300

    Update AgencyUsersTargetResource.php

--- app/Http/Resources/Api/V1/AgencyUsersTargetResource.php
+++ app/Http/Resources/Api/V1/AgencyUsersTargetResource.php
@@ -81,7 +81,7 @@ class AgencyUsersTargetResource extends JsonResource
             'uuid' => $this->uuid ?? '',
             'image' => $this->profile->avatar ?? '',
             'is_host' => $this->is_host,
-            'salary' => round((float) $agencySallary, 2),
+            'salary' => (float) $agencySallary, 2,
             'target' => [
                 'id' => @$target->target_id ?? 0,
                 'user_diamonds' => @$target->user_diamonds ?? 0,
