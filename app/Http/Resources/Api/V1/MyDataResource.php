e5eeb9b136f0d82efe0cdbf411e0ea67612b2be6
johnghattas <john.backend@utdsoftware.com>
2025-05-30 21:03:48 +0300

    Update MyDataResource.php

--- app/Http/Resources/Api/V1/MyDataResource.php
+++ app/Http/Resources/Api/V1/MyDataResource.php
@@ -216,7 +216,7 @@ class MyDataResource extends JsonResource
             'new_gift'          => (bool)$this->new_gift,
             'show_invite_code' => (bool)$this->userSetting?->show_invite_code ?? false,
             'wallet' => $this->wallet?->value ?? 0,
-            'user_types' => $userTypes ?? [],
+            'user_types' => $this->user_types,
             'wabble' => $this->getUserPack(12),
             'wabble_id' => $this->getUserPackId(12),
             "shipping-agency" => $this->shippingAgency ? [
