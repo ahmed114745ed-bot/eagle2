772312da68e8881e907f61a1c6bbf743f1e0e2b0
Erinyelkess <ariny.backend@utdsoftware.com>
2025-06-02 10:09:23 +0300

    edit permission

--- app/Admin/Controllers/AgencyController.php
+++ app/Admin/Controllers/AgencyController.php
@@ -451,16 +451,17 @@ class AgencyController extends MainController
                 </div>
             ";
         });
+        $permission = $this->permission_name;
 
-        $grid->actions(function ($actions) {
+        $grid->actions(function ($actions) use ($permission) {
             $model = $actions->row;
             // $actions->disableView(); // Disable the "View" action
             $actions->disableDelete();
-            if (Admin::user()->can('delete-switch-' . $this->permission_name) || Admin::user()->can('*')) {
+            if (Admin::user()->can('delete-switch-' . $permission) || Admin::user()->can('*')) {
 
                 $actions->add(new DeleteAgencyAction());
             }
-            if (Admin::user()->can('change-users-agency-switch-' .$this->permission_name) || Admin::user()->can('*')) {
+            if (Admin::user()->can('change-users-agency-switch-' . $permission) || Admin::user()->can('*')) {
 
                 $actions->add(new ChangeUsersAgencyAction($model->id));
             }
