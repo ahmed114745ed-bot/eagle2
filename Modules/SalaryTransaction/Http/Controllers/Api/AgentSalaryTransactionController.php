e86247d042b65421eea6722d7baa60c11172c23e
shami2024 <>
2025-06-03 13:08:05 +0300

    fix

--- Modules/SalaryTransaction/Http/Controllers/Api/AgentSalaryTransactionController.php
+++ Modules/SalaryTransaction/Http/Controllers/Api/AgentSalaryTransactionController.php
@@ -103,43 +103,36 @@ class AgentSalaryTransactionController extends Controller
         $agency = $usrAuth->shippingAgency;
         $search = $request->search;
         $type = $request->type;
-
+    
         if (!$agency) {
             return Common::apiResponse(0, __("api_responses.agency"));
         }
-
-        $query = Charge::where('is_used_transferred', false)
-            ->where('charger_type', 'agency')
-            ->where('charger_id', $agency->id);
-
-        if ($type === 'sent') {
-            $query->where('charger_id', $agency->id)->where('charger_type', 'agency');
-        } elseif ($type === 'received') {
-            $query->where(function ($q) use ($agency) {
-                $q->where('agency_id', $agency->id)->where('user_type', 'agency')
-                ->orWhere(function ($q2) use ($agency) {
-                    $q2->where('user_id', $agency->user_id)->where('user_type', 'user');
+    
+        $data = Charge::with('receiverAll', 'senderAll')
+            ->where('is_used_transferred', false);
+    
+        if ($type == 'sent') {
+            $data->where('charger_type', 'agency')
+                ->where('charger_id', $agency->id)
+                ->when($search, function ($q) use ($search) {
+                    $q->whereHas('receiver', function ($q2) use ($search) {
+                        $q2->where('uuid', 'like', "%{$search}%")
+                           ->orWhere('name', 'like', "%{$search}%");
+                    });
                 });
-            });
+        } elseif ($type == 'received') {
+            $data->where('agency_id', $agency->id)
+                ->when($search, function ($q) use ($search) {
+                    $q->whereHas('senderAll', function ($q2) use ($search) {
+                        $q2->where('uuid', 'like', "%{$search}%")
+                           ->orWhere('name', 'like', "%{$search}%");
+                    });
+                });
         }
-
-        $results = $query->orderByDesc('id')->get();
-
-        // فلترة بالبحث (بشكل يدوي بعد الاستعلام)
-        if ($search) {
-            $results = $results->filter(function ($item) use ($search, $type) {
-                if ($type === 'sent') {
-                    return $item->receiver && str_contains($item->receiver->uuid, $search);
-                } elseif ($type === 'received') {
-                    return $item->senderAll && str_contains($item->senderAll->uuid, $search);
-                }
-                return true;
-            })->values();
-        }
-
-        // تحويل إلى Collection جاهزة للـ Resource
-        $data = collect($results)->paginate(15); // أو استخدم LengthAwarePaginator لو أردت تقسيماً أدق
-
+    
+        $data = $data->orderByDesc('id')->paginate(15);
+    
+        dd($data);
         return Common::apiResponse(1, '', ChargeResourceforAgencyCharge::collection($data), 200);
     }
 
