e0b440c48d0f39fefc5666a30dd49e09ee3c8186
johnghattas <john.backend@utdsoftware.com>
2025-06-03 12:57:25 +0300

    Update GiftLogController.php

--- app/Http/Controllers/Api/V1/GiftLogController.php
+++ app/Http/Controllers/Api/V1/GiftLogController.php
@@ -267,7 +267,7 @@ class GiftLogController extends Controller
     public function gift_queue_cp(Request $request, UpdateUserWhenSendGift $updateUserWhenSendGift)
     {
 
-        Log::info(get_class($this) . ' - This is body ' . $request->json());
+        \Log::info(get_class($this) . ' - This is body ' . json_encode($request->all()));
         $close_open_gifts = settings()->get('close_open_gifts');
         if ($close_open_gifts == 1) {
             return Common::apiResponse(0, 'some thing wrong');
