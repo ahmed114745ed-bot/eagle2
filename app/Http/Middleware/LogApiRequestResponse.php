27acf42c17659e38bdad40f91ba9b0dec1f64f95
johnghattas <john.backend@utdsoftware.com>
2025-06-04 20:37:35 +0300

    Update LogApiRequestResponse.php

--- app/Http/Middleware/LogApiRequestResponse.php
+++ app/Http/Middleware/LogApiRequestResponse.php
@@ -21,7 +21,7 @@ class LogApiRequestResponse
         $user = \Auth::user();
 
         if ($user && $user->id === 2) {
-            \Log::channel('custom_log')->info('API Request by User ID 2 ', [
+            \Log::channel('custom_log')->info('API Request by User ID 2 '. PHP_EOL, [
                 'user_id' => $user->id,
                 'url' => $request->fullUrl(),
                 'method' => $request->method(),
