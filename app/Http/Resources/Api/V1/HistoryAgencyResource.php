03f0d0f98ac1c440055fc4f5902c5f4ba18e11f8
johnghattas <john.backend@utdsoftware.com>
2025-06-04 17:30:46 +0300

    Update HistoryAgencyResource.php

--- app/Http/Resources/Api/V1/HistoryAgencyResource.php
+++ app/Http/Resources/Api/V1/HistoryAgencyResource.php
@@ -44,7 +44,8 @@ class HistoryAgencyResource extends JsonResource
         $salary = 0;
         $target = 0;
 
-        if ( Auth::user()->id == $this->app_owner_id) {
+        $isOwner = Auth::user()->id == $this->app_owner_id;
+        if ($isOwner) {
             $salary = AgencySallary::where('agency_id', $this->id)
                 ->where('year', $year)
                 ->where('month', $month)
@@ -60,14 +61,12 @@ class HistoryAgencyResource extends JsonResource
 
         // UserSallary::join('targets', 'targets.id', '=', 'user_sallaries.target_id')->where('user_sallaries.user_agency_id', $this->id)->where('user_sallaries.year', $year)->where('user_sallaries.month', $month)->whereHas('target')->sum('targets.diamonds');
 
+
         return [
             'star' => ReceiverGiftLogResource::collection($giftLog),
             'heroes' => SenderGiftLogResource::collection($heroGiftLog),
-            $this->mergeWhen( Auth::user()->id  == $this->app_owner_id,
-            [
-                'salary' => $salary,
-                'target' => $target,
-            ])
+            'salary' => $isOwner ? $salary : 0,
+            'target' => $isOwner ? $target : 0,
             // $this->mergeWhen(request()->is_onwer_agency,
             //     [
             //         'salary' => @$salary ?? 0,
