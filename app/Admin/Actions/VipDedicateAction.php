2bd8dfa9c17f18b216678cb0e35a175f22a09e53
unknown <bolaphilip3@gmail.com>
2025-06-04 09:45:19 +0300

    vip dedicate error

--- app/Admin/Actions/VipDedicateAction.php
+++ app/Admin/Actions/VipDedicateAction.php
@@ -28,6 +28,7 @@ class VipDedicateAction extends Action
 
     public function handle(Request $request)
     {
+
         $user = User::query()->searchByUuid($request->user_uuid)->first();
         if (!$user) {
             return $this->response()->error(__('dashboard.userNotFound'))->refresh();
@@ -77,7 +78,7 @@ class VipDedicateAction extends Action
                 }
                 $userVip->save();
             }
-            Common::handelVip($vip, $user, expire: $request->days ?? 1, userVip: $userVip);
+            Common::handelVip($vip, $user, expire: $request->days ?? 1);
 
             DB::commit();
             CustomNotification::vips($user, $request->days, $vip->img);
