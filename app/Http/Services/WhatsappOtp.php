6069885fcb7e6e4c83b615a71b2074e1886f8c62
Erinyelkess <ariny.backend@utdsoftware.com>
2025-06-04 14:53:17 +0300

    edit

--- app/Http/Services/WhatsappOtp.php
+++ app/Http/Services/WhatsappOtp.php
@@ -61,8 +61,8 @@ class WhatsappOtp
         Log::info("Code matches: " . Code::query()->where('code', $code)->exists());
         Log::info("now hour: " . Carbon::now()->subHour()->toDateTimeString());
         Log::info("created at: " .  Code::query()->where('code', $code)->value('created_at'));
-      dd(Code::query()->where('phone', $phone)->where('code', $code)->where('created_at', '>', Carbon::now()->subHours()->toDateTimeString())->first());
-        return Code::query()->where('phone', $phone)->where('code', $code)->where('created_at', '>', Carbon::now()->subHours()->toDateTimeString())->exists();
+    //  dd(Code::query()->where('phone', $phone)->where('code', $code)->where('created_at', '>', Carbon::now()->subHours()->toDateTimeString())->first());
+        return Code::query()->where('phone', $phone)->where('code', $code)->where('created_at', '>', Carbon::now()->subHours())->exists();
     }
 
     public function resetCodes(string $phone)
