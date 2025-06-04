b15a9f8b4b18648bdfd191cc283daedef72878f5
shami2024 <>
2025-06-04 17:18:33 +0300

    Update AgencyService.php

--- app/Tik/Services/AgencyService.php
+++ app/Tik/Services/AgencyService.php
@@ -640,6 +640,8 @@ class AgencyService
 
     public function dailyReport($user, $month, $year ,$agencyId = null)
     {
+
+      
         $member = AgencyJoinRequest::where('user_id', $user->id)->where('status', 1)->first();
         $owner = Agency::where('app_owner_id', $user->id)->where('status', 1)->first();
         $joinedAgency = $member ??  $owner;
@@ -702,7 +704,9 @@ class AgencyService
         });
 
         $totalDays = $user->getTotalDaysJoinedAgency($joinedAgency->created_at);
-        $userInfoArray =  $user->getSallaryInfoByMonth($month, $year);
+     
+        $saMonth = ltrim($month, '0');
+        $userInfoArray =  $user->getSallaryInfoByMonth($saMonth, $year);
 
         $totalSalary = @$userInfoArray['total_salary'] ?? 0;
         $totalCutAmount = @$userInfoArray['total_cut_amount'] ?? 0;
