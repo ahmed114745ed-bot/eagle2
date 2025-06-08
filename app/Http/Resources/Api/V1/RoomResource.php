8ce031135f77a2dd2ad0839c1c8771190f7d675c
unknown <bolaphilip3@gmail.com>
2025-06-08 13:09:32 +0300

    room refactor

--- app/Http/Resources/Api/V1/RoomResource.php
+++ app/Http/Resources/Api/V1/RoomResource.php
@@ -66,8 +66,8 @@ class RoomResource extends JsonResource
             'have_luck_box' => (bool) $have_luck_box,
             'achievement_images' => $achievement_images,
             /** refactored */
-            'medals'               =>  @$this->owner?->enabledMedals ?? [],
-//            'medals'               => @$this->owner?->medals()?->where('is_enable', true)->get() ?? [],
+//            'medals'               =>  @$this->owner?->enabledMedals ?? [],
+            'medals'               => @$this->owner?->medals()?->where('is_enable', true)->get() ?? [],
             $this->mergeWhen($this->distance, [
                 'distance' => $this->distance,
             ]),
