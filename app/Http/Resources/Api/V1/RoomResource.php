a84008f4a2bbe3d44bc97c76fdc333bb8ce0cae8
unknown <bolaphilip3@gmail.com>
2025-06-08 14:00:07 +0300

    room refactor

--- app/Http/Resources/Api/V1/RoomResource.php
+++ app/Http/Resources/Api/V1/RoomResource.php
@@ -29,13 +29,13 @@ class RoomResource extends JsonResource
         $data = [
             'id' => $this->id,
             'owner_id' => $this->uid ?: 0,
-//            'owner_uuid' => $this->owner?->uuid ?: 0,
-            'owner_uuid' => $this->owner?->uuid_v2 ?: 0,
+            'owner_uuid' => $this->owner?->uuid ?: 0,
+//            'owner_uuid' => $this->owner?->uuid_v2 ?: 0,
             'room_id' => (string)($this->id ?: 0),
             'name' => $this->room_name ?: '',
             "mode" => $this->mode,
-//            'visitors_count' => $this->count_room_socket,
-            'visitors_count' => $this->count_room_socket_v2,
+            'visitors_count' => $this->count_room_socket,
+//            'visitors_count' => $this->count_room_socket_v2,
             'cover' => $this->room_cover ?: '',
             'class' => $this->myClass ?: new \stdClass(),
             'type' => $this->myType ?: new \stdClass(),
@@ -66,8 +66,8 @@ class RoomResource extends JsonResource
             'have_luck_box' => (bool) $have_luck_box,
             'achievement_images' => $achievement_images,
             /** refactored */
-            'medals'               =>  @$this->owner?->enabledMedals ?? [],
-//            'medals'               => @$this->owner?->medals()?->where('is_enable', true)->get() ?? [],
+//            'medals'               =>  @$this->owner?->enabledMedals ?? [],
+            'medals'               => @$this->owner?->medals()?->where('is_enable', true)->get() ?? [],
             $this->mergeWhen($this->distance, [
                 'distance' => $this->distance,
             ]),
