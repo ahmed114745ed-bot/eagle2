f5f884824307d696b43c7bb665d964f35af13997
unknown <bolaphilip3@gmail.com>
2025-06-08 14:46:06 +0300

    seeder remove duplicate keys in settings

--- database/seeders/PaymentGatewaysSeeder.php
+++ database/seeders/PaymentGatewaysSeeder.php
@@ -49,15 +49,15 @@ class PaymentGatewaysSeeder extends Seeder
         }
 
 
-        $duplicates = Setting::select('key', 'item_id', 'type')
-            ->groupBy('key', 'item_id', 'type')
+        $duplicates = Setting::select('key')
+            ->groupBy('key')
             ->havingRaw('COUNT(*) > 1')
             ->get();
 
         foreach ($duplicates as $dup) {
             $settings = Setting::where('key', $dup->key)
-                ->where('item_id', $dup->item_id)
-                ->where('type', $dup->type)
+//                ->where('item_id', $dup->item_id)
+//                ->where('type', $dup->type)
                 ->orderBy('id')
                 ->get();
 
