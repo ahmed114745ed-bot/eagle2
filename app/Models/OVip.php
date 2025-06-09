9eff9f1e493653be6d762a76fc3e74f617dc73ae
unknown <bolaphilip3@gmail.com>
2025-06-09 14:53:10 +0300

    ware tabs

--- app/Models/OVip.php
+++ app/Models/OVip.php
@@ -26,6 +26,8 @@ class OVip extends Model
 
         static::deleting(function ($oVip){
             $oVip->privilegs()->detach();
+
+            $oVip->wares()->forceDelete();
         });
     }
 
@@ -73,6 +75,11 @@ class OVip extends Model
         return $this->hasOne(Ware::class, 'level', 'level')
             ->where('type', 12)
             ->where('get_type', 1);
+    }
+
+    public function wares()
+    {
+        return $this->hasMany(Ware::class, 'level', 'level');
     }
 
     function getTranslation($key) {
