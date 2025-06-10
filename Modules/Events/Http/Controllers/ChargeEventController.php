955bbe303853c94f015435a74b0c3f5142b80812
shami2024 <>
2025-06-10 13:02:58 +0300

    target

--- Modules/Events/Http/Controllers/ChargeEventController.php
+++ Modules/Events/Http/Controllers/ChargeEventController.php
@@ -104,16 +104,16 @@ class ChargeEventController extends Controller
             return Common::apiResponse(0, 'لا يوجد تارجيت');
         }
         $total = $charges_sum_amount + $coin_logs_sum_obtained_coins;
-      //  dd($total ,$target->value);
+         //  dd($total ,$target->value);
         // $percentage = ((($charges_sum_amount + $coin_logs_sum_obtained_coins) * 100 ) /$target->value);
         $checkChargeEvent = UserChargeEvent::query()->where(["user_id" => $user->id, 'charge_event_id' => $request->target_id])->first();
           
         if ($checkChargeEvent) {
-            return Common::apiResponse(0, __('api_responses.dont_have_charge_target'));
+            return Common::apiResponse(0, __('you_have_charge_target'));
         }
 
         if ($total < $target->value) {
-            return Common::apiResponse(0, __('api_responses.dont_achieve_taregt'));
+              return Common::apiResponse(0, __('api_responses.dont_achieve_taregt'));
         }
 
         UserChargeEvent::create([
