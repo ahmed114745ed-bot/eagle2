f61e3607b13b1de883ce3fabc59a6d53a535288b
shami2024 <>
2025-06-10 15:55:51 +0300

    Update WeeklyEventGiftNController.php

--- Modules/Events/Http/Controllers/web/WeeklyEventGiftNController.php
+++ Modules/Events/Http/Controllers/web/WeeklyEventGiftNController.php
@@ -88,36 +88,38 @@ class WeeklyEventGiftNController extends MainController
     //         ->body($this->detail($id)));
     // }
 
-     public function show($id, Content $content)
-    {   $type = request('id');
-    
+    public function show( $id, Content $content)
+    {
+        $type = $id; // Using the second parameter as type
+        
         $content = $content
             ->header(trans('admin.index'))
             ->description(trans('admin.description'))
             ->breadcrumb(['text' => trans('admin.eventGift')]);
         
-        // Add grids based on type
+        // Create a row to hold our grid
+        $row = new \Encore\Admin\Layout\Row();
+        
         switch ($type) {
             case 1:
-                $content = $content->body($this->grid1());
+                $row->column(12, $this->grid1());
                 break;
             case 2:
-                $content = $content->body($this->grid2());
+                $row->column(12, $this->grid2());
                 break;
             case 3:
-                $content = $content->body($this->grid3());
+                $row->column(12, $this->grid3());
                 break;
             default:
-                // Default case - show all grids or handle differently
-                $content = $content->body(
-                    $this->grid1()
-                        ->row($this->grid2())
-                        ->row($this->grid3())
-                );
+                // Show all grids stacked vertically
+                $row->column(12, $this->grid1());
+                $row->column(12, $this->grid2());
+                $row->column(12, $this->grid3());
         }
         
-        return  $content;
+        return $content->body($row);
     }
+    
     protected function grid1()
     {
         $type = 1;
