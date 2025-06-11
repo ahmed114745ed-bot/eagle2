7cad346b4181d86e30d880e514d84bfd5a0c41a9
unknown <bolaphilip3@gmail.com>
2025-06-11 12:45:26 +0300

    paypal

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,6 +31,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
 //        CoreWalletsSeeder::class,
             PaymentGatewaysSeeder::class,
+
 //        AdminPermission::class,
 //        AdminNewPermission::class,
 //        AdminRoleBDSeeder::class,
