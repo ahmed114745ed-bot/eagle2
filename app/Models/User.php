0771877298952d52e0bdcc1b0836e2af145bbf05
shami2024 <>
2025-06-11 15:53:11 +0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -1471,15 +1471,18 @@ class User extends Authenticatable
     }
     public function lastSallary()
     {
-        $join = $this->latestJoin;
-        
-        if (!$join) return null;
-        $start = $join->join_date;
-        $end = $join->leave_date ?? now()->endOfMonth();
-      
         return $this->hasOne(UserSallary::class, 'user_id')
-            ->where('user_agency_id', $this->agency_id)
-            ->whereBetween('created_at', [$start, $end])
-            ->latestOfMany();
+        ->where(function ($query) {
+            $join = $this->latestJoin;
+            if ($join) {
+                $start = $join->join_date;
+                $end = $join->leave_date ?? now()->endOfMonth();
+                $query->where('user_agency_id', $this->agency_id)
+                      ->whereBetween('created_at', [$start, $end]);
+            } else {
+                $query->whereRaw('1 = 0');
+            }
+        })
+        ->latestOfMany();
     }
 }
