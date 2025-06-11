1e0f13aba46ec78226f6ed14b7bfc4f2f8d87980
shami2024 <>
2025-06-11 16:32:36 +0300

    Update AgencyUsersTargetResource.php

--- app/Http/Resources/Api/V1/AgencyUsersTargetResource.php
+++ app/Http/Resources/Api/V1/AgencyUsersTargetResource.php
@@ -166,11 +166,11 @@ class AgencyUsersTargetResource extends JsonResource
             'is_host' => $this->is_host,
             'salary' => (float) $agencySallary ?? 0,
             'target' => [
-                'id' => @$target->target_id ?? 0,
-                'user_diamonds' => (float) ($this->lastSallary?->achieved_diamond ?? 0),
-                'user_hours'    =>  $this->lastSallary?->achieved_hours ?? 0,
-                'user_days'     =>  $this->lastSallary?->achieved_days ?? 0,
-                'old_targets'  => $this->latestOldTarget(),
+                // 'id' => @$target->target_id ?? 0,
+                // 'user_diamonds' => (float) ($this->lastSallary?->achieved_diamond ?? 0),
+                // 'user_hours'    =>  $this->lastSallary?->achieved_hours ?? 0,
+                // 'user_days'     =>  $this->lastSallary?->achieved_days ?? 0,
+                // 'old_targets'  => $this->latestOldTarget(),
             ],
             // 'top_users' => SenderGiftLogResource::collection($giftLog),
            'top_users' => $giftLog->map(function ($log) {
