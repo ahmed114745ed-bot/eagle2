b280e7e6fb1024f5615fa50480677caf2a31a571
shami2024 <>
2025-06-12 12:36:42 +0300

    Update EnteranceController.php

--- app/Http/Controllers/Api/V1/Room/EnteranceController.php
+++ app/Http/Controllers/Api/V1/Room/EnteranceController.php
@@ -248,18 +248,20 @@ class EnteranceController extends Controller
         $room_pass = $request['room_pass'];
         // $owner_id = $request['owner_id'];
         $user = $request->user();
-        $owner_id = $request->input('owner_id');
         $room_id = $request->input('room_id');
-        if (!$owner_id && !$room_id) {
+        $owner_id = $request->input('owner_id');
+    
+        if (!$room_id && !$owner_id) {
             return Common::apiResponse(0, __('Please provide either owner_id or room_id.'));
         }
-        if (!$owner_id && $room_id) {
+        if ($room_id) {
             $room = Room::find($room_id);
             if (!$room) {
                 return Common::apiResponse(0, __('This room was not found.'));
             }
-            $owner_id = $room->uid;
+            $owner_id = $room->uid; 
         }
+    
 
         $ban = Common::ifRoomHasband($owner_id);
         if ($ban) {
