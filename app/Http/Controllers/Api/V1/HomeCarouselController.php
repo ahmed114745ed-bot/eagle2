e19ac8fb99e99819cd13c2f0e253325d193f3c55
shami2024 <>
2025-06-12 13:12:46 +0300

    Update HomeCarouselController.php

--- app/Http/Controllers/Api/V1/HomeCarouselController.php
+++ app/Http/Controllers/Api/V1/HomeCarouselController.php
@@ -14,13 +14,13 @@ class HomeCarouselController extends Controller
     public function index(Request $request)
     {
         $user =Auth::user();
-        // logger('Headers:', $request->headers->all());
-
-        // logger('Request Data:', $request->all());
-        if ($request->hasHeader('x-notification-id"')) {
-            $notificationId = $request->header('x-notification-id"');
-            $user->notification_id = $notificationId; 
-            $user->save();  
+        logger('Headers:', $request->headers->all());
+        logger('Request Data:', $request->all());
+    
+        if ($request->hasHeader('x-notification-id')) {
+            $notificationId = $request->header('x-notification-id');
+            $user->notification_id = $notificationId;
+            $user->save();
             return response()->json(['success' => true, 'message' => 'Notification ID updated successfully']);
         }
 
