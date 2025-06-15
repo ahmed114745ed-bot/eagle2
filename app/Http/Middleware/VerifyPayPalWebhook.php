276b6f80d35df4e6bd414ebe751f5b0f1347e020
unknown <bolaphilip3@gmail.com>
2025-06-15 13:34:01 +0300

    paypal logs

--- app/Http/Middleware/VerifyPayPalWebhook.php
+++ app/Http/Middleware/VerifyPayPalWebhook.php
@@ -18,7 +18,6 @@ class VerifyPayPalWebhook extends PayPalService
     public function handle(Request $request, Closure $next): Response
     {
         $headers = $request->headers;
-        info($headers);
 
         $verificationData = [
             'auth_algo'         => $headers->get('paypal-auth-algo'),
@@ -35,7 +34,7 @@ class VerifyPayPalWebhook extends PayPalService
         $response = Http::withToken($accessToken)
             ->post(config('paypal.base_url') . '/v1/notifications/verify-webhook-signature', $verificationData);
 
-
+        info($response);
         if ($response->json('verification_status') !== 'SUCCESS') {
             return response()->json(['status' => 'unauthorized'], 401);
         }
