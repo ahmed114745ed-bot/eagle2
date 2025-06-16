6f2c9cf0deacf5297b45b8192bf175997e71762f
unknown <bolaphilip3@gmail.com>
2025-06-16 12:49:56 +0300

    test stripe

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -85,7 +85,7 @@ class StripeController extends Controller
         // Your Stripe webhook secret, which you get from the Stripe dashboard
         $endpointSecret = $stripe_webhook_secret?->value; // Set this in your .env file
         Log::info('strip callback called '. $apiKey . ' '. $stripe_webhook_secret);
-        try {
+//        try {
             // Verify the webhook signature to ensure it's coming from Stripe
             $event = Webhook::constructEvent($payload, $sigHeader, $endpointSecret);
             info($event->type);
@@ -98,6 +98,7 @@ class StripeController extends Controller
 
                     $userId = $session->metadata->user_id;
                     $orderId = $session->metadata->order_id;
+                    info($orderId);
 
                     $this->makePayment($orderId, $userId);
                     info('completed');
@@ -133,15 +134,15 @@ class StripeController extends Controller
             info('Webhook Handled 200');
             // Return a 200 response to Stripe to acknowledge the webhook
             return response('Webhook Handled', 200);
-        } catch (SignatureVerificationException $e) {
-            // Invalid signature from Stripe
-            \Log::error("Invalid webhook signature: {$e->getMessage()}");
-            return response('Invalid Signature', 400);
-        } catch (\Exception $e) {
-            // General error handling
-            \Log::error("Webhook error: {$e->getMessage()}");
-            return response('Webhook Error: ' . $e->getMessage(), 500);
-        }
+//        } catch (SignatureVerificationException $e) {
+//            // Invalid signature from Stripe
+//            \Log::error("Invalid webhook signature: {$e->getMessage()}");
+//            return response('Invalid Signature', 400);
+//        } catch (\Exception $e) {
+//            // General error handling
+//            \Log::error("Webhook error: {$e->getMessage()}");
+//            return response('Webhook Error: ' . $e->getMessage(), 500);
+//        }
     }
 
     public function makePayment($orderId, int|string|null $userId)
