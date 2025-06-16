8bcf5e8875b632e74120c358bd3ad49e4604b78e
Erinyelkess <ariny.backend@utdsoftware.com>
2025-06-16 10:29:08 +0300

    edit

--- app/Models/AdminUser.php
+++ app/Models/AdminUser.php
@@ -20,6 +20,6 @@ class AdminUser extends Administrator
 
     public function managerAgencies(): HasManyThrough
     {
-        return $this->hasManyThrough(Agency::class, User::class, 'id', 'agency_manger_id', 'app_id', 'id')->where('agencies.type', 1);
+        return $this->hasManyThrough(Agency::class, User::class, 'id', 'agency_manger_id', 'app_id', 'id');
     }
 }
