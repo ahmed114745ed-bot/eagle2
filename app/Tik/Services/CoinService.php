0595f54f1efd82a787c3790ad7e2c6176bc81794
unknown <bolaphilip3@gmail.com>
2025-06-16 13:42:09 +0300

    test stripe

--- app/Tik/Services/CoinService.php
+++ app/Tik/Services/CoinService.php
@@ -56,7 +56,9 @@ class CoinService
             $data = [
                 'name' => $coin->coin . '_coins',
                 'amount' => $coin->usd,
-                'trx' => $log->trx
+                'trx' => $log->trx,
+                'order_id' => $log->id,
+                'user_id' => $user->id
             ];
             if ($request->pay_method == 'strip') {
                 $stripe_test_secret_key = Setting::where('key', 'stripe_test_secret_key')->first();
