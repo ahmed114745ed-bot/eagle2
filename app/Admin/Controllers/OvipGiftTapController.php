30b918b8e49aa6f57a63f7d8d90df3c30f1319bf
unknown <bolaphilip3@gmail.com>
2025-06-17 11:42:16 +0300

    remove image test

--- app/Admin/Controllers/OvipGiftTapController.php
+++ app/Admin/Controllers/OvipGiftTapController.php
@@ -267,11 +267,11 @@ class OvipGiftTapController extends MainController
 
             $form->image('show_img', trans('img'))->name(function ($file) {
                 return now()->timestamp . rand(0, 999) . '.' . $file->guessExtension();
-            })->default('1.png');
+            })->default('1.png')->removable();
             $form->file('img2', trans('svg'))
                 ->name(function ($file) {
                     return 'svga_' . Str::random(6) . '.' . $file->getClientOriginalExtension();
-                });
+                })->removable();
 
             $form->keyValue('key_json', 'key_json');
 
@@ -315,24 +315,7 @@ class OvipGiftTapController extends MainController
                 }
 
                 if ($form->show_img instanceof UploadedFile) {
-                    $allowedExtensions = [
-                        'svga',
-                        'mp4',
-                        'jpg',
-                        'jpeg',
-                        'png',
-                        'gif',
-                        'bmp',
-                        'tiff',
-                        'svg',
-                        'webp',
-                        'mov',
-                        'avi',
-                        'wmv',
-                        'flv',
-                        'mkv',
-                        'webm',
-                    ];
+                    $allowedExtensions = ['svga', 'mp4', 'jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff', 'svg', 'webp', 'mov', 'avi', 'wmv', 'flv', 'mkv', 'webm',];
 
                     $ext = strtolower($form->show_img->guessExtension());
 
