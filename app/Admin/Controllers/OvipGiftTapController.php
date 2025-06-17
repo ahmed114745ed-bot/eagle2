92443bb11d975519e4bac6c35ec39508d130aed3
Erinyelkess <ariny.backend@utdsoftware.com>
2025-06-17 15:53:43 +0300

    edit

--- app/Admin/Controllers/OvipGiftTapController.php
+++ app/Admin/Controllers/OvipGiftTapController.php
@@ -129,7 +129,7 @@ class OvipGiftTapController extends MainController
         $grid->model()->where('level', $level)->where('get_type', 1)->where('type', $type)->where('is_active_for_vip', 1);
 
         $grid->id(__('ID'));
-        if ($this->type == 18 || $this->type == 21) {
+        if ( $type == 18 ||  $type == 21) {
             $grid->column('color', __('Color'))->display(function ($color) {
                 return "<div style='width: 30px; height: 30px; background-color: {$color}; border: 1px solid #ccc; border-radius: 4px;'></div>";
             });
