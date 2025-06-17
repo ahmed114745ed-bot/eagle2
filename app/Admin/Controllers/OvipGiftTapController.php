9d404cc370ad47bdf0fef9cfc401d484d8cba565
Erinyelkess <ariny.backend@utdsoftware.com>
2025-06-17 14:56:57 +0300

    edit

--- app/Admin/Controllers/OvipGiftTapController.php
+++ app/Admin/Controllers/OvipGiftTapController.php
@@ -304,8 +304,8 @@ class OvipGiftTapController extends MainController
         if ((request('type') && (request('type') != 18 && request('type') != 21)) || ($form->isEditing() && $ware && ($ware->type != 18 && $ware->type != 21))) {
 
             $form->saving(function (Form $form) {
-             dd($form->model()->show_img,$form->model()->img2);
-                if (!($form->model()->show_img instanceof UploadedFile )&&! ($form->model()->img2 instanceof UploadedFile)) {
+
+                if (!($form->model()->show_img) && ! ($form->model()->img2)) {
                     $error = new MessageBag([
                         'title'   => 'Error',
                         'message' => 'Please upload at least one image',
