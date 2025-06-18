219acc12502259d8ad0522de28b3166639d3a3c3
unknown <bolaphilip3@gmail.com>
2025-06-18 15:37:32 +0300

    payment Link

--- app/Services/PayPalService.php
+++ app/Services/PayPalService.php
@@ -132,6 +132,7 @@ class PayPalService
     public function callback(Request $request): JsonResponse
     {
         $eventType = $request->get('event_type');
+        info($eventType);
         if ($eventType !== 'CHECKOUT.ORDER.APPROVED') {
             return response()->json(['status' => 'ignored', 'reason' => 'Event type not processed']);
         }
