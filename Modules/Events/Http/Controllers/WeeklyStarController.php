515718adc8c9ecfac2c9e663b26a2b60396b6b0d
Erinyelkess <ariny.backend@utdsoftware.com>
2025-06-18 10:51:26 +0300

    edit

--- Modules/Events/Http/Controllers/WeeklyStarController.php
+++ Modules/Events/Http/Controllers/WeeklyStarController.php
@@ -118,6 +118,7 @@ class WeeklyStarController extends Controller
         $nowDate = Carbon::now();
         $weeklyEvent = WeeklyStar::currentEvent()->weeklyStar()->with(['rewards'])
                                  ->first();
+                                 dd($weeklyEvent);
 
         if (!$weeklyEvent) {
             return Common::apiResponse(0, __('there is no weekly star now'), null, 422);
