4e047019c2fbd5ea27abf68acb03e2ed77328c3c
johnghattas <john.backend@utdsoftware.com>
2025-06-20 00:54:34 +0300

    update profile frame relation

--- app/Http/Resources/Api/V1/MyDataResource.php
+++ app/Http/Resources/Api/V1/MyDataResource.php
@@ -272,8 +272,8 @@ class MyDataResource extends JsonResource
             $this->mergeWhen($request->show_counter == true, [
                 'unread_counter'       =>  $counters,
             ]),
-            'profile_frame' => $this->packs->where('type', 28)->where('is_used', 1)->first()?->img2 ??'' /*common::wareUserVip($this->id, 28, 'img2')*/,
-            'profile_frame_id' => $this->packs->where('type', 28)->where('is_used', 1)->first()?->id ?? '' ,
+            'profile_frame' => $this->packs->where('type', 28)->where('is_used', 1)->first()?->ware->img2 ??'' /*common::wareUserVip($this->id, 28, 'img2')*/,
+            'profile_frame_id' => $this->packs->where('type', 28)->where('is_used', 1)->first()?->ware->id ?? '' ,
             'company_number' => Common::getConfig('company_number'),
             'special_id'          =>  @$this->specialId?->ware?->id ?? 0,
             'special_id_image'          =>  @$this->specialId?->ware?->show_img ?? "",
--- app/Repositories/User/UserRepository.php
+++ app/Repositories/User/UserRepository.php
@@ -184,7 +184,7 @@ class UserRepository extends Repository
             'ownAgency',
             'agencyUserJob' => fn($q) => $q->where('type', 'requestManger'),
             'agencyJoinRequest' => fn($q) => $q->where('status', '!=', 2),
-            'packs',
+            'packs.ware',
             'country',
             'manager'
         ])
