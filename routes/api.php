e997b9a74c21d3f49ab8497052079c7903348d02
shami2024 <>
2025-06-19 10:59:18 +0300

    use-pack

--- routes/api.php
+++ routes/api.php
@@ -458,6 +458,7 @@ Route::prefix(config('app.api_prefix'))->group(function () {
                 Route::post('/buyVip', [VipController::class, 'buyVip']);
                 Route::post('/buy-vip-percentage', [ControllersMallController::class, 'buyVip']);
                 Route::post('/use', [VipController::class, 'vip_use']);
+                Route::post('/use-pack', [VipController::class, 'pack_use']);
                 Route::post('/send-to-user', [VipController::class, 'vip_send']);
             });
             Route::get('levels/badges', [VipController::class, 'badges']);
