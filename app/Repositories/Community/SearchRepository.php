808bbb9d4cdf26c8a38924ad44323a733fc1b7c2
shami2024 <>
2025-06-22 16:48:57 +0300

    Update SearchRepository.php

--- app/Repositories/Community/SearchRepository.php
+++ app/Repositories/Community/SearchRepository.php
@@ -37,15 +37,34 @@ class SearchRepository implements SearchRepositoryInterface
 
         $blockedUserIds = array_unique(array_merge($blockedByMe, $blockedMe));
 
-        $user = User::likeSearchByUuid($keywords)
-            ->with(['packs' => function ($q) {
-                $q->where('type', 16)
-                    ->where('is_used', 1)
-                    ->where(function ($q) {
-                        $q->where('expire', 0)
+        // $user = User::likeSearchByUuid($keywords)
+        //     ->with(['packs' => function ($q) {
+        //         $q->where('type', 16)
+        //             ->where('is_used', 1)
+        //             ->where(function ($q) {
+        //                 $q->where('expire', 0)
+        //                 ->orWhere('expire', '>=', now()->timestamp);
+        //             });
+        //     }])->first();
+        $user = User::query()
+        ->where(function ($q) use ($keywords) {
+            $q->where('uuid', 'like', "%{$keywords}%")
+            ->orWhere('special_id', 'like', "%{$keywords}%");
+        })
+        ->with(['packs' => function ($q) {
+            $q->where('type', 16)
+                ->where('is_used', 1)
+                ->where(function ($q) {
+                    $q->where('expire', 0)
                         ->orWhere('expire', '>=', now()->timestamp);
-                    });
-            }])->first();
+                });
+        }])
+        ->addSelect([
+            '*',
+            DB::raw("((LENGTH(uuid) - LENGTH(REPLACE(uuid, '{$keywords}', ''))) / CHAR_LENGTH(uuid)) * 100 AS matching_percentage")
+        ])
+        ->orderByDesc('matching_percentage')
+        ->first();
       
         if (!$user || $user?->packs->isNotEmpty()) {
             return [];
