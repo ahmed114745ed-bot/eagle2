95fff68dd55f4982a77cf53f2803eb8ea048870f
shami2024 <>
2025-06-23 13:19:23 +0300

    Update AdminControllerServiceProvider.php

--- app/Admin/AdminControllerServiceProvider.php
+++ app/Admin/AdminControllerServiceProvider.php
@@ -9,6 +9,8 @@ class AdminControllerServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        \URL::forceScheme('https');
+
         if (!$this->app->routesAreCached()) {
             $this->mapBdRoutes();
         }
