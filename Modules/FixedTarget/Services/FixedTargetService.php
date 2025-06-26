1bac817f5e0015138354512831180e9137ca1c27
shami2024 <>
2025-06-26 15:07:46 +0300

    Update FixedTargetService.php

--- Modules/FixedTarget/Services/FixedTargetService.php
+++ Modules/FixedTarget/Services/FixedTargetService.php
@@ -270,7 +270,8 @@ class FixedTargetService
         if ($user->agency_id != 0 && @$user->type_user != 3) {
             $target = $this->targetInstance->getTarget($month_received);
             
-\Log::inf('$target',['$target'=>$target]);
+            \Log::inf('$target',['$target'=>$target]);
+            \Log::inf('$this->joinDate',['$this->joinDate'=>$this->joinDate]);
             if ($target) {
                 $hours = 0;
                 $days  = 0;
