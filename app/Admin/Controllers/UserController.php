0c61cd5216a429b0d7b90fa8d5583728da02f568
Erinyelkess <ariny.backend@utdsoftware.com>
2025-06-26 12:17:08 +0300

    test

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -815,6 +815,7 @@ class UserController extends MainController
 
     public function show($id, Content $content,)
     {
+        dd(123);
         $month = request('month'); // e.g., "5" for May
         $year = request('year');
         $start = request('start_at');
