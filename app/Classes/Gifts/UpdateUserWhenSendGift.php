36f7fd53158663d97070a92f61b547a3fd771c53
shami2024 <>
2025-06-26 15:36:01 +0300

    Update UpdateUserWhenSendGift.php

--- app/Classes/Gifts/UpdateUserWhenSendGift.php
+++ app/Classes/Gifts/UpdateUserWhenSendGift.php
@@ -65,8 +65,13 @@ class UpdateUserWhenSendGift
 
         $values['received_level'] = $receivedUser->received_level;
         \Log::info('receivedUser',['receivedUser->id' =>$receivedUser->id]);
-        \Log::info('values',['values' =>$values]);
-
+        $logValues = [
+            'salary_is_updated' => 1,
+            'monthly_diamond_received' => 'monthly_diamond_received + ' . $totalCoins,
+            'total_diamond_received' => 'total_diamond_received + ' . $totalCoins,
+        ];
+        
+        \Log::info('values', ['values' => $logValues]);
         User::where('id', $receivedUser->id)->lockForUpdate()
             ->update($values);
         $receivedUser->save();
