f7d210e645667c4dde762cebc95d1a363b9bdbe0
Erinyelkess <ariny.backend@utdsoftware.com>
2025-06-26 12:23:34 +0300

    edit

--- resources/views/css/dynamic-style.blade.php
+++ resources/views/css/dynamic-style.blade.php
@@ -530,8 +530,8 @@
 }
 
 .user-type-badges img {
-    width: 40px;
-    height: 40px;
+    width: 60px;
+    height: 60px;
     object-fit: cover;
     border-radius: 4px;
     transition: all 0.2s ease;
