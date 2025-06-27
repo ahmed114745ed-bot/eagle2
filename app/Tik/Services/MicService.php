cd1b359bb93868bca3624c566477a85d5b98aac3
makled98 <mkled.backend@utdsoftware.com>
2025-06-27 23:18:41 +0300

    Update MicService.php

--- app/Tik/Services/MicService.php
+++ app/Tik/Services/MicService.php
@@ -355,7 +355,7 @@ class MicService
         $position = $data['position'];
         $room = $this->roomRepository->findRoomUser($data['owner_id']);
         if (!$room) throw new Exception(__('room fot found'));
-        if ($user->id != $room->uid || !in_array($user->id, $room->admins ?? [])) {
+        if ($user->id != $room->uid && !in_array($user->id, $room->admins ?? [])) {
             throw new Exception(__('you do not have permission'));
         }
 
