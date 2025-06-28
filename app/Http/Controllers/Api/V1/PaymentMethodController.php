ff70fdf4e08a507705233019e716fcace2bfa02d
johnghattas <john.backend@utdsoftware.com>
2025-06-28 17:28:51 +0300

    Update PaymentMethodController.php

--- app/Http/Controllers/Api/V1/PaymentMethodController.php
+++ app/Http/Controllers/Api/V1/PaymentMethodController.php
@@ -134,7 +134,11 @@ class PaymentMethodController extends Controller
                     'message' => 'Transaction not found.',
                 ]);
             }
-            \Log::info('this response '.json_encode($query));
+            \Log::info('this response '.json_encode([
+                    'status' => $query['statusCode'] == 200,
+                    'trx' => $purchaseProduct->trx,
+                    'message' => $query['statusDescription'] ?? 'No description provided.',
+                ]));
             return response()->json([
                 'status' => $query['statusCode'] == 200,
                 'trx' => $purchaseProduct->trx,
