98823da8f5d414b15d27456aad7ebf9cbc724c7b
Erinyelkess <ariny.backend@utdsoftware.com>
2025-06-29 11:36:07 +0300

    edit

--- app/Http/Controllers/Api/V1/Room/EnteranceController.php
+++ app/Http/Controllers/Api/V1/Room/EnteranceController.php
@@ -555,7 +555,6 @@ class EnteranceController extends Controller
             if ($room->room_pass != null) {
                 $is_locked = true;
             }
-            dd($background_me ? @$background_me : $room->final_room_image);
             $data = [
                 "messageContent" => [
                     "message" => "changeBackground",
