d8fdd5b110ab0354de678132bb2f173a3e910ba3
shami2024 <>
2025-06-29 13:07:08 +0300

    logs

--- app/Tik/Services/EnteranceRoomServices.php
+++ app/Tik/Services/EnteranceRoomServices.php
@@ -140,7 +140,7 @@ class EnteranceRoomServices
     //////////////////////////////////////////////////////room visitors//////////////////////////////////////////
     public function updateRoomCountFromZego(Request $request)
     {
-        Log::info('req',[ 'key' =>$request->all()]);
+        Log::info('Zego req',[ 'key' =>$request->all()]);
 
         //        $app_secert='a23b121a64ee9fab4567a2d75d00269d';
         //        if (!$this->checkSignature($app_secert,$request->signature, $request->timestamp, $request->nonce)) {
