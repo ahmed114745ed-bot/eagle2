19ddaf62e1370e81a7f9a019e0e9b9b9a7663c6a
shami2024 <>
2025-07-01 15:39:52 +0300

    rm Router

--- app/Bd/routes.php
+++ app/Bd/routes.php
@@ -1,10 +1,7 @@
 <?php
 
-use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
 use App\Bd\Controllers\HomeController;
-
-
 use App\Bd\Controllers\WalletController;
 // use App\Bd\Controllers\RequestAgencyController;
 // use App\Bd\Controllers\ChargeController;
