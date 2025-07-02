fa0fc7226fc50d88f26b58031ceb77f0ec61dcf6
Erinyelkess <ariny.backend@utdsoftware.com>
2025-07-02 15:50:29 +0300

    change agency

--- app/Admin/Actions/ChangeUsersAgencyAction.php
+++ app/Admin/Actions/ChangeUsersAgencyAction.php
@@ -61,7 +61,7 @@ class ChangeUsersAgencyAction extends RowAction
             if (!$checkAgencyUser) {
                 UsersJoinedAgency::create([
                     'user_id' => $user->id,
-                    'agency_id' => $request->old_agency_id,
+                    'agency_id' =>$request->new_agency_id,
                     'type' => 2,
                     'join_date' => now(),
                     'status' =>'Joined'
