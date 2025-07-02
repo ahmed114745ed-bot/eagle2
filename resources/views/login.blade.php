f7688d77e6b14e609c592779c76b80dce3d4c26a
johnghattas <john.backend@utdsoftware.com>
2025-07-02 20:00:35 +0300

    Update login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -108,6 +108,12 @@
                 location.reload();
             });
         });
+
+        // Prevent double submission
+        $('form').on('submit', function () {
+            var $btn = $(this).find('button[type="submit"]');
+            $btn.prop('disabled', true).text('{{ trans('admin.logging_in') ?? 'Logging in...' }}');
+        });
     });
 </script>
 </body>
