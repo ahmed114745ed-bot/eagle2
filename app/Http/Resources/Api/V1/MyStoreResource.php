f3d10730916b40ba532003755f3cf647c2b5c85f
Erinyelkess <ariny.backend@utdsoftware.com>
2025-07-03 14:57:39 +0300

    my store

--- app/Http/Resources/Api/V1/MyStoreResource.php
+++ app/Http/Resources/Api/V1/MyStoreResource.php
@@ -29,7 +29,7 @@ class MyStoreResource extends JsonResource
 
         /** @var User $this*/
         $agency_owner = $this->agency;
-        $salary       =round( $this->salary,2);
+        $salary       = round($this->salary, 2);
         $sallary      = $salary; //
         $userSalary   = $sallary;
         if (($this->type_user == 2 || $this->type_user == 4)) {
@@ -44,7 +44,7 @@ class MyStoreResource extends JsonResource
             return $roomSalary->salary - $roomSalary->cut_amount;
         });
 
-        $diamonds = (in_array($this->type_user, [0,3])) ? $this->exchange_diamonds : $this->monthly_diamond_received;
+        $diamonds = (in_array($this->type_user, [0, 3])) ? $this->exchange_diamonds : $this->monthly_diamond_received;
 
         $data = [
 
@@ -54,15 +54,15 @@ class MyStoreResource extends JsonResource
                 'coins' => (string)$this->di,
                 'diamonds' =>  (string)$diamonds,
                 'silver_coins' => (string)$this->gold,
-                'usd' => (double)$sallary,
-                'user_usd' => (string) $userSalary ?? '',
+                'usd' => (float)$sallary,
+                'user_usd' => (string)truncateAndTrim($userSalary) ?? '',
                 'user_usd_new' => (string) (isset($userSalary) ? round($userSalary, 0) : ''),
                 'host_usd' => (string) @$hostSalary ?? '',
                 'pending_dollar' => (string) $pendingDollar ?? '',
                 'room_salary' => (string) $roomSalary ?? '',
                 'paid' =>  $paid ?? 0,
                 'wallet_balance' =>  $this->wallet?->current_balance ?? 0,
-            ], 
+            ],
 
         ];
 
