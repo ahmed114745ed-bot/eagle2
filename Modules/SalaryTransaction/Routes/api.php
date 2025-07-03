6c52ce3185738bf9519ff0a8890f6e890bd1a831
shami2024 <>
2025-07-03 10:28:42 +0300

    Update api.php

--- Modules/SalaryTransaction/Routes/api.php
+++ Modules/SalaryTransaction/Routes/api.php
@@ -39,7 +39,7 @@ Route::middleware(['auth:sanctum', 'checkLatestToken', 'generalBan', 'userBan', 
             Route::post('shipping-agencies', [AgentSalaryTransactionController::class, 'shipping_agencies']);
             Route::post('charge_co_for_users2', [AgentSalaryTransactionController::class, 'send_money_for_the_host']);
             Route::get('charge-agent-history', [AgentSalaryTransactionController::class, 'chargeCoForUserHistory']);
-            Route::get('charge-dollar-agent-history', [AgentSalaryTransactionController::class, 'chargeDollarForUserHistory']);
+            Route::get( 'hosts-agency-dollars-history', [AgentSalaryTransactionController::class, 'chargeDollarForUserHistory']);
             
         });
 
