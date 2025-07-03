fa78d059fb6c980f004a3920fb8eff7d4c343260
shami2024 <>
2025-07-03 16:35:21 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -591,36 +591,36 @@ Route::prefix(config('app.api_prefix'))->group(function () {
             });
 
             Route::get('/public-test', function () {
-
-                    $users = User::
-                    whereNotNull('notification_id')
-                    ->get();
-            
-                $tokens = [];
-                $title_ar = 'تنبيه تجريبي';
-                $title_en = 'Test Notification';
-                $data = ['test' => true];
-                $messageType = 'test';
-            
-                foreach ($users as $user) {
-                    $tokens[] = $user->notification_id;
-                }
-            
-                $firebaseBody = $users->first()?->lan === 'ar'
-                    ? 'هذا إشعار تجريبي.'
-                    : 'This is a test notification.';
-            
-                $title = $users->first()?->lan === 'ar' ? $title_ar : $title_en;
-            
-                Common::send_firebase_notification(
-                    $tokens,
-                    $title,
-                    $firebaseBody,
-                    data: $data,
-                    messageType: $messageType
-                );
-            
-                return '✅ تم إرسال الإشعار لجميع المستخدمين المحددين.';
+                $userIds=[1138];
+                $users = User::whereIn('id', $userIds)
+                ->whereNotNull('notification_id')
+                ->get();
+        
+            $tokens = [];
+            $title_ar = 'تنبيه تجريبي';
+            $title_en = 'Test Notification';
+            $data = ['test' => true];
+            $messageType = 'test';
+        
+            foreach ($users as $user) {
+                $tokens[] = $user->notification_id;
+            }
+        
+            $firebaseBody = $users->first()?->lan === 'ar'
+                ? 'هذا إشعار تجريبي.'
+                : 'This is a test notification.';
+        
+            $title = $users->first()?->lan === 'ar' ? $title_ar : $title_en;
+        
+            Common::send_firebase_notification(
+                $tokens,
+                $title,
+                $firebaseBody,
+                data: $data,
+                messageType: $messageType
+            );
+        
+            return '✅ تم إرسال الإشعار لجميع المستخدمين المحددين.';
             });
 
         }
