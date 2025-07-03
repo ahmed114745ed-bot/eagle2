7e8c9a41acf9d18729a0a7e2081d5798a376ae2e
Erinyelkess <ariny.backend@utdsoftware.com>
2025-07-03 13:57:16 +0300

    having users days

--- app/Models/User.php
+++ app/Models/User.php
@@ -211,12 +211,13 @@ class User extends Authenticatable
         if (! $year) {
             $year = now()->year;
         }
+        $hours_days = \Cache::get('hours_days') ?? 2;
         $subQuery = DB::table('live_times')
             ->select('uid', DB::raw('COUNT(*) AS entry_count'))
             ->whereMonth('created_at', $month)->whereYear('created_at', $year)
             ->where('uid', $this->id)
             ->groupBy('uid', DB::raw('DATE(created_at)')) // Group by uid and date
-            ->havingRaw('SUM(hours) > 1')
+            ->havingRaw('SUM(hours) >= ?', [$hours_days])
             ->get();
 
         return $subQuery->count('entry_count');
@@ -1579,21 +1580,21 @@ class User extends Authenticatable
         if ($this->type_user >= 1) {
             $userTypes[] = 1;
         }
-    
+
         if ($this->type_user >= 2) {
             $userTypes[] = 2;
         }
-    
+
         if ($this->hasShippingAgency()) {
             $userTypes[] = 3;
         }
-    
+
         if ($this->is_bd) {
             $userTypes[] = 4;
         }
-    
+
         $userTypes = array_unique($userTypes);
-    
+
         return empty($userTypes) ? [0] : $userTypes;
     }
 
