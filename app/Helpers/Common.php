f8ef5256db70364aa9a98cc31779e8bc6c500c51
shami2024 <>
2025-07-06 10:49:57 +0300

    Update Common.php

--- app/Helpers/Common.php
+++ app/Helpers/Common.php
@@ -680,7 +680,7 @@ class Common
             $data = collect($data)->map(function ($value) {
                 return is_array($value) || is_object($value) ? json_encode($value) : (string) $value;
             })->toArray();
-            // if ($tokens instanceof \Illuminate\Support\Collection) $tokens = $tokens->toArray();
+            if ($tokens instanceof \Illuminate\Support\Collection) $tokens = $tokens->toArray();
             //make group and get token
             // $token = self::makeGroup($tokens, $key,  $api_access_key);
             dispatch(new SendFirebaseNotificationJob(
