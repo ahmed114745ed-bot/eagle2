aa437669a3f911dcdf658e5b34e5623aa13d1faa
Bola <bolaphilip3@gmail.com>
2025-07-06 09:27:13 +0300

    edit migrations

--- database/migrations/2025_07_03_075501_add_kicked_by_column_to_users_joined_agencies_table.php
+++ database/migrations/2025_07_03_075501_add_kicked_by_column_to_users_joined_agencies_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users_joined_agencies', function (Blueprint $table) {
-            $table->foreignId('kicked_by_app')->nullable()->constrained('users')->cascadeOnUpdate();
-            $table->foreignId('kicked_by_admin')->nullable()->constrained('admin_users')->cascadeOnUpdate();
+            $table->foreignId('kicked_by_app')->nullable();
+            $table->foreignId('kicked_by_admin')->nullable();
         });
     }
 
@@ -23,7 +23,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users_joined_agencies', function (Blueprint $table) {
-            //
+            $table->dropColumn(['kicked_by_app', 'kicked_by_admin']);
         });
     }
 };
