abcf872fc490e1f7122459eafd550fd707f358e5
Erinyelkess <ariny.backend@utdsoftware.com>
2025-07-08 17:03:09 +0300

    bd account

--- app/Admin/Controllers/BdController.php
+++ app/Admin/Controllers/BdController.php
@@ -268,7 +268,7 @@ class BdController extends MainController
                     $ops2[$user->id] = $user->uuid . '_' . $user->name;
                 }
                 return $ops2;
-            })->ajax('/api/search/users-bd', 'id', 'name')->help('لا يمكن التعديل إلا إذا لم يكن هناك مستخدم مرتبط، أو كان المستخدم مرتبطًا لكن تم حذفه.');
+            })->ajax('/api/search/users-bd', 'id', 'name')->help('لا يمكن التعديل إلا إذا لم يكن هناك مستخدم مرتبط، أو كان المستخدم مرتبطًا لكن تم حذفه.')->rules('required');
         } else {
             $form->select('app_id', __('validation.select_user'))->options(function ($value) {
                 $ops2 = [];
@@ -276,7 +276,7 @@ class BdController extends MainController
                     $ops2[$user->id] = $user->uuid . '_' . $user->name;
                 }
                 return $ops2;
-            })->ajax('/api/search/users-bd', 'id', 'name');
+            })->ajax('/api/search/users-bd', 'id', 'name')->rules('required');
 
             $form->switch('default', __('set_as_default'))
                 ->help(__('make_bd_default'));
