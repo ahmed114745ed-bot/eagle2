8db8f320ccef74d0c7590383c36bb8226adc1395
Bola <bolaphilip3@gmail.com>
2025-07-09 09:54:10 +0300

    Charge agent resource

--- app/Helpers/CustomNotification.php
+++ app/Helpers/CustomNotification.php
@@ -554,6 +554,7 @@ class CustomNotification
         $firebaseBody        = ($user?->lan === 'ar') ? $body_ar : $body_en;
         $data['image'] = getImagePath($image);
         $icon = $data['image'];
+        info('before notification');
         Common::send_firebase_notification($tokens_notification, $this->appName($user->lan), $firebaseBody, icon: $icon, data: $data, messageType: 'ware-vip');
         Common::sendOfficialMessage($user->id, $body_en, '', titleAr: $body_ar);
         (new UserCounterServices)->eventUser($user, 'official-messages');
