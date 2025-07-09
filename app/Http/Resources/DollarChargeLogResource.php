5b32dd6cbc4801efd8886dd65ee5b4d20f11ceef
shami2024 <>
2025-07-09 14:54:30 +0300

    Update DollarChargeLogResource.php

--- app/Http/Resources/DollarChargeLogResource.php
+++ app/Http/Resources/DollarChargeLogResource.php
@@ -18,6 +18,7 @@ class DollarChargeLogResource extends JsonResource
     {
 
         $receiver=   Common::getReceiverInfo($this);
+        dd($receiver);
         $sender=   Common::getChargerInfo($this);
         $is_sender = $sender['id'] == Auth::user()->id;
       
