64ab44c01ac6f90dd0d0677664f0103a47acf174
shami2024 <>
2025-07-09 14:52:19 +0300

    Update ChargeRepository.php

--- app/Tik/Repositories/ChargeRepository.php
+++ app/Tik/Repositories/ChargeRepository.php
@@ -80,8 +80,8 @@ class ChargeRepository extends AbstractRepository
             ->where('charger_type', 'user')
             ->where('user_type', 'agency')
             ->whereNotNull('user_id')
-            // ->with('agency:id,name,img')
-            ->with(Common::chargerRelationsQuery())
+            ->with('receiverUser','receiverUser.profile','receiveragency')
+            // ->with(Common::chargerRelationsQuery())
             ->select('id', 'agency_id', 'amount', 'usd', 'created_at','charger_type','user_type')
             ->orderBy('created_at', 'desc')
             ->paginate($perPage, ['*'], 'page', $page);
