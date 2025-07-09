d8571b32b4464bc599d94e4a5b9432777e1e5fe3
shami2024 <>
2025-07-09 13:59:58 +0300

    Update ChargeResourceforAgencyCharge.php

--- app/Http/Resources/Api/V1/ChargeResourceforAgencyCharge.php
+++ app/Http/Resources/Api/V1/ChargeResourceforAgencyCharge.php
@@ -7,6 +7,7 @@ use App\Models\Admin;
 use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
 
 class ChargeResourceforAgencyCharge extends JsonResource
 {
@@ -49,17 +50,21 @@ class ChargeResourceforAgencyCharge extends JsonResource
 
         // 'charger' => Common::getChargerInfo($this),
         // 'receiver' => Common::getReceiverInfo($this)
-
+        $sender = Common::getChargerInfo($this);
+        $is_sender = $sender['id'] == Auth::user()->id;
         return [
             'id'   => $this->id ?: 0,
             // 'sender' => $sender_data,
             // 'receiver' => $receiver_data,
-            'sender' => Common::getChargerInfo($this),
+            'sender' => $sender,
             'receiver' =>  Common::getReceiverInfo($this),
             'value' => (int) $this->amount,
             'time' => ($this->created_at ? Carbon::parse($this->created_at)->format('Y-m-d h:i:s A') : null),
             'coins' =>  (int)$this->amount ?? 0,
             'usd' => $this->usd ?? 0,
+            'is_sender' => $is_sender ?? 0,
+            
+            
         ];
     }
 }
