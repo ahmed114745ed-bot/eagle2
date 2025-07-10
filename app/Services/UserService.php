b53d683dd8ba640b7ff7f7cb57f1e7c2acf9f5d0
Erinyelkess <ariny.backend@utdsoftware.com>
2025-07-10 14:20:00 +0300

    edit

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -1121,7 +1121,7 @@ class UserService
             // }
 
             // $progress = $exactlyValue == 0 ? 1 : $bar;
-            $progress  = 0;
+            $progress  = 100;
             $remaining = 0;
         } else {
             $progress  = 1;
