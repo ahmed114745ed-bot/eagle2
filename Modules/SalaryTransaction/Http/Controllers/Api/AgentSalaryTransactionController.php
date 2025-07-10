611d42b095728d480464b798898656820adfd485
shami2024 <>
2025-07-10 11:00:17 +0300

    bug  charges_count

--- Modules/SalaryTransaction/Http/Controllers/Api/AgentSalaryTransactionController.php
+++ Modules/SalaryTransaction/Http/Controllers/Api/AgentSalaryTransactionController.php
@@ -221,9 +221,7 @@ class AgentSalaryTransactionController extends Controller
         $countryId = $request->country_id;
         $paymentId = $request->payment_id;
 
-        $agencies = ShippingAgency::with("Countries", "AgencypaymentGateways")->withCount(['salaryRequests' => function ($query) {
-            $query->where('status', 3);
-        }])->whereHas('owner', fn($query) => $query->where('appear_charger_agency', 1))->with('owner')
+        $agencies = ShippingAgency::with("Countries", "AgencypaymentGateways")->withCount('charges')->whereHas('owner', fn($query) => $query->where('appear_charger_agency', 1))->with('owner')
             ->when($countryId, fn($q) => $q->whereHas('Countries', fn($q) => $q->where('country_id', $countryId)))
             ->when($paymentId, fn($q) => $q->whereHas('AgencypaymentGateways',  fn($q) => $q->where('payment_gateway_id', $paymentId)))
             ->paginate(15);
