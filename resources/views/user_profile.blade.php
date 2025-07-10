33b8fcc62899be3eb99b0e2b30555402264d468a
Bola <bolaphilip3@gmail.com>
2025-07-10 14:05:05 +0300

    admin image

--- resources/views/user_profile.blade.php
+++ resources/views/user_profile.blade.php
@@ -1737,6 +1737,7 @@
                                         </td>
                                         <td>{{ $userJoinAgency->status }}</td>
                                         <td>
+                                            {{ info('kickedby'.@$kickedBy) }}
                                             @if(isset($kickedBy))
                                                 <a href="{{ $kickedByUrl ?? '#' }}" target="_blank"
                                                    style="display: inline-flex; align-items: center; text-decoration: none;">
