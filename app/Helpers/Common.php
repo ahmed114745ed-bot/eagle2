baa03c1b1b209b9743abdd6783e274b6e45d2949
shami2024 <>
2025-07-10 14:21:00 +0300

    Update Common.php

--- app/Helpers/Common.php
+++ app/Helpers/Common.php
@@ -1293,11 +1293,20 @@ class Common
                 'created_at'   => now(),
                 'updated_at'   => now(),
             ];
+            logger()->info('[sendOfficialMessage] Prepared message for user:', $messageData);
+
         }
     
         if (!empty($data)) {
             OfficialMessage::insert($data);
+            logger()->info('[sendOfficialMessage] Bulk insert success', [
+                'count' => count($data),
+                'user_ids' => $userIds,
+            ]);
         }
+
+        logger()->warning('[sendOfficialMessage] No valid user IDs to insert message.');
+
 
         // OfficialMessage::query()->create(
         //     [
