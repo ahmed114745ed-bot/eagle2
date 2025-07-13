4632f2e99f094822a6d681fe0e5bf4ca9971b6ec
Erinyelkess <ariny.backend@utdsoftware.com>
2025-07-13 13:31:55 +0300

    colored_name

--- Modules/SalaryTransaction/Transformers/ChargeAgentResource.php
+++ Modules/SalaryTransaction/Transformers/ChargeAgentResource.php
@@ -21,7 +21,7 @@ class ChargeAgentResource extends JsonResource
     {
         //        if (!$this instanceof Agency) return [];
         $user = $this->owner;
-
+        $hasColor = Common::hasInPack($user->id, 18, true);
         $frame = Common::getUserDress(@$user?->id, @$user?->dress_1, 4, 'img2', true) ?: Common::getUserDress(@$user?->id, @$user?->dress_1, 4, 'img1', true);
         return [
             'agency_id' => $this->id,
@@ -41,7 +41,7 @@ class ChargeAgentResource extends JsonResource
             'charge_count' => $this->charges_count ?? 0,
             'image_color'          => @$user->color_image ?? null,
             'id_image'             => @$user->specialId?->ware?->show_img ?? '',
-
+            'colored_name' => $hasColor ? common::wareUserVip($user->id, 18, 'color') ?? '' : '',
             // 'charge_count' => $userDetails->charges_count ?? 0,
         ];
     }
