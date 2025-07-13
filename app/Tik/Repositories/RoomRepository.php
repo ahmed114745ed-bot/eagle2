8564c4995c9b93c71a2f608c8bdabe24f24d1829
Bola <bolaphilip3@gmail.com>
2025-07-13 12:15:34 +0300

    edit ordering in rooms to make it like the dash board

--- app/Tik/Repositories/RoomRepository.php
+++ app/Tik/Repositories/RoomRepository.php
@@ -113,18 +113,20 @@ class RoomRepository extends AbstractRepository
         ->where('room_status', 1);
 
         // الترتيب الأساسي: عدد الزوار أولاً ثم الدبوس ثم الساعة الساخنة
-        $result->orderByDesc('room_visitors_count')
-            ->orderByDesc('pin')
-            ->orderByDesc('hour_hot');
+        $result->orderByDesc('pin');
+
 
         // إذا كان make_rooms_top صحيحاً، نضيف شروط إضافية
         if ($topRooms) {
-            $result->where(function ($query) {
-                $query->where(fn($q) => $q->has("roomVisitors"))
-                    ->orWhere(fn($q) => $q->where('pin', 1))
-                    ->orWhere(fn($q) => $q->has("roomVisitors")->orWhere('count_room_socket','!=',0));
-            });
+//            $result->where(function ($query) {
+//                $query->where(fn($q) => $q->has("roomVisitors"))
+//                    ->orWhere(fn($q) => $q->where('pin', 1))
+//                    ->orWhere(fn($q) => $q->has("roomVisitors")->orWhere('count_room_socket','!=',0));
+//            });
+            $result->orderByDesc('room_visitors_count');
         }
+
+        $result->orderByDesc('hour_hot');
 
         // تصفية حسب البلد إذا تم توفيره
         if (!is_null($req->country_id)) {
