75dc6d260ece5c08e9259e459e2d5f6c8e3af52a
Erinyelkess <ariny.backend@utdsoftware.com>
2025-07-14 14:01:11 +0300

    edit

--- app/Http/Controllers/Api/V1/BoxController.php
+++ app/Http/Controllers/Api/V1/BoxController.php
@@ -57,7 +57,7 @@ class BoxController extends Controller
         if (($box->type == 0) && !$request->users_num) return Common::apiResponse(0, 'missing number of users', null, 422);
         if ($user->di < $box->coins)  return Common::apiResponse(0, 'low balance', null, 407);
 
-        $userBoxes =   BoxUse::where('end_at', '>=', $timestamp)->where('user_id', $user->id)->exists();
+        $userBoxes =   BoxUse::where('end_at', '<=', $timestamp)->where('user_id', $user->id)->exists();
         // if ($userBoxes) return Common::apiResponse(0, 'you send box ', null, 422);
         $label = '';
 
@@ -143,7 +143,8 @@ class BoxController extends Controller
     {
         $user = $request->user();
         $userId = $user->id;
-        $timestamp = Carbon::now()->timestamp;
+        $timezone = Common::timeZone();
+        $timestamp = Carbon::now($timezone)->timestamp;
 
         if (!$request->bid) return Common::apiResponse(0, 'missing params', null, 422);
 
@@ -153,7 +154,7 @@ class BoxController extends Controller
         if (!$box_use) {
             return Common::apiResponse(0, __("api.box_not_found"), null, 404);
         }
-
+dd($box_use['end_at'],$timestamp);
         if ($box_use['end_at'] < $timestamp) {
             return Common::apiResponse(0, __("box closed"), null, 404);
         }
