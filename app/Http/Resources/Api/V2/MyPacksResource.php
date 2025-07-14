de7055a2df418006ccfe69896ddf226dd961dc4d
johnghattas <john.backend@utdsoftware.com>
2025-07-14 13:49:17 +0300

    update packs logic

--- app/Http/Resources/Api/V2/MyPacksResource.php
+++ app/Http/Resources/Api/V2/MyPacksResource.php
@@ -11,85 +11,31 @@ class MyPacksResource extends JsonResource
 {
     public function toArray($request)
     {
-//        $user_id = $request->user_id ?:  $request->user()->id;
-        $type = $request->type;
-        if (in_array($type, [4, 5, 6, 7])) {
-//            $user_dress_after_i_changed = [
-//                4 => 1,
-//                5 => 2,
-//                6 => 3,
-//                7 => 4
-//            ];
-//            $dress_id = User::where(['id' => $user_id])->value("dress_" . $user_dress_after_i_changed[$type]);
-
-            $user_dress_map = [
-                4 => 'dress_1',
-                5 => 'dress_2',
-                6 => 'dress_3',
-                7 => 'dress_4',
-            ];
-
-            $dress_id = null;
-            if (in_array($type, [4, 5, 6, 7]) && isset($user_dress_map[$type])) {
-                $dress_column = $user_dress_map[$type];
-                $dress_id = $this->user?->$dress_column;
-            }
-        }
-        if (in_array($type, [4, 5, 6, 7])) {
-            $title    = empty($this->expire) ? "permanent" : date('Y-m-d H:i:s', $this->expire) . " expire";
-            $is_dress = $dress_id == $this->target_id ? 1 : 0;
-            $color   = $this->color ?: '';
-        } elseif ($type == 2) {
-            $title = "have" . $this->num . "value" . $this->num * $this->price . "diamond";
-            $color = '';
-        } else {
-            $title = "have" . $this->num . "indivual " . $this->title;
-            $color = $this->color ?: '';
-        }
-        $types       = [
-            '1' => 'gem',
-            '2' => 'gifts',
-            '3' => 'coupons',
-            '4' => 'avatar frames',
-            '5' => 'bubble boxes',
-            '6' => 'entry effects',
-            '7' => 'mic on the aperture',
-            '8' => 'badges',
-            '28' => 'profile frame',
-            '25' => 'special id'
-        ];
-        $get_types   = [
-            '1' => 'vip level automatic acquisition',
-            '2' => 'activities',
-            '3' => 'treasure box',
-            '4' => 'purchase',
-            '5' => 'background addition',
-            '6' => 'limited time purchase'
-        ];
         return [
             'id' => $this->id,
             'user_id' => $this->user_id,
-            'get_type' => __($get_types[$this->get_type]),
-            'type' => __($types[$this->type]),
+            'get_type' => $this->get_type_name,
+            'type' => $this->type_name,
             'target_id' => $this->target_id,
             'num' => $this->num,
-            'expire' => ($this->expire != 0  && !empty($pack->expire))? date("Y-m-d H:i:s", $this->expire) : 0,
+            'expire' => $this->formatted_expire,
             'is_read' => $this->is_read,
-            'created_at' => Carbon::parse($this->created_at)->setTimezone($request->hasHeader('tz') ? $request->header()['tz'][0] : 'UTC')->format('Y-m-d H:i:s') ?? '',
+            'created_at' => $this->created_at_formatted,
             'updated_at' => $this->updated_at,
             'sender_id' => $this->sender_id,
-            'is_used' => $this->is_used == 1 ? true : false,
+            'is_used' => (bool) $this->is_used,
             'use_num' => $this->use_num,
             'name' => $this->ware?->name,
             'show_img' => $this->ware?->show_img ?? '',
-            'svg'       =>  $this->ware?->img2 ?? '',
-            'price' => @$this->price ?? '',
-            'price_item' => @$this->price ?? '',
-            'image_type' => $this->ware->image_type ?? "",
-            'is_dress' => $is_dress ?? 0,
-            'title' => $title,
-            'color' => $color,
+            'svg' => $this->ware?->img2 ?? '',
+            'price' => $this->price ?? '',
+            'price_item' => $this->price ?? '',
+            'image_type' => $this->ware?->image_type ?? '',
+            'is_dress' => $this->is_dress,
+            'title' => $this->title_text,
+            'color' => $this->resolved_color,
             'using' => $this->using,
         ];
     }
+
 }
