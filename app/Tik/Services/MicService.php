64dfe58c9f553a44a5b0b8a08a9f3f690dbe0350
shami2024 <>
2025-07-14 09:09:54 +0300

    Update MicService.php

--- app/Tik/Services/MicService.php
+++ app/Tik/Services/MicService.php
@@ -377,6 +377,7 @@ class MicService
 
         // $microphone = $room->getOriginal('microphone');
         $microphone = $room->all_microphone;
+        logger('microphone:', [$microphone]);
 
         $microphone = $this->micType($type, $microphone, $position);
         $this->updateMic($room, $microphone);
@@ -408,6 +409,7 @@ class MicService
 
         $microphone = explode(',', $microphone);
         $current = $microphone[$position] ?? '0';
+        logger(' explode microphone:', [$microphone]);
 
         $user = '0';
         $status = '0';
@@ -435,6 +437,8 @@ class MicService
         } else {
             $microphone[$position] = $status;
         }
+        logger('implode microphone:', [implode(',', $microphone)]);
+
         return implode(',', $microphone);
         // $microphone = explode(',', $microphone);
         // if ($type == 'mute') {
