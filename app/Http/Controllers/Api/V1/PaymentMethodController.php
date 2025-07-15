cfe1f888c1ea15df64575fce0036dd7bb7d74c47
Bola <bolaphilip3@gmail.com>
2025-07-15 12:39:25 +0300

    log

--- app/Http/Controllers/Api/V1/PaymentMethodController.php
+++ app/Http/Controllers/Api/V1/PaymentMethodController.php
@@ -89,7 +89,7 @@ class PaymentMethodController extends Controller
             $order->pid = $fawryRefNumber;
             $paymentMethod->status = 'paid';
             $order->save();
-            
+
             return response()->json(['status' => 'success', 'message' => 'Payment successful.']);
         }
         if ($orderStatus === 'UNPAID') {
@@ -111,13 +111,13 @@ class PaymentMethodController extends Controller
     public function success(Request $request): JsonResponse
     {
         try {
+            info($request->all());
             $query = Arr::only($request->query(), [
                 'statusCode',
                 'statusDescription',
                 'merchantRefNumber',
             ]);
 
-            // Validate required parameters
             if (empty($query['merchantRefNumber']) || empty($query['statusCode'])) {
                 return response()->json([
                     'status' => false,
