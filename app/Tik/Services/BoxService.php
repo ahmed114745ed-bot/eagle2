6e1fe9ed2f0f97dbee9be17fccfbff616a65e452
Bola <bolaphilip3@gmail.com>
2025-07-16 15:33:04 +0300

    test jobs

--- app/Tik/Services/BoxService.php
+++ app/Tik/Services/BoxService.php
@@ -120,7 +120,7 @@ class BoxService
             'image' => $box->image,
             'is_closed' => false,
         ];
-        dispatch(new SuperLuckyBoxJob())->delay(now()->addSeconds(30))->onQueue('test-super-lucky-box');
+        dispatch(new SuperLuckyBoxJob())->delay(now()->addMinutes($box->duration))->onQueue('test-super-lucky-box');
         info('afterJob');
 
         //        dispatch(new SuperLuckyBoxJob())->delay(now()->seconds(30))->onQueue('');
