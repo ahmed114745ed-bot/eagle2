fca9f2380181e0a45df8ce67d57ea26924db2b02
johnghattas <john.backend@utdsoftware.com>
2025-07-16 12:58:47 +0300

    Update VersionController.php

--- app/Http/Controllers/VersionController.php
+++ app/Http/Controllers/VersionController.php
@@ -39,7 +39,7 @@ class VersionController extends Controller
         $isBan          = $this->haveBan(@$user->uuid);
         $isGiftUpdated  = $this->isUpdated('gifts_update_at', @$request->gift_time);
         $isIntroUpdated = $this->isUpdated('intro_updated_at', @$request->intro_time);
-        $isBubbleFrameUpdated = $this->isUpdated('bubble_frame_updated_at', @$request->bubble_frame_time);
+        $isBubbleFrameUpdated = $this->isUpdated('bubble_frame_updated_at', @$request->bubbles_frame_time);
         $isFrameUpdated = $this->isUpdated('frame_updated_at', @$request->frame_time);
         $isEmojiUpdated = $this->isUpdated('emoji_updated_at', @$request->emoji_time);
         $isExtraUpdated = $this->isUpdated('extra_updated_at', @$request->extra_time);
