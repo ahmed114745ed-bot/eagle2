9d767ea9ee1f12b00eb71624e7b0386b7ce5f0b8
shami2024 <>
2025-07-20 14:34:32 +0300

    Update UserCoinLogHelper.php

--- app/Helpers/UserCoinLogHelper.php
+++ app/Helpers/UserCoinLogHelper.php
@@ -40,7 +40,7 @@ class UserCoinLogHelper
             'to_date'       => $toDate ?? Carbon::now()->toDateString(),
         ];
 
-        Log::info('UserCoinLogHelper::log called', $logData);
+        \Log::info('UserCoinLogHelper::log called', $logData);
 
         return UserCoinLog::create($logData);
 
