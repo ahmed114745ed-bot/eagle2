d48d2eb770e11caaeff449b72ee79be20ed47f02
Erinyelkess <ariny.backend@utdsoftware.com>
2025-07-20 20:02:46 +0300

    web

--- Modules/LuckyBox/Routes/web.php
+++ Modules/LuckyBox/Routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Modules\LuckyBox\Http\Controllers\web\BoxUseController;
-use Modules\LuckyBox\Http\Controllers\web\LuckyBoxController;
+use Modules\LuckyBox\Http\Controllers\Web\BoxUseController;
+use Modules\LuckyBox\Http\Controllers\Web\LuckyBoxController;
 
 use Illuminate\Support\Facades\Route;
 
@@ -22,8 +22,8 @@ use Illuminate\Support\Facades\Route;
 Route::group(
     [
         'prefix'     => config('admin.route.prefix'),
-        'namespace'  => 'web',
-        'middleware' => [
+        'namespace'  =>         
+
             'web',
             'admin',
             'adminIp',
