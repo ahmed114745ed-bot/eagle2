eae5a71c12af309da7991cc0126702ed4d157394
Erinyelkess <ariny.backend@utdsoftware.com>
2025-07-20 20:16:04 +0300

    edit route

--- Modules/LuckyBox/Routes/web.php
+++ Modules/LuckyBox/Routes/web.php
@@ -33,8 +33,8 @@ Route::group(
         'as'         => config('admin.route.prefix') . '.',
     ],
     function () {
-        Route::resource('boxes', LuckyBoxController::class);
-        Route::get('lucy-box-settings', [LuckyBoxController::class, 'box_settings']);
-        Route::resource('thrown_boxes', BoxUseController::class);
+        Route::resource('lucky-boxes', LuckyBoxController::class);
+        Route::get('lucky-box-settings', [LuckyBoxController::class, 'box_settings']);
+        Route::resource('thrown-boxes', BoxUseController::class);
     }
 );
