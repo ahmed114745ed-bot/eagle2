7cd94c5883675de956ead1472414e6a367205e49
shami2024 <>
2025-07-21 15:49:19 +0300

    Update OvipGiftTapController.php

--- Modules/Vip/Http/Controllers/web/OvipGiftTapController.php
+++ Modules/Vip/Http/Controllers/web/OvipGiftTapController.php
@@ -323,7 +323,7 @@ class OvipGiftTapController extends MainController
             }
     
             if ($form->show_img instanceof UploadedFile) {
-                $allowed = ['svga', 'mp4', 'jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff', 'svg', 'webp', 'mov', 'avi', 'wmv', 'flv', 'mkv', 'webm'];
+                $allowed = ['svga','svg', 'mp4', 'jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff', 'svg', 'webp', 'mov', 'avi', 'wmv', 'flv', 'mkv', 'webm'];
                 $ext = strtolower($form->show_img->guessExtension());
     
                 if (!in_array($ext, $allowed)) {
@@ -334,7 +334,7 @@ class OvipGiftTapController extends MainController
             }
     
             if ($form->img2 instanceof UploadedFile) {
-                $allowed = ['svga', 'mp4', 'alpha', 'vap', 'png'];
+                $allowed = ['svga', 'mp4','svg','alpha', 'vap', 'png'];
                 $ext = strtolower($form->img2->guessExtension());
                 $originalExt = strtolower($form->img2->getClientOriginalExtension());
     
