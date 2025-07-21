e51eb6352eaf2ca6e9c99e6c4f26667887a5454d
Erinyelkess <ariny.backend@utdsoftware.com>
2025-07-21 14:01:14 +0300

    salary v2

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -414,6 +414,11 @@ Route::group(
             Route::delete('/{id}', [OvipGiftTapController::class, 'destroy'])->where('id', '[0-9]+');
         });
         Route::resource('vip_privilege', 'VipPrivilegeController');
+        // Route::get('/{id}/edit', [OvipGiftTapController::class, 'edit'])->where('id', '[0-9]+');
+        // Route::put('/{id}', [OvipGiftTapController::class, 'update'])->where('id', '[0-9]+');
+        // Route::delete('/{id}', [OvipGiftTapController::class, 'destroy'])->where('id', '[0-9]+');
+        // });
+        // Route::resource('vip_privilege', 'VipPrivilegeController');
         Route::resource('tickets', 'TicketController');
         Route::resource('pages', 'PageController');
         Route::resource('exchanges', 'ExchangeController');
@@ -477,9 +482,10 @@ Route::group(
         Route::resource('/uuid_dedicate', 'SpecialWareDedicateController');
         Route::get('/vips_dedicate', 'DedicateVipController@index');
         Route::resource('/bans', 'BanController');
-                Route::post('custom-delete-ban', [BanController::class, 'deleteBan']);
+        Route::post('custom-delete-ban', [BanController::class, 'deleteBan']);
 
         Route::resource('/bans-rooms', 'BanRoomsController');
+        Route::resource('salaries', 'SalariesController')->name('index', 'sallaries');
 
         Route::resource('/request-background-image', 'RequestBackgroundImageController');
         Route::resource('/group-chat', 'GroupChatController');
