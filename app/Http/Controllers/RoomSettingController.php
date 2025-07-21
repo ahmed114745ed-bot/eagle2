a3e91bd9c5232dd685511748fe67907ac5b1b58e
Bola <bolaphilip3@gmail.com>
2025-07-21 17:48:46 +0300

    null check

--- app/Http/Controllers/RoomSettingController.php
+++ app/Http/Controllers/RoomSettingController.php
@@ -5,16 +5,17 @@ namespace App\Http\Controllers;
 use App\Helpers\Common;
 use App\Models\Config;
 use App\Models\Setting;
+use Illuminate\Http\JsonResponse;
 
 class RoomSettingController extends Controller
 {
-    public function show(): \Illuminate\Http\JsonResponse
+    public function show(): JsonResponse
     {
         $paidRoom = Config::where('name', 'paid_room')->first();
         $paidRoomAmount = Config::where('name', 'paid_room_amount')->first();
         $data = [
-            $paidRoom->name => (bool)$paidRoom->value,
-            $paidRoomAmount->name => $paidRoomAmount->value,
+            $paidRoom->name => (bool)$paidRoom->value ?? false,
+            $paidRoomAmount->name => $paidRoomAmount->value ?? 0,
         ];
 
         return Common::apiResponse(true, '', $data, 200);
