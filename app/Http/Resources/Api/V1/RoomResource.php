e7da5a3a81720be2d6b243339b05b7ed0b52861d
johnghattas <john.backend@utdsoftware.com>
2025-07-22 16:17:32 +0300

    Update RoomResource.php

--- app/Http/Resources/Api/V1/RoomResource.php
+++ app/Http/Resources/Api/V1/RoomResource.php
@@ -58,7 +58,13 @@ class RoomResource extends JsonResource
             'room_background' => $this->final_room_image,
             'stream_type' => $this->type ?? 'audio',
             'is_live' => (bool)$this->is_live,
-            'is_lucky_box' => $this->is_lucky_box ?? false,
+            'is_lucky_box' => $this->boxUse()->where('room_uid', $this->uid)
+                ->where('not_used_num', '>', 0)
+                //            ->where('unused_coins', '>', 0)
+                ->where('end_at', '>=', now()->timestamp)
+                ->whereDoesntHave('picks', function ($q) use ($userId) {
+                    $q->where('user_id', $userId);
+                })->exists(),
             'country' => $this->country
                 ? new CountryResource($this->country)
                 : [
