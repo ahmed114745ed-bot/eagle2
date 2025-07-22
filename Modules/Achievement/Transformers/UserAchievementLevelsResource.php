8331c9fa7d03ae38ed45d582814a78aed5920750
makled98 <mkled.backend@utdsoftware.com>
2025-07-22 14:43:17 +0300

    Update UserAchievementLevelsResource.php

--- Modules/Achievement/Transformers/UserAchievementLevelsResource.php
+++ Modules/Achievement/Transformers/UserAchievementLevelsResource.php
@@ -15,6 +15,7 @@ class UserAchievementLevelsResource extends JsonResource
      */
     public function toArray($request)
     {
+        dd($this);
         $description = app()->getLocale() === 'ar' ? $this->ar_description : $this->en_description;
         $title = app()->getLocale() === 'ar' ? ' هذا الإنجاز مأخوذ من المشرف' : 'this achievement is taken from Admin';
         $achievementLevel = AchievementLevel::find($this->achievement_level_id);
