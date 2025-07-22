38093504345d57b3aa3bcd666e32ab92e82fcb26
shami2024 <>
2025-07-22 12:58:18 +0300

    Update LogUserCoinProfit.php

--- app/Jobs/LogUserCoinProfit.php
+++ app/Jobs/LogUserCoinProfit.php
@@ -6,7 +6,7 @@ use App\Helpers\Common;
 use App\Models\UserCoinLog;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Foundation\Bus\Dispatchable; // ← هذه مهمة جداً
+use Illuminate\Foundation\Bus\Dispatchable; 
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
