1f7773eacbbe19e30d9bf2216f270474b0a9d18e
makled98 <mkled.backend@utdsoftware.com>
2025-07-23 15:37:36 +0300

    Update UserCommon.php

--- app/Helpers/UserCommon.php
+++ app/Helpers/UserCommon.php
@@ -84,18 +84,11 @@ class UserCommon
 
     public static function getColoredName(User $user)
     {
-        $ownerId = optional($user->senderShippingAgency->owner)->id;
-
-        if (!$ownerId) {
-            return '';
-        }
-
-        $hasColor  = Common::hasInPack($ownerId, 18, true);
-        $colorName = $hasColor ? Common::wareUserVip($ownerId, 18, 'color') ?? '' : '';
-
+        $hasColor  = Common::hasInPack(@$user?->senderShippingAgency?->owner?->id, 18, true);
+        $colorName = $hasColor ? common::wareUserVip(@$user?->senderShippingAgency?->owner?->id, 18, 'color') ?? '' : '';
         return $colorName;
     }
-    
+
     public static function UserStatistic($userId, $type, bool $reals = false, \DateTime $startDate = null, \DateTime $endDate = null)
     {
         $timezone = getTimezone();
