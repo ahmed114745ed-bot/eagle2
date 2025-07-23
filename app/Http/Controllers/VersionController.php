860eeb274e95ab0d013e422e30b15508c63afef6
makled98 <mkled.backend@utdsoftware.com>
2025-07-23 12:53:11 +0300

    Update VersionController.php

--- app/Http/Controllers/VersionController.php
+++ app/Http/Controllers/VersionController.php
@@ -15,8 +15,6 @@ class VersionController extends Controller
 {
     public function versionAndCache(Request $request)
     {
-        $wapple = $this->isUpdated('wappel_frame_updated_at', @$request->wabbles_frame_time);
-
         $version = $request->version;
         $currentVersion  = settings()->get($request->OS == 'Huawei' ? 'huawei_current_version' : ($request->OS == 'IOS' ? 'ios_current_version' : 'android_current_version'));
 
@@ -47,6 +45,7 @@ class VersionController extends Controller
         $isEmojiUpdated = $this->isUpdated('emoji_updated_at', @$request->emoji_time);
         $isExtraUpdated = $this->isUpdated('extra_updated_at', @$request->extra_time);
         $agencyBadges =$this->isUpdated('badges_agency_update_at', @$request->badges_agency_time);
+        $wapple = $this->isUpdated('wappel_frame_updated_at', @$request->wabbles_frame_time);
         $isColorUpdated = $this->isUpdated('colors_updated_at', @$request->color_time);
         $ProfileFrameUpdated = $this->isUpdated('profile_frame_updated', @$request->profile_frame_updated);
         $reelSettings = Setting::where('key', 'reel_status')->first();
@@ -125,9 +124,9 @@ class VersionController extends Controller
      */
     public function isUpdated($key, $time): bool
     {
-        if ($time) {
-            $time /= 1000;
-        }
+        // if ($time) {
+        //     $time /= 1000;
+        // }
         $settingGiftUpdate = settings()->get($key);
         $isGiftUpdated     = true;
         // if ($settingGiftUpdate == null && $time != null) {
@@ -136,16 +135,13 @@ class VersionController extends Controller
         // } elseif ($time) {
         //     $isGiftUpdated = $settingGiftUpdate > $time;
         // }
-        Log::info([$key,$settingGiftUpdate,$time]);
         if ($settingGiftUpdate === null && $time !== null) {
             $isGiftUpdated = false;
         }
 
         if ($time !== null) {
-            dd('inside',$settingGiftUpdate,$time,$key );
             $isGiftUpdated = $settingGiftUpdate > $time;
         }
-            dd($settingGiftUpdate,$time,$key );
 
         return $isGiftUpdated;
     }
