5fb6d06f4f29408f14dc8f217a03c4503da389e8
johnghattas <john.backend@utdsoftware.com>
2025-07-24 22:09:43 +0300

    Update WareObserver.php

--- app/Observers/WareObserver.php
+++ app/Observers/WareObserver.php
@@ -43,8 +43,7 @@ class WareObserver
     {
         $isEnableOld = $ware->getOriginal('enable');
         $svgOld = $ware->getOriginal('img2');
-        $timezone = getTimezone();
-        $timestamp = Carbon::now($timezone)->timestamp;
+        $timestamp = Carbon::now()->timestamp;
         if ($ware->type == 12) {
             settings()->set('wappel_frame_updated_at',  $timestamp);
         }
