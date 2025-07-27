1bb172ea31a3d14c720f17836461287c6f2e6e0e
Erinyelkess <ariny.backend@utdsoftware.com>
2025-07-27 14:21:18 +0300

    colored name

--- app/Http/Resources/Api/V1/SenderGiftLogResource.php
+++ app/Http/Resources/Api/V1/SenderGiftLogResource.php
@@ -16,7 +16,7 @@ class SenderGiftLogResource extends JsonResource
      */
     public function toArray($request)
     {
-
+        $hasColor = Common::hasInPack(@$this->sender->id, 18, true);
         $data = [
             'id' => @$this->sender->id ?? 0, // both
             'uuid' => @$this->sender->uuid ?? '', // both
@@ -26,6 +26,8 @@ class SenderGiftLogResource extends JsonResource
             'id_image'             => @$this->sender->specialId?->ware?->show_img ?? '',
             'exp'   => $this->exp ?? '',
             'level' => Common::level_center_min(@$this->sender->id),
+            'colored_name' => $hasColor ? common::wareUserVip(@$this->sender->id, 18, 'color') ?? '' : '',
+
 
         ];
 
