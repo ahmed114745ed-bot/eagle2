9c27b1cd0135d9d8be567aab716c318f15b775e9
johnghattas <john.backend@utdsoftware.com>
2025-07-31 11:43:22 +0300

    Update HomeService.php

--- app/Tik/Services/HomeService.php
+++ app/Tik/Services/HomeService.php
@@ -142,7 +142,7 @@ class HomeService
         $packQuery = Pack::where('user_id', $user->id)
             ->where('type', $privilegeId);
 
-        if($isAvailable) $packQuery->where('is_used', true);
+        if(!$isAvailable) $packQuery->where('is_used', true);
 
         if (!$packQuery->exists()) {
             throw new \Exception(__('api.notWare'));
