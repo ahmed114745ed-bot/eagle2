895f19530d205e4e3197126385dbd3255d9832fa
makled98 <mkled.backend@utdsoftware.com>
2025-08-02 01:35:23 +0300

    Update ChargeSwitchAction.php

--- app/Admin/Actions/ChargeSwitchAction.php
+++ app/Admin/Actions/ChargeSwitchAction.php
@@ -12,6 +12,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Request;
 use Encore\Admin\Facades\Admin;
 use Encore\Admin\Auth\Permission;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
 class ChargeSwitchAction extends RowAction
@@ -27,8 +28,9 @@ class ChargeSwitchAction extends RowAction
     public function handle(Model $model)
     {
         Log::info(['before_transfer_salary' => $model->transfer_salary,'mode_id' => $model->id]);
-       $model->transfer_salary = $model->transfer_salary ? 0 : 1;
-        $model->save();
+        User::where("id", $model->id)->update([
+            'transfer_salary' => DB::raw('NOT transfer_salary')
+        ]);       
         Log::info(['after_transfer_salary' => $model->transfer_salary,'mode_id' => $model->id]);
 
         $message = $model->transfer_salary
