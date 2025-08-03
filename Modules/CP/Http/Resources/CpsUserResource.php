0c556b229e33cdbb3a3b9420b7f42de178fc7c9c
shami2024 <>
2025-08-03 16:47:00 +0300

    Update CpsUserResource.php

--- Modules/CP/Http/Resources/CpsUserResource.php
+++ Modules/CP/Http/Resources/CpsUserResource.php
@@ -29,15 +29,10 @@ class CpsUserResource extends JsonResource
             $query->where('user_one_id', $this->id)
                   ->orWhere('user_two_id', $this->id);
         })
-        ->where(function ($query) {
-            $query->where('status', 1)
-                  ->orWhere('status', 4);
-        })
         ->whereHas('cpRelation', function ($q) {
             $q->where('title', $this->relationType);
 
         })
-        ->with('cpRelation')
         ->first();
 
         dd($cp ,$this->relationType);
