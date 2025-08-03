a9166c97a466dbbe5187bf6980b3a40d09fc0d96
shami2024 <>
2025-08-03 15:50:29 +0300

    Update CpsUserResource.php

--- Modules/CP/Http/Resources/CpsUserResource.php
+++ Modules/CP/Http/Resources/CpsUserResource.php
@@ -50,9 +50,9 @@ class CpsUserResource extends JsonResource
                 'id' => $otherUser->id ?? 0,
                 'name' => $otherUser->name ?? '',
                 'uuid' => $otherUser->uuid ?? 0,
-                'image' => $otherUser->profile->avatar ?? null,
-            ], fn ($value, $key) => $key !== 'image' || !empty($value), ARRAY_FILTER_USE_BOTH),
-        ], fn ($value, $key) => $key !== 'image' || !empty($value), ARRAY_FILTER_USE_BOTH);
+                'image' => $otherUser->profile->avatar ?? '',
+            ], 
+        ];
     
    
     }
