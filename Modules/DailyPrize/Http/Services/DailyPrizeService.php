afb94f84a19176ca1b513f40a47986e5b2858b9d
johnghattas <john.backend@utdsoftware.com>
2025-08-04 14:41:52 +0300

    Update DailyPrizeService.php

--- Modules/DailyPrize/Http/Services/DailyPrizeService.php
+++ Modules/DailyPrize/Http/Services/DailyPrizeService.php
@@ -48,7 +48,8 @@ class DailyPrizeService
 
     public function getDayGift(int $day)
     {
-        $day = $day % ($this->dailyGiftsCount() ?: 1);
+        $day = ($day % ($this->dailyGiftsCount() ?: 1)) ;
+        if ($day == 0) $day = 7;
         return DailyGift::with('ware')->selectRaw('daily_gifts.*, (SELECT SUM((type - 1) * 7 + `order`) FROM daily_gifts as dg WHERE dg.id = daily_gifts.id) as day')
                         ->having('day', '=', $day)
                         ->first();
