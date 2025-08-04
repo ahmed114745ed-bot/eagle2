f0c999af6526c26804532e41979e09f45bc1ad24
Bola <bolaphilip3@gmail.com>
2025-08-04 15:31:24 +0300

    test cp notification

--- app/Helpers/Common.php
+++ app/Helpers/Common.php
@@ -783,8 +783,7 @@ class Common
 
         $result = json_decode($result);
 
-        info('result');
-        info($result);
+        info('result', ['result' => $result]);
         //remove group with $key if is group
         if ($result  && $isGroup) {
             self::removeGroupName($key, $token, $tokens, $api_access_key);
