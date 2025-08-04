c0abe1dd4d06e9d458f7a4afae2d5b7676ba9ced
Bola <bolaphilip3@gmail.com>
2025-08-04 14:23:09 +0300

    cp level log

--- Modules/CP/Transformers/CpProfileResource.php
+++ Modules/CP/Transformers/CpProfileResource.php
@@ -28,7 +28,9 @@ class CpProfileResource extends JsonResource
         $frame = $dress_1_data ?: $dress_1_fallback;
 
         $currentLevel = CpLevel::find($this->level_id);
-        $nextLevel    = CpLevel::where("id", ">", $this->level_id)->orderBy('id')->first();
+        $nextLevel = CpLevel::where("id", ">", $this->level_id)->orderBy('id')->first();
+        info('current level', ['current level' => $currentLevel]);
+        info('next level', ['next level' => $nextLevel]);
 
         $nextLevelPercentage = 0;
         $ratio = 0;
