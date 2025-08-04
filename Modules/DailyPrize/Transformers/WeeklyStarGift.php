ec2b0cdbb29235b2e24759e60cc7410a49dc0bf9
johnghattas <john.backend@utdsoftware.com>
2025-08-04 13:05:49 +0300

    Update WeeklyStarGift.php

--- Modules/DailyPrize/Transformers/WeeklyStarGift.php
+++ Modules/DailyPrize/Transformers/WeeklyStarGift.php
@@ -2,47 +2,57 @@
 
 namespace Modules\DailyPrize\Transformers;
 
-
-use App\Models\Ware;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class WeeklyStarGift extends JsonResource
 {
     public function toArray($request)
     {
+        $expire = '';
+        $type = '';
+        $image = '';
+
         switch ($this->gift_type) {
-            case "ware":
-                $expire = @$this->ware->expire . ' days';
-                $type = match (@$this->ware->type) {
+            case 'ware':
+                $expire = ($this->expire ?? $this->ware?->expire) . ' days';
+                $type = match ($this->ware?->type) {
                     6 => trans('Intro Frame'),
                     5 => trans('Bubble Frame'),
                     default => trans('Avatar Frame'),
                 };
-                $image = @$this->ware->show_img;
+                $image = $this->ware?->show_img;
                 break;
 
-            case "vip":
-                $expire = @$this->vip?->expire . ' days';
-                $type = @$this->vip->name;
-                $vipIcon = Ware::where('level',@$this->vip->level)->where('type' ,10)->where('get_type',1)->first();
-                $image = @$this->vip->img;
+            case 'vip':
+                $expire = ($this->expire ?? $this->vip?->expire) . ' days';
+                $type = $this->vip?->name ?? 'VIP';
+                $image = $this->vip?->img;
                 break;
 
-            case "achievement":
-                $expire = @$this->expire . ' days';
-                $type = "achievement";
-                $image = @$this->target[0] == '/' ? substr(@$this->target, 1) : @$this->target[0]??'';
+            case 'achievement':
+                $expire = ($this->expire ?? 0) . ' days';
+                $type = 'achievement';
+                $image = $this->resolveAchievementImage();
                 break;
+
             default:
-                $expire = @$this->target;
-                $type = "coins";
-                $image = "coin.png";
-                break;
+                $expire = $this->target ?? '0';
+                $type = 'coins';
+                $image = 'coin.png';
         }
 
         return [
             'name'  => "{$expire} /{$type}",
             'image' => $image,
         ];
+    }
+
+    protected function resolveAchievementImage(): string
+    {
+        if (is_string($this->target)) {
+            return str_starts_with($this->target, '/') ? substr($this->target, 1) : $this->target;
+        }
+
+        return 'achievement.png';
     }
 }
