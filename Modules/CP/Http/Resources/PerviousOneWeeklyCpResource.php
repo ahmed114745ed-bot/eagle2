4f381ad656068e4ea067997e48a4cf8260ae65f0
Bola <bolaphilip3@gmail.com>
2025-08-05 17:40:44 +0300

    cp images

--- Modules/CP/Http/Resources/PerviousOneWeeklyCpResource.php
+++ Modules/CP/Http/Resources/PerviousOneWeeklyCpResource.php
@@ -6,20 +6,20 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class PerviousOneWeeklyCpResource extends JsonResource
 {
-    
-    
+
+
     public function toArray($request)
     {
-       
+
         return [
-            
+
             'id' => $this?->id ?? 0,
             'user_one_id' => $this?->userOne->id ?? 0,
             'user_one_name' => $this?->userOne->name ?? '',
-            'user_one_image' => $this?->userOne->profile->avatar ?? '',
+            'user_one_image' => getImagePath($this?->userOne->profile->avatar)  ?? '',
             'user_two_id' => $this?->userTwo->id ?? 0,
             'user_two_name' => $this?->userTwo->name ?? '',
-            'user_two_image' => $this?->userTwo->profile->avatar ?? '',
+            'user_two_image' => getImagePath($this?->userTwo->profile->avatar) ?? '',
             'total_gift_price' =>numToString(intval( $this?->total_price ?? 0)) ,
             'level' => @$this?->level ?? 0,
 
