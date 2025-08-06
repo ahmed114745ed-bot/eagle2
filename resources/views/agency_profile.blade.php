6356ed9b0d588301460b91823d86990101d720ff
shami2024 <>
2025-08-06 15:42:25 +0300

    truncateAndTrim

--- resources/views/agency_profile.blade.php
+++ resources/views/agency_profile.blade.php
@@ -220,7 +220,7 @@
         .agency-avatar .logo-img {
             width: 100%;
             height: 100%;
-            object-fit: cover;
+            /* object-fit: cover; */
         }
 
         .agency-info {
