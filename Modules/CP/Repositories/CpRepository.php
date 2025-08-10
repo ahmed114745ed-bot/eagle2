d69fe858b222e8ceb50ffcec560397e58684d582
Bola <bolaphilip3@gmail.com>
2025-08-10 11:48:47 +0300

    test ranking

--- Modules/CP/Repositories/CpRepository.php
+++ Modules/CP/Repositories/CpRepository.php
@@ -216,10 +216,10 @@ class CpRepository
         return GiftLog::selectRaw('cp_id, SUM(giftNum * giftPrice) as total_gifts')
         ->whereNotNull("cp_id")
         ->with(['cp' => function ($query) use ($relationType) {
-            $query->select('id', 'di', 'level_id', 'user_one_id', 'user_two_id', 'cp_relation_id') 
-                ->whereHas("relation", function ($q) use ($relationType) {
-                    $q->where('type', $relationType);
-                })
+            $query->select('id', 'di', 'level_id', 'user_one_id', 'user_two_id', 'cp_relation_id')
+//                ->whereHas("relation", function ($q) use ($relationType) {
+//                    $q->where('type', $relationType);
+//                })
                 ->with(['relation']);
         }])
         ->when($type, function ($query) use ($type) {
