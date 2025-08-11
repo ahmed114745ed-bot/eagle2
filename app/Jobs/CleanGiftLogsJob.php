cd64e81e96a98671cfa9404193be4573d3c1c8d1
shami2024 <>
2025-08-11 12:59:55 +0300

    Update CleanGiftLogsJob.php

--- app/Jobs/CleanGiftLogsJob.php
+++ app/Jobs/CleanGiftLogsJob.php
@@ -23,14 +23,12 @@ class CleanGiftLogsJob  implements ShouldQueue
             $monthlyReceived = $user->monthly_diamond_received;
         
             $giftLogs = DB::table('gift_logs')
-            ->join('gifts', 'gift_logs.giftId', '=', 'gifts.id')
-            ->where('gift_logs.receiver_id', $user->id)
-            ->where('gifts.type', 6)
-            ->whereMonth('gift_logs.created_at', now()->month)
-            ->whereYear('gift_logs.created_at', now()->year)
-            ->orderBy('gift_logs.created_at', 'asc')
-            ->select('gift_logs.*') 
-            ->get();
+                ->where('receiver_id', $user->id)
+                ->where('type', 6)
+                ->whereMonth('created_at', now()->month)
+                ->whereYear('created_at', now()->year)
+                ->orderBy('created_at', 'asc')
+                ->get();
         
             $total = 0;
             $keepIds = [];
