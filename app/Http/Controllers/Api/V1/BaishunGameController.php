aef181a0f7affd5dbcd0ccc6293d39fe62d95753
Bola <bolaphilip3@gmail.com>
2025-08-11 15:29:21 +0300

    remove logs

--- app/Http/Controllers/Api/V1/BaishunGameController.php
+++ app/Http/Controllers/Api/V1/BaishunGameController.php
@@ -151,7 +151,6 @@ class BaishunGameController extends Controller
                 ]
             ];
 
-            info('im in game');
             $json = json_encode($d);
             dispatchJobToQueue(new AllOpeningRoomsZegoRequest($json, $user->id, $room?->id, false), 'heavyProcessing');
         }
