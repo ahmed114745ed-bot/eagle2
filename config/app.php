2ed49b3b786601e14a8e724797ae0b2efedc55ab
Erinyelkess <ariny.backend@utdsoftware.com>
2025-08-12 17:10:44 +0300

    edit

--- config/app.php
+++ config/app.php
@@ -97,7 +97,7 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
     'project_id' => env('PROJECT_ID', 3),
-    'utd_url' => env('UTD_URL', 'https://utd-test.utdsoftware.com/api'),
+    'utd_url' => env('UTD_URL', 'http://127.0.0.1:8000/api/'),
 
     'asset_url' => env('ASSET_URL', null),
 
