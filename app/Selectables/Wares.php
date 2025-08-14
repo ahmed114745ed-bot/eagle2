e4f4b39a01100880414c660f1256b9001a3ccd34
johnghattas <john.backend@utdsoftware.com>
2025-08-14 13:13:25 +0300

    Update Wares.php

--- app/Selectables/Wares.php
+++ app/Selectables/Wares.php
@@ -14,7 +14,7 @@ class Wares extends Selectable
 
     public function make()
     {
-        $this->grid->model()->whereIn('type', [4, 5, 6]);
+        $this->grid->model()->whereIn('type', [4, 5, 6, 28]);
         $this->column('id');
         $this->column('name');
         $this->column('show_img', __('show_img'))->image('', 30);
@@ -24,10 +24,11 @@ class Wares extends Selectable
             return handleShowImageWithTypes($this->id, $url, 10, 10);
         });
         $this->column('type', __('type'))->select(
-            [ 
+            [
                 4 => trans('Avatar Frame'),
                 5 => trans('Bubble Frame'),
                 6 => trans('Entering Special Effects'),
+                28 => trans('profile frame'),
             ]
         );
 
@@ -36,7 +37,7 @@ class Wares extends Selectable
             $filter->column(1 / 2, function ($filter) {
 
                 $filter->equal('type', __('type'))->select([
-                   
+
                     4 => trans('Avatar Frame'),
                     5 => trans('Bubble Frame'),
                     6 => trans('Entering Special Effects'),
