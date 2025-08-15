b1a9fc2cd1af6cd7f6e1fd5cbced232069280989
johnghattas <john.backend@utdsoftware.com>
2025-08-15 18:32:07 +0300

    update boom

--- Modules/RoomBoom/Transformers/RoomBoomRewardResource.php
+++ Modules/RoomBoom/Transformers/RoomBoomRewardResource.php
@@ -40,7 +40,7 @@ class RoomBoomRewardResource extends JsonResource
     {
         if ($this->target_type == 'gift') {
             $gift = $this->getGift();
-            return $gift->image_type;
+            return $gift?->image_type;
         }
     }
 
@@ -48,10 +48,10 @@ class RoomBoomRewardResource extends JsonResource
     {
         if ($this->target_type == 'ware') {
             $ware = $this->getWare();
-            $path = $ware->show_img ?? $ware?->img2;
+            $path = $ware?->show_img ?? $ware?->img2;
         }  elseif ($this->target_type == 'gift') {
             $gift = $this->getGift();
-            $path = $gift->show_img ?? $gift?->img;
+            $path = $gift?->show_img ?? $gift?->img;
         } elseif ($this->target_type == 'achievement') {
             $path = $this->target;
         }
