c5d01347b93d421cf7f15ab364aa64c3ebd5ce45
johnghattas <john.backend@utdsoftware.com>
2025-08-15 17:48:33 +0300

    Update PerviousOneWeeklyCpResource.php

--- Modules/CP/Http/Resources/PerviousOneWeeklyCpResource.php
+++ Modules/CP/Http/Resources/PerviousOneWeeklyCpResource.php
@@ -16,12 +16,12 @@ class PerviousOneWeeklyCpResource extends JsonResource
         return [
 
             'id' => $cp?->id ?? 0,
-            'user_one_id' => $cp?->userOne->id ?? 0,
-            'user_one_name' => $cp?->userOne->name ?? '',
-            'user_one_image' => $cp?->userOne->profile->avatar ?? '',
-            'user_two_id' => $cp?->userTwo->id ?? 0,
-            'user_two_name' => $cp?->userTwo->name ?? '',
-            'user_two_image' => $cp?->userTwo->profile->avatar ?? '',
+            'user_one_id' => $cp?->fromUser->id ?? 0,
+            'user_one_name' => $cp?->fromUser->name ?? '',
+            'user_one_image' => $cp?->fromUser->profile->avatar ?? '',
+            'user_two_id' => $cp?->toUser->id ?? 0,
+            'user_two_name' => $cp?->toUser->name ?? '',
+            'user_two_image' => $cp?->toUser->profile->avatar ?? '',
             'total_gift_price' =>numToString(intval( $this?->total_price ?? 0)) ,
             'level' => @$cp?->level ?? 0,
         ];
