f6b099dc86d6c5b9ad7ac960a996809a3896d91b
shami2024 <>
2025-08-17 09:58:17 +0300

    Update CpRepository.php

--- Modules/CP/Repositories/CpRepository.php
+++ Modules/CP/Repositories/CpRepository.php
@@ -243,27 +243,30 @@ class CpRepository
 
     public function getCpRanking($relationType, $type)
     {
-        return GiftLog::query()
-            ->selectRaw('cp_id, SUM(giftNum * giftPrice) as total_gifts')
+        $query = GiftLog::query()
+            ->select('cp_id')
+            ->selectRaw('SUM(giftNum * giftPrice) as total_gifts')
             ->whereNotNull('cp_id')
             ->whereHas('cp.relation', fn($q) => $q->where('type', $relationType))
-            ->with([
-                'cp:id,di,level_id,user_one_id,user_two_id,cp_relation_id',
-                'cp.relation:id,type'
-            ])
             ->when($type, function ($query) use ($type) {
                 return match ($type) {
                     1 => $query->whereBetween('created_at', [now()->startOfDay(), now()->endOfDay()]),
                     2 => $query->whereBetween('created_at', [now()->startOfWeek(), now()->endOfWeek()]),
                     3 => $query->whereMonth('created_at', now()->month)
-                               ->whereYear('created_at', now()->year),
+                            ->whereYear('created_at', now()->year),
                     default => $query
                 };
             })
             ->groupBy('cp_id')
             ->orderByDesc('total_gifts')
-            ->limit(20)
-            ->get();
+            ->limit(20);
+
+        return $query
+            ->get()
+            ->load([
+                'cp:id,di,level_id,user_one_id,user_two_id,cp_relation_id',
+                'cp.relation:id,type'
+            ]);
     }
     
 
