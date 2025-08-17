763b69b1d244daaf78f8a643e3e0e21fadc02f3f
shami2024 <>
2025-08-17 09:53:51 +0300

    Update CpRepository.php

--- Modules/CP/Repositories/CpRepository.php
+++ Modules/CP/Repositories/CpRepository.php
@@ -241,40 +241,31 @@ class CpRepository
         ->get();
     }
 
-
     public function getCpRanking($relationType, $type)
     {
-        return GiftLog::selectRaw('
-                gift_logs.cp_id,
-                SUM(gift_logs.giftNum * gift_logs.giftPrice) as total_gifts,
-                cps.id as cp_id,
-                cps.di,
-                cps.level_id,
-                cps.user_one_id,
-                cps.user_two_id,
-                cps.cp_relation_id,
-                cp_relations.type as relation_type
-            ')
-            ->join('cps', 'gift_logs.cp_id', '=', 'cps.id')
-            ->join('cp_relations', 'cps.cp_relation_id', '=', 'cp_relations.id')
-            ->whereNotNull('gift_logs.cp_id')
-            ->where('cp_relations.type', $relationType)
+        return GiftLog::query()
+            ->selectRaw('cp_id, SUM(giftNum * giftPrice) as total_gifts')
+            ->whereNotNull('cp_id')
+            ->whereHas('cp.relation', fn($q) => $q->where('type', $relationType))
+            ->with([
+                'cp:id,di,level_id,user_one_id,user_two_id,cp_relation_id',
+                'cp.relation:id,type'
+            ])
             ->when($type, function ($query) use ($type) {
-                switch ($type) {
-                    case 1: 
-                        return $query->whereBetween('gift_logs.created_at', [now()->startOfDay(), now()->endOfDay()]);
-                    case 2: 
-                        return $query->whereBetween('gift_logs.created_at', [now()->startOfWeek(), now()->endOfWeek()]);
-                    case 3: 
-                        return $query->whereMonth('gift_logs.created_at', now()->month)
-                                    ->whereYear('gift_logs.created_at', now()->year);
-                }
+                return match ($type) {
+                    1 => $query->whereBetween('created_at', [now()->startOfDay(), now()->endOfDay()]),
+                    2 => $query->whereBetween('created_at', [now()->startOfWeek(), now()->endOfWeek()]),
+                    3 => $query->whereMonth('created_at', now()->month)
+                               ->whereYear('created_at', now()->year),
+                    default => $query
+                };
             })
-            ->groupBy('gift_logs.cp_id')
+            ->groupBy('cp_id')
             ->orderByDesc('total_gifts')
-            ->take(20)
+            ->limit(20)
             ->get();
     }
+    
 
 
     public function getCpRankingWithOutRelation(int $type)
