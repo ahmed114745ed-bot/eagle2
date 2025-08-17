62ff2ad0f05b1532978fbd87ddb1a2b090626d82
shami2024 <>
2025-08-17 11:18:29 +0300

    CpRepository

--- Modules/CP/Repositories/CpRepository.php
+++ Modules/CP/Repositories/CpRepository.php
@@ -264,8 +264,7 @@ class CpRepository
                     2 => $query->whereBetween('gift_logs.created_at', [now()->startOfWeek(), now()->endOfWeek()]),
                     3 => $query->whereMonth('gift_logs.created_at', now()->month)
                                ->whereYear('gift_logs.created_at', now()->year),
-                    4 => $query->where('gift_logs.created_at', '>=', now()->subDays(7)),
-                    default => $query->whereBetween('gift_logs.created_at', [now()->startOfWeek(), now()->endOfWeek()]),
+                    default => $query
                 };
             })
             ->groupBy(
