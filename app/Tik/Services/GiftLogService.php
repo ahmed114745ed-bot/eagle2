ffdacc6120fc04e715ea1f720a1b53ffad2cefb2
shami2024 <>
2025-08-17 15:24:03 +0300

    Update GiftLogService.php

--- app/Tik/Services/GiftLogService.php
+++ app/Tik/Services/GiftLogService.php
@@ -17,6 +17,7 @@ use GuzzleHttp\Promise\Utils;
 use App\Events\GiftBannerEvent;
 use App\Jobs\UpdatePkAndSendToZigo;
 use App\Classes\Gifts\SendGiftService;
+use Illuminate\Support\Facades\DB;
 use Modules\Charizma\Jobs\UpdateSendCharismaToZigo;
 use Modules\CP\Http\Services\CpService;
 use App\Exceptions\NotInfMoneyException;
