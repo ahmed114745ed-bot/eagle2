d1db04f3cfa05eab3c5e358f3d3ae8a773a0ef90
Bola <bolaphilip3@gmail.com>
2025-08-17 14:46:04 +0300

    check in migration

--- Modules/Vip/Database/Migrations/2025_08_14_133348_add_columns_to_users_vips.php
+++ Modules/Vip/Database/Migrations/2025_08_14_133348_add_columns_to_users_vips.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users_vips', function (Blueprint $table) {
-            $table->softDeletes();
+            if (! Schema::hasColumn('users_vips', 'deleted_at')){
+                $table->softDeletes();
+            }
         });
     }
 
