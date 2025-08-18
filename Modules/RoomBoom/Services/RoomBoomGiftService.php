4cc4e62a13ecf4850633cb993aff3e966514ed64
Bola <bolaphilip3@gmail.com>
2025-08-18 18:07:33 +0300

    level inrerval check max as well

--- Modules/RoomBoom/Services/RoomBoomGiftService.php
+++ Modules/RoomBoom/Services/RoomBoomGiftService.php
@@ -31,7 +31,7 @@ class RoomBoomGiftService
         }
 
         $currentLevel = RoomBoomLevel::where('min_target', '<=', $newTotal)
-            ->where('target', '>=', $newTotal)
+            ->where('target', '>=', $currentTotal)
             ->orderBy('level')
             ->first();
 
