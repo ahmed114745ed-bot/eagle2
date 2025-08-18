b14845d8d60b4ce34f180e7c32404fc8a55330cc
Erinyelkess <ariny.backend@utdsoftware.com>
2025-08-18 10:02:32 +0300

    di

--- Modules/CP/Transformers/RankingResource.php
+++ Modules/CP/Transformers/RankingResource.php
@@ -17,7 +17,7 @@ class RankingResource extends JsonResource
                 'id' => $this->level_id,
                 'img' => $this->level?->img
             ],
-            'exp'           => $this->total_gifts,
+            'exp'           => $this->di,
             "userOne"       => [
                 "id"        => $this->fromUser?->id,
                 "uid"       => $this->fromUser?->uuid,
