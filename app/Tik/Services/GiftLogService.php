be8ac5457b32e3b51ca85edc4f046e5b5c55ad89
Bola <bolaphilip3@gmail.com>
2025-08-18 09:58:04 +0300

    change reward to pusher

--- app/Tik/Services/GiftLogService.php
+++ app/Tik/Services/GiftLogService.php
@@ -16,7 +16,6 @@ use GuzzleHttp\Promise\Utils;
 use App\Events\GiftBannerEvent;
 use App\Jobs\UpdatePkAndSendToZigo;
 use App\Classes\Gifts\SendGiftService;
-use Illuminate\Support\Facades\DB;
 use Modules\Charizma\Jobs\UpdateSendCharismaToZigo;
 use Modules\CP\Http\Services\CpService;
 use App\Tik\Repositories\GiftRepository;
