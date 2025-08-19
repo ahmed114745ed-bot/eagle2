cde749ee05b55c322581bfc0d02aac16683c3850
Bola <bolaphilip3@gmail.com>
2025-08-19 12:01:35 +0300

    room boom

--- app/Services/PayPalService.php
+++ app/Services/PayPalService.php
@@ -51,7 +51,7 @@ class PayPalService
                 'return_url'  => url("/api/paypal-return/$referenceId"),
                 'cancel_url'  => url('/api/paypal-cancel'),
                 'user_action' => 'PAY_NOW',
-                'landing_page' => 'BILLING',
+
             ],
             "purchase_units" => [
                 [
