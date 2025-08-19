998b35eab2cdb831a9228af5b4ca19723aefaff2
Bola <bolaphilip3@gmail.com>
2025-08-19 15:55:20 +0300

    remove logs

--- app/Services/PayPalService.php
+++ app/Services/PayPalService.php
@@ -51,7 +51,7 @@ class PayPalService
                 'return_url'  => url("/api/paypal-return/$referenceId"),
                 'cancel_url'  => url('/api/paypal-cancel'),
                 'user_action' => 'PAY_NOW',
-                'landing_page' => 'BILLING',
+                'landing_page' => 'GUEST_CHECKOUT',
                 'shipping_preference' => 'NO_SHIPPING',
             ],
             "purchase_units" => [
