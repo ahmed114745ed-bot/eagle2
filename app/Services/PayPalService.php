fb7b5203677848c1dff03b8f54d9e75728fc8587
shami2024 <>
2025-08-20 17:45:27 +0300

    Update PayPalService.php

--- app/Services/PayPalService.php
+++ app/Services/PayPalService.php
@@ -59,7 +59,15 @@ class PayPalService
                     "amount"       => [
                         "currency_code" => config('paypal.currency'),
                         "value"         => number_format($amount, 2),
-                    ]
+                    ],
+                    "shipping" => [
+                        "address" => [
+                            "address_line_1" => "Test Street",
+                            "admin_area_2"   => "London",
+                            "postal_code"    => "12345",
+                            "country_code"   => "GB" 
+                        ]
+                        ],
                 ]
             ],
         ];
