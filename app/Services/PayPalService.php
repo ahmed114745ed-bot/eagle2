fa03e197e7c9dbb2b1237591cc1e56e14905602c
shami2024 <>
2025-08-20 15:26:53 +0300

    Update PayPalService.php

--- app/Services/PayPalService.php
+++ app/Services/PayPalService.php
@@ -52,7 +52,7 @@ class PayPalService
                 'cancel_url'          => url('/api/paypal-cancel'),
                 'user_action'         => 'PAY_NOW',
                 'shipping_preference' => 'NO_SHIPPING',
-                'landing_page'        => 'BILLING',
+                'landing_page'        => 'NO_PREFERENCE',
             ],
             "purchase_units" => [
                 [
