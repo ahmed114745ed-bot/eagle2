b823e2407eabef7e6b26b14687a0b1158af3214e
Bola <bolaphilip3@gmail.com>
2025-08-20 17:15:39 +0300

    change last sender id logic

--- Modules/RoomBoom/Transformers/RoomBoomRewardResource.php
+++ Modules/RoomBoom/Transformers/RoomBoomRewardResource.php
@@ -51,7 +51,7 @@ class RoomBoomRewardResource extends JsonResource
             $path = $ware->show_img ?? $ware?->img2;
         }  elseif ($this->target_type == 'gift') {
             $gift = $this->getGift();
-            $path = $gift->img ?? $gift?->show_img;
+            $path = $gift->show_img ?? $gift?->img;
         } elseif ($this->target_type == 'achievement') {
             $path = $this->target;
         }
