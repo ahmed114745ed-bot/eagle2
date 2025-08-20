4b1bc42864a7c664d3af6a0e6ae9328f69055388
shami2024 <>
2025-08-20 15:18:00 +0300

    Update PayPalService.php

--- app/Services/PayPalService.php
+++ app/Services/PayPalService.php
@@ -51,8 +51,8 @@ class PayPalService
                 'return_url'          => url("/api/paypal-return/$referenceId"),
                 'cancel_url'          => url('/api/paypal-cancel'),
                 'user_action'         => 'PAY_NOW',
-                'landing_page'        => 'BILLING', 
-                'shipping_preference' => 'NO_SHIPPING',
+                'shipping_preference' => 'GET_FROM_FILE',
+                'landing_page'        => 'LOGIN',
             ],
             "purchase_units" => [
                 [
