ceca548d5f7d1a30216fea152a1ebf016a62e9f5
Bola <bolaphilip3@gmail.com>
2025-08-21 13:21:06 +0300

    test weekly cp

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -113,9 +113,6 @@ class Kernel extends ConsoleKernel
             ->everyThirtySeconds()
             ->runInBackground();
 
-        $schedule->command('weekly-cp-winner')
-            ->everyMinute();
-
         // $schedule->command('users:freeze-unfinished')
         //     ->everySecond()
         //     ->timezone(getTimezone())
