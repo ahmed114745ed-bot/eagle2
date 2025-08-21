42026e3668eabcf1ada74c990775bfed1550ae82
Bola <bolaphilip3@gmail.com>
2025-08-21 09:15:52 +0300

    gift input

--- Modules/RoomBoom/Http/Controllers/web/RoomBoomRewardController.php
+++ Modules/RoomBoom/Http/Controllers/web/RoomBoomRewardController.php
@@ -226,12 +226,10 @@ class RoomBoomRewardController extends MainController
 
         $form->select('target', __('Gift'))
             ->options(function ($id) {
-                $query = Gift::query()->pluck('name', 'id');
-
                 if ($id) {
                     $gift = Gift::find($id);
-                    if (!$query->has($gift->id)) {
-                        $query[$gift->id] = "{$gift->name}_{$gift->id}";
+                    if ($gift) {
+                        return [$gift->id => "{$gift->name}_{$gift->id}"];
                     }
                 }
                 return Gift::pluck('name', 'id');
