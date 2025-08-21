78ae6df1fec6cd984268ba854a3dfcdbe53849a8
shami2024 <>
2025-08-21 09:51:34 +0300

    paymentLink

--- app/Services/PayPalService.php
+++ app/Services/PayPalService.php
@@ -8,14 +8,29 @@ use App\Traits\User\PaymentTrait;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use PayPalCheckoutSdk\Core\PayPalHttpClient;
+use PayPalCheckoutSdk\Core\SandboxEnvironment;
+use PayPalCheckoutSdk\Orders\OrdersCreateRequest;
 
 class PayPalService
 {
     use PaymentTrait;
+
+
+    public function __construct()
+{
+    $clientId = config('paypal.client_id');
+    $clientSecret = config('paypal.client_secret');
+    $environment = new SandboxEnvironment($clientId, $clientSecret);
+    $this->client = new PayPalHttpClient($environment);
+}
+
    public static function redirectUrl()
    {
     return url("/admin/payment-with-method");
    }
+
+
 
     protected function getAccessToken(): string
     {
@@ -81,13 +96,46 @@ class PayPalService
             foreach ($response['links'] as $link) {
                 if ($link['rel'] === 'approve') {
                     $paymentLink = $link['href'];
-                    $paymentLink2 = $link['href'] . (str_contains($link['href'], '?') ? '&' : '?') . 'fundingSource=card&intent=capture&locale.x=en_IS';
                 }
             }
         }
-        return view('paypal_buttons', compact('paymentLink', 'paymentLink2'));
+        return $paymentLink;
+    }
 
-        // return $paymentLink;
+
+
+
+    public function createOrder($referenceId, $amount, $user)
+    {
+        $request = new OrdersCreateRequest();
+        $request->prefer('return=representation');
+        $request->body = [
+            "intent" => "CAPTURE",
+            "purchase_units" => [[
+                "reference_id" => $referenceId,
+                "amount" => [
+                    "currency_code" =>config('paypal.currency'),
+                    "value" => $amount
+                ]
+            ]],
+            "application_context" => [
+                "brand_name" => config('app.name'),
+                "landing_page" => "BILLING",
+                "user_action" => "PAY_NOW",
+                "return_url" => route('paypal.success'),
+                "cancel_url" => route('paypal.cancel')
+            ]
+        ];
+
+        $response = $this->client->execute($request);
+
+        foreach ($response->result->links as $link) {
+            if ($link->rel === 'approve') {
+                return $link->href;
+            }
+        }
+
+        throw new \Exception("PayPal approval link not found");
     }
 
     /**
