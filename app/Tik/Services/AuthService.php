bfe4489be19aa5ca0e78bee2c77d69e28e0f1764
Erinyelkess <ariny.backend@utdsoftware.com>
2025-08-21 14:32:55 +0300

    edit login

--- app/Tik/Services/AuthService.php
+++ app/Tik/Services/AuthService.php
@@ -139,12 +139,12 @@ class AuthService
         $client = new Google_Client();
 
         $client->setClientId("813834667937-svjtqjn4plrl84c3egcc9qd233864hv1.apps.googleusercontent.com");
-        // if (!$request['id_token']) throw new \Exception('google id token missing');
-        // $payload = $client->verifyIdToken($request['id_token']);
-        // if (!$payload) {
-        //     throw new \Exception('Google ID Token not found or invalid');
-        // }
-        // $google_id = $payload['sub'];
+        if (!$request['id_token']) throw new \Exception('google id token missing');
+        $payload = $client->verifyIdToken($request['id_token']);
+        if (!$payload) {
+            throw new \Exception('Google ID Token not found or invalid');
+        }
+        $google_id = $payload['sub'];
 
         $user = $this->userRepository->findByGoogleId($request['google_id']);
         $is_new = false;
