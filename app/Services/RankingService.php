ca1338b9eb4b3f6e6e44db33054405d161b36a17
Bola <bolaphilip3@gmail.com>
2025-08-21 18:02:03 +0300

    rank resource

--- app/Services/RankingService.php
+++ app/Services/RankingService.php
@@ -509,7 +509,7 @@ class RankingService
 
         $otherData = $countData < 4 ? [] : array_slice($dataArray, 3);
 
-        $perPage = request('per_page');
+        $perPage = request('per_page', 10);
         $currentPage = LengthAwarePaginator::resolveCurrentPage() ?: 1;
 
         $currentItems = array_slice($otherData, ($currentPage - 1) * $perPage, $perPage);
