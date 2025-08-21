dd859e5159c86d73358435c78b659d4caa2def8c
Bola <bolaphilip3@gmail.com>
2025-08-21 11:30:27 +0300

    logs

--- Modules/RoomBoom/Entities/RoomBoomReward.php
+++ Modules/RoomBoom/Entities/RoomBoomReward.php
@@ -63,18 +63,14 @@ class RoomBoomReward extends Model
                     if (request()->hasFile('achievement_target')) {
                         $file = request()->file('achievement_target');
 
-                        info('has file');
                         if ($file instanceof UploadedFile) {
                             $url = Common::upload('roomBoom', $file);
-
-                            info($url);
                             $model->target = $url;
                         }
+                    } else {
+                        $model->target = $model->getOriginal('target');
                     }
-//                    if (isset($model->achievement_target)) {
-//                        $model->target = $model->achievement_target;
-//                        unset($model->achievement_target);
-//                    }
+
                     unset($model->achievement_target);
                     break;
             }
