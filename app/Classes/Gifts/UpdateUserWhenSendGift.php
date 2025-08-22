8c6867c92082e1c334f6b8bb157c6b6ef8f7a361
johnghattas <john.backend@utdsoftware.com>
2025-08-22 19:22:19 +0300

    update send bag

--- app/Classes/Gifts/UpdateUserWhenSendGift.php
+++ app/Classes/Gifts/UpdateUserWhenSendGift.php
@@ -6,6 +6,7 @@ use App\Classes\Enums\NotificationType;
 use App\Exceptions\NotInfMoneyException;
 use App\Jobs\SendCustomOfficialMessageToUser;
 use App\Models\User;
+use Mockery\Exception;
 use Modules\Vip\Entities\Vip;
 use App\Models\UserGift;
 use Illuminate\Support\Facades\Config;
@@ -133,6 +134,9 @@ class UpdateUserWhenSendGift
         return $senderUser;
     }
 
+    /**
+     * @throws \Throwable
+     */
     public function sendFromBagAndRemoveGift(int $totalCoins, User $senderUser, int $giftId, int $number)
     {
         $senderUser->enableSaving = false;
@@ -143,17 +147,22 @@ class UpdateUserWhenSendGift
 
         $userGift = UserGift::where('user_id', $senderUser->id)
             ->where('gift_id', $giftId)
-            ->first();
+            ->where(function($query) {
+                $query->where('expire', 0)
+                    ->orWhereRaw('DATE_ADD(created_at, INTERVAL expire DAY) >= NOW()');
+            })->first();
 
-        if ($userGift) {
-            $userGift->quantity -= $number;
 
-            if ($userGift->quantity > 0) {
-                $userGift->save();
-            } else {
-                $userGift->delete();
-            }
+        throw_if(( !$userGift), \Exception::class, 'Receiver has reached maximum allowed gifts');
+
+        $userGift->quantity -= $number;
+
+        if ($userGift->quantity > 0) {
+            $userGift->save();
+        } else {
+            $userGift->delete();
         }
+
 
         (new UpgradeLevelServices())->checkUserLevelUpgrated($senderUser);
         if ($senderUser->total_sender_level > $lastSenderUser) {
