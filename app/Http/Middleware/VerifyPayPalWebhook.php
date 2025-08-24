481a3186735d2f69f2c4b78ac666210f9237ea6c
Bola <bolaphilip3@gmail.com>
2025-08-24 16:05:56 +0300

    reward refactor

--- app/Http/Middleware/VerifyPayPalWebhook.php
+++ app/Http/Middleware/VerifyPayPalWebhook.php
@@ -34,8 +34,6 @@ class VerifyPayPalWebhook extends PayPalService
         $response = Http::withToken($accessToken)
             ->post(config('paypal.base_url') . '/v1/notifications/verify-webhook-signature', $verificationData);
 
-        info('response', [$response]);
-
         if ($response->json('verification_status') !== 'SUCCESS') {
             return response()->json(['status' => 'unauthorized'], 401);
         }
