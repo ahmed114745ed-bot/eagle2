7a4d807d5c4a0a38872c46bed5232800b0765283
Bola <bolaphilip3@gmail.com>
2025-08-24 13:59:37 +0300

    smart paypal checkout

--- app/Services/PayPalService.php
+++ app/Services/PayPalService.php
@@ -66,8 +66,8 @@ class PayPalService
                 'return_url'  => url("/api/paypal-return/$referenceId"),
                 'cancel_url'  => url('/api/paypal-cancel'),
                 'user_action' => 'PAY_NOW',
-                'shipping_preference' => 'NO_SHIPPING',
-                'landing_page' => 'BILLING',
+//                'shipping_preference' => 'NO_SHIPPING',
+//                'landing_page' => 'BILLING',
             ],
             "purchase_units" => [
                 [
