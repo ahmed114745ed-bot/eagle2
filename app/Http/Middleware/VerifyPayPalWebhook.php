ea9f274c04231fdb2271d179895961aeeaea5175
Bola <bolaphilip3@gmail.com>
2025-08-24 13:08:31 +0300

    smart paypal checkout

--- app/Http/Middleware/VerifyPayPalWebhook.php
+++ app/Http/Middleware/VerifyPayPalWebhook.php
@@ -34,7 +34,7 @@ class VerifyPayPalWebhook extends PayPalService
         $response = Http::withToken($accessToken)
             ->post(config('paypal.base_url') . '/v1/notifications/verify-webhook-signature', $verificationData);
 
-        info('accessToken', [$response]);
+        info('response', [$response]);
 
         if ($response->json('verification_status') !== 'SUCCESS') {
             return response()->json(['status' => 'unauthorized'], 401);
