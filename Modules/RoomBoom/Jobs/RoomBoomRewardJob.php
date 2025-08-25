d35ff48e69c6b33eb8252fb0d4c135d0ad6615b1
Bola <bolaphilip3@gmail.com>
2025-08-25 12:04:48 +0300

    room boom job edit

--- Modules/RoomBoom/Jobs/RoomBoomRewardJob.php
+++ Modules/RoomBoom/Jobs/RoomBoomRewardJob.php
@@ -55,7 +55,9 @@ class RoomBoomRewardJob implements ShouldQueue
 
         $rewards = RoomBoomReward::where('room_boom_level_id', $level->id)->orderBy('priority')->get();
 
-        $rewardItems[] = $this->getRewardItems($rewards);
+        $rewardItems = [];
+
+        $this->getRewardItems($rewards, $rewardItems);
 
         $topContributorIds = $this->getTopContributorIds($roomId, $level->level);
 
@@ -207,11 +209,11 @@ class RoomBoomRewardJob implements ShouldQueue
         }
     }
 
-    public function getRewardItems($rewards)
+    public function getRewardItems($rewards, &$rewardItems): void
     {
         foreach ($rewards as $reward) {
             for ($i = 0; $i < $reward->quantity; $i++) {
-                return [
+                $rewardItems[] = [
                     'id' => $reward->id,
                     'target_type' => $reward->target_type,
                     'target' => $reward->target,
@@ -279,7 +281,6 @@ class RoomBoomRewardJob implements ShouldQueue
     protected function dispatchPendingNotifications(): void
     {
         if (!empty($this->achievementNotifications)) {
-            info($this->achievementNotifications['tokens']);
             Common::send_firebase_notification(
                 $this->achievementNotifications['tokens'],
                 $this->achievementNotifications['title'],
@@ -288,7 +289,6 @@ class RoomBoomRewardJob implements ShouldQueue
         }
 
         if (!empty($this->giftNotifications)) {
-            info($this->giftNotifications['tokens']);
             Common::send_firebase_notification(
                 $this->giftNotifications['tokens'],
                 $this->giftNotifications['title'],
