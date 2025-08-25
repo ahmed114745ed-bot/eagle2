34ffc44da589c2c6fe4483c7f34f9017731b41f9
Erinyelkess <ariny.backend@utdsoftware.com>
2025-08-25 17:36:55 +0300

    edit

--- app/Observers/AgencyObserver.php
+++ app/Observers/AgencyObserver.php
@@ -25,7 +25,7 @@ class AgencyObserver
         User::query()->where('id', $agency->app_owner_id)->update($updateData);
 
         $updateDataMonth['monthly_diamond_received'] = 0;
-        $userDiamond =   MonthlyDiamondReceive::query()->where('id', $agency->app_owner_id)->where('month', now()->month)->where('year', now()->year)->first();
+        $userDiamond =   MonthlyDiamondReceive::query()->where('user_id', $agency->app_owner_id)->where('month', now()->month)->where('year', now()->year)->first();
         if ($userDiamond) $userDiamond->update($updateDataMonth);
     }
 
