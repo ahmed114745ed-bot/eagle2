1fcf7a1d86df2baa7527bfd7cc71c429d38c7822
Abdallah  Ahmed Abdelfattah Abdallah <abdalla.dev@utdsoftware.com>
2025-08-25 18:07:35 +0300

    Update app.php

--- config/app.php
+++ config/app.php
@@ -221,7 +221,7 @@ return [
         App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\TelescopeServiceProvider::class,
+//       App\Providers\TelescopeServiceProvider::class,
         App\Providers\ConfigServiceProvider::class,
 //        App\Providers\ChatServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
