79e62ef928be7c93c5230f6ada6feb605436f9bf
makled98 <mkled.backend@utdsoftware.com>
2025-08-25 17:00:57 +0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -222,7 +222,7 @@ class User extends Authenticatable
 
     public function setMonthlyDiamondReceivedAttribute($value)
     {
-        $date = Carbon::now()->setTimezone(Common::timeZone());
+        $date = \Carbon\Carbon::now(getTimezone());;
 
         $record = $this->monthlyDiamondReceive()
             ->firstOrNew([
