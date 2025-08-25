c695344f0b1203b1ad383396efc05466c3ff9327
johnghattas <john.backend@utdsoftware.com>
2025-08-25 15:06:06 +0300

    Update SendFirebaseNotificationJob.php

--- app/Jobs/SendFirebaseNotificationJob.php
+++ app/Jobs/SendFirebaseNotificationJob.php
@@ -46,6 +46,7 @@ class SendFirebaseNotificationJob implements ShouldQueue
 
         $users =  User::select(['id', 'notification_id'])->whereIn('notification_id',$this->tokens)
             ->where('is_logout', 0)
+            ->where('notification_id', '!=', null)
             ->get();
 
         $hasInPack = $this->user && Common::hasInPack($this->user->id, 18, true);
