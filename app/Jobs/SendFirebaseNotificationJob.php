4fa91a6a5bb56cd8273ee7df2bb2db1d67f65137
Bola <bolaphilip3@gmail.com>
2025-08-25 14:05:17 +0300

    test notifications

--- app/Jobs/SendFirebaseNotificationJob.php
+++ app/Jobs/SendFirebaseNotificationJob.php
@@ -34,15 +34,15 @@ class SendFirebaseNotificationJob implements ShouldQueue
         $api_access_key = Common::getPublicGoogleAccessToken();
         $projectId = env('FIREBASE_PROJECT_NAME');
 
-//        $client = new Client([
-//            'base_uri' => "https://fcm.googleapis.com/v1/projects/{$projectId}/",
-//            'headers'  => [
-//                'Authorization' => 'Bearer ' . $api_access_key,
-//                'Content-Type'  => 'application/json',
-//            ]
-//        ]);
-//
-//        $promises = [];
+        $client = new Client([
+            'base_uri' => "https://fcm.googleapis.com/v1/projects/{$projectId}/",
+            'headers'  => [
+                'Authorization' => 'Bearer ' . $api_access_key,
+                'Content-Type'  => 'application/json',
+            ]
+        ]);
+
+        $promises = [];
 
         foreach ($this->tokens as $token) {
             $user= User::where('notification_id',$token)->first();
@@ -95,19 +95,19 @@ class SendFirebaseNotificationJob implements ShouldQueue
                 $payload['notification']['image'] = $this->data['image'];
             }
 
-//            $promises[$token] = $client->postAsync("messages:send", [
-//                'json' => ['message' => $payload]
-//            ]);
+            $promises[$token] = $client->postAsync("messages:send", [
+                'json' => ['message' => $payload]
+            ]);
 
-            $headers = [
-                'Authorization' => 'Bearer ' . $api_access_key,
-                'Content-Type'  => 'application/json',
-            ];
-
-            if (!$user->is_logout)     Http::withHeaders($headers)->post(
-                "https://fcm.googleapis.com/v1/projects/{$projectId}/messages:send",
-                ['message' => $payload]
-           );
+//            $headers = [
+//                'Authorization' => 'Bearer ' . $api_access_key,
+//                'Content-Type'  => 'application/json',
+//            ];
+//
+//            if (!$user->is_logout)     Http::withHeaders($headers)->post(
+//                "https://fcm.googleapis.com/v1/projects/{$projectId}/messages:send",
+//                ['message' => $payload]
+//           );
         }
 
         $end = microtime(true);
