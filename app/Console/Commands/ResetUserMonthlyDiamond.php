ee1eaa2accb482681e7784f7235151ffa77fbaf4
makled98 <mkled.backend@utdsoftware.com>
2025-08-26 10:52:18 +0300

    Update ResetUserMonthlyDiamond.php

--- app/Console/Commands/ResetUserMonthlyDiamond.php
+++ app/Console/Commands/ResetUserMonthlyDiamond.php
@@ -57,11 +57,11 @@ class ResetUserMonthlyDiamond extends Command
 
 
 
-            DB::statement("
-            UPDATE users
-            SET monthly_diamond_received = 0
-            WHERE agency_id != 0
-        ");
+        //     DB::statement("
+        //     UPDATE users
+        //     SET monthly_diamond_received = 0
+        //     WHERE agency_id != 0
+        // ");
 
 
         }catch (\Exception $exception){
