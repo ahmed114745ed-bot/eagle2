1284b6b9f65489ea34e5db91fc51d32c00641cb1
shami2024 <>
2025-08-26 14:11:02 +0300

    Update CpserviceCo.php

--- Modules/CP/Http/Services/CpserviceCo.php
+++ Modules/CP/Http/Services/CpserviceCo.php
@@ -48,16 +48,23 @@ class CpserviceCo
             }
         }
 
-        if ($cpRelation->cp_one == 1) {
+        if ($cpRelation == 1) {
             $existingCpOne = $this->cpRepository->checkExistingCpSendingOne($user->id, $cpRelation->id);
             $existingCptwo = $this->cpRepository->checkExistingCpSendingTwo($request->user_id, $cpRelation->id);
 
+        
             if ($existingCpOne) {
+                Log::info("CP Request blocked: User {$user->id} already sent CP to {$cpRelation->id}");
                 return Common::apiResponse(0, __('You have already sent a CP request before.'));
             }
+
             if ($existingCptwo) {
+                Log::info("CP Request blocked: User {$request->user_id} already received CP from {$cpRelation->id}");
                 return Common::apiResponse(0, __('They have already sent a CP request to you, please accept it.'));
             }
+
+            Log::info("CP Request sent: User {$user->id} is sending CP to {$request->user_id}");
+
         }
         $existingCp = $this->cpRepository->checkExistingCp($user->id, $request->user_id);
         if ($existingCp && $existingCp->status == CpStatus::PENDING->value && $cpRelation->type != 'solution') {
