65712fcdea04231a413bd738565d76914ee4bf7f
shami2024 <>
2025-08-26 19:20:55 +0300

    Update StripeService.php

--- app/Services/StripeService.php
+++ app/Services/StripeService.php
@@ -26,7 +26,7 @@ class StripeService {
                 'payment_method_types' => ['card'],
                 'line_items' => [[
                     'price_data' => [
-                        'currency' =>  $stripe_currency?->value ?? 'usd',
+                        'currency' =>   'usd',
                         'product_data' => [
                             'name' => $request->product_name,
                         ],
