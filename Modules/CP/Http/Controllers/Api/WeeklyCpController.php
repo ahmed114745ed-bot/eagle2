8facc6a51a2805e530bddee14d65ced923257e54
Erinyelkess <ariny.backend@utdsoftware.com>
2025-08-26 11:35:22 +0300

    edit

--- Modules/CP/Http/Controllers/Api/WeeklyCpController.php
+++ Modules/CP/Http/Controllers/Api/WeeklyCpController.php
@@ -55,7 +55,6 @@ class WeeklyCpController extends Controller
     {
         try {
             $data = $this->weeklyCpService->topOneCurrentWeeklyCp();
-            dd( $data);
         } catch (Exception $e) {
             return Common::apiResponse(0, $e->getMessage(), 422);
         }
