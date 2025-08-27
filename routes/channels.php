e6cef5d3be464bd5b561004d21649d973715fd8f
Bola <bolaphilip3@gmail.com>
2025-08-27 13:43:52 +0300

    presence

--- routes/channels.php
+++ routes/channels.php
@@ -52,7 +52,7 @@ Broadcast::channel('presence.user.{id}', function ($user, $id) {
         : false;
 });
 
-Broadcast::channel('room.boom.rewards.{roomId}', function ($user, $roomId) {
+Broadcast::channel('presence.room.boom.rewards.{roomId}', function ($user, $roomId) {
     $isInRoom = RoomVisitor::where('room_id', $roomId)
         ->where('user_id', $user->id)
         ->exists();
