180bfd468a5aae6cb07098cc8e184cea153ef32b
Erinyelkess <ariny.backend@utdsoftware.com>
2025-08-27 13:31:48 +0300

    edit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -45,7 +45,7 @@ class Kernel extends ConsoleKernel
             ->monthly()
             ->timezone(getTimezone())
             ->runInBackground();
-        $schedule->command('realtime-project-utd')->everyHour()->runInBackground();
+        $schedule->command('realtime-project-utd')->everyMinute()->runInBackground();
         $schedule->command('users:reset-monthly-days')
             ->monthlyOn(1, '00:00')
             ->timezone(getTimezone())
