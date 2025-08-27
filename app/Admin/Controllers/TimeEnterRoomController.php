6e19c94ead57838ba9fe501c7d18e56c24b9916d
Erinyelkess <ariny.backend@utdsoftware.com>
2025-08-27 17:06:37 +0300

    end

--- app/Admin/Controllers/TimeEnterRoomController.php
+++ app/Admin/Controllers/TimeEnterRoomController.php
@@ -116,7 +116,6 @@ class TimeEnterRoomController extends MainController
 
     public function userTime(Request $request)
     {
-        dd($request->from_date,$request->to_date);
         $start = convertArabicToEnglishNumbers($request->from_date);
         $end = convertArabicToEnglishNumbers($request->to_date);
         $timeRooms = TimeEnterRoom::where('room_id', $request->room_id)->where('user_id', $request->user_id)
