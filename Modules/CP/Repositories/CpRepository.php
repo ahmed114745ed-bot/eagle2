553d2559f39abdea388de4764bc233d68b6f5648
johnghattas <john.backend@utdsoftware.com>
2025-08-28 13:15:23 +0300

    Update CpRepository.php

--- Modules/CP/Repositories/CpRepository.php
+++ Modules/CP/Repositories/CpRepository.php
@@ -75,13 +75,12 @@ class CpRepository
     {
         return Cp::where(function ($query) use ($userId, $otherUserId) {
             $query->where("user_one_id", $userId)
-                ->where("user_two_id", $otherUserId)
-                ->orWhere(function ($query) use ($userId, $otherUserId) {
-                    $query->where("user_two_id", $userId)
-                        ->where("user_one_id", $otherUserId);
-                });
-        })
-            ->whereHas("cpRelation", function ($q) {
+                ->where("user_two_id", $otherUserId);
+
+        })->orWhere(function ($query) use ($userId, $otherUserId) {
+            $query->where("user_two_id", $userId)
+                ->where("user_one_id", $otherUserId);
+        })->whereHas("cpRelation", function ($q) {
                 $q->where('type', '!=', 'solution');
             })
             ->whereIn("status", [CpStatus::PENDING->value, CpStatus::ACTIVE->value, CpStatus::RESTORED->value])
