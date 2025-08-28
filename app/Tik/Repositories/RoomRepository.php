058efbc11a37dcd7e367749a7dde8da557d95884
Bola <bolaphilip3@gmail.com>
2025-08-28 22:44:24 +0300

    room pagination

--- app/Tik/Repositories/RoomRepository.php
+++ app/Tik/Repositories/RoomRepository.php
@@ -249,7 +249,7 @@ class RoomRepository extends AbstractRepository
             $q->where('type', $roomType);
         })->when($roomType == 'live', function ($q) use ($roomType) {
             $q->whereIn('type', ['single_live', 'multi_live']);
-            })->simplePaginate(10);
+            })->paginate(10);
     }
 
     public function getRoomsByGameId($gameId = null, array $with = [])
