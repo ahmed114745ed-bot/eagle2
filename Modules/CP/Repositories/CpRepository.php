72cfdc069602fbfc9f221fb6c363b5aca65be393
johnghattas <john.backend@utdsoftware.com>
2025-08-28 13:24:10 +0300

    Update CpRepository.php

--- Modules/CP/Repositories/CpRepository.php
+++ Modules/CP/Repositories/CpRepository.php
@@ -73,7 +73,18 @@ class CpRepository
 
     public function checkExistingCp($userId, $otherUserId)
     {
-        return null;
+        return Cp::where(function ($query) use ($userId, $otherUserId) {
+            $query->where("user_one_id", $userId)
+                ->where("user_two_id", $otherUserId);
+
+        })->orWhere(function ($query) use ($userId, $otherUserId) {
+            $query->where("user_two_id", $userId)
+                ->where("user_one_id", $otherUserId);
+        })->whereHas("cpRelation", function ($q) {
+                $q->where('type', '!=', 'solution');
+            })
+            ->whereIn("status", [CpStatus::PENDING->value, CpStatus::ACTIVE->value, CpStatus::RESTORED->value])
+            ->first();
     }
 
     public function checkExistingSecondUserCp($otherUserId, $relationId)
