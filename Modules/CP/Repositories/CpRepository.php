91879757f89f1aba049b6cbf0643055fb20c2096
Erinyelkess <ariny.backend@utdsoftware.com>
2025-08-28 12:43:32 +0300

    edit

--- Modules/CP/Repositories/CpRepository.php
+++ Modules/CP/Repositories/CpRepository.php
@@ -313,7 +313,7 @@ class CpRepository
     {
         $t = is_numeric($type) ? (int) $type : null;
         $timezone = getTimezone();
-        $now = \Carbon\Carbon::now($timezone);
+        $now = \Carbon\Carbon::now();
 
         return GiftLog::query()
             ->selectRaw('
