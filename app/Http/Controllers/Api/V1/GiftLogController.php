95909da82168f40a9113bbbcb29bed468ed7c248
makled98 <mkled.backend@utdsoftware.com>
2025-08-30 19:53:38 +0300

    Update GiftLogController.php

--- app/Http/Controllers/Api/V1/GiftLogController.php
+++ app/Http/Controllers/Api/V1/GiftLogController.php
@@ -267,7 +267,7 @@ class GiftLogController extends Controller
     }
     public function gift_queue_cp(Request $request, UpdateUserWhenSendGift $updateUserWhenSendGift)
     {
-       Log::info('Sync request data:', $request->all());
+      dd($request->all());
         $close_open_gifts = settings()->get('close_open_gifts');
 
         if ($close_open_gifts == 1) {
