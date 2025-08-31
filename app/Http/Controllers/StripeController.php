046d76803176757b14f886c39b420ca12e3c3b40
shami2024 <>
2025-08-31 10:34:05 +0300

    Update StripeController.php

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -98,7 +98,7 @@ class StripeController extends Controller
     
                     if (!empty($session->metadata->order_id)) {
                         $orderId = $session->metadata->order_id;
-                        $trxId   = $session->id;
+                        $trxId   = $session->payment_intent;
                         Log::info("checkout.session.completed: order_id={$orderId}, trx={$trxId}");
                     } else {
                         $trxId   = $session->id;
