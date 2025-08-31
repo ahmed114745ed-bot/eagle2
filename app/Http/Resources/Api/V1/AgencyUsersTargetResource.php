43b82e169c7673942ae8e33794b5bab15a6dd225
shami2024 <>
2025-08-31 13:15:28 +0300

    Update AgencyUsersTargetResource.php

--- app/Http/Resources/Api/V1/AgencyUsersTargetResource.php
+++ app/Http/Resources/Api/V1/AgencyUsersTargetResource.php
@@ -103,7 +103,7 @@ class AgencyUsersTargetResource extends JsonResource
 
 
         $hasColor = Common::hasInPack(@$this->id, 18, true);
-
+        
         return [
             'id' => $this->id ?? 0,
             'name' => $this->name ?? '',
@@ -114,7 +114,7 @@ class AgencyUsersTargetResource extends JsonResource
             'id_image'             => @$this->specialId?->ware?->show_img ?? '',
             'salary' => (float) $agencySallary ?? 0,
             'target' => [
-                'id' => @$target->target_id ?? 0,
+                'id' => @$this->lastSallary?->target_id ?? 0,
                 'user_diamonds' => (float) ($this->lastSallary?->achieved_diamond ?? 0),
                 'user_hours'    =>  $this->lastSallary?->achieved_hours ?? 0,
                 'user_days'     =>  $this->lastSallary?->achieved_days ?? 0,
