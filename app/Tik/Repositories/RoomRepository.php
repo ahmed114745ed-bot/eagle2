f04764a3bd3d2ecac2325abe569a10346aa498ef
Bola <bolaphilip3@gmail.com>
2025-08-31 14:20:10 +0300

    optimize rooms but still pagination

--- app/Tik/Repositories/RoomRepository.php
+++ app/Tik/Repositories/RoomRepository.php
@@ -248,7 +248,7 @@ class RoomRepository extends AbstractRepository
             $q->where('type', $roomType);
         })->when($roomType == 'live', function ($q) use ($roomType) {
             $q->whereIn('type', ['single_live', 'multi_live']);
-            })->aginate(10);
+            })->paginate(10);
     }
 
     public function getRoomsByGameId($gameId = null, array $with = [])
