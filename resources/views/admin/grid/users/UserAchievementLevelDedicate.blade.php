4fc0d3d1ae2a023ee903906cfb06b294d91a7057
Erinyelkess <ariny.backend@utdsoftware.com>
2025-08-31 11:03:46 +0300

    test

--- resources/views/admin/grid/users/UserAchievementLevelDedicate.blade.php
+++ resources/views/admin/grid/users/UserAchievementLevelDedicate.blade.php
@@ -4,7 +4,7 @@
 <style>
 
     .uniform-image {
-    width: 350px;   /* fixed width */
+    width: 500px;   /* fixed width */
     height: 200px;  /* fixed height */
     object-fit: cover;   /* crop nicely without distortion */
     border-radius: 8px;  /* optional: rounded corners for nicer UI */
