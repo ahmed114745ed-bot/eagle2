ed87900f0cdf20deb71b1e484c06bcb37e750a9d
shami2024 <>
2025-08-31 17:53:38 +0300

    Update VipCommon.php

--- Modules/Vip/Helpers/VipCommon.php
+++ Modules/Vip/Helpers/VipCommon.php
@@ -238,7 +238,7 @@ class VipCommon
                 'vip_user_id' => $userVip->id,
                 'is_used'     => $userVip->is_used,
                 'using'       => 1,
-                'receive_type'=> 'vip-'.$userVip->id
+                'receive_type'=> 'vip-'.$userVip->level
 
            ]);
         }
