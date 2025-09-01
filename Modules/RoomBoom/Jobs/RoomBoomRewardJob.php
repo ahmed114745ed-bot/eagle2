8037488de5877d0bf291404bcb3d4413c5eed48b
Bola <bolaphilip3@gmail.com>
2025-09-01 14:23:09 +0300

    room boom

--- Modules/RoomBoom/Jobs/RoomBoomRewardJob.php
+++ Modules/RoomBoom/Jobs/RoomBoomRewardJob.php
@@ -186,9 +186,9 @@ class RoomBoomRewardJob implements ShouldQueue
             'updated_at' => now()
         ];
 
-        $this->giftNotifications['user_ids'][] = $userId;
+        $this->achievementNotifications['user_ids'][] = $userId;
         if ($token) {
-            $this->giftNotifications['tokens'][] = $token;
+            $this->achievementNotifications['tokens'][] = $token;
         }
     }
 
