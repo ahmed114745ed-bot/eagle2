c436ea82074b6802640312a2af4ef4f978ce9ee8
Bola <bolaphilip3@gmail.com>
2025-09-02 15:06:52 +0300

    room boom

--- Modules/RoomBoom/Jobs/RoomBoomRewardJob.php
+++ Modules/RoomBoom/Jobs/RoomBoomRewardJob.php
@@ -76,7 +76,7 @@ class RoomBoomRewardJob implements ShouldQueue
 
         $this->distributeTopContributors($topContributorIds, $rewardItems);
 
-        $this->distributeLastTriggerSender($lastTriggerSenderId, $topContributorIds, $rewards);
+        $this->distributeLastTriggerSender($lastTriggerSenderId, $topContributorIds, $rewards, $rewardItems);
 
         $this->distributeVisitorRewards($rewardItems, $room);
 
@@ -104,16 +104,16 @@ class RoomBoomRewardJob implements ShouldQueue
     /**
      * @throws \Exception
      */
-    public function distributeLastTriggerSender($lastTriggerSenderId, $topContributorIds, $rewards): void
+    public function distributeLastTriggerSender($lastTriggerSenderId, $topContributorIds, $rewards, &$rewardItems): void
     {
         if ($lastTriggerSenderId && !in_array($lastTriggerSenderId, $topContributorIds)) {
             if ($rewards->isNotEmpty()){
 
                 if (empty($topContributorIds)) {
-                    info('topContributorIds');
+                    info('not topContributorIds');
                     $chosenReward = $this->getNextAvailableReward($rewardItems);
                 } else {
-                    info('not topContributorIds');
+                    info('topContributorIds');
                     $chosenReward = $rewards->random();
                 }
 
