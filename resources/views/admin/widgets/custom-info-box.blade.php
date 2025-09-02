71577a1dbaf8cc1505fa281bb0288c1a790a817f
shami2024 <>
2025-09-02 14:49:19 +0300

    Update custom-info-box.blade.php

--- resources/views/admin/widgets/custom-info-box.blade.php
+++ resources/views/admin/widgets/custom-info-box.blade.php
@@ -1,4 +1,4 @@
-<div class="small-box bg-{{ $color }}" {!! $attributes !!} style="display:flex; align-items:center; justify-content:space-between; padding:15px;">
+<div class="small-box bg-{{ $color }}" {!! $attributes !!} style="display:flex; align-items:center; justify-content:space-between; padding:15px; overflow: scroll;">
     <div class="inner">
         <h3 style="margin:0;">{!! $info !!}</h3>
         <p style="margin:0;">{{ $name }}</p>
