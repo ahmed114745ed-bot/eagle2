94e75e6a334ef76d768dd34eeb40ff9f001fd9c5
shami2024 <>
2025-09-02 14:52:47 +0300

    Update custom-info-box.blade.php

--- resources/views/admin/widgets/custom-info-box.blade.php
+++ resources/views/admin/widgets/custom-info-box.blade.php
@@ -1,4 +1,5 @@
-<div class="small-box bg-{{ $color }}" {!! $attributes !!} style="display:flex; align-items:center; justify-content:space-between; padding:15px; overflow: scroll;">
+<div class="small-box bg-{{ $color }}" {!! $attributes !!} 
+     style="display:flex; align-items:center; justify-content:space-between; padding:15px; overflow: auto; scrollbar-width: none; -ms-overflow-style: none;">
     <div class="inner">
         <h3 style="margin:0;">{!! $info !!}</h3>
         <p style="margin:0;">{{ $name }}</p>
@@ -7,3 +8,10 @@
         <i class="fa fa-{{ $icon }}"></i>
     </div>
 </div>
+
+<style>
+/* لإخفاء scrollbar في Webkit (Chrome, Safari, Edge) */
+.small-box::-webkit-scrollbar {
+    display: none;
+}
+</style>
