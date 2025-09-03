05a45e28f31ab01b5dabf31c1a004accad4b1aab
Bola <bolaphilip3@gmail.com>
2025-09-03 11:13:27 +0300

    ware management

--- app/Admin/Controllers/WareTabController.php
+++ app/Admin/Controllers/WareTabController.php
@@ -2,6 +2,7 @@
 
 namespace App\Admin\Controllers;
 
+use App\Admin\Extensions\CustomImage;
 use App\Admin\Services\FileService;
 use Modules\Vip\Entities\OVip;
 use App\Models\Ware;
@@ -300,30 +301,34 @@ class WareTabController extends MainController
         $form->image('show_img', trans('img'))->name(function ($file) {
             return now()->timestamp . rand(0, 999) . '.' . $file->guessExtension();
         })->default('1.png');
-        $form->file('img2', trans('svg'))
-            ->uniqueName()
-            ->help(
-                (function () use ($form) {
-                    $defaultImage = asset("images/image.png");
-                    $url = getImagePath($form->model()->img2) ?? $defaultImage;
-                    if (!isImageExists($url)) {
-                        $url = $defaultImage;
-                    }
-                    $uniqueId = 'form_' . ($form->model()->id ?? uniqid());
-
-                    return handleShowImageWithTypes($uniqueId, $url, 50, 50);
-                })()
-            );
-
-        $form->display('img2', 'Preview')->with(function ($value) {
-            if (!$value) {
-                return null;
-            }
-            $url = getImagePath($value) ?? asset("images/image.png");
-            $uniqueId = 'form_' . ($this->id ?? uniqid());
-
-            return handleShowImageWithTypes($uniqueId, $url, 100, 100, 10);
-        });
+        $form->extend('customimage', CustomImage::class);
+        $form->customimage('img2', 'SVG/Animation');
+//        $form->file('img2', trans('svg'))
+//            ->uniqueName();
+//        $form->file('img2', trans('svg'))
+//            ->uniqueName()
+//            ->help(
+//                (function () use ($form) {
+//                    $defaultImage = asset("images/image.png");
+//                    $url = getImagePath($form->model()->img2) ?? $defaultImage;
+//                    if (!isImageExists($url)) {
+//                        $url = $defaultImage;
+//                    }
+//                    $uniqueId = 'form_' . ($form->model()->id ?? uniqid());
+//
+//                    return handleShowImageWithTypes($uniqueId, $url, 50, 50);
+//                })()
+//            );
+//
+//        $form->display('img2', 'Preview')->with(function ($value) {
+//            if (!$value) {
+//                return null;
+//            }
+//            $url = getImagePath($value) ?? asset("images/image.png");
+//            $uniqueId = 'form_' . ($this->id ?? uniqid());
+//
+//            return handleShowImageWithTypes($uniqueId, $url, 100, 100, 10);
+//        });
 
 
 
---
+++ app/Admin/Extensions/CustomImage.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Admin\Extensions;
+
+use App\Admin\Fields\Image;
+
+class CustomImage extends Image
+{
+    protected $view = 'admin.custom-image';
+}
