5e997531dc22c76592c9b07b5d926def56bbe1cf
Bola <bolaphilip3@gmail.com>
2025-09-03 15:33:57 +0300

    distinct chat between users

--- Modules/Chat/Http/Controllers/ChatMessagesController.php
+++ Modules/Chat/Http/Controllers/ChatMessagesController.php
@@ -68,6 +68,7 @@ class ChatMessagesController extends Controller
         info($chatRoom->type);
         info($total_message);
         info($totalDistinctUsers);
+
         if ($chatRoom->type == 'guest' && $total_message >= 3 && $totalDistinctUsers < 2) {
             return response()->json([
                 'status' => 404,
