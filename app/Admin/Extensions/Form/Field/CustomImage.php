b4cda8f43f4596e399bc21fb0409fd75bac49e0f
Bola <bolaphilip3@gmail.com>
2025-09-03 11:36:53 +0300

    ware management

--- app/Admin/Extensions/Form/Field/CustomImage.php
+++ app/Admin/Extensions/Form/Field/CustomImage.php
@@ -15,9 +15,9 @@ class CustomImage extends Image
         $ext = strtolower(pathinfo($url, PATHINFO_EXTENSION));
         $uniqueId = 'file_' . uniqid();
 
-        if (in_array($ext, ['png','jpg','jpeg','gif','webp','svg'])) {
-            return "<img src='{$url}' class='file-preview-image img-responsive' style='max-height:150px'>";
-        }
+//        if (in_array($ext, ['png','jpg','jpeg','gif','webp','svg'])) {
+//            return "<img src='{$url}' class='file-preview-image img-responsive' style='max-height:150px'>";
+//        }
 
         return handleShowImageWithTypes($uniqueId, $url, 100, 100, 10);
     }
