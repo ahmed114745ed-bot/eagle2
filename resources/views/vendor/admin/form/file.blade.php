c25b2289ba76af458f4d697dcd443d73c9942a64
Bola <bolaphilip3@gmail.com>
2025-09-03 12:08:32 +0300

    ware management

--- resources/views/vendor/admin/form/file.blade.php
+++ resources/views/vendor/admin/form/file.blade.php
@@ -6,6 +6,22 @@
 
         @include('admin::form.error')
 
+        @if($value)
+            @php
+                $url = \Illuminate\Support\Facades\Storage::disk(config('admin.upload.disk'))->url($value);
+                $ext = strtolower(pathinfo($url, PATHINFO_EXTENSION));
+                $uniqueId = 'file_' . uniqid();
+            @endphp
+
+            <div style="margin-bottom:10px;">
+                @if(in_array($ext, ['png','jpg','jpeg','gif','webp','svg']))
+                    <img src="{{ $url }}" class="img img-thumbnail" style="max-height:150px">
+                @else
+                    {!! handleShowImageWithTypes($uniqueId, $url, 100, 100, 10) !!}
+                @endif
+            </div>
+        @endif
+
         <input type="file" class="{{$class}}" name="{{$name}}" {!! $attributes !!} />
 
         @include('admin::form.help-block')
