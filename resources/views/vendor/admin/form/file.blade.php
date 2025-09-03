d7746f29bca616073efd4c89aec5d0dc30ef553e
Bola <bolaphilip3@gmail.com>
2025-09-03 12:06:02 +0300

    ware management

--- resources/views/vendor/admin/form/file.blade.php
+++ resources/views/vendor/admin/form/file.blade.php
@@ -14,44 +14,49 @@
 </div>
 
 <script>
-    $('input[name="img2"]').fileinput({
-        // allow .svga as "object" preview
-        allowedPreviewTypes: ['image', 'html', 'text', 'video', 'audio', 'flash', 'object'],
+    $(function () {
+        const $input = $('input[name="img2"]');
 
-        // tell FileInput: when ext=svga, treat it special
-        previewFileExtSettings: {
-            // keep defaults
-            'image': function(ext) {
-                return /(jpg|jpeg|png|gif|bmp|svg|webp)$/i.test(ext);
+        // destroy the admin-initialized fileinput
+        $input.fileinput('destroy');
+
+        // now reinit with your custom config
+        $input.fileinput({
+            overwriteInitial: true,
+            initialPreviewAsData: true,
+            allowedPreviewTypes: ['image', 'html', 'text', 'video', 'audio', 'flash', 'object'],
+            previewFileExtSettings: {
+                'image': function(ext) {
+                    return /(jpg|jpeg|png|gif|bmp|svg|webp)$/i.test(ext);
+                },
+                'svga': function(ext) {
+                    return /(svga)$/i.test(ext);
+                }
             },
-            // define our new type
-            'svga': function(ext) {
-                return /(svga)$/i.test(ext);
+            previewContentTemplates: {
+                svga:
+                    '<div class="file-preview-frame krajee-default kv-preview-thumb">' +
+                    '<div class="kv-file-content">' +
+                    '<div class="svga-player" style="width:100px;height:100px;"></div>' +
+                    '</div>' +
+                    '<div class="file-thumbnail-footer">' +
+                    '<div class="file-footer-caption">{caption}</div>' +
+                    '</div>' +
+                    '</div>'
             }
-        },
+        });
 
-        // define how to render .svga
-        previewContentTemplates: {
-            svga:
-                '<div class="file-preview-frame krajee-default kv-preview-thumb">' +
-                '<div class="kv-file-content">' +
-                '<div class="svga-player" style="width:100px;height:100px;"></div>' +
-                '</div>' +
-                '<div class="file-thumbnail-footer">' +
-                '<div class="file-footer-caption">{caption}</div>' +
-                '</div>' +
-                '</div>'
-        }
-    });
-
-    // After fileinput initialized:
-    $('.svga-player').each(function() {
-        var container = this;
-        var player = new SVGA.Player(container);
-        var parser = new SVGA.Parser(container);
-        parser.load($(container).closest('.file-preview-frame').find('.kv-file-caption').text(), function(videoItem) {
-            player.setVideoItem(videoItem);
-            player.startAnimation();
+        // init SVGA players after fileinput renders
+        $input.on('fileloaded fileimageloaded fileloadederror', function(event, file, previewId, index, reader) {
+            $('.svga-player').each(function() {
+                let container = this;
+                let player = new SVGA.Player(container);
+                let parser = new SVGA.Parser(container);
+                parser.load(file.previewAsData || file.name, function(videoItem) {
+                    player.setVideoItem(videoItem);
+                    player.startAnimation();
+                });
+            });
         });
     });
 </script>
