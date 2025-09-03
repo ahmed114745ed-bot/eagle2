484bd0a782dcc24252a6102898f54822a1277e44
Bola <bolaphilip3@gmail.com>
2025-09-03 17:23:07 +0300

    distinct chat between users

--- Modules/Chat/Http/Services/ChatRoomService.php
+++ Modules/Chat/Http/Services/ChatRoomService.php
@@ -127,17 +127,19 @@ class ChatRoomService
             ];
         }
 
+        // Get top chats
         $topChats = $user->chats->pluck('id')->toArray();
 
+        // Get user chats (friends)
         $friends = ChatRoom::withCount([
             'messages as distinct_users_count' => function ($query) {
                 $query->select(DB::raw("COUNT(DISTINCT user_id)"));
             }
-        ])
-            ->whereHas('messages')
+        ])->WhereHas('messages')
             ->select(
                 'chat_rooms.*',
-                DB::raw('(SELECT MAX(created_at) FROM chat_messages WHERE chat_messages.chat_room_id = chat_rooms.id) AS last_message_created_at')
+                DB::raw('(SELECT MAX(created_at) FROM chat_messages WHERE chat_messages.chat_room_id = chat_rooms.id) AS last_message_created_at'),
+                DB::raw('(SELECT COUNT(DISTINCT user_id) FROM chat_messages WHERE chat_messages.chat_room_id = chat_rooms.id) AS distinct_users_count')
             )
             ->where(function ($q) use ($topChats, $user) {
                 $q->where(function ($query) use ($topChats, $user) {
@@ -145,7 +147,9 @@ class ChatRoomService
                         ->where('chat_rooms.user_id', $user->id)
                         ->where(function ($sub) {
                             $sub->where('chat_rooms.type', 'friends')
-                                ->orWhere('chat_rooms.type', 'guest');
+                                ->orWhere(function ($sq) {
+                                    $sq->where('chat_rooms.type', 'guest');
+                                });
                         });
                 })
                     ->orWhere(function ($query) use ($topChats, $user) {
@@ -153,11 +157,12 @@ class ChatRoomService
                             ->where('chat_rooms.user_id2', $user->id)
                             ->where(function ($sub) {
                                 $sub->where('chat_rooms.type', 'friends')
-                                    ->orWhere('chat_rooms.type', 'guest');
+                                    ->orWhere(function ($sq) {
+                                        $sq->where('chat_rooms.type', 'guest');
+                                    });
                             });
                     });
-            })
-            ->havingRaw("((chat_rooms.type = 'friends') OR (chat_rooms.type = 'guest' AND distinct_users_count >= 2))")
+            })->havingRaw("((chat_rooms.type = 'friends') OR (chat_rooms.type = 'guest' AND distinct_users_count >= 2))")
             ->when($uuid, function ($q) use ($uuid) {
                 $q->where(function ($q) use ($uuid) {
                     $q->whereHas('userOne', function ($qq) use ($uuid) {
