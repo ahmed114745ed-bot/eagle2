0aae9dd1124c089d79d6b9d5bafca3f574e9b2f5
Erinyelkess <ariny.backend@utdsoftware.com>
2025-09-03 10:05:31 +0300

    svga ware

--- app/Admin/Controllers/WareTabController.php
+++ app/Admin/Controllers/WareTabController.php
@@ -301,30 +301,26 @@ class WareTabController extends MainController
         $form->image('show_img', trans('img'))->name(function ($file) {
             return now()->timestamp . rand(0, 999) . '.' . $file->guessExtension();
         })->default('1.png');
+        // $form->file('img2', trans('svg'))
+        //     ->name(function ($file) {
+        //         return 'svga_' . Str::random(6) . '.' . $file->getClientOriginalExtension();
+        //     });
+
         $form->file('img2', trans('svg'))
             ->name(function ($file) {
                 return 'svga_' . Str::random(6) . '.' . $file->getClientOriginalExtension();
             });
 
-        // $form->file('img2', trans('svg'))
-        //     ->name(function ($file) {
-        //         return 'svga_' . Str::random(6) . '.' . $file->getClientOriginalExtension();
-        //     })
-        //     ->customFormat(function ($value) use ($form) {
-        //         if (!$value) {
-        //             return null;
-        //         }
+        $form->display('img2', 'Preview')->with(function ($value) {
+            if (!$value) {
+                return null;
+            }
+            $url = getImagePath($value) ?? asset("images/image.png");
+            $uniqueId = 'form_' . ($this->id ?? uniqid());
 
-        //         $url = getImagePath($value) ?? asset("images/image.png");
+            return handleShowImageWithTypes($uniqueId, $url, 100, 100, 10);
+        });
 
-        //         // Safely get model id, fallback to uniqid() if creating new
-        //         $uniqueId = 'form_' . ($form->model()->id ?? uniqid());
-
-        //         $preview = handleShowImageWithTypes($uniqueId, $url, 100, 100, 10);
-
-        //         // Return both filename + preview
-        //         return $value . "<br>" . $preview;
-        //     });
 
 
 
