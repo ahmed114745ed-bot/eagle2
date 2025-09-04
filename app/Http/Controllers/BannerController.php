967f3f8cb11f9c7d136d408d867985f411158c8c
Bola <bolaphilip3@gmail.com>
2025-09-04 14:56:12 +0300

    splash

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -80,21 +80,12 @@ class BannerController extends Controller
             $q->where('is_active', true)
                 ->whereNotNull('publish_at')
                 ->where(function ($subQuery) use ($now) {
+                    //publish_at
                     $subQuery->whereRaw("DATE_ADD(created_at, INTERVAL COALESCE(expire, 0) DAY) > ?", [$now])
                         ->orWhereNull('expire')
                         ->orWhere('expire', 0);
             });
         })->where("user_id", $user->id)->get();
-
-//        $dataShow = UserBannerShow::whereHas("banner", function ($q) use ($now) {
-//            $q->where('is_active', true)
-//                ->whereNotNull('publish_at')
-//                ->where(function ($query) use ($now) {
-//                    $query->whereRaw("DATE_ADD(publish_at, INTERVAL COALESCE(expire, 0) DAY) > ?", [$now])
-//                        ->orWhereNull('expire')
-//                        ->orWhere('expire', 0);
-//                });
-//        })->where("user_id", $user->id)->get();
 
         $ids = $dataShow->pluck('banner_id');
         $banners = $this->bannerServices->index2($ids);
