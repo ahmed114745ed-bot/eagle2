7f37fcf4a48c6e4e944dba0067f9d7005f4d5923
johnghattas <john.backend@utdsoftware.com>
2025-09-04 12:02:12 +0300

    Update GiftLogController.php

--- app/Http/Controllers/Api/V1/GiftLogController.php
+++ app/Http/Controllers/Api/V1/GiftLogController.php
@@ -357,6 +357,7 @@ class GiftLogController extends Controller
 
     public function sendLuckyGift2(Request $request, UpdateUserWhenSendGift $updateUserWhenSendGift)
     {
+        abort(503);
         $stopLucky = settings()->get('stop_luckyGift');
         if ($stopLucky == 1) {
             return Common::apiResponse(0, __('api_responses.try_again'));
