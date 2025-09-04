51a62241cd30bd5b5b92644779a057550cd6d350
Bola <bolaphilip3@gmail.com>
2025-09-04 13:33:11 +0300

    splash

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -76,6 +76,7 @@ class BannerController extends Controller
     {
         $user = Auth::user();
         $now = now();
+
         $dataShow = UserBannerShow::whereHas("banner", function ($q) use ($now) {
             $q->where('is_active', true)
                 ->whereNotNull('publish_at')
@@ -85,7 +86,6 @@ class BannerController extends Controller
                         ->orWhere('expire', 0);
                 });
         })->where("user_id", $user->id)->get();
-
 
         $ids = $dataShow->pluck('banner_id');
         $banners = $this->bannerServices->index2($ids);
