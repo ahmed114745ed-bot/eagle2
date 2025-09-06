5b65f3024197b305ac650bbcb5a5e06b42a3f213
johnghattas <john.backend@utdsoftware.com>
2025-09-06 20:29:16 +0300

    Update UserController.php

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -224,7 +224,7 @@ class UserController extends MainController
                 if (! $user) {
                     return __('No User');
                 }
-                return ''/*app(UserService::class)->adminUserAvatar($user)*/;
+                return app(UserService::class)->adminUserAvatar($user);
             });
 
 
