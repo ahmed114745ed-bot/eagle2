ad0f3e37e9b625473dd205be4e7006201b613956
shami2024 <>
2025-09-09 12:38:49 +0300

    Update RoomRepository.php

--- app/Tik/Repositories/RoomRepository.php
+++ app/Tik/Repositories/RoomRepository.php
@@ -321,6 +321,7 @@ class RoomRepository extends AbstractRepository
         $audio = (clone $query)->where('type', 'audio')->first();
         $live  = (clone $query)->where('type', 'live')->first();
     
+        dd($audio ,$live);
         return [
             'audio' => $audio
                 ? new RoomResource($audio)
