a16d0b9a87b7ebfe8b176e85a863e1f1534d5ae4
johnghattas <john.backend@utdsoftware.com>
2025-09-11 16:06:37 +0300

    Update UserRepository.php

--- app/Repositories/User/UserRepository.php
+++ app/Repositories/User/UserRepository.php
@@ -197,8 +197,8 @@ class UserRepository extends Repository
                 ->where('is_used', 1)
                 ->with(['ware']),
             'UserVip' => fn($q) => $q->with('OVip:id,img'),
-            'receiverLevel:id,img',
-            'senderLevel:id,img',
+            'receiverLevel:id,img,level',
+            'senderLevel:id,img,level',
             'chargeLevel:id,img,level',
             'agency' => fn($q) => $q->with(['owner' => fn($q) => $q->select(['id'])->with('profile:id,user_id,avatar')]),
             'profile',
