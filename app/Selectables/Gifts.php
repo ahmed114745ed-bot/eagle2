63e93b3dcb2644428c9f3ec3ccb02504fe42bb83
Bola <bolaphilip3@gmail.com>
2025-09-11 17:09:56 +0300

    room boom wares

--- app/Selectables/Gifts.php
+++ app/Selectables/Gifts.php
@@ -19,7 +19,11 @@ class Gifts extends Selectable
         $this->column('id');
         $this->column('name');
         $this->column('img',__('Image'))->image();
-        $this->column('price', __('Price'));
+        $this->column('price', __('price'))->display(function ($coin) {
+            $path = 'coin.png';
+            $url = getImagePath($path);
+            return handleShowImageWithTypes($this->id, $url, 50, 50);
+        });
 
         $this->filter(function (Filter $filter) {
             $filter->like('name');
