24cdee28256c699c08a55c3791e002a61a37353a
shami2024 <>
2025-09-11 16:58:20 +0300

    Update UserResource.php

--- app/Http/Resources/Api/V1/UserResource.php
+++ app/Http/Resources/Api/V1/UserResource.php
@@ -88,7 +88,7 @@ class UserResource extends JsonResource
              'change_room_effect'   => new ShowUserSettingResource($this->userDataSetting),
              'chat_setting'         => new ChatSettingResource($this->chatSetting),
              'manger_type'          => new MangerTypeResource($this->manager),
-             'top_three_support'    => $userHandling->getTopThreeSupport($this->id),
+             'top_three_support'    => [],
              'level' => [
                 'receiver_img' => $this->receiverLevel?->img ?? '',
                 'sender_img'   => $this->senderLevel?->img  ?? '',
