29ad7b956a98cbfb05d76ae306c636d6e3017cff
shami2024 <>
2025-09-14 15:08:49 +0300

    Update RankingService.php

--- app/Services/RankingService.php
+++ app/Services/RankingService.php
@@ -167,6 +167,8 @@ class RankingService
         ]); 
     
         $key = $types[$type] . '_' . $class;
+        $this->transformData3($data, $class, $keywords, $rel);
+
         return $this->prepareResponse3($data, $user, $type, $keywords, $user->id, $class, $limit);
 
         return new RankingUserV2Resource([
