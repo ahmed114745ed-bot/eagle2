5bbf86c2eb989885085a2934f7ab93e680fa24dd
shami2024 <>
2025-09-14 18:01:26 +0300

    Update RankingRepository.php

--- app/Repositories/RankingRepository.php
+++ app/Repositories/RankingRepository.php
@@ -95,7 +95,7 @@ class RankingRepository
         ];
     
         if ($role === 'roomOwner') {
-            $relations['ownerRoom'] = fn($q) => $q->with('ownerRoom');
+            // $relations['ownerRoom'] = fn($q) => $q->with('ownerRoom');
         }
     
         return $relations;
