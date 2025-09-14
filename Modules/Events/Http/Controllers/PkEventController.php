886eab67228ee57b8ac61fde5aec52a59743ed2d
Erinyelkess <ariny.backend@utdsoftware.com>
2025-09-14 12:51:20 +0300

    test pk

--- Modules/Events/Http/Controllers/PkEventController.php
+++ Modules/Events/Http/Controllers/PkEventController.php
@@ -49,7 +49,7 @@ class PkEventController extends Controller
             ->orderByDesc('totalGiftNum');
 
         // Eager load relation based on type
-        if ($type === 2) {
+        if ($type === 3) {
             $query->with(['roomOwner.ownerRoom:id,uid,room_name,room_cover']);
         } else {
             $query->with([$relation => function ($q) {
