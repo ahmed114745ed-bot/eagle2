170ef4a26ce395af9c30e8a39e348a98125bedfd
Bola <bolaphilip3@gmail.com>
2025-09-15 14:24:52 +0300

    pusher statistics

--- resources/views/pusher-statistics.blade.php
+++ resources/views/pusher-statistics.blade.php
@@ -1,32 +1,26 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="container">
-        <h2>Pusher Channels (from REST API)</h2>
-
-        <table class="table table-sm table-bordered">
-            <thead>
-            <tr>
-                <th>Channel Name</th>
-                <th>Occupied</th>
-                <th>User Count</th>
-                <th>Subscriptions</th>
-            </tr>
-            </thead>
-            <tbody>
-            @forelse($channels as $name => $channel)
-                <tr>
-                    <td>{{ $name }}</td>
-                    <td>{{ $channel['occupied'] ? 'Yes' : 'No' }}</td>
-                    <td>{{ $channel['user_count'] ?? '-' }}</td>
-                    <td>{{ $channel['subscription_count'] ?? '-' }}</td>
-                </tr>
-            @empty
-                <tr>
-                    <td colspan="4" class="text-center">No active channels</td>
-                </tr>
-            @endforelse
-            </tbody>
-        </table>
+@if($error)
+    <div class="alert alert-danger">
+        <strong>Error:</strong> {{ $error }}
     </div>
-@endsection
+@endif
+
+@if(!empty($channels))
+    <table class="table table-bordered">
+        <thead>
+        <tr>
+            <th>Channel Name</th>
+            <th>User Count</th>
+            <th>Subscription Count</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($channels as $name => $channel)
+            <tr>
+                <td>{{ $name }}</td>
+                <td>{{ $channel['user_count'] ?? 'N/A' }}</td>
+                <td>{{ $channel['subscription_count'] ?? 'N/A' }}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+@endif
