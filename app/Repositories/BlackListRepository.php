c19b1a4dc0f3b61c9b165e90de0086364d99880e
johnghattas <john.backend@utdsoftware.com>
2025-09-17 23:23:56 +0300

    Update BlackListRepository.php

--- app/Repositories/BlackListRepository.php
+++ app/Repositories/BlackListRepository.php
@@ -39,7 +39,7 @@ class BlackListRepository
         return \Cache::remember(
             $cacheKey,
             now()->addMinutes(5),
-            fn() => BlackList::where(fn($q) => $q->where('user_id', $authId)->orWhere('from_id', $userId))
+            fn() => BlackList::where(fn($q) => $q->where('user_id', $authId)->orWhere('from_uid', $userId))
                 ->orWhere(fn($q) => $q->where('user_id', $userId)->orWhere('from_id', $authId))
                 ->exists()
         );
