820185bf9867389495a89f18429c9686a8ea239b
johnghattas <john.backend@utdsoftware.com>
2025-09-18 16:55:17 +0300

    update profile frame from my data

--- app/Http/Resources/Api/V1/MyDataResource.php
+++ app/Http/Resources/Api/V1/MyDataResource.php
@@ -160,6 +160,8 @@ class MyDataResource extends JsonResource
             'country_name' => $this->country ? (app()->getLocale() == 'en' ? $this->country->e_name : $this->country->name) : '',
             'country_hidden' => UserPackHelper::hasHideCountry($this->resource),
 
+            'profile_frame_id' => UserPackHelper::getProfileFrameId($this->resource),
+
             'is_first' => (bool)$this->is_points_first,
             'is_agency_request' => (bool)$this->agencyJoinRequest->where('status', '!=', 2)->count(),
             'has_room' => (bool)$ownerRoom,
@@ -235,7 +237,6 @@ class MyDataResource extends JsonResource
             $this->mergeWhen($request->show_counter == true, [
                 'unread_counter'       =>  $counters,
             ]),
-            'profile_frame_id' => $this->getProfileFrame()?->id ?? '',
             'company_number' => Common::getConfig('company_number'),
             'special_id'          =>  @$this->specialId?->ware?->id ?? 0,
             'special_id_image'          =>  @$this->specialId?->ware?->show_img ?? "",
