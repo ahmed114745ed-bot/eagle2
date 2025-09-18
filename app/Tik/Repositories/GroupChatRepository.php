6711209e8484dcc50db7491c4eff52abfed1390b
Erinyelkess <ariny.backend@utdsoftware.com>
2025-09-18 15:49:20 +0300

    test group chat

--- app/Tik/Repositories/GroupChatRepository.php
+++ app/Tik/Repositories/GroupChatRepository.php
@@ -20,13 +20,14 @@ class GroupChatRepository extends AbstractRepository
     public function getWithPaginate()
     {
         return $this->model->whereHas('user')->with([
-            'user.profile:id,user_id,avatar',
-            // 'user.UserVip',
-
-            // 'parent',
-            'user.packs'  => fn($q) => $q->whereIn('type', [25, 18, 4])->where('is_used', true)->with('ware:id,value'),
-            'parent.user.profile:id,user_id,avatar',
-            'parent.user.packs' => fn($q) => $q->whereIn('type', [25, 18, 4])->where('is_used', true)->with('ware:id,value'),
+            'user.profile:id,user_id,avatar,birthday,gender',
+            'user.UserVip',
+           
+           // 'parent',
+            'user.packs'  => fn($q) => $q->whereIn('type', [25,18,4])->where('is_used', true)->with('ware:id,value'),
+            'parent.user.profile:id,user_id,avatar,birthday,gender',
+            'parent.user.UserVip',
+            'parent.user.packs' => fn($q) => $q->whereIn('type', [25,1814])->where('is_used', true)->with('ware:id,value'),
         ])->orderBy('created_at', 'DESC')->paginate(10);
     }
 }
