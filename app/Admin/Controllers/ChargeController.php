b3bbbb80f1868740aac924b704284fe45ae40aab
Erinyelkess <ariny.backend@utdsoftware.com>
2025-09-21 10:46:09 +0300

    edit

--- app/Admin/Controllers/ChargeController.php
+++ app/Admin/Controllers/ChargeController.php
@@ -58,8 +58,7 @@ class ChargeController extends MainController
             'owner:id,name,uuid',
             'owner.profile:id,user_id,avatar',
             'owner.packs' => fn($q) => $q->whereIn('type', [25])->where('is_used', true)->with('ware:id,value')
-        ])->withSum('owner.userSalaries as total_user_salary', \DB::raw('sallary - cut_amount'))
-            ->withSum('owner.roomSalaries as total_room_salary', \DB::raw('salary - cut_amount'));
+        ]);
         $grid->disableRowSelector();
 
         $grid->filter(function (Grid\Filter $filter) {
