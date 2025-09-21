15b522f707a7ea70420b2cc7be9cd9615214d661
Erinyelkess <ariny.backend@utdsoftware.com>
2025-09-21 10:25:29 +0300

    test

--- app/Admin/Controllers/ChargeController.php
+++ app/Admin/Controllers/ChargeController.php
@@ -97,16 +97,16 @@ class ChargeController extends MainController
                 $profileUrl = url("admin/shipping-agencies/profile/{$id}");
 
                 return "
-        <div style='display: flex; align-items: center; gap: 10px;'>
-            $image
-            <a href='{$profileUrl}' target='_blank' style='text-decoration: none; color: inherit;'>
-                <div>
-                    <div>$name</div>
-                    <small style='color: #888;'>ID: {$id}</small>
-                </div>
-            </a>
-        </div>
-    ";
+                        <div style='display: flex; align-items: center; gap: 10px;'>
+                            $image
+                            <a href='{$profileUrl}' target='_blank' style='text-decoration: none; color: inherit;'>
+                                <div>
+                                    <div>$name</div>
+                                    <small style='color: #888;'>ID: {$id}</small>
+                                </div>
+                            </a>
+                        </div>
+                    ";
             });
 
 
