557c7b63ed7cf8f12c8aa8ee08ea924b1605fafc
Erinyelkess <ariny.backend@utdsoftware.com>
2025-09-22 14:11:57 +0300

    ag user refact

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -160,7 +160,7 @@ class UserController extends MainController
 
         // Optimize eager loading
         $grid->model()
-            ->select(['id', 'name', 'sender_level', 'received_level', 'device_token', 'agency_id', 'uuid', 'special_id', 'di','can_play', 'huawei_version', 'android_version', 'ios_version', 'transfer_salary', 'is_bd'])
+            ->select(['id', 'name', 'sender_level', 'received_level', 'device_token', 'agency_id','family_id', 'uuid', 'special_id', 'di', 'can_play', 'huawei_version', 'android_version', 'ios_version', 'transfer_salary', 'is_bd'])
             ->with([
                 'profile',
                 'agency',
