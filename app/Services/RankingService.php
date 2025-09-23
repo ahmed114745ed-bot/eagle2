a712097aa1b047c892bc187d3b84147083fa670d
shami2024 <>
2025-09-23 11:27:55 +0300

    rm logs

--- app/Services/RankingService.php
+++ app/Services/RankingService.php
@@ -177,8 +177,6 @@ class RankingService
         ]);
     
         $topUsers = $data->take(3); 
-        \Log::info('Top User Resources:', $topUsers->toArray());
-
         $topResources = $topUsers->map(fn($item) => new TopUserResource($item));
         
        
