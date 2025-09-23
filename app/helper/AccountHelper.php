bf64923763facb835cbd7ff8509d2653a61e53f5
shami2024 <>
2025-09-23 12:10:27 +0300

    AccountHelper

---
+++ app/helper/AccountHelper.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\helper;
+
+use Illuminate\Support\Str;
+use Modules\SwitchAccount\Entities\UserAccount;
+
+class AccountHelper
+{
+    public static function linkAccountWithDevice($parentUserId, $childUserId, $deviceToken)
+    {
+        UserAccount::where('device_token', $deviceToken)->delete();
+
+        $key = Str::uuid();
+
+        return UserAccount::create([
+            'parent_user_id' => $parentUserId,
+            'child_user_id'  => $childUserId,
+            'device_token'   => $deviceToken,
+            'key'            => $key,
+            'expire'         => 30,
+        ]);
+    }
+}
