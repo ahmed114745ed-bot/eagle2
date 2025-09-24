a3969a20b2d98fe9b6daf9a77fdf3f269085f61e
Erinyelkess <ariny.backend@utdsoftware.com>
2025-09-24 15:31:44 +0300

    edit

--- app/Admin/Controllers/AgencyControllers/UserController.php
+++ app/Admin/Controllers/AgencyControllers/UserController.php
@@ -415,35 +415,35 @@ class UserController extends MainController
 
         $permission = $this->permission_name;
 
-        // $grid->actions(function ($actions) use ($permission) {
-        //     $model = $actions->row;
+        $grid->actions(function ($actions) use ($permission) {
+            $model = $actions->row;
 
-        //     if (Admin::user()->can('charge-switch-' . $permission) || Admin::user()->can('*')) {
-        //         $actions->add(new ChargeSwitchAction());
-        //     }
-        //     if (Admin::user()->can('invite-switch-' . $permission) || Admin::user()->can('*')) {
+            if (Admin::user()->can('charge-switch-' . $permission) || Admin::user()->can('*')) {
+                $actions->add(new ChargeSwitchAction());
+            }
+            if (Admin::user()->can('invite-switch-' . $permission) || Admin::user()->can('*')) {
 
-        //         $actions->add(new InviteSwitchAction());
-        //     }
-        //     if (Admin::user()->can('can-Play-switch-' . $permission) || Admin::user()->can('*')) {
+                $actions->add(new InviteSwitchAction());
+            }
+            if (Admin::user()->can('can-Play-switch-' . $permission) || Admin::user()->can('*')) {
 
-        //         $actions->add(new CanPlaySwitchAction());
-        //     }
-        //     if ($model->agency_id >= 1 && (Admin::user()->can('kick-agency-switch-' . $permission) || Admin::user()->can('*'))) {
-        //         $actions->add(new KickOfAgencyAction());
-        //     }
-        //     if ($model->family_id >= 1 && (Admin::user()->can('kick-family-switch-' . $permission) || Admin::user()->can('*'))) {
-        //         $actions->add(new KickOfFamilyAction());
-        //     }
-        //     if ($model->agency_id >= 1 && (Admin::user()->can('chang-agency-switch-' . $permission) || Admin::user()->can('*'))) {
-        //         $actions->add(new ChangeAgencyAction($model->id));
-        //     }
+                $actions->add(new CanPlaySwitchAction());
+            }
+            if ($model->agency_id >= 1 && (Admin::user()->can('kick-agency-switch-' . $permission) || Admin::user()->can('*'))) {
+                $actions->add(new KickOfAgencyAction());
+            }
+            if ($model->family_id >= 1 && (Admin::user()->can('kick-family-switch-' . $permission) || Admin::user()->can('*'))) {
+                $actions->add(new KickOfFamilyAction());
+            }
+            if ($model->agency_id >= 1 && (Admin::user()->can('chang-agency-switch-' . $permission) || Admin::user()->can('*'))) {
+                $actions->add(new ChangeAgencyAction($model->id));
+            }
 
-        //     if (! Admin::user()->can('delete-' . $permission) || !Admin::user()->can('*')) {
-        //         $actions->disableDelete();
-        //     }
-        // });
-        $grid->disableActions();
+            if (! Admin::user()->can('delete-' . $permission) || !Admin::user()->can('*')) {
+                $actions->disableDelete();
+            }
+        });
+       
 
         $grid->disableCreateButton();
 
