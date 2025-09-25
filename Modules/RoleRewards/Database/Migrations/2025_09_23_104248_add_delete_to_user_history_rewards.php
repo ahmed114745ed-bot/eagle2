db0925f52831d3d858e164f9127c5bff07683267
Erinyelkess <ariny.backend@utdsoftware.com>
2025-09-25 13:54:12 +0300

    test

--- Modules/RoleRewards/Database/Migrations/2025_09_23_104248_add_delete_to_user_history_rewards.php
+++ Modules/RoleRewards/Database/Migrations/2025_09_23_104248_add_delete_to_user_history_rewards.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('user_history_rewards', function (Blueprint $table) {
-         //   $table->softDeletes();
+            $table->softDeletes();
         });
     }
 
