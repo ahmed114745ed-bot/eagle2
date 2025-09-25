130f1b1dafef1f4b21c487c4237b095d598ba40c
shami2024 <>
2025-09-25 10:18:34 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Api\V1\GiftLogController;
 use App\Http\Controllers\PayPalController;
 use App\Http\Controllers\BdSalaryMigrationController;
 use App\Jobs\UpdateUserFollowCountsJob;
+use App\Models\Ban;
 use App\Models\CoinLog;
 use App\Models\PaymentCoin;
 use App\Models\Room;
@@ -506,3 +507,19 @@ Route::get('/update-user-follow-counts', function () {
 //    $paymentTypes = PaymentCoin::pluck('type')->toArray();
 //    CoinLog::whereIn('method', $paymentTypes)->delete();
 //});
+
+
+Route::get('/fix-bans-user-id', function () {
+    $bans = Ban::all();
+
+    foreach ($bans as $ban) {
+        $user = User::where('uuid', $ban->uid)->first();
+
+        if ($user) {
+            $ban->user_id = $user->id;
+            $ban->save();
+        }
+    }
+
+    return "done";
+});
