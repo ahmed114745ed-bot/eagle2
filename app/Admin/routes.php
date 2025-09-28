7a91eed44dd9d81b48c9e3999768d7fe5ae15165
Erinyelkess <ariny.backend@utdsoftware.com>
2025-09-28 13:51:47 +0300

    name

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -425,7 +425,7 @@ Route::group(
         Route::resource('usersBd', BdController::class);
         Route::resource('usersBd-settings', BdSelectController::class);
 
-        Route::resource('superadmin-users', SuperAdminController::class);
+        Route::resource('   ', SuperAdminController::class);
         Route::resource('superadmin-users-settings', SuperAdminSelectController::class);
 
         Route::post('toggle-salary-transfer', [BdSelectController::class, 'toggleSalaryTransfer'])->name('bd.toggle-salary-transfer');
