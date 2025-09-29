8dce9b977b5988969b1a1273a55283d350adef05
Erinyelkess <ariny.backend@utdsoftware.com>
2025-09-29 11:38:25 +0300

    test

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -291,7 +291,7 @@ class Room extends Model
         if ($this->mode === 8) {
             return BaCKGROUND_IMAGE_MODE_8;
         }
-         dd($this->backgroundImage?->img);
+         dd(request()->default_background);
         return $this->backgroundImage?->img
             ?? $this->background?->img
             ?? $this->defaultBackground?->img
