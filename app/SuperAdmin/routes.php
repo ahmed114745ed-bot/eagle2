f711acb76db767fe0b6a353dff94b71dd18011e8
shami2024 <>
2025-09-30 14:29:11 +0300

    HomeController

--- app/SuperAdmin/routes.php
+++ app/SuperAdmin/routes.php
@@ -109,7 +109,12 @@ Route::group(
         Route::get('/charges', [ChargeController::class, 'index'])->name('charges');
         Route::post('wallet/charge', [WalletController::class, 'charge'])->name('wallet.charge');
 
-        Route::get('peak-hours', [HomeController::class, 'peakHours'])->name('admin.peak-hours');
         Route::get('rooms-activity', [HomeController::class, 'roomsActivity'])->name('admin.rooms-activity');
+   
+   
+        Route::get('peak-hours', [HomeController::class, 'peakHours'])->name('admin.peak-hours');
+        Route::get('users-online-stats', [HomeController::class, 'onlineStats'])
+        ->name('users.online.stats');
+   
     }
 );
