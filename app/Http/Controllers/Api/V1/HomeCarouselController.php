7eb35ccf5c1c79394e06d43a1aa59981227e90e8
Bola <bolaphilip3@gmail.com>
2025-10-02 17:35:21 +0300

    update online users just in the dashboard

--- app/Http/Controllers/Api/V1/HomeCarouselController.php
+++ app/Http/Controllers/Api/V1/HomeCarouselController.php
@@ -21,13 +21,13 @@ class HomeCarouselController extends Controller
         }
 
         $items = HomeCarousel::query()->with('user','room','generalRole','countriesLite')
-                ->when($displayAt, function ($q) use ($displayAt) {
+            ->when($displayAt, function ($q) use ($displayAt) {
                     $q->where(function ($sub) use ($displayAt) {
                         $sub->where('display_at', $displayAt)
                             ->orWhere(function ($query) use ($displayAt) {
                                 $query->whereRaw("JSON_VALID(display_at) and JSON_CONTAINS(display_at, '\"$displayAt\"')");
                             });
-                    });
+                    })->orWhere("display_$displayAt", 1);
                 })
             ->where('enable', 1)
             ->orderBy('sort')
