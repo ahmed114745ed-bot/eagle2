a6eea9db2a864470816b85967f7c727bd58786f7
shami2024 <>
2025-10-02 11:12:17 +0300

    fix name

--- app/Classes/Room/RoomComments.php
+++ app/Classes/Room/RoomComments.php
@@ -37,7 +37,7 @@ class RoomComments
             $ms = [
                 'messageContent' => [
                     'msg' => 'yellowBanner',
-                    'event' => 'room.comment.event',
+                    'event' => 'room-comment-event',
                     'uId' => $user->id,
                     'umsg' => $data['message'],
                     'oid' => @$room->uid,
