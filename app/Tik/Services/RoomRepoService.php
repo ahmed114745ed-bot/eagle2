9cbe6f6226a8af788e272c621d0dc137b88a0485
shami2024 <>
2025-10-07 15:55:04 +0300

    Update RoomRepoService.php

--- app/Tik/Services/RoomRepoService.php
+++ app/Tik/Services/RoomRepoService.php
@@ -235,7 +235,7 @@ class RoomRepoService
 
     public function changePasswordRoom($ownerId)
     {
-        $room =  $this->findRoomUser($ownerId);
+        $room =  $this->findAudioRoomUser($ownerId);
         if ($room) {
             $room->room_pass = '';
             $this->repository->updateRoomUser($room);
