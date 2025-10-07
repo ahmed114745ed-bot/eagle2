52ebe7c66c6bd171dde5c1ef0d9002e376a0f3ca
bola philip <bolaphilip3@gmail.com>
2025-10-07 04:43:55 +0300

    ware management test

--- app/Admin/Controllers/WareTabController.php
+++ app/Admin/Controllers/WareTabController.php
@@ -536,10 +536,8 @@ class WareTabController extends MainController
             $model = $form->model();
             if ($form->img2 instanceof UploadedFile) {
                 $originalName = $form->img2->getClientOriginalName();
-
-                $path = $form->img2->storeAs('images', $originalName, 'public');
-                $url = Storage::disk('public')->url('images/' . $originalName);
-
+                info('form image', [$form->img2]);
+                info('originalName', [$originalName]);
                 $model->update(['img2' => $originalName]);
             }
 
