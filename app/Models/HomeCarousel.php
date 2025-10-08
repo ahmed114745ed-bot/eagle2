2797d38794067bc80250a9fffb0045e09c05d1d8
johnghattas <john.backend@utdsoftware.com>
2025-10-08 17:01:24 +0300

    Update HomeCarousel.php

--- app/Models/HomeCarousel.php
+++ app/Models/HomeCarousel.php
@@ -188,7 +188,7 @@ class HomeCarousel extends Model
     protected function getDisplayAtAttribute($value)
     {
         $decoded = $this->displays?->pluck('display_type')->toArray();
-        return is_array($decoded) ? $decoded : [];
+        return $decoded->toString();
     }
 
 
