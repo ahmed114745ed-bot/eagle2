e2711c79389d46cc9c7e504ae912d82d796f61ce
johnghattas <john.backend@utdsoftware.com>
2025-10-10 19:29:05 +0300

    Update SendGiftBanner.php

--- app/Listeners/SendGiftBanner.php
+++ app/Listeners/SendGiftBanner.php
@@ -20,6 +20,6 @@ class SendGiftBanner
      */
     public function handle(object $event): void
     {
-        Log::info('GiftBannerEvent triggered', ['data' => $event->giftData]);
+        \Log::info('GiftBannerEvent triggered', ['data' => $event->giftData]);
     }
 }
