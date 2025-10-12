806988ff8c170f696bcf19673a83f87a7e0dddad
bola philip <bolaphilip3@gmail.com>
2025-10-12 16:33:32 +0300

    preview super admin

--- app/Admin/Controllers/SuperAdminController.php
+++ app/Admin/Controllers/SuperAdminController.php
@@ -482,7 +482,7 @@ class SuperAdminController extends MainController
                 //                break;
         }
 
-        return view('superadmin.super_admin_profile', compact('superAdmin', 'agencies'));
+        return view('superadmin.super_admin_profile', compact('superAdmin', 'agencies','totalCharges','totalSpent'));
     }
 
     public function profilePreview()
