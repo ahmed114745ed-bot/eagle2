e21892cfe688bdf4b96d79691af182ba38a54a80
Erinyelkess <ariny.backend@utdsoftware.com>
2025-10-13 16:44:59 +0300

    edit

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -594,7 +594,7 @@ Route::group(
         Route::resource('settings', SettingController::class)
         ->except(['update'])
         ->names('admin.settings');
-         Route::resource('helper_links', LinkViewController::class);
+         Route::resource('helper-links', LinkViewController::class);
 
         Route::resource('room-settings', RoomSettingsController::class);
         Route::resource('charges-settings', ChargesSettingController::class);
