2db5244cf033eb974bc95af6c7b9461f4e5767c8
shami2024 <>
2025-10-13 16:36:52 +0300

    firebase-messaging-

--- resources/views/vendor/admin/partials/header.blade.php
+++ resources/views/vendor/admin/partials/header.blade.php
@@ -275,13 +275,5 @@
 </style>
 
 <script>
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/firebase-messaging-sw.js')
-        .then((registration) => {
-            console.log('✅ Service Worker registered:', registration);
-        })
-        .catch((err) => {
-            console.error('❌ Service Worker registration failed:', err);
-        });
-}
+
 </script>
