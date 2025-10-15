61e5c69b08c34ae30d148258815b7066164f9dcd
bola philip <bolaphilip3@gmail.com>
2025-10-15 15:08:05 +0300

    countries

--- database/seeders/CleanUpDuplicateCountriesSeeder.php
+++ database/seeders/CleanUpDuplicateCountriesSeeder.php
@@ -357,7 +357,7 @@ class CleanUpDuplicateCountriesSeeder extends Seeder
                     'iso'        => $iso ?: $country->iso,
                     'iso3'       => $iso3 ?: $country->iso3,
                     'phone_code' => $phone ?: $country->phone_code,
-                    'status'     => $country->statu == 1 ? 1 : 0,
+                    'status'     => $country->status == 1 ? 1 : 0,
                     'flag'       => $imagePath,
                 ]);
 
