9755e85b9bfe2157f806eea5e2e4f42cc2b8d6d5
bola philip <bolaphilip3@gmail.com>
2025-10-15 16:31:07 +0300

    edit country_id seeders

--- database/seeders/SyncAgencyCountrySeeder.php
+++ database/seeders/SyncAgencyCountrySeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Agency;
+use App\Models\Bd;
 use Illuminate\Database\Seeder;
 
 class SyncAgencyCountrySeeder extends Seeder
@@ -13,8 +14,31 @@ class SyncAgencyCountrySeeder extends Seeder
             ->whereNull('country_id')
             ->chunk(100, function ($agencies) {
                 foreach ($agencies as $agency) {
+                    if ($agency->bd && $agency->bd->country_id) {
+                        $agency->country_id = $agency->bd->country_id;
+                    }
                     if ($agency->owner && $agency->owner->country_id) {
                         $agency->country_id = $agency->owner->country_id;
+                    }
+
+                    if ($agency->country_id) {
+                        try {
+                            $agency->save();
+                        }catch (\Exception $e){}
+                    }
+                }
+            });
+
+        $bds = Bd::where('default', 1)->whereNotNull('country_id')->get()->keyBy('country_id');
+
+        Agency::whereNull('bd_id')
+            ->whereNotNull('country_id')
+            ->chunk(100, function ($agencies) use ($bds){
+                foreach ($agencies as $agency) {
+                    $bd = $bds->get($agency->country_id);
+
+                    if ($bd) {
+                        $agency->bd_id = $bd->id;
                         $agency->save();
                     }
                 }
--- database/seeders/SyncBdCountrySeeder.php
+++ database/seeders/SyncBdCountrySeeder.php
@@ -3,18 +3,41 @@
 namespace Database\Seeders;
 
 use App\Models\Bd;
+use App\Models\SuperAdmin;
 use Illuminate\Database\Seeder;
 
 class SyncBdCountrySeeder extends Seeder
 {
     public function run(): void
     {
+        $superAdmins = SuperAdmin::all();
+
         Bd::with('appUser')
             ->whereNull('country_id')
             ->chunk(100, function ($bds) {
                 foreach ($bds as $bd) {
-                    if ($bd->appUser && $bd->appUser->country_id) {
+                    if ($bd->parent && $bd->parent->country_id) {
+                        $bd->country_id = $bd->parent->country_id;
+                    } elseif ($bd->appUser && $bd->appUser->country_id) {
                         $bd->country_id = $bd->appUser->country_id;
+                    }
+
+                    if ($bd->country_id) {
+                        $bd->save();
+                    }
+                }
+            });
+
+        $superAdmins = SuperAdmin::all()->keyBy('country_id');
+
+        Bd::whereNull('parent_id')
+            ->whereNotNull('country_id')
+            ->chunk(100, function ($bds) use ($superAdmins){
+                foreach ($bds as $bd) {
+                    $superAdmin = $superAdmins->get($bd->country_id);
+
+                    if ($superAdmin) {
+                        $bd->parent_id = $superAdmin->id;
                         $bd->save();
                     }
                 }
