e903603108b6acc041a534724b14425f48f68271
johnghattas <john.backend@utdsoftware.com>
2025-10-16 12:59:50 +0300

    Update super_admin_country.blade.php

--- resources/views/superAdmin/super_admin_country.blade.php
+++ resources/views/superAdmin/super_admin_country.blade.php
@@ -438,7 +438,7 @@
         $('#loadingOverlay').removeClass('hidden');
 
         $.ajax({
-            url: `country/${countryId}/stats`,
+            url: {{route('country.stats', ['id' => $country->id])}},
             method: 'GET',
             success: function(data) {
                 renderStats(data);
