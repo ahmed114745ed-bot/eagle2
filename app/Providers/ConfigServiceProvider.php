4b4bfba539f06cec94cd4649e220e51a4d4d2ee7
shami2024 <>
2025-10-16 18:27:54 +0300

    Update ConfigServiceProvider.php

--- app/Providers/ConfigServiceProvider.php
+++ app/Providers/ConfigServiceProvider.php
@@ -6,6 +6,7 @@ use App\Helpers\CacheHelper;
 use App\Helpers\Common;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\ServiceProvider;
 
 class ConfigServiceProvider extends ServiceProvider
@@ -27,14 +28,33 @@ class ConfigServiceProvider extends ServiceProvider
 
         CacheHelper::cacheConfig();
 
-        $config = getPusherConfig();
 
-        if ($config) {
-            Config::set('broadcasting.connections.pusher.key', @$config['app_key']);
-            Config::set('broadcasting.connections.pusher.secret', @$config['app_secret']);
-            Config::set('broadcasting.connections.pusher.app_id', @$config['app_id']);
-            Config::set('broadcasting.connections.pusher.options.cluster', @$config['app_cluster']);
+        try {
+            $config = getPusherConfig();
+
+            if (
+                $config &&
+                !empty($config['app_key']) &&
+                !empty($config['app_secret']) &&
+                !empty($config['app_id'])
+            ) {
+                Config::set('broadcasting.connections.pusher.key', $config['app_key']);
+                Config::set('broadcasting.connections.pusher.secret', $config['app_secret']);
+                Config::set('broadcasting.connections.pusher.app_id', $config['app_id']);
+                Config::set('broadcasting.connections.pusher.options.cluster', $config['app_cluster'] ?? 'mt1');
+            } else {
+                Log::warning('⚠️ Pusher configuration missing or invalid in database.');
+            }
+
+        } catch (\Throwable $e) {
+            Log::error('❌ BroadcastServiceProvider error: ' . $e->getMessage());
         }
+        // if ($config) {
+        //     Config::set('broadcasting.connections.pusher.key', @$config['app_key']);
+        //     Config::set('broadcasting.connections.pusher.secret', @$config['app_secret']);
+        //     Config::set('broadcasting.connections.pusher.app_id', @$config['app_id']);
+        //     Config::set('broadcasting.connections.pusher.options.cluster', @$config['app_cluster']);
+        // }
         $requestPath = \Request::path();
 
         if (\Str::startsWith($requestPath, 'preview')) { //admin.route.prefix,admin.auth.controller
