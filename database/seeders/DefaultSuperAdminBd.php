90269a17ba29142b8dc97b3091716fa571f93f08
bola philip <bolaphilip3@gmail.com>
2025-10-16 15:31:13 +0300

    default super admin bd seeder

---
+++ database/seeders/DefaultSuperAdminBd.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Bd;
+use App\Models\SuperAdmin;use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DefaultSuperAdminBd extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $defaultSuperAdmin = SuperAdmin::where('default', 1)->where('country_id', 0 )->first();
+
+        if (!$defaultSuperAdmin){
+            $defaultSuperAdmin = SuperAdmin::create([
+                'username' => 'defaultSuperAdmin',
+                'password' => Hash::make('defaultSuperAdmin'),
+                'name' => 'default Super Admin',
+                'type' => 'superadmin',
+                'default' => 1,
+                'country_id' => 0,
+            ]);
+        }
+
+        $defaultBd = Bd::where('default', 1)->where('country_id', 0 )->first();
+
+        if ($defaultBd){
+            $defaultBd->update(['parent_id' => $defaultSuperAdmin->id]);
+        }else {
+            Bd::create([
+                'username' => 'defaultBd',
+                'password' => Hash::make('defaultBd'),
+                'name' => 'default Bd',
+                'type' => 'bd',
+                'default' => 1,
+                'country_id' => 0,
+            ]);
+        }
+    }
+}
