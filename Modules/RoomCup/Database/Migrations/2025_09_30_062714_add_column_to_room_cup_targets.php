6faaf6937a35bd0071ddf75b30921c8e05437a68
shami2024 <>
2025-10-19 12:25:07 +0300

    room_cup_targets

--- Modules/RoomCup/Database/Migrations/2025_09_30_062714_add_column_to_room_cup_targets.php
+++ Modules/RoomCup/Database/Migrations/2025_09_30_062714_add_column_to_room_cup_targets.php
@@ -11,11 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('room_cup_targets', function (Blueprint $table) {
-            $table->decimal('total_profit', 8, 2)->default(0);
-            $table->integer('owner_percentage')->default(0);
-            $table->integer('admin_percentage')->default(0);
-        });
+        // Schema::table('room_cup_targets', function (Blueprint $table) {
+        //     $table->decimal('total_profit', 8, 2)->default(0);
+        //     $table->integer('owner_percentage')->default(0);
+        //     $table->integer('admin_percentage')->default(0);
+        // });
     }
 
     /**
@@ -23,10 +23,10 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('room_cup_targets', function (Blueprint $table) {
-            $table->dropColumn('total_profit');
-            $table->dropColumn('owner_percentage');
-            $table->dropColumn('admin_percentage');
-        });
+        // Schema::table('room_cup_targets', function (Blueprint $table) {
+        //     $table->dropColumn('total_profit');
+        //     $table->dropColumn('owner_percentage');
+        //     $table->dropColumn('admin_percentage');
+        // });
     }
 };
