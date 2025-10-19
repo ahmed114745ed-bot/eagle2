b9e18f3fb1a452f43ef186b7694154161ac52e17
shami2024 <>
2025-10-19 12:29:48 +0300

    Update AggregateCoinGameUsers.php

--- app/Console/Commands/AggregateCoinGameUsers.php
+++ app/Console/Commands/AggregateCoinGameUsers.php
@@ -37,7 +37,7 @@ class AggregateCoinGameUsers extends Command
             
             SELECT user_id, game_id, coins, type, created_at 
             FROM coin_game_users_archive 
-            WHERE DATE(created_at) = ?
+            WHERE DATE(created_at) = ? AND game_id IS NOT NULL
         ) t
         GROUP BY user_id, game_id, DATE(created_at)
         ON DUPLICATE KEY UPDATE
