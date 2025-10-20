1a321e00b2c119d332eefdf9d289d32e924d4373
Erinyelkess <ariny.backend@utdsoftware.com>
2025-10-20 14:17:55 +0300

    official message

--- app/Models/OfficialMessageAdmin.php
+++ app/Models/OfficialMessageAdmin.php
@@ -2,7 +2,8 @@
 
 namespace App\Models;
 
-use App\Facades\CustomNotification;
+
+use App\Jobs\OfficialMessageJob;
 use App\Traits\TimestampsWithTimezone;
 use Illuminate\Database\Eloquent\Model;
 
@@ -19,11 +20,25 @@ class OfficialMessageAdmin extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function userOfficialMessages()
+    {
+        return $this->hasMany(UserOfficialMessage::class);
+    }
+
     protected static function boot()
     {
         parent::boot();
         self::creating(function ($model) {
-            CustomNotification::officialMsg($model);
+            $featureIdsText = request('feature_ids');
+            if (is_array(request('feature_ids'))) {
+                $featureIds = array_filter(request('feature_ids')); // remove nulls
+                $featureIdsText = implode(',', $featureIds);
+
+                unset(request()['feature_ids']);
+            }
+            $model->feature_ids  = $featureIdsText;
+
+            
         });
     }
 }
