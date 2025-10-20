005e1f0bfaefbced6e93a8b6eb9e22db2b8a4d0e
shami2024 <>
2025-10-20 13:23:38 +0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,24 +1,12 @@
-// Give the service worker access to Firebase Messaging.
-// Note that you can only use Firebase Messaging here. Other Firebase libraries
-// are not available in the service worker.
+
 importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
 
-// Initialize the Firebase app in the service worker by passing in
-// your app's Firebase config object.
-// https://firebase.google.com/docs/web/setup#config-object
-firebase.initializeApp({
- 
-    apiKey: "AIzaSyCn9-Z_mz6zlq6A86bvLDkhrrqNuXSd9xg",
-    authDomain: "eagle-24712.firebaseapp.com",
-    projectId: "eagle-24712",
-    storageBucket: "eagle-24712.firebasestorage.app",
-    messagingSenderId: "817000206466",
-    appId: "1:817000206466:web:55bd3dd1ff3848eb91249b"
-});
 
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
+const firebaseConfig = window.firebaseConfig;
+
+console.log('Firebase Config:', firebaseConfig);
+
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
@@ -26,7 +14,6 @@ messaging.onBackgroundMessage((payload) => {
         '[firebase-messaging-sw.js] Received background message ',
         payload
     );
-    // Customize notification here
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
