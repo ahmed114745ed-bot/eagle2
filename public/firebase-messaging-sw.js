e48359396d20c59150777f88165c38d0d8b164b8
shami2024 <>
2025-10-20 13:39:48 +0300

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,6 +1,7 @@
 //Import Firebase scripts (required for background notifications)
 importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
+console.log('Firebase Config:');
 console.log('Firebase Config:', window.firebaseConfig);
 
 const firebaseConfig = window.firebaseConfig;
