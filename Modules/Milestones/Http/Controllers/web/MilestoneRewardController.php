1edec396859b9ad05d903cacb0e395038f9738de
Erinyelkess <ariny.backend@utdsoftware.com>
2025-10-20 16:42:15 +0300

    image badge

--- Modules/Milestones/Http/Controllers/web/MilestoneRewardController.php
+++ Modules/Milestones/Http/Controllers/web/MilestoneRewardController.php
@@ -98,7 +98,7 @@ class MilestoneRewardController
                 } elseif ($this->type === "achievement") {
                     $path = $this->reward;
                 } elseif ($this->type === "badge") {
-                    $path = $this->rewardable?->icon;
+                    $path = $this->rewardable?->image;
                 } else {
                     $path = 'coin.png';
                 }
