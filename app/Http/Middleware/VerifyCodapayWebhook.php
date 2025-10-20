90ee4530a088e1dd313338e2b8cd7a8a8a387d67
bola philip <bolaphilip3@gmail.com>
2025-10-20 10:12:16 +0300

    codapay

--- app/Http/Middleware/VerifyCodapayWebhook.php
+++ app/Http/Middleware/VerifyCodapayWebhook.php
@@ -35,7 +35,7 @@ class VerifyCodapayWebhook
 
         $secretKey = config('codapay.api_key');
 
-        $computedChecksum = md5($secretKey . $txnId . $orderId . $amount);
+        $computedChecksum = md5($orderId . $txnId . $amount . $secretKey);
 
         if ($computedChecksum !== $checksum) {
             Log::warning('Codapay checksum verification failed', [
