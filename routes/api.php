185606a8af91560253a5d483db78de46f6964fb7
shami2024 <>
2025-10-20 15:27:04 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -102,8 +102,8 @@ Route::prefix(config('app.api_prefix'))->group(function () {
     Route::get('paypal-cancel/{orderId}', [PayPalService::class, 'cancel'])->name('paypal.cancel');
 
     Route::get('codapay-callback', [CodapayService::class, 'callback'])->name('codapay.callback')->middleware(['verify.codapay.webhook']);
-    Route::get('codapay-success', [CodapayService::class, 'success'])->name('paypal.success');
-    Route::get('codapay-failed', [CodapayService::class, 'failed'])->name('paypal.cancel');
+    Route::get('codapay-success', [CodapayService::class, 'success'])->name('codapay.success');
+    Route::get('codapay-failed', [CodapayService::class, 'failed'])->name('codapay.cancel');
 
     Route::prefix('config')->group(function () {
         Route::post('app-check', [VersionController::class, 'versionAndCache']);
