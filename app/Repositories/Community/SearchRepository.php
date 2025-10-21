f0fda1e95e2d9634574c02b984bae9ded71a2c09
Erinyelkess <ariny.backend@utdsoftware.com>
2025-10-21 15:28:10 +0300

    official message

--- app/Repositories/Community/SearchRepository.php
+++ app/Repositories/Community/SearchRepository.php
@@ -86,7 +86,7 @@ class SearchRepository implements SearchRepositoryInterface
             ->get();
     }
 
-    public function searchRoomsV2(int $userId, string $keywords,array $blockedUserIds, int $page = 1): array|Collection
+    public function searchRoomsV2(int $userId, string $keywords, array $blockedUserIds, int $page = 1): array|Collection
     {
         $user = User::query()
             ->fitterByUuid($keywords)
@@ -184,7 +184,7 @@ class SearchRepository implements SearchRepositoryInterface
         return $users;
     }
 
-    public function userSearchHandV2(int $userId, string $keywords,array $blockedUserIds, int $page = 1)
+    public function userSearchHandV2(int $userId, string $keywords, array $blockedUserIds, int $page = 1)
     {
         if (!$userId || !$keywords) {
             return [];
@@ -223,7 +223,7 @@ class SearchRepository implements SearchRepositoryInterface
             ->orderByDesc('total_score')
             ->take(10)
             ->get();
-//            ->paginate(10, ['*'], 'page', $page);
+        //            ->paginate(10, ['*'], 'page', $page);
 
         return $users;
     }
@@ -290,7 +290,9 @@ class SearchRepository implements SearchRepositoryInterface
             ->get();
 
         $official = OfficialMessage::query()
-            ->whereIn('user_id', [0, $userId])
+            ->whereHas('userOfficialMessages', function ($q) use ($userId) {
+                $q->where('user_id', $userId);
+            })
             ->where('type', 2)
             ->orderBy('created_at', 'desc')
             ->get();
