43db22ac71e60732d2a566f5836a7887c5970ca9
johnghattas <john.backend@utdsoftware.com>
2025-10-21 11:41:58 +0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <meta name="google-site-verification" content="v-EtxSADTjVnPFPEM566a9PgYZdpGpVjdobob4vlB58" />
         <title>Laravel</title>
 
         <!-- Fonts -->
