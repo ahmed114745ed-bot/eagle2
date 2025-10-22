14484cb56d569299fd3b29ece613028a0908e44f
makled98 <mkled.backend@utdsoftware.com>
2025-10-22 15:49:07 +0300

    Update MyDataResource.php

--- app/Http/Resources/Api/V1/MyDataResource.php
+++ app/Http/Resources/Api/V1/MyDataResource.php
@@ -149,7 +149,7 @@ class MyDataResource extends JsonResource
             'vip' =>  [
                 'id'             => @$vipData->id,
                 'level'          => @$vipData->level,
-                'vip_upload_gif' => UserPackHelper::getGif($this->resource) ? true : false,
+                'vip_upload_gif' => UserPackHelper::hasPack($this->resource,22),
                 'vip_img'        => UserPackHelper::getVipIcon($this->resource),
                 'colored_name'   => UserPackHelper::getColorName($this->resource),
             ],
