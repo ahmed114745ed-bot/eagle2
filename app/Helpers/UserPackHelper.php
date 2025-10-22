1b0c9e7e8a869066958aac3835ab65c15aed1a51
makled98 <mkled.backend@utdsoftware.com>
2025-10-22 15:45:49 +0300

    Update UserPackHelper.php

--- app/Helpers/UserPackHelper.php
+++ app/Helpers/UserPackHelper.php
@@ -50,9 +50,7 @@ class UserPackHelper
 
     public static function getGif(User $user)
     {
-        Log::info('gif',[self::getPacks($user)
-            ->where('type', 22)
-            ->first()]);
+        Log::info('gif',[self::getPacks($user)->first()]);
         return self::getPacks($user)
             ->where('type', 22)
             ->first();
