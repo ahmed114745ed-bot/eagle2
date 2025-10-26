041f03da0e2a4a5c9644c883ed6e61ee837119d4
Erinyelkess <ariny.backend@utdsoftware.com>
2025-10-26 16:12:26 +0300

    edit

--- routes/web.php
+++ routes/web.php
@@ -174,19 +174,25 @@ Route::get("download-charge-agency-transactions/{agencyId}", function ($agencyId
 });
 
 Route::get('/run-seeders', function () {
+    config(['database.default' => 'mysql']);
 
-    // Run multiple seeders one by one
-    Artisan::call('db:seed', ['--class' => 'CleanUpDuplicateCountriesSeeder']);
-    Artisan::call('db:seed', ['--class' => 'DefaultSuperAdminBdSeeder']);
-    Artisan::call('db:seed', ['--class' => 'SyncBdCountrySeeder']);
-    Artisan::call('db:seed', ['--class' => 'SyncAgencyCountrySeeder']);
-    Artisan::call('db:seed', ['--class' => 'PermissionTypeSeeder']);
-    Artisan::call('db:seed', ['--class' => 'SuperAdminRoleSeeder']);
+    $seeders = [
+        'CleanUpDuplicateCountriesSeeder',
+        'DefaultSuperAdminBdSeeder',
+        'SyncBdCountrySeeder',
+        'SyncAgencyCountrySeeder',
+        'PermissionTypeSeeder',
+        'SuperAdminRoleSeeder',
+    ];
 
-    return response()->json([
-        'status' => 'success',
-        'message' => '✅ All seeders executed successfully.'
-    ]);
+    $output = [];
+
+    foreach ($seeders as $seeder) {
+        Artisan::call('db:seed', ['--class' => $seeder, '--force' => true]);
+        $output[] = Artisan::output();
+    }
+
+    return response('<pre>' . implode("\n", $output) . '</pre>');
 });
 Route::get('/clear_clear', function () {
 
