ae384225d96775a8e021103e17d8c3fb408362c8
bola philip <bolaphilip3@gmail.com>
2025-10-26 10:47:13 +0300

    log iso_numeric

--- app/Services/CodapayService.php
+++ app/Services/CodapayService.php
@@ -26,6 +26,7 @@ class CodapayService
         $this->apiKey = config('codapay.api_key');
         $this->projectId = config('codapay.project_id');
         $this->country = auth()->user()?->country?->iso_numeric ?? config('codapay.country');
+        info(auth()->user()?->country?->iso_numeric);
 
 //        $this->baseUrl = 'https://airtime.codapayments.com/airtime';
 //        $this->apiKey = 'live_JI4WS6k27hHslcUOcmC9SGFDiyo';
