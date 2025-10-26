a1c3663d8ba2548ac1f403c0b774cbfb12012be4
bola philip <bolaphilip3@gmail.com>
2025-10-26 11:46:47 +0300

    done, remove logs

--- app/Services/CodapayService.php
+++ app/Services/CodapayService.php
@@ -26,9 +26,6 @@ class CodapayService
         $this->apiKey = config('codapay.api_key');
         $this->projectId = config('codapay.project_id');
         $this->country = auth()->user()?->country?->iso_numeric ?? config('codapay.country');
-        info(auth()->user());
-        info(auth()->user()?->country);
-        info(auth()->user()?->country?->iso_numeric);
 
 //        $this->baseUrl = 'https://airtime.codapayments.com/airtime';
 //        $this->apiKey = 'live_JI4WS6k27hHslcUOcmC9SGFDiyo';
