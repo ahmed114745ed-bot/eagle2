38e86e36b72ec92de1d8b7672ba96970e9a4fb34
shami2024 <>
2025-10-27 16:51:38 +0300

    Update sidebar.blade.php

--- resources/views/vendor/admin/partials/sidebar.blade.php
+++ resources/views/vendor/admin/partials/sidebar.blade.php
@@ -228,103 +228,125 @@
 
 
 {{--                area manager--}}
-                   @if (in_array(Admin::user()->type, ['area-manager', 'sub_area_manager']))
-                    @php
-                        $areaManagerLinks = [
-                            ['uri' => '/', 'icon' => 'fa-home', 'title' => __('Dashboard')],
-                            [
-                                'uri' => '#',
-                                'icon' => 'fa-users',
-                                'title' => __('Super Admin'),
-                                'children' => [
-                                    ['uri' => '/superadmin-users', 'icon' => 'fa-users', 'title' => __('Super Admin')],
-                                ],
-                            ],
-                            [
-                                'uri' => '#',
-                                'icon' => 'fa-briefcase',
-                                'title' => __('BD'),
-                                'permission' => null,
-                                'children' => [
-                                    ['uri' => '/user-Bds', 'icon' => 'fa-briefcase', 'title' => __('BD'), 'permission' => 'Bds'],
-                                    ['uri' => '/professional-bd', 'icon' => 'fa-plane', 'title' => __('Professional BD'), 'permission' => 'professional-bd'],
-                                ],
-                            ],
-                            ['uri' => '/users', 'icon' => 'fa-users', 'title' => __('Users')],
-                            [
-                                'uri' => '#',
-                                'icon' => 'fa-building',
-                                'title' => __('Agencies'),
-                                'permission' => null,
-                                'children' => [
-                                    ['uri' => '/agencies', 'icon' => 'fa-list', 'title' => __('Host Agencies'), 'permission' => 'agency'],
-                                    ['uri' => '/charge-agencies', 'icon' => 'fa-users', 'title' => __('Shipping Agencies'), 'permission' => 'shipping-agency'],
-                                    ['uri' => '/ag/users', 'icon' => 'fa-users', 'title' => __('Hosts'), 'permission' => 'host'],
-                                    ['uri' => '/ag/professional/users', 'icon' => 'fa-plane', 'title' => __('Professional Host'), 'permission' => 'professional-users'],
-                                ],
-                            ],
-                            [
-                                'uri' => '#',
-                                'icon' => 'fa-building',
-                                'title' => __('rooms'),
-                                'permission' => null,
-                                'children' => [
-                                    ['uri' => '/rooms', 'icon' => 'fa-home', 'title' => __('rooms'), 'permission' => 'rooms'],
-                                    ['uri' => '/live-rooms', 'icon' => 'fa-home', 'title' => __('Live Rooms'), 'permission' => 'live-rooms'],
-                                ],
-                            ],
-                            [
-                                'uri' => '#',
-                                'icon' => 'fa-home',
-                                'title' => __('Advertisements'),
-                                'permission' => null,
-                                'children' => [
-                                    // ['uri' => '/home-carousel', 'icon' => 'fa-home', 'title' => __('HomeCarousel'), 'permission' => 'banner'],
-                                    ['uri' => '/official-message', 'icon' => 'fa-list', 'title' => __('Official messages'), 'permission' => 'banner'],
-                                ],
-                            ],
-                            [
-                            'uri' => '#',
-                            'icon' => 'fa-home',
-                            'title' => __('Employees and Permissions'),
-                            'permission' => null,
-                            'children' => [
-                                ['uri' => '/roles', 'icon' => 'fa-home', 'title' => __('roles'), 'permission' => 'roles'],
-                                ['uri' => '/auth-users', 'icon' => 'fa-home', 'title' => __('users'), 'permission' => 'auth-users'],
-                            ],
-                        ],
-                        ];
-                    @endphp
+@if (in_array(Admin::user()->type, ['area-manager', 'sub_area_manager']))
+    @php
+        $areaManagerLinks = [
+            ['uri' => '/', 'icon' => 'fa-home', 'title' => __('Dashboard'), 'permission' => 'dashboard'],
+            [
+                'uri' => '#',
+                'icon' => 'fa-users',
+                'title' => __('Super Admin'),
+                'permission' => null,
+                'children' => [
+                    ['uri' => '/superadmin-users', 'icon' => 'fa-users', 'title' => __('Super Admin'), 'permission' => 'superadmin'],
+                ],
+            ],
+            [
+                'uri' => '#',
+                'icon' => 'fa-briefcase',
+                'title' => __('BD'),
+                'permission' => null,
+                'children' => [
+                    ['uri' => '/user-Bds', 'icon' => 'fa-briefcase', 'title' => __('BD'), 'permission' => 'Bds'],
+                    ['uri' => '/professional-bd', 'icon' => 'fa-plane', 'title' => __('Professional BD'), 'permission' => 'professional-bd'],
+                ],
+            ],
+            ['uri' => '/users', 'icon' => 'fa-users', 'title' => __('Users'), 'permission' => 'users'],
+            [
+                'uri' => '#',
+                'icon' => 'fa-building',
+                'title' => __('Agencies'),
+                'permission' => null,
+                'children' => [
+                    ['uri' => '/agencies', 'icon' => 'fa-list', 'title' => __('Host Agencies'), 'permission' => 'agency'],
+                    ['uri' => '/charge-agencies', 'icon' => 'fa-users', 'title' => __('Shipping Agencies'), 'permission' => 'shipping-agency'],
+                    ['uri' => '/ag/users', 'icon' => 'fa-users', 'title' => __('Hosts'), 'permission' => 'host'],
+                    ['uri' => '/ag/professional/users', 'icon' => 'fa-plane', 'title' => __('Professional Host'), 'permission' => 'professional-users'],
+                ],
+            ],
+            [
+                'uri' => '#',
+                'icon' => 'fa-building',
+                'title' => __('rooms'),
+                'permission' => null,
+                'children' => [
+                    ['uri' => '/rooms', 'icon' => 'fa-home', 'title' => __('rooms'), 'permission' => 'rooms'],
+                    ['uri' => '/live-rooms', 'icon' => 'fa-home', 'title' => __('Live Rooms'), 'permission' => 'live-rooms'],
+                ],
+            ],
+            [
+                'uri' => '#',
+                'icon' => 'fa-home',
+                'title' => __('Advertisements'),
+                'permission' => null,
+                'children' => [
+                    ['uri' => '/official-message', 'icon' => 'fa-list', 'title' => __('Official messages'), 'permission' => 'banner'],
+                ],
+            ],
+            [
+                'uri' => '#',
+                'icon' => 'fa-home',
+                'title' => __('Employees and Permissions'),
+                'permission' => null,
+                'children' => [
+                    ['uri' => '/roles', 'icon' => 'fa-home', 'title' => __('roles'), 'permission' => 'roles'],
+                    ['uri' => '/auth-users', 'icon' => 'fa-home', 'title' => __('users'), 'permission' => 'auth-users'],
+                ],
+            ],
+        ];
 
-                    @foreach($areaManagerLinks as $link)
-                        @if(isset($link['children']))
-                            <li class="treeview">
-                                <a href="#">
-                                    <i class="fa {{ $link['icon'] }}"></i>
-                                    <span>{{ $link['title'] }}</span>
-                                    <i class="fa fa-angle-left pull-right"></i>
-                                </a>
-                                <ul class="treeview-menu">
-                                    @foreach($link['children'] as $child)
-                                        <li>
-                                            <a href="{{ areaManager_url($child['uri']) }}">
-                                                <i class="fa {{ $child['icon'] }}"></i>
-                                                <span>{{ $child['title'] }}</span>
-                                            </a>
-                                        </li>
-                                    @endforeach
-                                </ul>
-                            </li>
-                        @else
-                            <li>
-                                <a href="{{ areaManager_url($link['uri']) }}">
-                                    <i class="fa {{ $link['icon'] }}"></i>
-                                    <span>{{ $link['title'] }}</span>
-                                </a>
-                            </li>
-                        @endif
-                    @endforeach
-                @endif
+        function hasPermission($permission) {
+            if (Admin::user()->can('*')) return true;
+            if (is_null($permission)) return true;
+            return Admin::user()->can('browse-' . $permission);
+        }
+
+        function hasVisibleChildren($children) {
+            foreach ($children as $child) {
+                if (hasPermission($child['permission'] ?? null)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    @endphp
+
+    @foreach($areaManagerLinks as $link)
+        @if(isset($link['children']))
+            @if(hasVisibleChildren($link['children']))
+                <li class="treeview">
+                    <a href="#">
+                        <i class="fa {{ $link['icon'] }}"></i>
+                        <span>{{ $link['title'] }}</span>
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </a>
+                    <ul class="treeview-menu">
+                        @foreach($link['children'] as $child)
+                            @if(hasPermission($child['permission'] ?? null))
+                                <li>
+                                    <a href="{{ areaManager_url($child['uri']) }}">
+                                        <i class="fa {{ $child['icon'] }}"></i>
+                                        <span>{{ $child['title'] }}</span>
+                                    </a>
+                                </li>
+                            @endif
+                        @endforeach
+                    </ul>
+                </li>
+            @endif
+        @else
+            @if(hasPermission($link['permission'] ?? null))
+                <li>
+                    <a href="{{ areaManager_url($link['uri']) }}">
+                        <i class="fa {{ $link['icon'] }}"></i>
+                        <span>{{ $link['title'] }}</span>
+                    </a>
+                </li>
+            @endif
+        @endif
+    @endforeach
+@endif
+
 
                 @php
                 $adminTypes = ['bd', 'superadmin', 'sub_super_admin', 'area-manager', 'sub_area_manager'];
