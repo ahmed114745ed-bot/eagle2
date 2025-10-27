6d3a338db0b5ee3d58fb623691cbf1a5161bed18
shami2024 <>
2025-10-27 16:15:26 +0300

    AreaManager

--- app/Models/AreaManager.php
+++ app/Models/AreaManager.php
@@ -22,6 +22,19 @@ class AreaManager extends Authenticatable
         self::addGlobalScope('AreaManagerOnly', function (Builder $builder) {
             $builder->where('type', 'area-manager');
         });
+
+
+        static::deleting(function ($manager) {
+            $defaultManager = self::where('default', 1)->first();
+    
+            if (!$defaultManager) {
+                throw new \Exception('❌ لا يمكن الحذف — لا يوجد مدير افتراضي محدد.');
+            }
+            if ($manager->id !== $defaultManager->id) {
+                \App\Models\Country::where('area_manager_id', $manager->id)
+                    ->update(['area_manager_id' => $defaultManager->id]);
+            }
+        });
     }
 
     public function appUser()
