0ea04518045204936fabeeeba6e3734a6b930c9b
Erinyelkess <ariny.backend@utdsoftware.com>
2025-10-28 15:21:32 +0300

    milestone

--- app/Admin/Controllers/SuperAdminController.php
+++ app/Admin/Controllers/SuperAdminController.php
@@ -247,8 +247,22 @@ class SuperAdminController extends MainController
             //            }
         });
 
-        if (Admin::user()->can('choose-switch-' . $permission) || Admin::user()->can('*')) {
-            $grid->tools(function (Grid\Tools $tools) {});
+        if (Admin::user()->can('browse-milestone') || Admin::user()->can('*')) {
+            $grid->tools(function (Grid\Tools $tools) {
+                $url = url('admin/milestones'); // Generates absolute URL for /admin/milestones
+                $milestone = __('milestone');   // Translates 'milestone' via your language files
+
+                $customButtonHTML = <<<HTML
+                <div style="display: contents; align-items: center;">
+                    <a href="{$url}" class="btn btn-sm btn-info" style="margin-right: 10px;">
+                         {$milestone}
+                    </a>
+                </div>
+            HTML;
+
+                // Append the custom HTML button to the grid's toolbar
+                $tools->append($customButtonHTML);
+            });
         }
 
         $grid->disableRowSelector();
