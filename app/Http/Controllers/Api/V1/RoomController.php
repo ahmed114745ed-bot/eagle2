e83bc8cdc9867793580f7b8f251817de801c915a
shami2024 <>
2025-10-28 10:42:24 +0300

    CalculateRoomCupRewards

--- app/Http/Controllers/Api/V1/RoomController.php
+++ app/Http/Controllers/Api/V1/RoomController.php
@@ -1324,7 +1324,7 @@ class RoomController extends Controller
         if (!in_array($admin_id, $vis_arr)) return Common::apiResponse(0, 'This user is not in this room', null, 404);
 
         $roomAdmin = $room->room_admin;
-        $roomMax   = $room->max_admin;
+        $roomMax   = $room->total_admins;
         $adm_arr   = ($roomAdmin == '') ? [] : explode(",", trim($roomAdmin));
         if (count($adm_arr) > 0 && $adm_arr[0] == '') unset($adm_arr[0]);
         $adm_arr   = array_unique($adm_arr);
