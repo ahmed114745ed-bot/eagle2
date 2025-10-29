02a73472798bfcb477b6062b0aa50134f0a9c222
shami2024 <>
2025-10-29 15:53:27 +0300

    Update EncorUsersController.php

--- app/AreaManager/Controllers/EncorUsersController.php
+++ app/AreaManager/Controllers/EncorUsersController.php
@@ -323,7 +323,7 @@ class EncorUsersController extends AdminController
         $form->ignore(['password_confirmation']);
         $form->hidden('type', __('Type'))->value(PermissionType::SUB_AREA_MANAGER->value);
 
-        $form->multipleSelect('roles', trans('admin.roles'))->options($roleModel::all()->where('id', $roleAuthId)->where('type', PermissionType::AREA_MANAGER->value)->pluck('name', 'id'));
+        $form->multipleSelect('roles', trans('admin.roles'))->options($roleModel::all()->where('admin_id', $roleAuthId)->where('type', PermissionType::AREA_MANAGER->value)->pluck('name', 'id'));
         // $form->multipleSelect('permissions', trans('admin.permissions'))->options($permissionModel::all()->pluck('name', 'id'));
 
         $form->display('created_at', trans('admin.created_at'));
