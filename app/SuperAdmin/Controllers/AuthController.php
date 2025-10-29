6df5eddd9c77d8a3e0bc67d61d0908aa2c7ff686
Erinyelkess <ariny.backend@utdsoftware.com>
2025-10-29 12:23:34 +0300

    edit

--- app/SuperAdmin/Controllers/AuthController.php
+++ app/SuperAdmin/Controllers/AuthController.php
@@ -118,8 +118,8 @@ class AuthController extends BaseAuthController
     {
         $username = $request->username;
         $code     = $request->code;
-
-        $auth = \App\Models\Admin::where('username', $username)->first();
+        $type = $request->type;
+        $auth = \App\Models\Admin::where('username', $username)->where('type', $type)->first();
         if (!$auth) {
             return response()->json([
                 'success' => false,
@@ -146,7 +146,7 @@ class AuthController extends BaseAuthController
             $current = Cookie::get($cookie_name);
         }
 
-        $redirect = superadmin_url('change-password-view') . '?username=' . urlencode($username);
+        $redirect = superadmin_url('change-password-view') . '?username=' . urlencode($username) . '&type=' . urlencode($type);
 
 
         return response()->json([
