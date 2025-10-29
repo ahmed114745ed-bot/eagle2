076dda3db7a420b6cd5d04e032ec49c3a7524a2b
Erinyelkess <ariny.backend@utdsoftware.com>
2025-10-29 14:25:59 +0300

    edit

--- app/AreaManager/Controllers/OfficialMessageController.php
+++ app/AreaManager/Controllers/OfficialMessageController.php
@@ -48,7 +48,7 @@ class OfficialMessageController extends MainController
     {
         $grid = new Grid(new OfficialMessage);
         $countryID = session('country_id');
-        $grid->model()->where('admin_type', PermissionType::AREA_MANAGER->value)->where('type', 2)->orderByDesc('id');
+        $grid->model()->where('admin_id', auth()->id())->where('type', 2)->orderByDesc('id');
 
         $grid->filter(function (Grid\Filter $filter) {
             $filter->expand();
