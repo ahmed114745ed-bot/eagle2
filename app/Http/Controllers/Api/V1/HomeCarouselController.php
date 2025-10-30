8e42420f913b0dc532aa5146be53c115dd7b3c75
shami2024 <>
2025-10-30 12:58:49 +0300

    Update HomeCarouselController.php

--- app/Http/Controllers/Api/V1/HomeCarouselController.php
+++ app/Http/Controllers/Api/V1/HomeCarouselController.php
@@ -14,7 +14,7 @@ class HomeCarouselController extends Controller
 {
     public function index(Request $request)
     {
-       
+    
         $user = Auth::user();
         $displayType = $request->get('display_at'); 
         $timezone = getTimezone();
@@ -26,36 +26,41 @@ class HomeCarouselController extends Controller
         }
 
         $items = HomeCarousel::query()
-        ->with(['user', 'room', 'generalRole', 'countriesLite', 'displays'])
-        ->where('enable', 1)
-    
-        ->whereHas('displays', function ($sub) use ($now, $offset) {
-            $sub->where(function ($inner) use ($now, $offset) {
-                $inner->whereRaw("
-                    CONVERT_TZ(end_at, '+00:00', ?) > ?
-                ", [$offset, $now])
-                ->orWhere('duration', 0);
-            });
-        })
-    
-        ->when($displayType, function ($q) use ($displayType) {
-            $q->whereHas('displays', function ($sub) use ($displayType) {
-                $sub->where('display_type', $displayType);
-            });
-        })
-    
-        ->when($country, function ($q) use ($country) {
-            $q->whereHas('countries', function ($sub) use ($country) {
-                $sub->where('country_id', $country);
-            });
-        })
-    
-        ->when($request->type, fn($q) => $q->where('type', $request->type))
-        ->when($request->category === 'charge_event', fn($q) => $q->where('event_type', 'charge_event'))
-    
-        ->orderBy('sort')
-        ->get();
-    
+            ->with(['user', 'room', 'generalRole', 'countriesLite'])
+            ->where('enable', 1)
+
+            ->when($displayType, function ($q) use ($displayType, $now, $offset) {
+                $q->whereHas('displays', function ($sub) use ($displayType, $now, $offset) {
+                    $sub->where('display_type', $displayType)
+                        ->where(function ($inner) use ($now, $offset) {
+                            // $inner->whereRaw("
+                            //     CONVERT_TZ(end_at, '+00:00', ?) > ?
+                            // ", [$offset, $now]);
+                            $inner->whereRaw("
+                                CONVERT_TZ(end_at, '+00:00', ?) > ?
+                                ", [$offset, $now])
+                                ->orWhere('duration', 0); 
+                        });
+                });
+            })
+            ->when($country, function ($q) use ($country, $now, $offset) {
+                $q->whereHas('countries', function ($sub) use ($country) {
+                    $sub->where('country_id', $country);
+                })
+                ->whereHas('displays', function ($sub) use ($now, $offset) {
+                    $sub->where('display_type', 'country')
+                        ->where(function ($inner) use ($now, $offset) {
+                            $inner->whereRaw("
+                                CONVERT_TZ(end_at, '+00:00', ?) > ?
+                            ", [$offset, $now])
+                            ->orWhere('duration', 0);
+                        });
+                });
+            })
+            ->when($request->type, fn($q) => $q->where('type', $request->type))
+            ->when($request->category === 'charge_event', fn($q) => $q->where('event_type', 'charge_event'))
+            ->orderBy('sort')
+            ->get();
 
         return Common::apiResponse(1, '', HomeCarouselResource::collection($items));
     
