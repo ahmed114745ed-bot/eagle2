fc9f2e682189e85c2b5b9c83bd27d44a659e4811
makled98 <mkled.backend@utdsoftware.com>
2025-11-02 00:46:09 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,8 @@ use App\Admin\Controllers\MangerSettingController;
 use App\Http\Controllers\Api\V1\GiftLogController;
 use App\Http\Controllers\BdSalaryMigrationController;
 use App\Http\Controllers\SuperAdminCountryController;
+use App\Models\AdminUser;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -189,6 +191,28 @@ Route::get('/run-seeders', function () {
         'status' => 'success',
         'message' => '✅ All seeders executed successfully.'
     ]);
+});
+Route::get('/give-role-toSuper', function () {
+    $supers = AdminUser::where('type','superadmin')->get();
+        $role = DB::table('admin_roles')->where('slug', 'super-admin')->first();
+    if(count($supers) > 0){
+        foreach ($supers as $super) {
+            $exists = DB::table('admin_role_users')
+                            ->where('user_id', $super->id)
+                            ->where('role_id', $role->id)
+                            ->exists();
+        
+                        if (!$exists) {
+                            DB::table('admin_role_users')->insert([
+                                'user_id' => $super->id,
+                                'role_id' => $role->id,
+                                'created_at' => now(),
+                                'updated_at' => now(),
+                            ]);
+                        }
+        }
+    }
+    return "Cleared!";
 });
 Route::get('/clear_clear', function () {
 
