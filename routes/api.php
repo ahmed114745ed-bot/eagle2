90c810d15f366fff6669bfa0727c412dd47d6fc8
bola philip <bolaphilip3@gmail.com>
2025-11-02 15:24:30 +0200

    test mics

--- routes/api.php
+++ routes/api.php
@@ -261,6 +261,7 @@ Route::prefix(config('app.api_prefix'))->group(function () {
 
                 Route::post('liveTime', [MicrophoneController::class, 'lifeTime']);
                 Route::post('up-microphone', [MicrophoneController::class, 'upMicrophone']);
+                Route::post('up-microphone2', [MicrophoneController::class, 'upMicrophone2']);
                 Route::post('leave-microphone', [MicrophoneController::class, 'goMicrophone']);
                 Route::post('kick_microphone', [MicrophoneController::class, 'kickMicrophone']);
                 Route::post('mute_microphone', [MicrophoneController::class, 'mute_microphone']);
