d721d63c8fa1696695d2f352b876a665672d16da
shami2024 <>
2025-11-02 16:58:47 +0200

    Update ChatRoomPusherV2Resource.php

--- Modules/Chat/Http/Resources/ChatRoomPusherV2Resource.php
+++ Modules/Chat/Http/Resources/ChatRoomPusherV2Resource.php
@@ -12,31 +12,26 @@ class ChatRoomPusherV2Resource extends JsonResource
 
     public function toArray(Request $request)
     {
-        if($this->user_id == $request->user()->id)
-        {
-            $user2 = User::find($this->user_id);  
-        }
-        else{
-            $user2 = User::find($this->user_id2); 
-        }
-        \Log::info('Chat Receiver: ', ['receiver_id' => $user2->id, 'receiver_name' => $user2->name , 'id' => $this->id]);
-
         $currentUserId = $request->user()->id;
+        $user2 = $this->user_id == $currentUserId ? User::find($this->user_id2) : User::find($this->user_id);
+    
+        \Log::info('Chat Receiver: ', ['receiver_id' => $user2->id, 'receiver_name' => $user2->name , 'chat_room_id' => $this->id]);
+    
         $total_undread_message = ChatMessage::where('chat_room_id', $this->id)
-        ->where('user_id', '!=', $currentUserId) 
-        ->where('status', '!=', 'seen')
-        ->count();
-        
-        \Log::info(' $total_undread_message: ', ['total_undread_message' =>  $total_undread_message]);
-
+            ->where('user_id', $user2->id)
+            ->whereRaw("LOWER(status) != 'seen'")
+            ->count();
+    
+        \Log::info('Total unread messages: ', ['total_undread_message' => $total_undread_message]);
+    
         return [
-            'user_id'             => $user2->id,
-            'name'                => $user2->name,
-            'img'                 => @$user2->profile->avatar,
-            'chat_id'             => $this->id,
-            'type'                => $this->type,
-            'unread_message'      => $total_undread_message,
-            'last_message'        => @ new ChatMessageResource( $this->messages[0]),
+            'user_id'        => $user2->id,
+            'name'           => $user2->name,
+            'img'            => @$user2->profile->avatar,
+            'chat_id'        => $this->id,
+            'type'           => $this->type,
+            'unread_message' => $total_undread_message,
+            'last_message'   => @$this->messages->first() ? new ChatMessageResource($this->messages->first()) : null,
         ];
     }
 }
