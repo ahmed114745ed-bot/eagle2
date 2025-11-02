5ac1366f69bf0105abea49f9c7eaa8fc4ef9d50d
shami2024 <>
2025-11-02 17:16:59 +0200

    Update ChatMessagesController.php

--- Modules/Chat/Http/Controllers/ChatMessagesController.php
+++ Modules/Chat/Http/Controllers/ChatMessagesController.php
@@ -72,7 +72,8 @@ class ChatMessagesController extends Controller
             ], 404);
         }
 
-        //get user 2
+        \Log::info('$chatRoom->user_id: ', ['$chatRoom->user_id' => $chatRoom->user_id, 'user->id' => $user->id ]);
+        \Log::info('$chatRoom->user_id: ', [ '$request->user_id' => $request->user_id]);
         if ($chatRoom->user_id == $user->id) {
             $user2 = User::withoutAppends()->find($chatRoom->user_id2);
         } else {
