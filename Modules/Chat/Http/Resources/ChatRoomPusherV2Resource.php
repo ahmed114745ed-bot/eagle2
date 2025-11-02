778d8c75082436b239c9265f5c82cd7cb72580a1
shami2024 <>
2025-11-02 16:29:23 +0200

    Update ChatRoomPusherV2Resource.php

--- Modules/Chat/Http/Resources/ChatRoomPusherV2Resource.php
+++ Modules/Chat/Http/Resources/ChatRoomPusherV2Resource.php
@@ -14,10 +14,10 @@ class ChatRoomPusherV2Resource extends JsonResource
     {
         if($this->user_id == $request->user()->id)
         {
-            $user2 = User::find($this->user_id2); 
+            $user2 = User::find($this->user_id);  
         }
         else{
-            $user2 = User::find($this->user_id);  
+            $user2 = User::find($this->user_id2); 
         }
         \Log::info('Chat Receiver: ', ['receiver_id' => $user2->id, 'receiver_name' => $user2->name]);
 
