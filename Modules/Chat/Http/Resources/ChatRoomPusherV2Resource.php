49600db0d4437adbc1316f9a940e5d9eeca47d03
shami2024 <>
2025-11-02 16:24:34 +0200

    ChatRoomPusherV2Resource

---
+++ Modules/Chat/Http/Resources/ChatRoomPusherV2Resource.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Modules\Chat\Http\Resources;
+
+use Modules\Chat\Entities\ChatMessage;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ChatRoomPusherV2Resource extends JsonResource
+{
+
+    public function toArray(Request $request)
+    {
+        if($this->user_id == $request->user()->id)
+        {
+            $user2 = User::find($this->user_id);  
+        }
+        else{
+            $user2 = User::find($this->user_id2); 
+        }
+        \Log::info('Chat Receiver: ', ['receiver_id' => $user2->id, 'receiver_name' => $user2->name]);
+
+
+        $total_undread_message = ChatMessage::where('chat_room_id',$this->id)->where('user_id',$user2->id)->where('status','not Like','seen')->count();
+        return [
+            'user_id'             => $user2->id,
+            'name'                => $user2->name,
+            'img'                 => @$user2->profile->avatar,
+            'chat_id'             => $this->id,
+            'type'                => $this->type,
+            'unread_message'      => $total_undread_message,
+            'last_message'        => @ new ChatMessageResource( $this->messages[0]),
+        ];
+    }
+}
