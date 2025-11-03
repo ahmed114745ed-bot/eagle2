b2102470e420a1365f5a699fd3b6f846293fbd3c
shami2024 <>
2025-11-03 16:04:35 +0200

    Update ChatRoomPusherV2Resource.php

--- Modules/Chat/Http/Resources/ChatRoomPusherV2Resource.php
+++ Modules/Chat/Http/Resources/ChatRoomPusherV2Resource.php
@@ -12,16 +12,16 @@ class ChatRoomPusherV2Resource extends JsonResource
 
     public function toArray(Request $request)
     {
-        $currentUserId = $request->user()->id;
-        $user2 = $this->user_id == $currentUserId ? User::find($this->user_id) : User::find($this->user_id2);
+        if($this->user_id == $request->user()->id)
+        {
+            $user2 = User::find($this->user_id);  
+        }
+        else{
+            $user2 = User::find($this->user_id2);  
+        }
 
+        $total_undread_message = ChatMessage::where('chat_room_id',$this->id)->where('user_id',$user2->id)->where('status','not Like','seen')->count();
         \Log::info('Chat Receiver: ', ['receiver_id' => $user2->id, 'receiver_name' => $user2->name , 'chat_room_id' => $this->id]);
-
-        $total_undread_message = ChatMessage::where('chat_room_id', $this->id)
-            ->where('user_id', '!=',$user2->id)
-            ->whereRaw("LOWER(status) != 'seen'")
-            ->count();
-
         \Log::info('Total unread messages: ', ['total_undread_message' => $total_undread_message]);
 
         return [
