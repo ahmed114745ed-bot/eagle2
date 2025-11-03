d2211ac70d6690f1db8fa03263697b5d6cfade42
shami2024 <>
2025-11-03 12:00:41 +0200

    Update routes.php

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -512,8 +512,8 @@ Route::group(
 //        Route::get('userBd/select', [BdSelectController::class, 'index'])->name('userBd.select');
 
 
-//        Route::post('superadmin-users/make-default', [SuperAdminSelectController::class, 'makeDefault'])->name('make-superadmin-default');
-//        Route::get('superadmin-users/select', [SuperAdminSelectController::class, 'index'])->name('superadmin-users.select');
+       Route::post('superadmin-users/make-default', [SuperAdminSelectController::class, 'makeDefault'])->name('make-superadmin-default');
+       Route::get('superadmin-users/select', [SuperAdminSelectController::class, 'index'])->name('superadmin-users.select');
 
 
         // Route::resource('ovip', 'OVipController');
