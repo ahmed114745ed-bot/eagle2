3a39bf16da2ec3506f636ffa667d1c1af6388e60
shami2024 <>
2025-11-04 10:34:28 +0200

    Update ChatRoomPusherV2Resource.php

--- Modules/Chat/Http/Resources/ChatRoomPusherV2Resource.php
+++ Modules/Chat/Http/Resources/ChatRoomPusherV2Resource.php
@@ -15,9 +15,9 @@ class ChatRoomPusherV2Resource extends JsonResource
         $authUser = $request->user();
     
         if ($authUser && $this->user_id == $authUser->id) {
-            $user2 = User::find($this->user_id2);
-        } else {
             $user2 = User::find($this->user_id);
+        } else {
+            $user2 = User::find($this->user_id2);
         }
     
         $total_unread_message = ChatMessage::where('chat_room_id', $this->id)
