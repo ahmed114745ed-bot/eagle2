c5e917b56ffebd1ad869ea27ba00ceeccdae359b
bola philip <bolaphilip3@gmail.com>
2025-11-05 17:54:15 +0200

    log images

--- Modules/Form/Http/Controllers/FormTemplateController.php
+++ Modules/Form/Http/Controllers/FormTemplateController.php
@@ -328,12 +328,12 @@ class FormTemplateController extends Controller
                 $storedFiles = [];
                 foreach ($fileInput as $file) {
                     if ($file && $file->isValid()) {
-                        $storedFiles[] = Common::upload('data', $file, 'public');
+                        $storedFiles[] = Common::upload('data', $file);
                     }
                 }
                 $data[$key] = $storedFiles;
             } elseif ($fileInput instanceof \Illuminate\Http\UploadedFile && $fileInput->isValid()) {
-                $path = Common::upload('data', $fileInput, 'public');
+                $path = Common::upload('data', $fileInput);
                 $data[$key] = $path;
             } else {
                 info("Unexpected file input type for {$key}");
