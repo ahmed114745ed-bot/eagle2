c6717d4629f4d0bc1287beac969cce9deef48f7f
bola philip <bolaphilip3@gmail.com>
2025-11-09 12:25:40 +0200

    deleted rooms

--- Modules/Chat/Http/Services/ChatRoomService.php
+++ Modules/Chat/Http/Services/ChatRoomService.php
@@ -173,6 +173,23 @@ class ChatRoomService
                         });
                 });
             })
+            ->where(function ($q) use ($user) {
+                $q->where(function ($sub) use ($user) {
+                    $sub->where('user_id', $user->id)
+                        ->where(function ($inner) {
+                            $inner->whereNull('user_1_deleted');
+                        });
+                })
+                    ->orWhere(function ($sub) use ($user) {
+                        $sub->where('user_id', '!=', $user->id)
+                            ->where(function ($inner) {
+                                $inner->whereNull('user_2_deleted');
+                            });
+                    })
+                    ->orWhere(function ($sub) use ($user) {
+                        $sub->whereNotNull('user_1_deleted')->whereNotNull('user_2_deleted');
+                    });
+            })
             ->groupBy([
                 'chat_rooms.id',
                 'chat_rooms.user_id',
