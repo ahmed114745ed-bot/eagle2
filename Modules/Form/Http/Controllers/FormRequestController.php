a90006be0320e19e7a4d8b7c06dd06b1acc68737
Erinyelkess <ariny.backend@utdsoftware.com>
2025-11-10 14:04:31 +0200

    edit lang

--- Modules/Form/Http/Controllers/FormRequestController.php
+++ Modules/Form/Http/Controllers/FormRequestController.php
@@ -515,10 +515,10 @@ $(document).off('pjax:end').on('pjax:end', function() {
 
         $renderer = new FormRenderService();
 
-        $show->field('data','')->as(function ($jsonData) use ($renderer) {
+        $show->field('data', __('dodo'))->as(function ($jsonData) use ($renderer) {
             $data = json_decode($jsonData, true);
             return $renderer->renderFormData($data);
-        })->unescape()->hideLabel();
+        })->unescape();
 
         return $show;
     }
