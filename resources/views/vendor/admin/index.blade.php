d21f2cad18c513a3a1d546337a4196d5d2016bb8
Bola-Philip <77309023+Bola-Philip@users.noreply.github.com>
2025-11-11 17:18:23 +0200

    short cut

--- resources/views/vendor/admin/index.blade.php
+++ resources/views/vendor/admin/index.blade.php
@@ -11,7 +11,7 @@
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
 {{--    <link rel="shortcut icon" href="{{ getAppLogo() }}" type="image/png">--}}
-{{--    <link rel="shortcut icon" href="{{ asset('android-chrome-512x512.png') }}" type="image/png">--}}
+    <link rel="shortcut icon" href="{{ asset('images/android-chrome-512x512.png') }}" type="image/png">
 
 {{--    @if(!is_null($favicon = Admin::favicon()))--}}
 {{--    <link rel="shortcut icon" href="{{$favicon}}">--}}
