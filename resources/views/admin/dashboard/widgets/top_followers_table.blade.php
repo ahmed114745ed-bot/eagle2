6c1a6dea5f185ef2756c95576bc8795a29707308
Bola-Philip <77309023+Bola-Philip@users.noreply.github.com>
2025-11-12 11:32:04 +0200

    statistics images

--- resources/views/admin/dashboard/widgets/top_followers_table.blade.php
+++ resources/views/admin/dashboard/widgets/top_followers_table.blade.php
@@ -74,8 +74,8 @@
     }
 
     .avatar-cell img {
-        width: 40px;
-        height: 40px;
+        width: 60px;
+        height: 60px;
         object-fit: cover;
         border-radius: 50%;
     }
