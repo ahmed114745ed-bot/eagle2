82ed8dc7028b18362fc148a7bfe8b6bf2ad152ab
Erinyelkess <ariny.backend@utdsoftware.com>
2025-11-12 10:40:50 +0200

    edit

--- app/Admin/Controllers/AgencyControllers/UserController.php
+++ app/Admin/Controllers/AgencyControllers/UserController.php
@@ -465,7 +465,7 @@ class UserController extends MainController
      */
     protected function grid()
     {
-        $countryID =session('filter_country_id');
+        $countryID =$countryID = empty((array)session('filter_country_id')) ? Common::areaCountries() : (array)session('filter_country_id');
 
         $grid = new Grid(new User());
         $haveCoins = (request()->have_coins == 1);
