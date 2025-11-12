5000f91e13ef690948cd9e438087c61e8d159a93
Erinyelkess <ariny.backend@utdsoftware.com>
2025-11-12 11:57:42 +0200

    edit

--- Modules/AreaManager/Entities/AreaManager.php
+++ Modules/AreaManager/Entities/AreaManager.php
@@ -79,7 +79,7 @@ class AreaManager extends Authenticatable
             $url = getImagePath($country->flag);
 
             if ($url) {
-                $html .= handleShowImageWithTypes($country->id, $url, 5, 5, 4, 'contain');
+                $html .= handleShowImageWithTypes($country->id, $url, 20, 10, 4);
             // '<img src="' . e( $url) . '" alt="' . e($country->name) . '" style="width: 100px; height: 100px; object-fit: contain; border-radius: 4px; margin-right: 4px;">';
 
             }
