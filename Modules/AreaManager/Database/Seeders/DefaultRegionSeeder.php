37ad4a743b7a1f30b96a27c588bb6ba458cbc246
shami2024 <>
2025-11-13 15:08:35 +0200

    AreaManagerSeeder

--- Modules/AreaManager/Database/Seeders/DefaultRegionSeeder.php
+++ Modules/AreaManager/Database/Seeders/DefaultRegionSeeder.php
@@ -11,22 +11,6 @@ class DefaultRegionSeeder extends Seeder
 {
     public function run()
     {
-        $defaultManager = AreaManager::where('default', 1)->first();
 
-        if (!$defaultManager) {
-            return;
-        }
-
-        $defaultRegion = Region::firstOrCreate(
-            ['name' => 'Default Region for Default Manager', 'manager_id' => $defaultManager->id]
-        );
-
-        $countriesWithoutRegion = Country::whereDoesntHave('regions')->get();
-
-        foreach ($countriesWithoutRegion as $country) {
-            $country->regions()->attach($defaultRegion->id);
-        }
-
-        $this->command->info('✅');
     }
 }
