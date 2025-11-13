4fa6b6b91c30a9e61bc73d11e988974b80167b97
Bola-Philip <77309023+Bola-Philip@users.noreply.github.com>
2025-11-13 13:16:49 +0200

    update flags

--- database/seeders/FlagSyrianSeeder.php
+++ database/seeders/FlagSyrianSeeder.php
@@ -67,11 +67,11 @@ class FlagSyrianSeeder extends Seeder
 
         Country::where('iso3', 'ISR')->update([
             'iso' => 'PS',
-            'name' => 'Palestine',
-            'name_ar' => 'فلسطين',
+            'e_name' => 'Palestine',
+            'name' => 'فلسطين',
             'iso3' => 'PSE',
             'numcode' => '275',
-            'phonecode' => '970',
+            'phone_code' => '+970',
             'iso_numeric' => '275',
             'currency_numeric' => null,
         ]);
