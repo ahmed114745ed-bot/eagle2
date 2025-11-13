fee2c49fa04a94fe91ba3fa0f35df89b778b3ff6
Erinyelkess <ariny.backend@utdsoftware.com>
2025-11-13 15:01:41 +0200

    edit

--- Modules/AreaManager/Http/Controllers/ChargeController.php
+++ Modules/AreaManager/Http/Controllers/ChargeController.php
@@ -17,8 +17,9 @@ use Encore\Admin\Widgets\InfoBox;
 use App\Enums\Charges\UserTypeEnum;
 use Illuminate\Support\Facades\Auth;
 use App\Admin\Controllers\MainController;
-use Modules\AreaManager\Entities\SubAreaManager;
 use Modules\SuperAdmin\Entities\SuperAdmin;
+use Modules\AreaManager\Entities\AreaManager;
+use Modules\AreaManager\Entities\SubAreaManager;
 
 
 class ChargeController extends MainController
@@ -109,7 +110,12 @@ class ChargeController extends MainController
                     if ($this->input) {
                         $query->where('charger_id', $this->input);
                     }
-                }, __('created by'))->select(SubAreaManager::where('parent_id', $authId)->pluck('name', 'id')->toArray());
+                }, __('created by'))->select(
+                    // Combine SubAreaManagers and AreaManager themselves
+                    SubAreaManager::where('parent_id', $authId)->pluck('name', 'id')
+                        ->merge(AreaManager::where('id', $authId)->pluck('name', 'id'))
+                        ->toArray()
+                );
             }
 
 
