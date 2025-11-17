9781ac6242b7e9c2e5bd02363c3c42b6d58b52bd
shami2024 <>
2025-11-17 11:23:20 +0200

    Update 2025_09_13_173741_marged3_coin_game_users_archive_columns.php

--- database/migrations/2025_09_13_173741_marged3_coin_game_users_archive_columns.php
+++ database/migrations/2025_09_13_173741_marged3_coin_game_users_archive_columns.php
@@ -9,17 +9,6 @@ return new class extends Migration
 {
     public function up(): void
     {
-        // coin_game_users
-        DB::statement("ALTER TABLE coin_game_users ADD INDEX idx_cgu_user_id (user_id)"); 
-        DB::statement("ALTER TABLE coin_game_users ADD INDEX idx_cgu_created_at (created_at)");
-        DB::statement("ALTER TABLE coin_game_users ADD INDEX idx_cgu_user_created (user_id, created_at)");
-        DB::statement("ALTER TABLE coin_game_users ADD INDEX idx_cgu_type (type)");
-
-        // coin_game_users_archive
-        DB::statement("ALTER TABLE coin_game_users_archive ADD INDEX idx_cgua_user_id (user_id)");
-        DB::statement("ALTER TABLE coin_game_users_archive ADD INDEX idx_cgua_created_at (created_at)");
-        DB::statement("ALTER TABLE coin_game_users_archive ADD INDEX idx_cgua_user_created (user_id, created_at)");
-        DB::statement("ALTER TABLE coin_game_users_archive ADD INDEX idx_cgua_type (type)");
 
         // Create view
         DB::statement("
@@ -48,14 +37,6 @@ return new class extends Migration
     {
         DB::statement("DROP VIEW IF EXISTS coin_game_users_merged");
 
-        DB::statement("ALTER TABLE coin_game_users DROP INDEX idx_cgu_user_id");
-        DB::statement("ALTER TABLE coin_game_users DROP INDEX idx_cgu_created_at");
-        DB::statement("ALTER TABLE coin_game_users DROP INDEX idx_cgu_user_created");
-        DB::statement("ALTER TABLE coin_game_users DROP INDEX idx_cgu_type");
-
-        DB::statement("ALTER TABLE coin_game_users_archive DROP INDEX idx_cgua_user_id");
-        DB::statement("ALTER TABLE coin_game_users_archive DROP INDEX idx_cgua_created_at");
-        DB::statement("ALTER TABLE coin_game_users_archive DROP INDEX idx_cgua_user_created");
-        DB::statement("ALTER TABLE coin_game_users_archive DROP INDEX idx_cgua_type");
+  
     }
 };
