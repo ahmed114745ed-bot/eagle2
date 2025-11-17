e13e6874241a8b1f0a24c7ac0b94c341fd1ea507
shami2024 <>
2025-11-17 17:05:15 +0200

    bd created_by

--- Modules/SuperAdmin/Http/Controllers/SuperAdmin/BdController.php
+++ Modules/SuperAdmin/Http/Controllers/SuperAdmin/BdController.php
@@ -248,6 +248,9 @@ class BdController extends MainController
             }
         }
 
+        $grid->column('created_by', 'Creator')->display(function ($creatorId) {
+            return app(\App\Admin\Services\CreatorService::class)->show($creatorId);
+        });
         $grid->column('created_at', __('Created at'))->display(function ($date) {
             $carbonDate = Carbon::parse($date);
             $locale = App::getLocale();
