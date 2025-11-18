ce5178062dcd569a2a57f81308750354947b7bb8
shami2024 <>
2025-11-18 14:20:47 +0200

    Update VerifyGameCoinSignature.php

--- app/Http/Middleware/VerifyGameCoinSignature.php
+++ app/Http/Middleware/VerifyGameCoinSignature.php
@@ -14,20 +14,22 @@ class VerifyGameCoinSignature
         $roundId     = $request->input('roundId');
         $uid         = $request->input('uid');
         $coin        = $request->input('coin');
+        $type         = $request->input('type');
         $rewardType  = $request->input('rewardType');
         $winId       = $request->input('winId', '');
+        $token       = $request->input('token');
         $sign        = $request->input('sign');
 
         $key = config('games.leader_CC_game_key'); 
 
-        if (!$orderId || !$gameId || !$roundId || !$uid || !$coin || !$rewardType || !$sign) {
+        if (!$orderId || !$gameId || !$roundId || !$uid || !$coin || !$rewardType || $type || !$sign || $token ) {
             return response()->json([
                 'errorCode' => 4005,
                 'errorMsg'  => 'Missing signature parameters'
             ], 400);
         }
 
-        $expectedSign = md5($orderId . $gameId . $roundId . $uid . $coin . $rewardType . $winId . $key);
+        $expectedSign = md5($orderId . $gameId . $roundId . $uid . $coin . $type . $rewardType  . $winId . $token . $key);
 
         if (!hash_equals(strtolower($expectedSign), strtolower($sign))) {
             return response()->json([
