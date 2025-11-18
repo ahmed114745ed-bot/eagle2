5e364fe531bc78417f283047275aac0ac52477e4
shami2024 <>
2025-11-18 10:03:29 +0200

    Update Conversation.php

--- Modules/Chat/Events/Conversation.php
+++ Modules/Chat/Events/Conversation.php
@@ -12,7 +12,7 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 
-class Conversation implements ShouldBroadcastNow
+class Conversation implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
