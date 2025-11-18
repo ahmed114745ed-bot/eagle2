8ef3cbaa890f20ac4556ada465f487c91d6f8661
shami2024 <>
2025-11-18 10:30:45 +0200

    rm logs

--- Modules/Chat/Events/Conversation.php
+++ Modules/Chat/Events/Conversation.php
@@ -40,12 +40,12 @@ class Conversation implements ShouldBroadcast
     {
         $data = (array) $this->message;
 
-        Log::info('Broadcasting Conversation Event', [
-            'chat_room_id' => $this->check_room->id,
-            'message_id' => $this->message->id,
-            'status' => $this->message->status,
-            'payload' => $data
-        ]);
+        // Log::info('Broadcasting Conversation Event', [
+        //     'chat_room_id' => $this->check_room->id,
+        //     'message_id' => $this->message->id,
+        //     'status' => $this->message->status,
+        //     'payload' => $data
+        // ]);
     
         return $data;
     
