1884b75e5558b94d3ddaf91141f12ffd03f62c45
shami2024 <>
2025-11-19 14:38:25 +0200

    ChatMessageV2Resource

--- Modules/Chat/Http/Resources/ChatRoomResource.php
+++ Modules/Chat/Http/Resources/ChatRoomResource.php
@@ -24,19 +24,7 @@ class ChatRoomResource extends JsonResource
 
         $total_undread_message = ChatMessage::where('chat_room_id', $this->id)->where('user_id', 'not Like', $user->id)->where('status', 'not Like', 'seen')->count();
         $hasColor = Common::hasInPack($user2, 18, true);
-        $filteredMessages = $this->messages->filter(function ($msg) use ($request) {
 
-            if ($msg->user_id == $request->user()->id) {
-                return is_null($msg->user_1_deleted); 
-            }
-        
-            return is_null($msg->user_2_deleted); 
-        });
-        
-        $lastMessage = $filteredMessages->sortByDesc('id')->first();
-        
-
-        $latestMessage = $filteredMessages->sortByDesc('id')->first();
         return [
             'user_id'             => @$user2->id,
             'name'                => @$user2->name,
@@ -45,11 +33,15 @@ class ChatRoomResource extends JsonResource
             'chat_id'             => $this->id,
             'unread_message'      => $total_undread_message,
             'colored_name'        => $hasColor ? common::wareUserVip($user2, 18, 'color') ?? '' : '',
-            'last_message'        =>    $lastMessage ? new ChatMessageV2Resource($lastMessage) : null,
-
+            'last_message' => @new ChatMessageV2Resource(
+                $this->messages
+                    ->filter(function ($msg) {
+                        return is_null($msg->user_1_deleted)
+                            || (!is_null($msg->user_1_deleted) && !is_null($msg->user_2_deleted));
+                    })
+                    ->sortByDesc('id')
+                    ->first()
+            ),
             ];
     }
 }
-
-
-
