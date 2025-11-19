cf021f771873ef0e5d29cdd541f3528062a7daca
shami2024 <>
2025-11-19 12:02:04 +0200

    Update LuckyGiftService.php

--- app/Services/Gifts/LuckyGiftService.php
+++ app/Services/Gifts/LuckyGiftService.php
@@ -203,7 +203,7 @@ class LuckyGiftService
             $message = null;
             $total_user_win += ($totalGiftPrice * $cashback_percentage);
             $total_count_win += 1;
-            $cashback_percentage = 0;
+            // $cashback_percentage = 0;
             //            $this->save_data_win_for_user($user->id,$totalGiftPrice,$cashback_percentage);
         }
 
@@ -243,6 +243,7 @@ class LuckyGiftService
         $responseData['user_coins'] = $userCoins;
         $responseData['gift_num'] = $receiversCount * $number * $count;
         $responseData['total_price'] = $totalPrice;
+        $responseData['cashback_percentage'] = $cashback_percentage;
 
         //update user coins and diamond and sender level
         $totalDiamond           = $totalPrice * $count;
