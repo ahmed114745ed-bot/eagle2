df0759e97bab7f3991a7fe8fece5ab3da5df62d4
shami2024 <>
2025-11-19 11:56:38 +0200

    Update LuckyGiftService.php

--- app/Services/Gifts/LuckyGiftService.php
+++ app/Services/Gifts/LuckyGiftService.php
@@ -135,6 +135,9 @@ class LuckyGiftService
             $ownerWallet->coins += $price; //        $appWallet->save();
             //        $ownerWallet->save();
             $isWinner       = $this->is_winner($gift);
+            \Log::info('⚡ isWinner check result', [
+                'isWinner'           => $isWinner,
+            ]);
             $isPopular      = false;
             $totalGiftPrice = $giftPrice * $number;
             $appWalletCoins = $appWallet->coins;
