afc71bf3ebdcfe076b0cd4ebc216601f99bf4688
Erinyelkess <ariny.backend@utdsoftware.com>
2025-11-19 15:02:52 +0200

    edit

--- app/Http/Resources/AllGameResource.php
+++ app/Http/Resources/AllGameResource.php
@@ -11,6 +11,12 @@ class AllGameResource extends JsonResource
     public function toArray($request)
     {
         $type = \request()->type ?? 0;
+         $typeList = [
+        0 => __('joy'),
+        1 => __('OX'),
+        2 => __('Baishun'),
+        3 => __('Leader cc'),
+    ];
         return [
             'id'      =>  $this->id,
             'name'      => (auth()->user()->lan == "ar" ? $this->name : $this->name_en),
@@ -20,7 +26,8 @@ class AllGameResource extends JsonResource
             'high_safety' => (intval(@$this->hight_image) ?? 0),
             'high' => intval($this->in_room == 1 ? (floatval($this->hight?? 0) ) : null),
             'in_room' => @$this->in_room,
-            'is_hot' => rand(0,1)
+            'is_hot' => rand(0,1),
+            'type' => $typeList[$this->type] ?? '',
         ];
     }
 }
