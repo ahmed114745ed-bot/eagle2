720e09f7d42bfadb02aa7c37eb02823df373134f
Erinyelkess <ariny.backend@utdsoftware.com>
2025-11-23 12:23:09 +0200

    edit

--- Modules/SuperAdmin/Http/Controllers/Admin/RestoreSuperAdminController.php
+++ Modules/SuperAdmin/Http/Controllers/Admin/RestoreSuperAdminController.php
@@ -5,10 +5,11 @@ namespace Modules\SuperAdmin\Http\Controllers\Admin;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
-use Modules\SuperAdmin\Entities\SuperAdmin;
 use Encore\Admin\Facades\Admin;
-use App\Admin\Controllers\MainController;
 use Encore\Admin\Layout\Content;
+use App\Admin\Controllers\MainController;
+use Modules\SuperAdmin\Entities\SuperAdmin;
+use Modules\SuperAdmin\Actions\Admin\RestoreSuperAdminAction;
 
 class RestoreSuperAdminController extends MainController
 {
@@ -109,7 +110,7 @@ class RestoreSuperAdminController extends MainController
         if (Admin::user()->can('restore-switch-' . $this->permission_name) || Admin::user()->can('*')) {
             $grid->column('return', __('restore'))->display(function () {
                 $superAdmin = SuperAdmin::where('country_id', $this->country_id)->first();
-                return  $superAdmin ? '<span style="color: red;">' . __('can not restore this super admin') . '</span>' : (new \App\Admin\Actions\RestoreSuperAdminAction($this->id))->render();
+                return  $superAdmin ? '<span style="color: red;">' . __('can not restore this super admin') . '</span>' : (new RestoreSuperAdminAction($this->id))->render();
             });
         }
 
