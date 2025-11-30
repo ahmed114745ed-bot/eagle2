4e8308498897cf720ee9369465ac16106bba939a
Erinyelkess <ariny.backend@utdsoftware.com>
2025-11-30 15:23:58 +0200

    time zone

--- app/Traits/HostLevelTrait.php
+++ app/Traits/HostLevelTrait.php
@@ -15,18 +15,15 @@ trait HostLevelTrait
     public function scopeFilterByEventType(Builder $query, $eventType)
     {
         $timezone = Common::timeZone();
-        return $query
-            ->when($eventType == 'daily', function ($query) use ($timezone) {
-                $query->whereDate('created_at', Carbon::now($timezone)->toDateString());
-            })
-
+        return $query->when($eventType == 'daily', function ($query) use ($timezone) {
+            $query->whereDate('created_at', Carbon::now($timezone)->toDateString());
+        })
             ->when($eventType == 'weekly', function ($query) use ($timezone) {
                 $startOfWeek = Carbon::now($timezone)->startOfWeek(Carbon::SATURDAY);
                 $endOfWeek   = Carbon::now($timezone)->endOfWeek(Carbon::FRIDAY);
 
                 $query->whereBetween('created_at', [$startOfWeek, $endOfWeek]);
             })
-
             ->when($eventType == 'monthly', function ($query) use ($timezone) {
                 $startOfMonth = Carbon::now($timezone)->startOfMonth();
                 $endOfMonth   = Carbon::now($timezone)->endOfMonth();
