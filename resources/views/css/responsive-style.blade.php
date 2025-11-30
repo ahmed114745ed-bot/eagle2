0afe404a0c6a447cb40f56fd104d31cbe625d680
Bola-Philip <77309023+Bola-Philip@users.noreply.github.com>
2025-11-30 11:52:29 +0200

    design

--- resources/views/css/responsive-style.blade.php
+++ resources/views/css/responsive-style.blade.php
@@ -56,7 +56,7 @@
         }
 
         .rtl .navbar-custom-menu>.navbar-nav>li>.dropdown-menu {
-            position: absolute;
+            position: relative;
             right: 0 !important;
         }
 
