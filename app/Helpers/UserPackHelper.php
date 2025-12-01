585f80eb4bc1b3b2ad552a732df734fe3ed4f1f3
makled98 <mkled.backend@utdsoftware.com>
2025-12-01 12:34:56 +0200

    Update UserPackHelper.php

--- app/Helpers/UserPackHelper.php
+++ app/Helpers/UserPackHelper.php
@@ -19,7 +19,7 @@ class UserPackHelper
     public static function getFrameImage(User $user) : string
     {
         $ware = self::getFrameWare($user);
-        Log::info('frame ware',[$ware]);
+        LogHelper::info('frame ware',[$ware]);
         return $ware?->img2 ?? ($ware?->img1 ?? '');
     }
 
