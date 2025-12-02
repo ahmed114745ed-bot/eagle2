1083c2cb2ad881d22c5a691d8bd647b2e4d979c1
Erinyelkess <ariny.backend@utdsoftware.com>
2025-12-02 16:45:38 +0200

    edit

--- app/Admin/Services/CreatorService.php
+++ app/Admin/Services/CreatorService.php
@@ -116,6 +116,6 @@ class CreatorService
 
     protected function creatorUrl(int $id): string
     {
-        return url("admin/users/{$id}");
+        return url("admin/auth/users/{$id}");
     }
 }
