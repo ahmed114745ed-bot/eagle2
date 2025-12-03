0b7a867eaec0b8a96e17fed2628480977f7c5b21
shami2024 <>
2025-12-03 16:30:34 +0200

    Update RankingRepositoryV2.php

--- app/Repositories/RankingRepositoryV2.php
+++ app/Repositories/RankingRepositoryV2.php
@@ -284,6 +284,7 @@ class RankingRepositoryV2
     
 
         $type = (int) $type;
+        
         [$start, $end] = match ($type) {
             0 => [$now->copy()->startOfHour(), $now->copy()->endOfHour()],
             1 => [$now->copy()->startOfDay(), $now->copy()->endOfDay()],
@@ -294,10 +295,7 @@ class RankingRepositoryV2
             3 => [$now->copy()->startOfMonth(), $now->copy()->endOfMonth()],
             default => [null, null],
         };
-            \Log::info('Filtering dates', [
-            'start' => $start->toDateTimeString(),
-            'end' => $end->toDateTimeString(),
-        ]);
+      
         if ($start && $end) {
       
             $query->whereBetween('created_at', [$start, $end]);
