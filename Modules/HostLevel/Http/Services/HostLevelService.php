cc2e9647cea2872f2d9672835449077e1d7b843f
Erinyelkess <ariny.backend@utdsoftware.com>
2025-12-03 11:53:37 +0200

    test

--- Modules/HostLevel/Http/Services/HostLevelService.php
+++ Modules/HostLevel/Http/Services/HostLevelService.php
@@ -144,7 +144,7 @@ class HostLevelService
             ->where('diamonds', '<=', $diamonds)
             ->orderBy('level', 'asc')
             ->get();
-
+       dd($hostLevels);
         return [
             'next' => [
                 'next_level' => $nextLevel->level ?? 0,
