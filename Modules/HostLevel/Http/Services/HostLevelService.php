f44673f603e68d3bf4bbcd4db59244eb6377aca1
Erinyelkess <ariny.backend@utdsoftware.com>
2025-12-04 10:42:17 +0200

    edit

--- Modules/HostLevel/Http/Services/HostLevelService.php
+++ Modules/HostLevel/Http/Services/HostLevelService.php
@@ -43,9 +43,9 @@ class HostLevelService
         $lastPickLevel = $user->lastHostLevelWinnerByEvent($eventType)->first();
 
         $diamonds = $this->computeDiamonds($user->id) ?? 0;
-        $level = HostLevel::where('diamonds', '<=', $diamonds)->orderByDesc('level') > value('level');
+        $level = HostLevel::where('diamonds', '<=', $diamonds)->orderByDesc('level')->value('level');
 
-        return [$diamonds, $nextLevel->level ?? 0, $lastPickLevel->hostLevel->level ?? 0,$level];
+        return [$diamonds, $nextLevel->level ?? 0, $lastPickLevel->hostLevel->level ?? 0, $level];
     }
 
 
