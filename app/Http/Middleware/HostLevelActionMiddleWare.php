f070639593fd23a1e17016c63273e1c7d25271aa
Erinyelkess <ariny.backend@utdsoftware.com>
2025-12-07 10:45:22 +0200

    handling error

--- app/Http/Middleware/HostLevelActionMiddleWare.php
+++ app/Http/Middleware/HostLevelActionMiddleWare.php
@@ -26,7 +26,11 @@ class HostLevelActionMiddleWare
 
 
         $utdHostLevel = $getSetting('host_level_action') ?? 0;
-        if (!$utdHostLevel) abort(403, __('This feature is currently unavailable. Please contact the application administration.'));
+        // dd($utdHostLevel);
+        if (!$utdHostLevel) {
+
+            return response()->view('feature_blocked', [], 403);
+        }
 
         return $next($request);
     }
