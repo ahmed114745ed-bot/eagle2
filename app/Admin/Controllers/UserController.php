a87bce1126dbccd1fa88fa818d1db68ac56d935e
shami2024 <>
2025-12-07 15:04:16 +0200

    Update UserController.php

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -639,6 +639,8 @@ class UserController extends MainController
             ->paginate(10, ['*'], 'badges_page');
         
         $curantBalance = wallet_available_by_user($id);
+        $availableBalance = wallet_available_by_user($id);
+       
         $walletLogs = WalletLog::where('user_id', $user->id) ->orderBy('id', 'DESC') ->paginate(20);
        
        
