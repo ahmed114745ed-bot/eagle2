34179f502e07dab07dff31520864e413a6d56296
shami2024 <>
2025-12-09 12:45:08 +0200

    Update GiftLogService.php

--- app/Tik/Services/GiftLogService.php
+++ app/Tik/Services/GiftLogService.php
@@ -151,7 +151,6 @@ class GiftLogService
            
             $cpIds = [];
             $cpEnableAllGifts = getSettingCash('cp_enable_all_gifts');
- Log::info('Gift data:', (array)$gift);
 
 Log::info("CP Check: cpId={$cpId?->id}, cpEnableAllGifts={$cpEnableAllGifts}, giftId={$gift->id}, giftCategoryType={$gift->category?->type}");
 
