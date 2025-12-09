226669e928cb2d9ac566ad1509bd77f38276da08
Erinyelkess <ariny.backend@utdsoftware.com>
2025-12-09 11:51:06 +0200

    emoji

--- routes/api.php
+++ routes/api.php
@@ -393,7 +393,6 @@ Route::prefix(config('app.api_prefix'))->group(function () {
             });
             Route::prefix('gift-categories')->group(function () {
                 Route::get('/', [GiftCategoryController::class, 'index']);
-
             });
 
             Route::get('my_gifts', [GiftLogController::class, 'giftLogsList']);
@@ -442,7 +441,7 @@ Route::prefix(config('app.api_prefix'))->group(function () {
                 Route::get('/list', [ExchangeController::class, 'exchangeList']);
                 Route::get('/v2/list', [ExchangeController::class, 'exchangeSettingNumber']);
                 Route::post('/make', [ExchangeController::class, 'exchangeSave']);
-                 Route::post('/v2/make', [ExchangeController::class, 'exchangeCoin']);
+                Route::post('/v2/make', [ExchangeController::class, 'exchangeCoin']);
                 Route::get('/logs', [ExchangeController::class, 'exchangeLogs']);
             });
 
@@ -527,7 +526,9 @@ Route::prefix(config('app.api_prefix'))->group(function () {
 
 
             Route::prefix('emojis')->group(function () {
+                Route::get('/categories', [EmojiController::class, 'categories']);
                 Route::get('/', [EmojiController::class, 'index']);
+                Route::get('/v2', [EmojiController::class, 'all']);
                 Route::get('/{id}', [EmojiController::class, 'show']);
             });
             // start levels
@@ -753,4 +754,3 @@ Route::get('gifts-by-id', function (Request $request) {
 
 
 Route::post('/countries-in-polygon', [CountriesInPolygonController::class, 'getCountriesInPolygon']);
-
