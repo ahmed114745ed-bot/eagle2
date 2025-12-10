17478c81b92a5658c83b5b16c95107fdafb3f7fb
Erinyelkess <ariny.backend@utdsoftware.com>
2025-12-10 11:03:28 +0200

    edit

--- Modules/RankingReward/Http/Controllers/WinnerRankingController.php
+++ Modules/RankingReward/Http/Controllers/WinnerRankingController.php
@@ -106,7 +106,6 @@ class WinnerRankingController extends  MainController
             } elseif (@$this->reward->target_type == 'vip') {
                 $vips = OVip::find($this->reward->target);
                 $path = $vips->img ?? '';
-                dd( $path,$vips);
             } elseif (@$this->reward->target_type == 'badge') {
                 // $vips = Badge::find($this->target);
                 $path = @$this->reward->badge->image ?? '';
