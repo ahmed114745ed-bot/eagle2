e86e1ac0b3d2de0ea6765cb46a957d097a1f950e
Bola-Philip <77309023+Bola-Philip@users.noreply.github.com>
2025-12-10 15:14:31 +0200

    get bucket

--- routes/web.php
+++ routes/web.php
@@ -1110,3 +1110,7 @@ Route::get('/run-lucky-gift-unit-test', function () {
     return response('<pre>'.e($output).'</pre>');
 });
 
+Route::get('/get-bucket', function () {
+
+    return env('GOOGLE_CLOUD_STORAGE_BUCKET');
+});
