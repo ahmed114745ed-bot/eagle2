f1a2baa18fc8498b29ede7923b02e73a1c09e32f
shami2024 <>
2025-12-10 16:42:38 +0200

    vendor.admin.partials.menu

--- resources/views/vendor/admin/partials/menu.blade.php
+++ resources/views/vendor/admin/partials/menu.blade.php
@@ -1,45 +1,49 @@
+
+<link rel="stylesheet" href="{{ asset('css/admin-menu.css') }}">
+
+<!-- JS -->
+<script src="{{ asset('js/admin-menu.js') }}"></script>
+
 @php
     $uri = \Illuminate\Support\Arr::get($item, 'uri', '');
     $shouldHideBd = Str::startsWith($uri, 'bd/') && !Admin::user()->inRoles(['bd']);
+
+    $renderedMenu = $renderedMenu ?? [];
+    $itemId = $item['id'] ?? null;
 @endphp
 
-@if(!$shouldHideBd 
-    && Admin::user()->visible(\Illuminate\Support\Arr::get($item, 'roles', [])) 
-    && Admin::user()->can(\Illuminate\Support\Arr::get($item, 'permission')))
-    
+@if(!$shouldHideBd
+    && Admin::user()->visible(\Illuminate\Support\Arr::get($item, 'roles', []))
+    && Admin::user()->can(\Illuminate\Support\Arr::get($item, 'permission'))
+    && (!is_null($itemId) && !in_array($itemId, $renderedMenu)))
+
+    @php
+        $renderedMenu[] = $itemId;
+        $href = url()->isValidUrl($item['uri']) ? $item['uri'] : admin_url($item['uri']);
+        $titleTranslation = 'admin.menu_titles.' . trim(str_replace(' ', '_', strtolower($item['title'])));
+    @endphp
+
     @if(!isset($item['children']))
         <li class="menu-item">
-            @php
-                $href = url()->isValidUrl($item['uri']) ? $item['uri'] : admin_url($item['uri']);
-            @endphp
             <a href="{{ $href }}" class="menu-link">
                 <i class="fa {{ $item['icon'] }} menu-icon"></i>
-                @php
-                    $titleTranslation = 'admin.menu_titles.' . trim(str_replace(' ', '_', strtolower($item['title'])));
-                @endphp
-                <span class="menu-title">
-                    {{ Lang::has($titleTranslation) ? __($titleTranslation) : admin_trans($item['title']) }}
-                </span>
+                <span class="menu-title">{{ Lang::has($titleTranslation) ? __($titleTranslation) : admin_trans($item['title']) }}</span>
             </a>
         </li>
     @else
         <li class="treeview menu-item">
             <a href="#" class="treeview-toggle menu-link">
                 <i class="fa {{ $item['icon'] }} menu-icon"></i>
-                @php
-                    $titleTranslation = 'admin.menu_titles.' . trim(str_replace(' ', '_', strtolower($item['title'])));
-                @endphp
-                <span class="menu-title">
-                    {{ Lang::has($titleTranslation) ? __($titleTranslation) : admin_trans($item['title']) }}
-                </span>
+                <span class="menu-title">{{ Lang::has($titleTranslation) ? __($titleTranslation) : admin_trans($item['title']) }}</span>
                 <i class="fa fa-angle-left pull-right treeview-arrow"></i>
             </a>
 
             <ul class="treeview-menu collapse">
                 @foreach($item['children'] as $child)
-                    @include('admin::partials.menu', $child)
+                    @include('vendor.admin.partials.menu', ['item' => $child, 'renderedMenu' => $renderedMenu])
                 @endforeach
             </ul>
         </li>
     @endif
 @endif
+
