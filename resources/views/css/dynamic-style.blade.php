a351df2f5c22e82873ecedc1c970bd287ea5f51d
shami2024 <>
2025-12-11 16:27:21 +0200

    Update dynamic-style.blade.php

--- resources/views/css/dynamic-style.blade.php
+++ resources/views/css/dynamic-style.blade.php
@@ -1051,7 +1051,8 @@
         font-weight: 600 !important;
         padding: 12px 16px !important;
         text-transform: uppercase !important;
-        font-size: 0.875rem !important;
+        /* font-size: 0.875rem !important; */  /* يافنان اوعي تفعل دي بتأثر علي كل الداش بورد     ***** امضاء  شامي***** */
+
         letter-spacing: 0.05em !important;
     }
 
