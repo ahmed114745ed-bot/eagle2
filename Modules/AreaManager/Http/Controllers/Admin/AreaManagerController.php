c6566b0024f5413308523d24824a346e7ee54f87
Erinyelkess <ariny.backend@utdsoftware.com>
2025-12-14 11:52:53 +0200

    edit

--- Modules/AreaManager/Http/Controllers/Admin/AreaManagerController.php
+++ Modules/AreaManager/Http/Controllers/Admin/AreaManagerController.php
@@ -96,7 +96,7 @@ class AreaManagerController extends MainController
     protected function grid()
     {
         $grid = new Grid(new AreaManager());
-        $grid->model()->with(['appUser.packs', 'regionArea', 'creator'])->orderByDesc('id');
+        $grid->model()->with(['appUser.packs', 'regionArea','appUser', 'creator','appUser.profile'])->orderByDesc('id');
 
         $grid->filter(function ($filter) {
             $filter->like('appUser.uuid', __('App User UUID'));
