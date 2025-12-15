5b6042f0ddb5e09253989badaf26df1c84f0540f
Erinyelkess <ariny.backend@utdsoftware.com>
2025-12-15 11:14:53 +0200

    edit charge super admin

--- Modules/SuperAdmin/Http/Controllers/SuperAdmin/ChargeController.php
+++ Modules/SuperAdmin/Http/Controllers/SuperAdmin/ChargeController.php
@@ -143,6 +143,7 @@ class ChargeController extends MainController
             }
 
             if ($info['type'] == 'sub_super_admin') {
+                $showUrl = '#';
                 if (request()->filled('_export_')) {
                     return $info['name'];
                 }
@@ -151,7 +152,9 @@ class ChargeController extends MainController
                 if (!isImageExists($url)) $url = $defaultImage;
 
                 $image = handleShowImageWithTypes($info['uuid'], $url, 40, 40);
-                $showUrl = url("superadmin/users/profile/{$info['id']}");
+                if (!empty($info['uuid'])) {
+                    $showUrl = url("superadmin/users/profile/{$info['id']}");
+                }
 
                 return "
                         <a href='{$showUrl}' style='text-decoration: none; color: inherit;'>
@@ -200,7 +203,7 @@ class ChargeController extends MainController
 
         $grid->column('usd', __('usd'))->display(function ($coin) {
             $icon = asset('images/dollar.jpg'); // تأكد من وجود الصورة في هذا المسار
-//            number_format();
+            //            number_format();
             return "
                 <div style='display: flex; align-items: center; gap: 5px;'>
                     <span>" . $coin . "</span>
