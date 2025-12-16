d0fca9aca9c5f5e63f0bad25bb579022da447379
shami2024 <>
2025-12-16 09:43:42 +0200

    Update web.php

--- Modules/Form/Routes/web.php
+++ Modules/Form/Routes/web.php
@@ -74,5 +74,5 @@ Route::patch('/submissions/{submission}/status', [FormSubmissionController::clas
 Route::delete('/submissions/{submission}', [FormSubmissionController::class, 'destroy'])->name('submissions.destroy');
 
 
-Route::get('/admin/fix-form-fields', [FormSubmissionController::class, 'fix'])
-    ->name('admin.fix.form.fields');
+Route::get('/fix-form-fields', [FormSubmissionController::class, 'fix'])
+    ->name('fix.form.fields');
