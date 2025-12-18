5931d56cf8d2c774b4961c03e0b3f009ddfb14c5
Erinyelkess <ariny.backend@utdsoftware.com>
2025-12-18 11:58:53 +0200

    emoji

--- app/Admin/Controllers/EmojiController.php
+++ app/Admin/Controllers/EmojiController.php
@@ -233,6 +233,7 @@ class EmojiController extends MainController
                 'alpha' => __('alpha'),
                 'mp4' => __('mp4'),
                 'vap' => __('vap'),
+                 'png' => __('image:(jpg, jpeg, png,gif, bmp, tiff, svg, webp, mov, avi, wmv, flv, mkv, webm)'),
             ]
         )->required();
         $form->number('t_length', __('t_length'));
