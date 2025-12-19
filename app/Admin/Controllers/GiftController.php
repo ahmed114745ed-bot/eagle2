fc17e18cff4372e3d29c7e5a7d96d768aa896a89
makled98 <mkled.backend@utdsoftware.com>
2025-12-19 13:40:10 +0200

    Update GiftController.php

--- app/Admin/Controllers/GiftController.php
+++ app/Admin/Controllers/GiftController.php
@@ -20,8 +20,9 @@ use Illuminate\Support\Facades\App;
 use App\Admin\Actions\MoveGiftCategory;
 
 use App\Admin\Actions\Grid\MoveGroupsGifts;
+use App\Models\Setting;
 use Encore\Admin\Controllers\HasResourceActions;
-
+use Encore\Admin\Auth\Permission;
 class GiftController extends MainController
 {
     use HasResourceActions;
@@ -458,5 +459,14 @@ class GiftController extends MainController
 
 
         return $form;
+    }
+
+     public function luckyGiftSettings(Content $content)
+    {
+        if (!Admin::user()->can('*')) {
+            Permission::check('browse-' . 'lucky-gift-setting');
+        }
+        $config = Setting::whereIn('key', ['app_wallet_lucky_gift', 'owner_lucky_gift', 'host_lucky_gift'])->pluck('value', 'key')->toArray();
+        return $content->view('lucky_gift', compact('config'));
     }
 }
