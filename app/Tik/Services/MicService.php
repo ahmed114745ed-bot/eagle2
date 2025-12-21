8ccb76593f372d145ffdfa15e55616ad183f7a4e
makled98 <mkled.backend@utdsoftware.com>
2025-12-21 11:50:26 +0200

    Update MicService.php

--- app/Tik/Services/MicService.php
+++ app/Tik/Services/MicService.php
@@ -397,19 +397,27 @@ class MicService
     public function checkExistingCpLovly($userId, $otherUserId)
     {
         Log::info("🔍 [CP] Checking existing CP between user {$userId} and user {$otherUserId}", []);
-        Log::info("🔍 [CP] Querying CPs...", Cp::where(function ($query) use ($userId, $otherUserId) {
-            $query->where("user_one_id", $userId)
-                ->where("user_two_id", $otherUserId)
-                ->orWhere(function ($query) use ($userId, $otherUserId) {
-                    $query->where("user_two_id", $userId)
-                        ->where("user_one_id", $otherUserId);
-                });
-        })->relation()
-            /// TODO convert these status to enum
-            ->whereIn("status", [CpStatus::PENDING->value, CpStatus::ACTIVE->value, CpStatus::RESTORED->value])
-            // ->where("cp_relation_id",5)
-            ->first()->toArray());
-            
+        $cp = Cp::where(function ($query) use ($userId, $otherUserId) {
+                $query->where("user_one_id", $userId)
+                    ->where("user_two_id", $otherUserId)
+                    ->orWhere(function ($query) use ($userId, $otherUserId) {
+                        $query->where("user_two_id", $userId)
+                                ->where("user_one_id", $otherUserId);
+                    });
+            })
+            // ->relation()
+            ->whereIn("status", [
+                CpStatus::PENDING->value,
+                CpStatus::ACTIVE->value,
+                CpStatus::RESTORED->value
+            ])
+            ->first();
+
+        Log::info("🔍 [CP] Querying CPs...", [
+            'cp' => $cp?->toArray()
+        ]);
+
+
         return Cp::where(function ($query) use ($userId, $otherUserId) {
             $query->where("user_one_id", $userId)
                 ->where("user_two_id", $otherUserId)
@@ -417,7 +425,7 @@ class MicService
                     $query->where("user_two_id", $userId)
                         ->where("user_one_id", $otherUserId);
                 });
-        })->relation()
+        })
             /// TODO convert these status to enum
             ->whereIn("status", [CpStatus::PENDING->value, CpStatus::ACTIVE->value, CpStatus::RESTORED->value])
             // ->where("cp_relation_id",5)
