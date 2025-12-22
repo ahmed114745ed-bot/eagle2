7a821481d40a4809fd217a4e53f01407f277c79f
Bola-Philip <77309023+Bola-Philip@users.noreply.github.com>
2025-12-22 15:52:27 +0200

    updates

--- resources/views/css/responsive-style.blade.php
+++ resources/views/css/responsive-style.blade.php
@@ -457,7 +457,7 @@
             margin-top: -7% !important;
         }
 
-        .dashboard-container .row {
+        .dashboard-container .tab-pane fade show active .col-md-12 mb-4 .stats-container .row {
             display: flex !important;
         }
 
