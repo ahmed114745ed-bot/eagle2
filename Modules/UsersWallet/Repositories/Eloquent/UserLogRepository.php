70671fe59e49d74388bc6f8865c491e7236a4005
shami2024 <>
2025-12-22 10:28:54 +0200

    Update UserLogRepository.php

--- Modules/UsersWallet/Repositories/Eloquent/UserLogRepository.php
+++ Modules/UsersWallet/Repositories/Eloquent/UserLogRepository.php
@@ -19,7 +19,7 @@ class UserLogRepository extends AbstractRepository
     }
 
 
-    public function index($userId, $type = null, $startDate = null, $endDate = null, $page, $perPage)
+    public function index($userId, $type = null, $startDate = null, $endDate = null, $page = 1, $perPage = 10)
     {
         $start = $startDate ? Carbon::parse($startDate)->startOfDay() : null;
         $end   = $endDate ? Carbon::parse($endDate)->endOfDay() : Carbon::now()->endOfDay();
