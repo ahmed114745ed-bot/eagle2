7b020cdd05340ccc8c6968e7f50e94c87b65fdbc
Erinyelkess <ariny.backend@utdsoftware.com>
2025-12-22 11:24:42 +0200

    edit

--- app/Admin/Controllers/AgencyControllers/UserController.php
+++ app/Admin/Controllers/AgencyControllers/UserController.php
@@ -474,7 +474,7 @@ class UserController extends MainController
             ->when($countryID, fn($q) => $q->whereIn('country_id', $countryID))
             ->select(['id', 'name', 'uuid', 'special_id','country_id', 'sender_level', 'received_level', 'agency_id', 'family_id',  'can_play', 'is_host', 'transfer_salary', 'is_bd', 'device_token', 'di'])
             ->with([
-                'ownerRoom:id,uid',
+                // 'ownerRoom:id,uid',
                 'profile:id,user_id,avatar',
                 'agency:id,name,img',
                 'userSetting',
