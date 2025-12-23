e7eceb4fcfd0dbb0d86cbcf99b9a765557ce08f6
shami2024 <>
2025-12-23 11:01:05 +0200

    Update WalletController.php

--- Modules/UsersWallet/Http/Controllers/Api/WalletController.php
+++ Modules/UsersWallet/Http/Controllers/Api/WalletController.php
@@ -34,7 +34,6 @@ class WalletController extends Controller
         $user = $request->user();
         $data = $this->walletService->history($user->id, $request->type, $request->start_date, $request->end_date, $request->page, $request->per_page);
 
-      \Log::info('Wallet History Data: ', ['data' => $data]);
         return Common::apiResponse(true, '', UserCoinLogResource::collection($data), 200);
     }
 
