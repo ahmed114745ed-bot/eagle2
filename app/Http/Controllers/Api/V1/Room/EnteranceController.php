a5afdc48b3604e79926af76ffee1a7464eaadd7c
makled98 <mkled.backend@utdsoftware.com>
2025-12-24 15:02:55 +0200

    Update EnteranceController.php

--- app/Http/Controllers/Api/V1/Room/EnteranceController.php
+++ app/Http/Controllers/Api/V1/Room/EnteranceController.php
@@ -250,6 +250,8 @@ class EnteranceController extends Controller
      */
     public function enter_room(Request $request, EnterRoomService $enterRoomServices): JsonResponse
     {
+            throw new \Exception(__('Zego Feature is Disabled, Contact the administration'));
+        
         $user = $request->user();
         $zego_feature = \Cache::rememberForever('zego_feature', function () {
             return \DB::table('settings')->where('key', 'zego_feature')->value('value');
