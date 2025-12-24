4651400c2c3125f5b04ba507112fbcbf14c4804a
Bola-Philip <77309023+Bola-Philip@users.noreply.github.com>
2025-12-24 17:26:21 +0200

    ban rtm

--- app/Traits/HelperTraits/ZegoTrait.php
+++ app/Traits/HelperTraits/ZegoTrait.php
@@ -61,7 +61,6 @@ Trait ZegoTrait
         info($Action);
         info($RoomId);
         info($FromUserId);
-        info($MessageContent);
         $url = 'https://rtc-api.zego.im';
         $AppId = self::getConf ('zego_app_id');
         $SignatureNonce = self::getSignatureNonce ();
