0ffa842f545983413e69702c15f14a1cfecfbbe7
Erinyelkess <ariny.backend@utdsoftware.com>
2025-12-24 11:23:27 +0200

    image

--- app/Selectables/Badges.php
+++ app/Selectables/Badges.php
@@ -18,7 +18,7 @@ class Badges extends Selectable
             $this->column('image', __('image'))->display(function ($path) {
             /** @var Ware $this */
             $url = getImagePath($path);
-            return handleShowImageWithTypes($this->id, $url, 20, 20);
+            return handleShowImageWithTypes($this->id, $url, 50, 50);
         });
 
         $this->column('priority', __('Priority'))->sortable();
