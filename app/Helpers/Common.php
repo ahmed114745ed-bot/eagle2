e21326b7e1d66cbb2d801a05296312356c218480
Erinyelkess <ariny.backend@utdsoftware.com>
2025-12-25 18:00:33 +0200

    logs

--- app/Helpers/Common.php
+++ app/Helpers/Common.php
@@ -1519,8 +1519,8 @@ class Common
         $zego_server_secret = Common::getConfig('zego_server_secret');
         $zego_app_id = Common::getConfig('zego_app_id');
         $app_sign = Common::getConfig('app_sign');
-       
-        if ((!$zego_token && !$zegoClientId)|| ($sounZego == '3' || $vedioZego == '3')) {
+
+        if ((!$zego_token && !$zegoClientId) || ($sounZego == '3' || $vedioZego == '3')) {
             $zegoData = [
                 'zego_app_id'        =>  '',
                 'zego_server_secret' =>  '',
@@ -1533,7 +1533,7 @@ class Common
                 'zego_server_secret' => $zego_server_secret ?? '',
                 'zego_app_sign'      =>  $app_sign ?? '',
             ];
-           // dd($zegoData);
+            // dd($zegoData);
         } else {
             $data = decryptToArray($zego_token, $zegoClientId);
 
@@ -1542,6 +1542,12 @@ class Common
                 'zego_server_secret' => $data['server_secret'] ?? '',
                 'zego_app_sign'      => $data['app_sign'] ?? '',
             ];
+            Log::info('ZEGO | mapped zego data', [
+                'zegoData' => $zegoData,
+            ]);
+            Log::info('secret key', [
+                'zego_token' => $zegoClientId,
+            ]);
         }
 
 
