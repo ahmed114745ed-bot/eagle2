76159d2f829417d0a3eb3aa6fbbb9e96f795687e
makled98 <mkled.backend@utdsoftware.com>
2025-12-28 11:17:38 +0200

    Update octane.php

--- config/octane.php
+++ config/octane.php
@@ -48,39 +48,39 @@ return [
     */
 
     'listeners' => [
-        WorkerStarting::class => [
-            EnsureUploadedFilesAreValid::class,
-            EnsureUploadedFilesCanBeMoved::class,
-        ],
+        // WorkerStarting::class => [
+        //     EnsureUploadedFilesAreValid::class,
+        //     EnsureUploadedFilesCanBeMoved::class,
+        // ],
 
-        RequestReceived::class => [
-            ...Octane::prepareApplicationForNextOperation(),
-            ...Octane::prepareApplicationForNextRequest(),
-            FlushAuthenticationState::class,  // ⭐ مهم جداً لحل مشكلة "غير مصدق"
-            FlushSessionState::class,
-            FlushLocaleState::class,
-            FlushQueuedCookies::class,
-        ],
+        // RequestReceived::class => [
+        //     ...Octane::prepareApplicationForNextOperation(),
+        //     ...Octane::prepareApplicationForNextRequest(),
+        //     FlushAuthenticationState::class,  // ⭐ مهم جداً لحل مشكلة "غير مصدق"
+        //     FlushSessionState::class,
+        //     FlushLocaleState::class,
+        //     FlushQueuedCookies::class,
+        // ],
 
-        RequestTerminated::class => [
-            FlushTemporaryContainerInstances::class,
-            // DisconnectFromDatabases::class,
-            CollectGarbage::class,
-            ReportException::class,
-            StopWorkerIfNecessary::class,
-        ],
+        // RequestTerminated::class => [
+        //     FlushTemporaryContainerInstances::class,
+        //     // DisconnectFromDatabases::class,
+        //     CollectGarbage::class,
+        //     ReportException::class,
+        //     StopWorkerIfNecessary::class,
+        // ],
 
-        TaskReceived::class => [
-            ...Octane::prepareApplicationForNextOperation(),
-        ],
+        // TaskReceived::class => [
+        //     ...Octane::prepareApplicationForNextOperation(),
+        // ],
 
-        TickReceived::class => [
-            ...Octane::prepareApplicationForNextOperation(),
-        ],
+        // TickReceived::class => [
+        //     ...Octane::prepareApplicationForNextOperation(),
+        // ],
 
-        WorkerStopping::class => [
-            //
-        ],
+        // WorkerStopping::class => [
+        //     //
+        // ],
     ],
 
     /*
