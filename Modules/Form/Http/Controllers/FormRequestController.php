264538631a6cd690a4b65256d90e41b31bbb69a8
Erinyelkess <ariny.backend@utdsoftware.com>
2025-12-28 14:38:22 +0200

    performance

--- Modules/Form/Http/Controllers/FormRequestController.php
+++ Modules/Form/Http/Controllers/FormRequestController.php
@@ -9,14 +9,11 @@ use App\Models\Agency;
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
-use Encore\Admin\Widgets\Box;
 use App\Models\ShippingAgency;
 use Encore\Admin\Facades\Admin;
 use Encore\Admin\Layout\Content;
 use App\Admin\Services\UserService;
 use App\Admin\Controllers\MainController;
-use Encore\Admin\Controllers\AdminController;
-use Modules\Form\Entities\FormField;
 use Modules\Form\Entities\FormRequest;
 use Modules\Form\Services\FormRenderService;
 
@@ -63,7 +60,15 @@ class FormRequestController extends MainController
         $grid = new Grid(new FormRequest());
 
         $grid->model()
-            ->with(['user', 'template', 'bd'])
+            ->with([
+                'user',
+                'user.country',
+                'user.senderLevel',
+                'user.receiverLevel',
+                'user.packs' => fn($q) => $q->whereIn('type', [25])->where('is_used', true)->with('ware:id,value'),
+                'template',
+                'bd'
+            ])
             ->where('form_template_type', $type)->orderByDesc('id');
 
         $grid->column('user', __('user'))
