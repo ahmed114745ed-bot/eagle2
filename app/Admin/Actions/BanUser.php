ae708024b56455a5b53e58bb63ca5411f0bcf13f
makled98 <mkled.backend@utdsoftware.com>
2025-12-28 15:19:14 +0200

    Update BanUser.php

--- app/Admin/Actions/BanUser.php
+++ app/Admin/Actions/BanUser.php
@@ -163,6 +163,11 @@ class BanUser extends Action
                     ]
                 ];
                 $json = json_encode($ms);
+                Log::info('ZEGO0000000 | sendToZego params unableToEnterRoom', [
+                    'json' => $json,
+                    'room_id' => @$user->room->id,
+                    'user_id' => $user->id,
+                ]);
                 Common::sendToZego('SendCustomCommand', @$user->room->id, $user->room->uid, $json);
             } else if ($route == 'rooms/up_microphone' && $user->room) {
                 Log::info('ban user send unableToUPMicrophone to zego for user id ' . $user->id);
