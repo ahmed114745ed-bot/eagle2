ac88c420560d8ba54a027cc67a3755684ed4c898
Erinyelkess <ariny.backend@utdsoftware.com>
2025-12-29 17:05:21 +0200

    lang

--- database/seeders/EmojiCategorySeeder.php
+++ database/seeders/EmojiCategorySeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Country;
+use App\Models\CountryCategory;
 use App\Models\Emoji;
 use App\Models\EmojiCategory;
 
@@ -12,19 +14,39 @@ class EmojiCategorySeeder extends Seeder
 {
     public function run(): void
     {
+        $emojiCategory = EmojiCategory::where('type', 'default')->first();
+        if (!$emojiCategory) {
+            $emojiCategory = EmojiCategory::create([
+                'title' => [
+                    'en' => 'default',
+                    'ar' => 'افتراضي',
+                    'tr' => 'Varsayılan',
+                    'hi' => 'डिफ़ॉल्ट',
+                    'id' => 'default',
+                ],
+                'type' => 'default',
+            ]);
+        }
 
-        $emojiCategory = EmojiCategory::create([
-            'title' => [
-                'en' => 'default',
-                'ar' => 'افتراضي',
-                'tr' => 'Varsayılan',
-                'hi' => 'डिफ़ॉल्ट',
-                'id' => 'default',
-            ],
-            'type' => 'default',
-        ]);
 
         Emoji::whereNull('emoji_category_id')
+            ->update(['emoji_category_id' => $emojiCategory->id]);
+
+        $emojiCategory = CountryCategory::where('type', 'default')->first();
+        if (!$emojiCategory) {
+            $emojiCategory = CountryCategory::create([
+                'title' => [
+                    'en' => 'default',
+                    'ar' => 'افتراضي',
+                    'tr' => 'Varsayılan',
+                    'hi' => 'डिफ़ॉल्ट',
+                    'id' => 'default',
+                ],
+                'type' => 'default',
+            ]);
+        }
+
+        Country::whereNull('emoji_category_id')
             ->update(['emoji_category_id' => $emojiCategory->id]);
     }
 }
