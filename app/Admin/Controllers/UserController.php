4a37d67170360fc37c55bdafba9ba492a25ac326
Erinyelkess <ariny.backend@utdsoftware.com>
2025-12-30 16:43:47 +0200

    edit

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -563,7 +563,7 @@ class UserController extends MainController
                 $giftSLogs = (clone $giftBaseQuery)
                     ->with([
                         'receiver:id,name,uuid,special_id',
-                        'sender :id,name,uuid,special_id',
+                        'sender:id,name,uuid,special_id',
                         // 'sender.packs' => function ($q) {
                         //     $q->whereIn('type', [25])
                         //         ->where('is_used', true)
