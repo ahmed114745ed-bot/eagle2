74c8c617430cbc5eab34dbcba87b3a5db91c96c8
Erinyelkess <ariny.backend@utdsoftware.com>
2025-12-30 10:07:28 +0200

    from request

--- Modules/Form/Http/Controllers/FormRequestController.php
+++ Modules/Form/Http/Controllers/FormRequestController.php
@@ -60,8 +60,7 @@ class FormRequestController extends MainController
         $grid = new Grid(new FormRequest());
 
         $grid->model()
-        //->select(['id', 'name', 'bd_id', 'whatsapp_number', 'submitted_by', 'form_template_type', 'status'])
-
+            ->select(['id', 'name', 'bd_id', 'whatsapp_number', 'submitted_by', 'form_template_type', 'status'])
             ->with([
                 'user',
                 'user.country',
@@ -291,6 +290,9 @@ class FormRequestController extends MainController
 
         $grid->disableActions();
         $grid->disableCreateButton();
+        $grid->disableExport();
+        $grid->disableColumnSelector();
+        $grid->disableRowSelector();
 
         return $grid;
     }
