d7c31d822392defc9c1152be11db82a16f003c2b
Bola-Philip <77309023+Bola-Philip@users.noreply.github.com>
2025-12-30 14:30:40 +0200

    update

--- Modules/Form/Http/Controllers/FormRequestController.php
+++ Modules/Form/Http/Controllers/FormRequestController.php
@@ -73,9 +73,9 @@ class FormRequestController extends MainController
                 animation: pulse 2s infinite;
             }
             @keyframes pulse {
-                0% { box-shadow: 0 0 0 0 rgba(220, 53, 69, 0.7); }
-                70% { box-shadow: 0 0 0 10px rgba(220, 53, 69, 0); }
-                100% { box-shadow: 0 0 0 0 rgba(220, 53, 69, 0); }
+                0% { box-shadow: 0 0 0 0 rgba(var(--primary-color-rgb), 0.7); }
+                70% { box-shadow: 0 0 0 10px rgba(var(--primary-color-rgb), 0); }
+                100% { box-shadow: 0 0 0 0 rgba(var(--primary-color-rgb), 0); }
             }
         ');
 
