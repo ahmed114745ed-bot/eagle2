525ff34c7ae22c18cfa794b1b1f2d95874ee0a97
Erinyelkess <ariny.backend@utdsoftware.com>
2025-12-30 10:30:59 +0200

    image level

--- resources/views/user_profile.blade.php
+++ resources/views/user_profile.blade.php
@@ -1099,14 +1099,14 @@
                     <div class="meta-item">
                         <span class="meta-label">{{__('level')}}:</span>
                         <span class="meta-value">
-                                    <img src="{{ getImagePath(Common::level_center($user)['sender_img']) }}"
+                                    <img src="{{ getImagePath($user->senderLevel->img) }}"
                                          style="height: 24px;">
                                 </span>
                     </div>
                     <div class="meta-item">
                         <span class="meta-label">{{__('Receiver Level')}}:</span>
                         <span class="meta-value">
-                                <img src="{{ getImagePath(Common::level_center($user)['receiver_img']) }}"
+                                <img src="{{ getImagePath($user->receiverLevel->img) }}"
                                      style="height: 24px;">
                             </span>
                     </div>
