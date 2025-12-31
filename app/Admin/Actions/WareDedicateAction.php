3f57aa44fd1d1d15dc78b41b9202829b4b5a80a4
Erinyelkess <ariny.backend@utdsoftware.com>
2025-12-31 12:07:19 +0200

    ware

--- app/Admin/Actions/WareDedicateAction.php
+++ app/Admin/Actions/WareDedicateAction.php
@@ -37,7 +37,7 @@ class WareDedicateAction extends Action
     }
     public function handle(Request $request)
     {
-                 $countryID = empty((array)session('filter_country_id')) ? Common::areaCountries() : (array)session('filter_country_id');
+        $countryID = empty((array)session('filter_country_id')) ? Common::areaCountries() : (array)session('filter_country_id');
 
         $user = User::query()
             ->when($countryID, fn($q) => $q->whereIn('country_id', $countryID))
@@ -48,46 +48,16 @@ class WareDedicateAction extends Action
         }
 
         $ware = Ware::find($request->id);
-        if ($ware->type == 25) {
+        if (!$ware) {
+            return $this->response()->error(__('not found'))->refresh();
+        }
+        if ($ware && $ware->type == 25) {
             $special_id_check = Pack::query()->where('target_id', $ware->id)->first();
             if ($special_id_check) {
                 return $this->response()->error(__('dashboard.taken'))->refresh();
             }
         }
 
-        // $pack = Pack::query()->where('user_id', $user->id)->where('target_id', $ware->id)->first();
-        // if ($pack) {
-        //     if ($pack->expire == 0) return $this->response()->error(__('dashboard.chickTaken'))->refresh();
-        //     if ($pack->expire > now()->timestamp) {
-        //         if ($ware->expire != 0) {
-        //             DB::beginTransaction();
-        //             try {
-        //                 $pack->using == 1 ? $pack->expire += (($request->days ?? $ware->expire) * 86400) : $pack->days += $request->days ?? $ware->expire;
-        //                 $pack->dash_user_id = auth()->id();
-        //                 $pack->save();
-        //                 if ($ware->type == 25) {
-        //                     $user->special_id = $ware->value;
-        //                     $user->save();
-        //                 }
-        //                 DB::commit();
-        //                 //  Common::sendOfficialMessage ($user->id,__('congratulations'),StringFacade::gotGift($ware->name), 1, SubTypeMessagesType::GOT_GIFT);
-        //                 // $tokens_notfacion[] = DB::table('users')->where('id', $user->id)->value('notification_id');
-        //                 // $title = 'Tik Chat';
-        //                 // $body = __('لقد حصلت على اهداء') . $request->user()->name;
-        //                 //  Common::send_firebase_notification($tokens_notfacion,$title,$body);
-        //                 (new UserCounterServices)->eventUser($user, 'mybag', 1);
-        //                 return $this->response()->success(__('dashboard.successful'));
-        //             } catch (\Exception $exception) {
-        //                 DB::rollBack();
-        //                 return $this->response()->error('خطا غير متوقع');
-        //             }
-        //         } else {
-        //             return $this->response()->error(__('dashboard.chickTaken'));
-        //         }
-        //     } else {
-        //         $pack->delete();
-        //     }
-        // }
         DB::beginTransaction();
         try {
             $types = [8, 9, 12, 14, 17];
@@ -107,7 +77,7 @@ class WareDedicateAction extends Action
 
             $arr['is_read'] = 1;
             $arr['days'] = $request->days ? $request->days ?? $ware->expire : 0;
-            $arr['receive_type'] ='wares-dash-dedicate';
+            $arr['receive_type'] = 'wares-dash-dedicate';
 
             $enableVipAuto = Common::getConf('enable_vip_auto') ?? "false";
             // $arr['is_used'] = $enableVipAuto === "true" ? 1 : 0;
