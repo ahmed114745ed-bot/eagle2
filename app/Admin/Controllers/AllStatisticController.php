9959efa3896556db18b018cde994cace635a28f0
Erinyelkess <ariny.backend@utdsoftware.com>
2026-01-04 13:51:35 +0200

    soon

--- app/Admin/Controllers/AllStatisticController.php
+++ app/Admin/Controllers/AllStatisticController.php
@@ -5,7 +5,7 @@ namespace App\Admin\Controllers;
 use App\Helpers\Common;
 use App\Models\Charge;
 use App\Models\CoinLog;
-use App\Models\MonthlyDiamondReceive;
+use Encore\Admin\Widgets\Box;
 use App\Models\User;
 use App\Models\GameWallet;
 use App\Models\UserSallary;
@@ -981,16 +981,14 @@ class AllStatisticController extends MainController
 
     public function index2(Content $content)
     {
+        $box = new Box(
+            __('Coming Soon'),
+            '<div style="text-align:center; padding:30px; font-size:20px;">🚧</div>'
+        );
+
         return $content
             ->title(__('Home'))
             ->description(__('General Statistics'))
-            ->row(function (Row $row) {
-                $row->column(12, function () {
-                    return '
-                <div style="text-align:center; padding:40px; font-size:20px; font-weight:bold;">
-                    🚧 ' . __('Coming Soon') . '
-                </div>';
-                });
-            });
+            ->row($box);
     }
 }
