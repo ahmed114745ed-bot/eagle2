c72c2c8edc001d7b0793c131e4bb38ea52401a88
Erinyelkess <ariny.backend@utdsoftware.com>
2026-01-05 11:35:13 +0200

    test

--- app/Admin/Controllers/SuperAdminRewardControllerHistory.php
+++ app/Admin/Controllers/SuperAdminRewardControllerHistory.php
@@ -210,7 +210,7 @@ class SuperAdminRewardControllerHistory extends MainController
             $grid->column('no_reward', __('No reward'));
         } else {
             $grid->column('members', __('rewards'))->expand(function ($model) {
-                $mempers = $model->packageRewards()->get()
+                $mempers = $this->packageRewards()->get()
                     
                     ->map(function ($memper) {
 
