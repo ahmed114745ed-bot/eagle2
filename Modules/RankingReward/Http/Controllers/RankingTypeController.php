223fb21a992e366d4d456e710b894d8e68f070a1
Bola-Philip <77309023+Bola-Philip@users.noreply.github.com>
2026-01-06 09:04:08 +0200

    svg

--- Modules/RankingReward/Http/Controllers/RankingTypeController.php
+++ Modules/RankingReward/Http/Controllers/RankingTypeController.php
@@ -115,8 +115,8 @@ class RankingTypeController extends MainController
                     <div class='modal-dialog modal-lg'>
                         <div class='modal-content'>
                             <div class='modal-header'>
-                                <h5 class='modal-title'>${modalTitle}</h5>
                                 <button type='button' class='close' data-dismiss='modal'>&times;</button>
+                                <h5 class='modal-title'>${modalTitle}</h5>
                             </div>
                             <div class='modal-body'>Loading...</div>
                         </div>
