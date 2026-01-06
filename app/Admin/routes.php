2b800b36d6ee151e772aaeb1b81c1d30e47a7e2d
Erinyelkess <ariny.backend@utdsoftware.com>
2026-01-06 07:55:00 +0200

    rewards history

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -611,9 +611,11 @@ Route::group(
         Route::resource('charge-agencies', AppearChargerAgencyController::class);
         Route::resource('users-joined-agencies', UsersJoinedAgencyController::class)->middleware('web-agency-feature');
         Route::resource('super-package-rewards', SuperPackageController::class);
-       Route::resource('admin-rewards-histories', SuperAdminRewardControllerHistory::class);
-        Route::get('admin-rewards', [SuperAdminRewardController::class,'index']);
-               Route::get('admin-rewards/{id}', [SuperAdminRewardController::class, 'getRewards']);
+        Route::get('admin-rewards-histories', [SuperAdminRewardControllerHistory::class, 'index']);
+         Route::get('admin-rewards-histories/{id}', [SuperAdminRewardControllerHistory::class, 'getRewards']);
+
+        Route::get('admin-rewards', [SuperAdminRewardController::class, 'index']);
+        Route::get('admin-rewards/{id}', [SuperAdminRewardController::class, 'getRewards']);
 
         //    dd( Admin::menu(function ($menu) {
         //         $menu->add('Custom Page', ['route' => 'admin.AppSitiingCOnfigController'])
