6ec3f6917ae131e2a1988276fff6f85c6004fcd2
Erinyelkess <ariny.backend@utdsoftware.com>
2026-01-06 10:34:04 +0200

    phone

--- app/Admin/Controllers/TicketController.php
+++ app/Admin/Controllers/TicketController.php
@@ -35,7 +35,7 @@ class TicketController extends MainController
      */
     public function show($id, Content $content)
     {
-        return parent::show($id,$content
+        return parent::show($id, $content
             ->title(trans('Tickets'))
             ->body($this->detail($id)));
     }
@@ -49,7 +49,7 @@ class TicketController extends MainController
      */
     public function edit($id, Content $content)
     {
-        return parent::edit($id,$content
+        return parent::edit($id, $content
             ->title(trans('Tickets'))
             ->body($this->form()->edit($id)));
     }
@@ -68,12 +68,17 @@ class TicketController extends MainController
     protected function grid()
     {
         $grid = new Grid(new Ticket);
-        $countryID =session('filter_country_id');
+        $countryID = session('filter_country_id');
 
-        $grid->model()->when($countryID, fn($q) => $q->whereHas('user', fn($q) => $q->where('country_id', $countryID)));
+        $grid->model()->with([
+            'user',
+            'user.agency',
+            'user.profile:user_id,avatar',
+            'user.packs' => fn($q) => $q->whereIn('type', [25])->where('is_used', true)->with('ware:id,value'),
+        ])->when($countryID, fn($q) => $q->whereHas('user', fn($q) => $q->where('country_id', $countryID)));
 
 
-        $grid->id(__ ('ID_tiket'));
+        $grid->id(__('ID_tiket'));
         // $grid->user_id( __ ('ID'));
         $grid->column('user_id', __('User Info'))->display(function () {
             $user = $this->user;
@@ -102,9 +107,6 @@ class TicketController extends MainController
                     </div>";
         });
 
-        // $grid->column('contact_num',__ ('contact'));
-        // $grid->column('problem',__ ('problem'));
-        // $grid->column('description',__ ('description'))->limit(15);
         $grid->column('contact_num', __('contact'))->display(function ($description) {
             $limitedDescription = mb_substr($description, 0, 40) . (strlen($description) > 20 ? '...' : '');
             return "<a href='#' class='view-description' data-description=\"" . htmlentities($description) . "\">$limitedDescription</a>";
@@ -138,22 +140,19 @@ class TicketController extends MainController
                 });
             }); ");
 
-            $grid->column('img', __('img'))->display(function ($img) {
+        $grid->column('img', __('img'))->display(function ($img) {
 
-                $defaultImage = asset('images/image.png');
-                $url = getImagePath($img) ?? $defaultImage;
+            $defaultImage = asset('images/image.png');
+            $url = getImagePath($img) ?? $defaultImage;
 
-                // Check if the image exists
-                if (!isImageExists($url)) {
-                    $url = $defaultImage;
-                }
-                return $url;
-            })->image('', 30);
-        $grid->column('status',__ ('status'))->switch (Common::getSwitchStates ());
-//        $grid->admin_id('admin_id');
-//        $grid->created_at(trans('admin.created_at'));
-//        $grid->updated_at(trans('admin.updated_at'));
-        $this->extendGrid ($grid);
+            // Check if the image exists
+            if (!isImageExists($url)) {
+                $url = $defaultImage;
+            }
+            return $url;
+        })->image('', 30);
+        $grid->column('status', __('status'))->switch(Common::getSwitchStates());
+        $this->extendGrid($grid);
         $grid->disableExport();
 
         return $grid;
@@ -170,16 +169,16 @@ class TicketController extends MainController
         $show = new Show(Ticket::findOrFail($id));
 
         $show->id('ID');
-//        $show->user_id('user_id');
-        $show->field('contact_num',__ ('contact'));
-        $show->field('problem',__ ('problem'));
-        $show->field('description',__ ('description'));
-        $show->field('img',__ ('img'))->image ('',80);
-        $show->field('status',__ ('status'))->using ([0=>"closed",1=>"open"]);
-//        $show->admin_id('admin_id');
-//        $show->created_at(trans('admin.created_at'));
-//        $show->updated_at(trans('admin.updated_at'));
-        $this->extendShow ($show);
+        //        $show->user_id('user_id');
+        $show->field('contact_num', __('contact'));
+        $show->field('problem', __('problem'));
+        $show->field('description', __('description'));
+        $show->field('img', __('img'))->image('', 80);
+        $show->field('status', __('status'))->using([0 => "closed", 1 => "open"]);
+        //        $show->admin_id('admin_id');
+        //        $show->created_at(trans('admin.created_at'));
+        //        $show->updated_at(trans('admin.updated_at'));
+        $this->extendShow($show);
         return $show;
     }
 
@@ -194,15 +193,15 @@ class TicketController extends MainController
         $this->disableFormTools($form);
 
         $form->display('ID');
-//        $form->text('user_id', 'user_id');
+        //        $form->text('user_id', 'user_id');
         $form->text('contact_num', __('contact'));
         $form->text('problem', __('problem'));
         $form->textarea('description', __('description'));
         $form->image('img', __('img'));
-        $form->switch('status', __('status'))->states (Common::getSwitchStates ());
-//        $form->text('admin_id', 'admin_id');
-//        $form->display(trans('admin.created_at'));
-//        $form->display(trans('admin.updated_at'));
+        $form->switch('status', __('status'))->states(Common::getSwitchStates());
+        //        $form->text('admin_id', 'admin_id');
+        //        $form->display(trans('admin.created_at'));
+        //        $form->display(trans('admin.updated_at'));
 
         return $form;
     }
