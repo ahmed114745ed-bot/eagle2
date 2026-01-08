b060b4015603340446c37e37cda72a52c240076b
Bola-Philip <77309023+Bola-Philip@users.noreply.github.com>
2026-01-08 08:08:08 +0200

    form filters

--- Modules/AreaManager/Http/Controllers/AgencyController.php
+++ Modules/AreaManager/Http/Controllers/AgencyController.php
@@ -483,7 +483,6 @@ class AgencyController extends MainController
                 flex-wrap: wrap;
                 align-items: center;
                 justify-content: space-between;
-                padding: 10px;
             }
 
             .pagination-info {
