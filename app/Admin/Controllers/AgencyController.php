db082f61d7208b7555d6949d73b0951ea3eb721d
Erinyelkess <ariny.backend@utdsoftware.com>
2026-01-11 12:52:09 +0200

    agency bd required

--- app/Admin/Controllers/AgencyController.php
+++ app/Admin/Controllers/AgencyController.php
@@ -700,7 +700,7 @@ class AgencyController extends MainController
 
         if (!$form->isEditing()) {
             $form->row(function ($row) {
-                $row->width(12)->select('bd_id', __('bd id'))->options($this->bdOptions())->ajax('/api/search/users-bd2', 'id', 'name');
+                $row->width(12)->select('bd_id', __('bd id'))->options($this->bdOptions())->ajax('/api/search/users-bd2', 'id', 'name')->rules('required');
                 $row->width(12)->select('app_owner_id', __('app owner id'))->options($this->ownerOptions())->ajax('/api/search/users3', 'id', 'name')->rules('required');
                 $row->width(12)->hidden('agency_manger_id', __('app manger id'));
                 $row->width(12)->text('name', __('agency name'))->rules('required');
