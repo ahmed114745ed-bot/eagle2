207feb9e99425cf857b5a7a9257388f4ef0658c0
Erinyelkess <ariny.backend@utdsoftware.com>
2026-01-11 09:02:27 +0200

    profile agent

--- Modules/SalaryTransaction/Http/Controllers/AgentRequestController.php
+++ Modules/SalaryTransaction/Http/Controllers/AgentRequestController.php
@@ -69,10 +69,12 @@ class AgentRequestController extends MainController
         $countryID = session('filter_country_id');
 
         $grid->model()
-        ->with(['agency',
-        'agent',
-        'agent.packs' => fn($q) => $q->whereIn('type', [25])->where('is_used', true)->with('ware:id,value')
-        ])
+            ->with([
+                'agency',
+                'agent',
+                'agent.profile',
+                'agent.packs' => fn($q) => $q->whereIn('type', [25])->where('is_used', true)->with('ware:id,value')
+            ])
             ->when($countryID, fn($q) =>
             $q->where(function ($q) use ($countryID) {
                 $q->whereHas('agent', fn($q) => $q->where('country_id', $countryID))
