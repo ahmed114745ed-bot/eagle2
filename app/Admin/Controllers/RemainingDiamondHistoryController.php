c3f2e65a2d44abe564acecddb43a50af8a1bf1cb
Erinyelkess <ariny.backend@utdsoftware.com>
2026-01-12 09:26:00 +0200

    diamond

--- app/Admin/Controllers/RemainingDiamondHistoryController.php
+++ app/Admin/Controllers/RemainingDiamondHistoryController.php
@@ -81,6 +81,8 @@ class RemainingDiamondHistoryController extends MainController
             'user.profile',
             'user.packs' => fn($q) => $q->whereIn('type', [25])->where('is_used', true)->with('ware:id,value'),
             'user.country',
+            'user.senderLevel',
+            'user.receiverLevel',
         ]);
 
         $grid->column('id', __('Id'));
