973147261c094f2e841ea51bc32d63d518aff470
Erinyelkess <ariny.backend@utdsoftware.com>
2026-01-12 13:04:10 +0200

    room

--- Modules/RoomBoom/Http/Controllers/web/RoomBoomWinnerController.php
+++ Modules/RoomBoom/Http/Controllers/web/RoomBoomWinnerController.php
@@ -33,7 +33,6 @@ class RoomBoomWinnerController extends MainController
             'reward.ware',
             'boom.roomBoomLevel',
             'boom.totalRoomGift.room',
-             'boom.totalRoomGift.room.country',
             'user',
             'user.profile',
             'user.country',
