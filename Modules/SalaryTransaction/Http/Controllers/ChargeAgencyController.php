fcb0f9d3d8c4079c018b38ed44e3362a1ff44f3e
Erinyelkess <ariny.backend@utdsoftware.com>
2026-01-13 11:40:17 +0200

    agency

--- Modules/SalaryTransaction/Http/Controllers/ChargeAgencyController.php
+++ Modules/SalaryTransaction/Http/Controllers/ChargeAgencyController.php
@@ -64,12 +64,13 @@ class ChargeAgencyController extends MainController
         $grid = new Grid(new EntitiesChargeAgency());
         $countryID = session('filter_country_id');
 
-        $grid->model()->with([
-            'agency',
-            'agency.owner',
-            'agency.owner.profile',
-           // 'agency.owner.packs' => fn($q) => $q->whereIn('type', [25])->where('is_used', true)->with('ware:id,value')
-        ])
+        $grid->model()
+        // ->with([
+        //     'agency',
+        //     'agency.owner',
+        //     'agency.owner.profile',
+        //     'agency.owner.packs' => fn($q) => $q->whereIn('type', [25])->where('is_used', true)->with('ware:id,value')
+        // ])
             ->when($countryID, fn($q) => $q->whereHas('agency', fn($q) => $q->where('country_id', $countryID)))
             ->whereHas('agency');
 
