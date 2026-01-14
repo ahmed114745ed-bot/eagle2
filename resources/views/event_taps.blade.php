3c676a8917d624c10f9fa7fc516fb11a3d791168
Bola-Philip <77309023+Bola-Philip@users.noreply.github.com>
2026-01-14 07:33:46 +0200

    design

--- resources/views/event_taps.blade.php
+++ resources/views/event_taps.blade.php
@@ -2,11 +2,7 @@
     .box.box-solid {
         border: 1px solid #d2d6de;
         border-radius: 3px;
-        box-shadow: 0 1px 1px rgba(0,0,0,0.1);
-    }
-
-    .settings-sidebar {
-        margin: 20px;
+        box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
     }
 
     .box-header .box-title {
@@ -33,21 +29,19 @@
 <div class="box box-solid">
     @php $currentRoute = url()->current(); @endphp
 
-    <div class="settings-sidebar">
-        <div class="settings-menu">
-            @foreach([
-                ['url' => admin_url('pk-events'), 'label' => __('Pk Events')],
-                ['url' => admin_url('target-events'), 'label' => __('Target Events')],
-                ['url' => admin_url('weekly-events-new'), 'label' => __('Weekly Events')],
-                ['url' => admin_url('event-period'), 'label' => __('Event Period')],
-            ] as $tab)
-                <button
-                    type="button"
-                    onclick="window.location.href='{{ $tab['url'] }}'"
-                    class="{{ Str::contains($currentRoute, $tab['url']) ? 'active' : '' }}">
-                    {{ $tab['label'] }}
-                </button>
-            @endforeach
-        </div>
+    <div class="settings-menu">
+        @foreach([
+            ['url' => admin_url('pk-events'), 'label' => __('Pk Events')],
+            ['url' => admin_url('target-events'), 'label' => __('Target Events')],
+            ['url' => admin_url('weekly-events-new'), 'label' => __('Weekly Events')],
+            ['url' => admin_url('event-period'), 'label' => __('Event Period')],
+        ] as $tab)
+            <button
+                type="button"
+                onclick="window.location.href='{{ $tab['url'] }}'"
+                class="{{ Str::contains($currentRoute, $tab['url']) ? 'active' : '' }}">
+                {{ $tab['label'] }}
+            </button>
+        @endforeach
     </div>
 </div>
