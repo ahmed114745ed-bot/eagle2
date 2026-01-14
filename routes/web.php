bd6c3e3629d4e0f1c56d253684c9bc8df43f9b73
makled98 <mkled.backend@utdsoftware.com>
2026-01-14 08:16:20 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1174,3 +1174,66 @@ Route::get('/octane-reload', function () {
         ], 500);
     }
 });
+
+Route::get('/debug/pusher-config', function () {
+    $dbKeys = ['pusher_app_id', 'pusher_app_key', 'pusher_app_secret', 'pusher_app_cluster'];
+    $fromDatabase = \App\Models\Config::whereIn('name', $dbKeys)->pluck('value', 'name')->toArray();
+
+    $fromCache = Cache::get('pusher_config');
+
+    $fromConfig = [
+        'key' => Config::get('broadcasting.connections.pusher.key'),
+        'secret' => Config::get('broadcasting.connections.pusher.secret'),
+        'app_id' => Config::get('broadcasting.connections.pusher.app_id'),
+        'cluster' => Config::get('broadcasting.connections.pusher.options.cluster'),
+    ];
+
+    $fromDefault = [
+        'key' => Config::get('broadcasting.pusher-default.key'),
+        'secret' => Config::get('broadcasting.pusher-default.secret'),
+        'app_id' => Config::get('broadcasting.pusher-default.app_id'),
+        'cluster' => Config::get('broadcasting.pusher-default.options.cluster'),
+    ];
+
+    $fromEnv = [
+        'key' => env('PUSHER_APP_KEY'),
+        'secret' => env('PUSHER_APP_SECRET'),
+        'app_id' => env('PUSHER_APP_ID'),
+        'cluster' => env('PUSHER_APP_CLUSTER'),
+    ];
+
+    $cacheInfo = [
+        'pusher_config_exists' => Cache::has('pusher_config'),
+        'exp_percentages_exists' => Cache::has('exp_percentages'),
+        'exp_percentages' => Cache::get('exp_percentages'),
+    ];
+
+    return response()->json([
+        'from_database' => $fromDatabase,
+        'from_cache' => $fromCache,
+        'from_config_runtime' => $fromConfig,
+        'from_config_default' => $fromDefault,
+        'from_env' => $fromEnv,
+        'cache_info' => $cacheInfo,
+        'config_cached' => file_exists(base_path('bootstrap/cache/config.php')),
+    ], 200, [], JSON_PRETTY_PRINT);
+});
+Route::get('/test-pusher-config', function () {
+    $pusherConfig = getPusherConfig();
+    $laravelConfig = [
+        'key' => config('broadcasting.connections.pusher.key'),
+        'secret' => config('broadcasting.connections.pusher.secret'),
+        'app_id' => config('broadcasting.connections.pusher.app_id'),
+        'cluster' => config('broadcasting.connections.pusher.options.cluster'),
+    ];
+    
+    return response()->json([
+        'from_helper_function' => $pusherConfig,
+        'from_laravel_config' => $laravelConfig,
+        'cache_info' => [
+            'environment' => app()->environment(),
+            'cache_driver' => config('cache.default'),
+        ],
+        'timestamp' => now()->toDateTimeString(),
+    ], 200, [], JSON_PRETTY_PRINT);
+});
