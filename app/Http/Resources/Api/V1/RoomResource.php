4ddee889af58e75ac298d03f628cc3bcc03886c0
Erinyelkess <ariny.backend@utdsoftware.com>
2026-01-15 07:28:49 +0200

    room mine

--- app/Http/Resources/Api/V1/RoomResource.php
+++ app/Http/Resources/Api/V1/RoomResource.php
@@ -137,14 +137,14 @@ class RoomResource extends JsonResource
 
     protected function owner()
     {
-        return new UserResource(User::query()->find($this->uid));
+        return new UserResource($this->owner);
     }
 
     protected function admins()
     {
         $ids = explode(',', $this->room_admin);
         $ids = $this->removeOwner($ids);
-        return UserResource::collection(User::query()->whereIn('id', $ids)->get());
+        return UserResource::collection($this->admins);
     }
 
 
