3fcde1fd7de103b8390e394609399a269d4904d7
Bola-Philip <77309023+Bola-Philip@users.noreply.github.com>
2026-01-15 11:03:22 +0200

    app feature

--- resources/views/css/dynamic-style.blade.php
+++ resources/views/css/dynamic-style.blade.php
@@ -3110,6 +3110,13 @@
         color: var(--primary-color);
     }
 
+    .table td,
+    .table th {
+        white-space: normal !important;
+        word-break: break-word;
+        overflow-wrap: anywhere;
+    }
+
     .new-form {
         background: #ffffff;
         border-radius: 16px;
