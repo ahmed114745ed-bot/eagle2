c356329743971c65779cdf15df0635370132115c
shami2024 <>
2026-01-25 07:40:37 +0200

    EmojiController

--- app/Admin/Controllers/EmojiController.php
+++ app/Admin/Controllers/EmojiController.php
@@ -42,9 +42,22 @@ class EmojiController extends MainController
             $grid = $this->grid();
             Log::info('Grid created successfully');
             
+            // Log the rendered grid HTML length
+            $gridHtml = $grid->render();
+            Log::info('Grid HTML length: ' . strlen($gridHtml));
+            Log::info('Grid HTML first 500 chars: ' . substr($gridHtml, 0, 500));
+            
+            // Check if table has rows
+            if (strpos($gridHtml, '<tr') !== false) {
+                preg_match_all('/<tr/', $gridHtml, $matches);
+                Log::info('Number of <tr> tags in grid: ' . count($matches[0]));
+            } else {
+                Log::warning('No <tr> tags found in grid HTML!');
+            }
+            
             return parent::index($content
                 ->title(trans('Emojis'))
-                ->body($grid));
+                ->body($gridHtml));
         } catch (\Exception $e) {
             Log::error('Error in EmojiController@index: ' . $e->getMessage());
             Log::error('Stack trace: ' . $e->getTraceAsString());
@@ -121,26 +134,26 @@ class EmojiController extends MainController
 
 
         // Header tabs
-        $grid->header(function () use ($filterType) {
-            $locale = App::getLocale();
+        // $grid->header(function () use ($filterType) {
+        //     $locale = App::getLocale();
 
-            $tabs = ['all' => __('All')];
-            $categories = EmojiCategory::orderBy('id')->get();
-            foreach ($categories as $category) {
-                $title = $category->title[$locale] ?? $category->title['en'] ?? '';
-                $tabs[$category->id] = $title;
-            }
+        //     $tabs = ['all' => __('All')];
+        //     $categories = EmojiCategory::orderBy('id')->get();
+        //     foreach ($categories as $category) {
+        //         $title = $category->title[$locale] ?? $category->title['en'] ?? '';
+        //         $tabs[$category->id] = $title;
+        //     }
 
-            $html = '<div class="nav-tabs-custom"><ul class="nav nav-tabs">';
-            foreach ($tabs as $key => $label) {
-                $active = $filterType == $key ? 'active' : '';
-                $url = request()->fullUrlWithQuery(['filter' => $key]);
-                $html .= "<li class='{$active}'><a href='{$url}'>{$label}</a></li>";
-            }
-            $html .= '</ul></div>';
+        //     $html = '<div class="nav-tabs-custom"><ul class="nav nav-tabs">';
+        //     foreach ($tabs as $key => $label) {
+        //         $active = $filterType == $key ? 'active' : '';
+        //         $url = request()->fullUrlWithQuery(['filter' => $key]);
+        //         $html .= "<li class='{$active}'><a href='{$url}'>{$label}</a></li>";
+        //     }
+        //     $html .= '</ul></div>';
 
-            return $html;
-        });
+        //     return $html;
+        // });
 
         // Apply filter to the grid
         $grid->model()->when($filterType !== 'all', function ($q) use ($filterType) {
@@ -186,17 +199,18 @@ class EmojiController extends MainController
             });
         }
 
-        Admin::style("
-            .rtl .column-emoji .rtlSvga{
-                direction: ltr;
-            }
-        ");
-        // Optional: remove table-responsive for large screens
-        Admin::script("
-        if (window.innerWidth >= 1024) {
-            $('.table-responsive').removeClass('table-responsive');
-        }
-    ");
+        // Admin::style("
+        //     .rtl .column-emoji .rtlSvga{
+        //         direction: ltr;
+        //     }
+        // ");
+        // // Optional: remove table-responsive for large screens
+        // Admin::script("
+        //     if (window.innerWidth >= 1024) {
+        //         $('.table-responsive').removeClass('table-responsive');
+        //     }
+        // ");
+        
         $permission    = $this->permission_name;
         $grid->actions(function ($actions) use ($permission) {
             $model = $actions->row;
