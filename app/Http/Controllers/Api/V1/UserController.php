f0d1f1abbb25dfa475604d7284daf837ac62f182
makled98 <mkled.backend@utdsoftware.com>
2026-01-26 14:54:59 +0200

    Update UserController.php

--- app/Http/Controllers/Api/V1/UserController.php
+++ app/Http/Controllers/Api/V1/UserController.php
@@ -1569,14 +1569,7 @@ class UserController extends Controller
             'sender_level' => intval($user->senderLevel?->level),
             'next_sender_level' => intval($user->next_sender_level_info['next_level'] ?? 0),
             'remaining_to_next_level' => $remaining ?? 0,
-            'sender_per' => round(Common::userLevelPer($user), 2),
-            // DEBUG - remove after testing
-            'debug' => [
-                'total_diamond_send' => $user->total_diamond_send,
-                'sub_sender_num' => $user->sub_sender_num,
-                'total_sender_diamonds' => $user->total_sender_diamonds,
-                'total_sender_level' => $user->total_sender_level,
-            ],
+            'sender_per' => round(Common::userLevelPer($user), 6),  // 6 decimals for high levels
         ];
         return Common::apiResponse(true, 'success', $data);
     }
