c961967f1140857f2668135a3954ecf8e1c0e590
makled98 <mkled.backend@utdsoftware.com>
2026-01-27 10:44:10 +0200

    Update MomentsServiceProvider.php

--- packages/Utd/Moments/src/MomentsServiceProvider.php
+++ packages/Utd/Moments/src/MomentsServiceProvider.php
@@ -50,16 +50,16 @@ class MomentsServiceProvider extends ServiceProvider
         Route::prefix('api')
             ->middleware('api')
             ->namespace($this->namespace)
-            ->group(__DIR__ . '/../routes/api.php');
+            ->group(__DIR__ . '/../Routes/api.php');
 
         Route::middleware('web')
             ->namespace($this->namespace)
-            ->group(__DIR__ . '/../routes/web.php');
+            ->group(__DIR__ . '/../Routes/web.php');
 
         Route::prefix('api/utd')
             ->middleware(['api', 'localization'])
             ->namespace($this->namespace)
-            ->group(__DIR__ . '/../routes/utd.php');
+            ->group(__DIR__ . '/../Routes/utd.php');
     }
 
     /**
