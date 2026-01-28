e4b587d9d2e5be4f1998f730b0f3a3a22a218f08
shami2024 <>
2026-01-28 12:13:03 +0200

    LeaderCCgameController

--- app/Http/Controllers/Api/V1/LeaderCCgameController.php
+++ app/Http/Controllers/Api/V1/LeaderCCgameController.php
@@ -44,6 +44,13 @@ class LeaderCCgameController extends Controller
 
     public function userInformation(Request $request)
     {
+        Log::info('LeaderCCgameController@userInformation', [
+            'uid' => $request->uid,
+            'gameId' => $request->gameId,
+            'token' => $request->token,
+            'all' => $request->all(),
+        ]);
+
         return $this->safe(function () use ($request) {
 
             if (!$request->uid || !$request->gameId || !$request->token) {
@@ -75,6 +82,16 @@ class LeaderCCgameController extends Controller
 
     public function updateGameCoin(Request $request)
     {
+        Log::info('LeaderCCgameController@updateGameCoin', [
+            'orderId' => $request->orderId,
+            'gameId' => $request->gameId,
+            'roundId' => $request->roundId,
+            'uid' => $request->uid,
+            'coin' => $request->coin,
+            'type' => $request->type,
+            'all' => $request->all(),
+        ]);
+
         return $this->safe(function () use ($request) {
 
             $required = ['orderId','gameId','roundId','uid','coin','type','rewardType','token','sign'];
@@ -157,6 +174,15 @@ class LeaderCCgameController extends Controller
 
     public function makeUpOrders(Request $request)
     {
+        Log::info('LeaderCCgameController@makeUpOrders', [
+            'orderId' => $request->orderId,
+            'gameId' => $request->gameId,
+            'roundId' => $request->roundId,
+            'uid' => $request->uid,
+            'coin' => $request->coin,
+            'all' => $request->all(),
+        ]);
+
         return $this->safe(function () use ($request) {
 
             $validator = Validator::make($request->all(), [
--- app/Http/Middleware/VerifyGameSignature.php
+++ app/Http/Middleware/VerifyGameSignature.php
@@ -6,6 +6,7 @@ use App\Helpers\SignatureHelper;
 use Carbon\Carbon;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class VerifyGameSignature
@@ -17,6 +18,12 @@ class VerifyGameSignature
      */
     public function handle(Request $request, Closure $next)
     {
+        Log::info('VerifyGameSignature', [
+            'signature' => $request->get('signature'),
+            'signature_nonce' => $request->get('signature_nonce'),
+            'timestamp' => $request->get('timestamp'),
+        ]);
+
         $signature = $request->get('signature');
         $signatureNonce = $request->get('signature_nonce');
         $timestamp = $request->get('timestamp');
