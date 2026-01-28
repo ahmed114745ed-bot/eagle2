6cfbf2001ef44f9ae43abd7c59d64e194c7ed53d
shami2024 <>
2026-01-28 13:24:28 +0200

    Update VerifyLeaderCCMiddleWare.php

--- app/Http/Middleware/VerifyLeaderCCMiddleWare.php
+++ app/Http/Middleware/VerifyLeaderCCMiddleWare.php
@@ -8,6 +8,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Laravel\Sanctum\PersonalAccessToken;
 
 class VerifyLeaderCCMiddleWare
@@ -30,6 +31,7 @@ class VerifyLeaderCCMiddleWare
 
      
         if (!$key) {
+            Log::error('VerifyLeaderCC: Missing game key');
             return response()->json([
                 'errorCode' => 4005,
                 'errorMsg'  => 'Missing or invalid parameters key',
@@ -39,6 +41,7 @@ class VerifyLeaderCCMiddleWare
         if ($request->has('orderId')) {
             $orderId = $request->orderId;
             if (Cache::has("order_$orderId")) {
+                Log::error('VerifyLeaderCC: Duplicate orderId', ['orderId' => $orderId]);
                 return response()->json([
                     'errorCode' => 10003,
                     'errorMsg'  => 'Order already exists'
@@ -49,6 +52,7 @@ class VerifyLeaderCCMiddleWare
             $token = $request->token;
             $userId = $this->findUserByToken($token);
             if (!$userId) {
+                Log::error('VerifyLeaderCC: User not found by token', ['token' => $token]);
                 return response()->json([
                     'errorCode' => 10003,
                     'errorMsg'  => 'user not found'
@@ -62,6 +66,7 @@ class VerifyLeaderCCMiddleWare
                 $requiredParams = ['orderId','gameId','roundId','uid','coin','type','rewardType','token','sign'];
                 foreach ($requiredParams as $p) {
                     if (!$request->has($p)) {
+                        Log::error('VerifyLeaderCC: Missing param for change-balance', ['missing' => $p, 'body' => $request->all()]);
                         return response()->json([
                             'errorCode' => 4005,
                             'errorMsg' => 'Missing signature parameters'
@@ -87,6 +92,7 @@ class VerifyLeaderCCMiddleWare
                 $requiredParams = ['gameId','uid','token','roomId','sign'];
                 foreach ($requiredParams as $p) {
                     if (!$request->has($p)) {
+                        Log::error('VerifyLeaderCC: Missing param for get-user-info/make-up-orders', ['missing' => $p, 'body' => $request->all()]);
                         return response()->json([
                             'errorCode' => 4005,
                             'errorMsg' => 'Missing signature parameters'
@@ -102,6 +108,7 @@ class VerifyLeaderCCMiddleWare
                 break;
 
             default:
+                Log::error('VerifyLeaderCC: Endpoint not allowed', ['path' => $path]);
                 return response()->json([
                     'errorCode' => 4006,
                     'errorMsg' => 'Endpoint not allowed for this middleware'
@@ -111,6 +118,11 @@ class VerifyLeaderCCMiddleWare
         $expectedSign = md5($rawString);
 
         if (!hash_equals(strtolower($expectedSign), strtolower($request->input('sign')))) {
+            Log::error('VerifyLeaderCC: Signature mismatch', [
+                'rawString' => $rawString,
+                'expectedSign' => $expectedSign,
+                'receivedSign' => $request->input('sign'),
+            ]);
             return response()->json([
                 'errorCode' => 10004,
                 'errorMsg' => 'Verify signature fail'
