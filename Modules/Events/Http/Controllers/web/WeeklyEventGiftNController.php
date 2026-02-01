981335d543c5fb61d2c55fd92bdeae4c674df8a2
Erinyelkess <ariny.backend@utdsoftware.com>
2026-02-01 10:29:32 +0200

    weekly view

--- Modules/Events/Http/Controllers/web/WeeklyEventGiftNController.php
+++ Modules/Events/Http/Controllers/web/WeeklyEventGiftNController.php
@@ -386,6 +386,12 @@ class WeeklyEventGiftNController extends MainController
             .file-input .input-group.file-caption-main {
                 display: flex !important;
             }
+                 .file-input .input-group.file-caption-main .btn-file {
+            padding: 5px 15px !important;
+            border-radius: 6px !important;
+            font-size: 12px !important;
+            margin-left: -3333% !important;
+        }
             </style>
         ');
     
