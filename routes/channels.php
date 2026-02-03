3675ae7f429e2d8e1b755038f0248ec0f3e8be67
Bola-Philip <77309023+Bola-Philip@users.noreply.github.com>
2026-02-03 11:19:29 +0200

    testing

--- routes/channels.php
+++ routes/channels.php
@@ -55,6 +55,9 @@ Broadcast::channel('presence.user.{id}', function ($user, $id) {
 });
 
 Broadcast::channel('room.boom.rewards.{roomId}', function ($user, $roomId) {
+    if (!$user) {
+        return false;
+    }
     return [
         'id'   => $user->id,
        // 'name' => $user->name,
