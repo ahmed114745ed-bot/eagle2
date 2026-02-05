1e966b22afe01ba127094e697bc0f57c43f1b6dc
makled98 <mkled.backend@utdsoftware.com>
2026-02-05 10:16:33 +0200

    Update moments.php

--- packages/Utd/Moments/config/moments.php
+++ packages/Utd/Moments/config/moments.php
@@ -1,18 +1,5 @@
 <?php
 
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | External Models Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Configure external model dependencies to make the package independent
-    |
-    */
-    'models' => [
-        'gift' => env('MOMENTS_GIFT_MODEL', 'Utd\Gifts\Entities\Gift'),
-        
-        // User Model
-        'user' => env('MOMENTS_USER_MODEL', 'App\Models\User'),
-    ],
+    'default_sort' => env('MOMENT_VIEWER_DEFAULT_SORT', 'random'),
 ];
