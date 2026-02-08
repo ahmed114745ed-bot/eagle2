44468a0e34a564e198b3071281ba5b3738647dea
shami2024 <>
2026-02-08 08:14:59 +0200

    Update TimestampsWithTimezone.php

--- app/Traits/TimestampsWithTimezone.php
+++ app/Traits/TimestampsWithTimezone.php
@@ -3,40 +3,10 @@
 namespace App\Traits;
 
 /**
- * TimestampsWithTimezone Alias
- * 
- * توجه إلى Package Trait إذا كان موجوداً أو تستخدم النسخة المحلية
+ * Alias for backward compatibility
+ * @deprecated Use Utd\Gifts\Traits\TimestampsWithTimezone instead
  */
-if (trait_exists('\\Utd\\Gifts\\Traits\\TimestampsWithTimezone')) {
-    // استخدم النسخة من Package
-    class_alias('\\Utd\\Gifts\\Traits\\TimestampsWithTimezone', '\\App\\Traits\\TimestampsWithTimezone');
-} else {
-    // Fallback للنسخة المحلية
-    use App\Helpers\Common;
-    use Illuminate\Support\Carbon;
-
-    trait TimestampsWithTimezone
-    {
-        public function getCreatedAtAttribute($value)
-        {
-            $tz = request()->header('tz', Common::timeZone());
-            if (!isValidTimezone($tz)) {
-                $tz = 'UTC';
-            }
-
-            return \Carbon\Carbon::parse($value)->setTimezone($tz)->format('Y-m-d H:i:s');
-        }
-
-        public function getUpdatedAtAttribute($value)
-        {
-            $tz = request()->header('tz', Common::timeZone());
-            if (!in_array($tz, timezone_identifiers_list())) {
-                $tz = 'UTC';
-            }
-
-            return Carbon::parse($value)->setTimezone($tz)->format('Y-m-d H:i:s');
-        }
-
-    }
+trait TimestampsWithTimezone
+{
+    use \Utd\Gifts\Traits\TimestampsWithTimezone;
 }
-
