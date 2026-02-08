f9061396f06eeebd6610965adedb2f2d13fb12a4
Erinyelkess <ariny.backend@utdsoftware.com>
2026-02-08 07:17:10 +0200

    body

--- app/Helpers/UserCommon.php
+++ app/Helpers/UserCommon.php
@@ -486,8 +486,8 @@ class UserCommon
         $title = __('congratulations');
         $body = '';
         if ($feature) {
+            $body = $user->name . ':' . __('wareGiftNotification', ['wareName' => $ware->name, 'type' => $feature->name]);
             Log::info('Adding event ware to user: ' . $user->id . ', ware: ' . $ware->id . ', feature: ' . $feature->name);
-            $body= $user->name . ':' . __('wareGiftNotification', ['wareName' => $ware->name, 'type' => $feature->name]);
         } else {
             $body = $user->name . ':' .  __('You have received a gift: :ware', ['ware' => $ware->name]);
         }
