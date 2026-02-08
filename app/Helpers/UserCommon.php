751a74b2f769e4ab629d58c3d8633938949b66b8
Erinyelkess <ariny.backend@utdsoftware.com>
2026-02-08 07:10:23 +0200

    common

--- app/Helpers/UserCommon.php
+++ app/Helpers/UserCommon.php
@@ -484,9 +484,10 @@ class UserCommon
     public static function addEvintsWareToUser(User $user, Ware $ware, $expir, $sender = null, $receiveType = null, $isUsed = null, $feature = null)
     {
         $title = __('congratulations');
+        $body = '';
         if ($feature) {
             Log::info('Adding event ware to user: ' . $user->id . ', ware: ' . $ware->id . ', feature: ' . $feature->name);
-            $title = $user->name . ':' . __('wareGiftNotification', ['wareName' => $ware->name, 'type' => $feature->name]);
+            $body= $user->name . ':' . __('wareGiftNotification', ['wareName' => $ware->name, 'type' => $feature->name]);
         } else {
             $body = $user->name . ':' .  __('You have received a gift: :ware', ['ware' => $ware->name]);
         }
