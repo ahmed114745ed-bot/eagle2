4d0ada014f98b97517dc9c3716cfd8baa5695326
shami2024 <>
2026-02-08 12:30:50 +0200

    ClassResolver

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -97,9 +97,6 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        // Increase memory limit for production
-        @ini_set('memory_limit', '512M');
-        
         $this->dashboardAdminConfig();
         $this->setupAppSettings();
         $this->setupLanguages();
