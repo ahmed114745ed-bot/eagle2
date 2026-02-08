e0d58c12176b9f331b34150d1ca81c12bf1a4217
shami2024 <>
2026-02-08 09:56:51 +0200

    Create GiftLogRepository.php

---
+++ app/Tik/Repositories/GiftLogRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Tik\Repositories;
+
+/**
+ * Backward compatibility alias
+ * This class extends the new package repository
+ */
+class GiftLogRepository extends \Utd\Gifts\Repositories\GiftLogRepository
+{
+}
