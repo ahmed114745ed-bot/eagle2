89393997e43847924206486d279ef8346843e887
Erinyelkess <ariny.backend@utdsoftware.com>
2026-02-09 12:45:36 +0200

    badge image show

--- Modules/Badge/Http/Controllers/web/BadgeController.php
+++ Modules/Badge/Http/Controllers/web/BadgeController.php
@@ -68,7 +68,7 @@ class BadgeController extends MainController
                 $path =   $this->images->firstWhere('language', app()->getLocale())?->image;
                 /** @var Ware $this */
                 $url = getImagePath($path);
-                return handleShowImageWithTypes($this->id, $url, 50, 50, objectFit: 'cover');
+                return handleShowImageWithTypes($this->id, $url, 100, 100, 4, 'contain');
             });
         }
         $grid->column('priority', __('Priority'))->sortable();
