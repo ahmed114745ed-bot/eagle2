51f7daed79b4e898dafbec23115fb5e1a102830f
Erinyelkess <ariny.backend@utdsoftware.com>
2026-02-09 13:39:53 +0200

    badge

--- resources/views/multiBadges.blade.php
+++ resources/views/multiBadges.blade.php
@@ -1,104 +1,3 @@
-{{-- 
- @php
-    $enabledLanguages = Cache::rememberForever('languages', function () {
-        return \App\Models\Language::where('is_enabled', true)
-            ->pluck('name', 'code')
-            ->toArray();
-    });
-
-    $languages = $enabledLanguages;
-
-    // default first tab
-    $defaultLang = array_key_first($languages);
-
-    // previously selected tab (restore)
-    $selectedLang = request()->input('tab', $defaultLang);
-
-@endphp
-
-<ul class="nav nav-tabs" role="tablist">
-    @foreach ($languages as $code => $label)
-        <li class="nav-item">
-            <a class="nav-link {{ $code === $selectedLang ? 'active' : '' }}"
-               href="#lang-{{ $code }}"
-               data-toggle="tab"
-               data-lang="{{ $code }}">
-                {{ __($label) }}
-            </a>
-        </li>
-    @endforeach
-</ul>
-
-
-<div class="tab-content p-3 border border-top-0">
-    @php $first = true; @endphp
-    @foreach ($languages as $code => $label)
-        @php
-            $imageData = $badgeImages[$code] ?? null;
-        @endphp
-
-        <div class="tab-pane fade {{ $code === $selectedLang ? 'show active' : '' }}" id="lang-{{ $code }}">
-
-           
-            <div class="form-group">
-                <label>{{ __('Image') }} ({{ $label }})</label>
-                @if(!empty($imageData->show_image))
-                    <div>
-                        <img src="{{ getImagePath( $imageData->show_image) }}" alt="" height="80">
-                    </div>
-                @endif
-                <input type="file" name="images[{{ $code }}][image]" class="form-control">
-            </div>
-
-            
-            <div class="form-group">
-                <label>{{ __('Default Image') }} ({{ $label }})</label>
-                @if(!empty($imageData->image))
-                    <div>
-                        <img src="{{ getImagePath( $imageData->image) }}" alt="" height="80">
-                    </div>
-                @endif
-                <input type="file" name="images[{{ $code }}][default_image]" class="form-control">
-            </div>
-
-            
-            <div class="form-group">
-                <label>{{ __('Image Type') }} ({{ $label }})</label>
-                <select name="images[{{ $code }}][image_type]" class="form-control" required>
-                    @foreach(\App\Enums\ImageType::options() as $key => $val)
-                        <option value="{{ $key }}" {{ ($imageData->image_type ?? '') == $key ? 'selected' : '' }}>
-                            {{ $val }}
-                        </option>
-                    @endforeach
-                </select>
-            </div>
-
-        </div>
-        @php $first = false; @endphp
-    @endforeach
-</div>
-
-
-
-
-
-<script>
-    $(document).ready(function () {
-
-        // Force selected tab to show on initial load
-        $('.nav-tabs a.active').tab('show');
-
-        // Save selected tab when user switches
-        $('.nav-tabs a[data-toggle="tab"]').on('click', function () {
-            let selected = $(this).data('lang');
-            $('<input>').attr({
-                type: 'hidden',
-                name: 'tab',
-                value: selected
-            }).appendTo('form');
-        });
-    });
-</script> --}}
 
 
 @php
@@ -154,9 +53,9 @@
             <div class="form-group mb-2">
                 <label class="mb-1">{{ __('Image') }} ({{ $label }})</label>
 
-                @if(!empty($imageData?->show_image))
+                @if(!empty($imageData?->image))
                     <div class="mb-1">
-                        <img src="{{ getImagePath($imageData->show_image) }}" height="70">
+                        <img src="{{ getImagePath($imageData->image) }}" height="70">
                     </div>
                 @endif
 
@@ -171,9 +70,9 @@
             <div class="form-group mb-2">
                 <label class="mb-1">{{ __('Default Image') }} ({{ $label }})</label>
 
-                @if(!empty($imageData?->image))
+                @if(!empty($imageData?->show_image ))
                     <div class="mb-1">
-                        <img src="{{ getImagePath($imageData->image) }}" height="70">
+                        <img src="{{ getImagePath($imageData->show_image) }}" height="70">
                     </div>
                 @endif
 
