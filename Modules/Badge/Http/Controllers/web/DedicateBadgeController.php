85e6897c8f4a8f5046ff137fc5eb82dbb8f5961d
Erinyelkess <ariny.backend@utdsoftware.com>
2026-02-09 11:40:02 +0200

    dedicate badge notification

--- Modules/Badge/Http/Controllers/web/DedicateBadgeController.php
+++ Modules/Badge/Http/Controllers/web/DedicateBadgeController.php
@@ -35,7 +35,8 @@ class DedicateBadgeController extends MainController
         $grid->column('id', __('ID'));
         $grid->column('name', __('name'));
         if (!request()->filled('_export_')) {
-            $grid->column('image', __('image'))->display(function ($path) {
+            $grid->column('images.image', __('image'))->display(function ($path) {
+                $path =   $this->images->firstWhere('language', app()->getLocale())?->image;
                 /** @var Ware $this */
                 $url = getImagePath($path);
                 return handleShowImageWithTypes($this->id, $url, 50, 50);
