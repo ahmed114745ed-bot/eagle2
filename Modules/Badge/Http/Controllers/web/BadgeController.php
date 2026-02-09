eeb68c8d397eabfc7378032442eaafd97f64c9e6
Erinyelkess <ariny.backend@utdsoftware.com>
2026-02-09 12:27:11 +0200

    image size

--- Modules/Badge/Http/Controllers/web/BadgeController.php
+++ Modules/Badge/Http/Controllers/web/BadgeController.php
@@ -68,7 +68,7 @@ class BadgeController extends MainController
                 $path =   $this->images->firstWhere('language', app()->getLocale())?->image;
                 /** @var Ware $this */
                 $url = getImagePath($path);
-                return handleShowImageWithTypes($this->id, $url, 50, 50);
+                return handleShowImageWithTypes($this->id, $url, 90, 90);
             });
         }
         $grid->column('priority', __('Priority'))->sortable();
