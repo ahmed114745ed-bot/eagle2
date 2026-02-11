00e19b3e5b66719123ded8ffc35d041674900cdb
Bola-Philip <77309023+Bola-Philip@users.noreply.github.com>
2026-02-11 09:29:31 +0200

    edit reponse

--- packages/Utd/LuckyBox/src/Http/Resources/BoxUseResource.php
+++ packages/Utd/LuckyBox/src/Http/Resources/BoxUseResource.php
@@ -2,6 +2,9 @@
 
 namespace Utd\LuckyBox\Http\Resources;
 
+use App\Helpers\Common;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class BoxUseResource extends JsonResource
@@ -14,24 +17,40 @@ class BoxUseResource extends JsonResource
      */
     public function toArray($request)
     {
+        // $startTime = Carbon::createFromTimestamp($this->start_at);
+        // $currentTime = Carbon::now();
+        // if ($startTime >= $currentTime) {
+        //     $rem_time = $startTime->diffInSeconds($currentTime);
+        // } else {
+        //     $rem_time = 0;
+        // }
+
+
+        // $rem_time = Carbon::createFromTimestamp($this->start_at)->diffInSeconds(now());
+
+
+        if (!$this->user instanceof User) return [];
         return [
             'id' => $this->id,
-            'box_id' => $this->box_id,
-            'user_id' => $this->user_id,
-            'coins' => $this->coins,
-            'start_at' => $this->start_at,
-            'end_at' => $this->end_at,
-            'room_uid' => $this->room_uid,
-            'room_id' => $this->room_id,
+            'user' => [
+                'id'        => $this->user->id,
+                'uuid'      => $this->user->uuid,
+                'image'     => $this->user->profile->avatar,
+                'name'      => $this->user->name,
+                'is_follow'            => @(bool)Common::IsFollow(@$request->user()->id, $this->user->id), // user data  ----
+            ],
+            'coins' => $this->box->coins,
+            //            'end_at'=>$this->end_at,
+            //            'room_uid'=>$this->room_uid,
+            //            'room_id'=>$this->room_id,
             'users_num' => $this->users_num,
-            'used_num' => $this->used_num,
-            'used_coins' => $this->used_coins,
-            'not_used_num' => $this->not_used_num,
-            'unused_coins' => $this->unused_coins,
+            //            'not_used_num'=>$this->not_used_num,
             'type' => $this->type == 1 ? 'super' : 'normal',
-            'label' => $this->label,
-            'image' => $this->image,
-            'is_closed' => $this->is_closed,
+            //            'label'=>$this->label,
+            //            'image'=>$this->image,
+            //            'rem_time'=>$this->type == 1 ? $rem_time : 0
+            // 'rem_time' => $rem_time,
+            "end_time" => Carbon::createFromTimestamp($this->end_at)->toDateTimeString(),
         ];
     }
 }
