61fe0aaf272b4aeffff93353b41c3ff0baf3b9bd
shami2024 <>
2026-02-16 09:23:54 +0200

    Update TimestampsWithTimezone.php

--- app/Traits/TimestampsWithTimezone.php
+++ app/Traits/TimestampsWithTimezone.php
@@ -13,17 +13,41 @@ trait TimestampsWithTimezone
      */
     public function getCreatedAtAttribute($value)
     {
-        if (trait_exists(\Utd\Gifts\Traits\TimestampsWithTimezone::class)) {
-            return \Utd\Gifts\Traits\TimestampsWithTimezone::getCreatedAtAttribute($value);
+        if ($formatted = $this->formatUsingPackageTrait(__FUNCTION__, $value)) {
+            return $formatted;
         }
+
         return \Illuminate\Support\Carbon::parse($value)->format('Y-m-d H:i:s');
     }
 
     public function getUpdatedAtAttribute($value)
     {
-        if (trait_exists(\Utd\Gifts\Traits\TimestampsWithTimezone::class)) {
-            return \Utd\Gifts\Traits\TimestampsWithTimezone::getUpdatedAtAttribute($value);
+        if ($formatted = $this->formatUsingPackageTrait(__FUNCTION__, $value)) {
+            return $formatted;
         }
+
         return \Illuminate\Support\Carbon::parse($value)->format('Y-m-d H:i:s');
+    }
+
+    protected function formatUsingPackageTrait(string $method, $value)
+    {
+        if (! trait_exists(\Utd\Gifts\Traits\TimestampsWithTimezone::class)) {
+            return null;
+        }
+
+        static $proxy = null;
+
+        if ($proxy === null) {
+            $proxy = new class {
+                use \Utd\Gifts\Traits\TimestampsWithTimezone;
+
+                public function format(string $method, $value)
+                {
+                    return $this->{$method}($value);
+                }
+            };
+        }
+
+        return $proxy->format($method, $value);
     }
 }
