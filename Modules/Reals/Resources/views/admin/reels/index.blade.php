7781d4b197f77916892517fed24a9105c4595359
shami2024 <>
2026-02-16 11:22:36 +0200

    Update index.blade.php

--- Modules/Reals/Resources/views/admin/reels/index.blade.php
+++ Modules/Reals/Resources/views/admin/reels/index.blade.php
@@ -21,6 +21,10 @@
         @include('reals::admin.reels.partials.styles')
 
     </style>
+    @php
+        $reelsManagerFile = public_path('modules/reals/js/reels-manager.js');
+        $reelsManagerVersion = file_exists($reelsManagerFile) ? filemtime($reelsManagerFile) : time();
+    @endphp
     <script data-exec-on-popstate src="{{ asset('modules/reals/js/reels-manager.js') }}?v={{ $reelsManagerVersion }}" data-reels-manager="true"></script>
     <script data-exec-on-popstate>
         (function () {
