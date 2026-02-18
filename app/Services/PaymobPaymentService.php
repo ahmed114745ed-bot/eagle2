5e721bcfb398cc57049b57d8335b448193d07fbe
Bola-Philip <77309023+Bola-Philip@users.noreply.github.com>
2026-02-18 09:29:46 +0200

    change payment link

--- app/Services/PaymobPaymentService.php
+++ app/Services/PaymobPaymentService.php
@@ -80,8 +80,8 @@ class PaymobPaymentService
     public function createPaymentLink($amount, $name, $description = '', $email = null, $phone = null, $expiresAt = null, $isLive = false)
     {
         $utdUrl = config("services.utd_paymob.utd_url");
-        // Use paymob-payment-link endpoint
-        $baseUrl = preg_replace('/\/api\/.*$/', '/api/paymob-payment-link', $utdUrl);
+        // Use paymob-intention endpoint
+        $baseUrl = preg_replace('/\/api\/.*$/', '/api/paymob-intention', $utdUrl);
 
         $merchantCode = config("services.utd_paymob.utd_paymob_merchant_code");
         $secure_key = config("services.utd_paymob.utd_paymob_secret");
