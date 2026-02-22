bb4c57208fb95003625e2829532321af2b6d9ab6
Erinyelkess <ariny.backend@utdsoftware.com>
2026-02-22 11:11:25 +0200

    room boom level

--- Modules/RoomBoom/Http/Controllers/web/PercentageBoomController.php
+++ Modules/RoomBoom/Http/Controllers/web/PercentageBoomController.php
@@ -20,7 +20,7 @@ class PercentageBoomController extends MainController
         $percentages = BoomPercentage::get();
         return parent::index($content
             ->title(__('settings'))
-            ->body(view('percentageBoom', compact(['percentages']))));
+            ->body(view('percentageBoom', compact(['percentages',]))));
     }
 
 
@@ -58,7 +58,7 @@ class PercentageBoomController extends MainController
 
             $percentage->save();
         }
-        settings()->set('boom_themes', time());
+
         admin_toastr(__('Saved successfully'), 'success');
         return back();
     }
