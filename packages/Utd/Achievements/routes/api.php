b07cd472f2bef140da976f20ba3cab2387186615
Bola-Philip <77309023+Bola-Philip@users.noreply.github.com>
2026-02-24 08:53:44 +0200

    fix: Update composer dependencies to use wildcard versions and adjust API route middleware prefix

--- packages/Utd/Achievements/routes/api.php
+++ packages/Utd/Achievements/routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 use Utd\Achievements\Http\Controllers\AchievementController;
 use Utd\Achievements\Http\Controllers\AchievementLevelController;
 
-Route::middleware(['auth:sanctum', 'update.last.seen'])->group(function () {
+Route::middleware(['auth:sanctum', 'update.last.seen'])->prefix('api')->group(function () {
     Route::prefix('achievement')->group(function () {
         Route::get('/{id}', [AchievementController::class, 'get_all']);
         Route::get('/', [AchievementController::class, 'get_all']);
