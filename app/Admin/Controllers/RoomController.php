e79ba9e99d7aa7e64e316b1911dd2b06f12cddc0
makled98 <mkled.backend@utdsoftware.com>
2026-02-24 11:29:37 +0200

    fix(point):Paginate rooms, include created_at, use left joins
    Add pagination to the Room grid (20 per page), include rooms.created_at in the selected fields, and change country-related joins to leftJoin with an orderByRaw that places NULL country ids last. These changes prevent rows from being excluded when users or countries are missing and improve grid performance/UX by limiting results per page.

--- app/Admin/Controllers/RoomController.php
+++ app/Admin/Controllers/RoomController.php
@@ -256,6 +256,7 @@ class RoomController extends MainController
     {
         $grid = new Grid(new Room);
         $grid->model()->with(['microphones.user.profile']);
+        $grid->paginate(20);
         $filterType = request('filter', 'all');
         $user = auth()->user();
 
@@ -345,6 +346,7 @@ class RoomController extends MainController
                 'rooms.room_admin',
                 'rooms.level',
                 'rooms.level_id',
+                'rooms.created_at',
                 \DB::raw("
         CASE rooms.room_status
             WHEN 1 THEN 100
@@ -517,9 +519,9 @@ class RoomController extends MainController
 
             case 'country':
                 $grid->model()
-                    ->join('users', 'rooms.uid', '=', 'users.id')
-                    ->join('countries', 'users.country_id', '=', 'countries.id')
-                    ->orderBy('countries.id');
+                    ->leftJoin('users', 'rooms.uid', '=', 'users.id')
+                    ->leftJoin('countries', 'users.country_id', '=', 'countries.id')
+                    ->orderByRaw('countries.id IS NULL, countries.id ASC');
                 break;
 
             default:
