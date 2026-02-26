eee5293776f65a7628ddb253117ffce150c2d9ab
Bola-Philip <77309023+Bola-Philip@users.noreply.github.com>
2026-02-26 09:28:37 +0200

    Remove DailyPrize module components including migrations, seeders, controllers, routes, views, and service provider. This cleanup eliminates unused code and prepares for a refactor or redesign of the DailyPrize functionality.

--- packages/Utd/DailyPrize/resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('dailyprize::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('dailyprize.name') !!}
-    </p>
-@endsection
