f9c3d0be8fe77aa81fc8564c89a8e8fc8bd0d121
shami2024 <>
2026-03-02 09:41:50 +0200

    debug: Add detailed logging for PK and Charisma job dispatches in GiftLogService.

--- app/Tik/Services/GiftLogService.php
+++ app/Tik/Services/GiftLogService.php
@@ -166,14 +166,30 @@ class GiftLogService
                 }
             }
 
-            if ($room->lastPk != null) {
+            Log::info("DEBUG: PK and Charisma Check", [
+                'room_id' => $room->id,
+                'lastPk' => $room->lastPk,
+                'charizma_status' => $room->charizma_status,
+                'userId' => $userId
+            ]);
 
+            if ($room->lastPk != null) {
+                Log::info("DEBUG: Dispatching UpdatePkAndSendToZigo", [
+                    'room_id' => $room->id,
+                    'user_id' => $user->id,
+                    'price' => ($gift->price * $number)
+                ]);
                 dispatch(new UpdatePkAndSendToZigo($user->id, $room->id, $receivedUsers->pluck('id')->toArray(), ($gift->price * $number), $room))
                     ->afterCommit()
                     ->onQueue('updatePk');
             }
 
             if ($room->charizma_status) {
+                Log::info("DEBUG: Dispatching UpdateSendCharismaToZigo", [
+                    'room_id' => $room->id,
+                    'userId' => $userId,
+                    'price' => ($gift->price * $number)
+                ]);
                 dispatch(new UpdateSendCharismaToZigo($room->id, $receivedUsers->pluck('id')->toArray(), ($gift->price * $number), $userId))
                     ->afterCommit()
                     ->onQueue('default');
