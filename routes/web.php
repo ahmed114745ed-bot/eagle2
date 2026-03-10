54d71fcd309abfe9bb51fc6ea6f51a82ab219c57
makled98 <mkled.backend@utdsoftware.com>
2026-03-10 14:15:32 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1253,7 +1253,9 @@ Route::get('/run-lucky-gift-unit-test', function () {
 });
 
 Route::post('/__debugbar/screen', function (\Illuminate\Http\Request $request) {
-    Debugbar::info('Viewport:', $request->all());
+    if (class_exists(\Barryvdh\Debugbar\Facades\Debugbar::class)) {
+        \Barryvdh\Debugbar\Facades\Debugbar::info('Viewport:', $request->all());
+    }
     return response()->json(['ok' => true]);
 });
 
