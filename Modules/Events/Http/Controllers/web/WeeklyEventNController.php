5bc553e6d6133c024e89f480fe1f836c25daef99
Erinyelkess <ariny.backend@utdsoftware.com>
2026-03-12 12:59:58 +0200

    fix: remove debug dd() statement from WeeklyEventNController
    Remove leftover dd() call in the form method that was preventing
    the weekly event form from rendering properly.

--- Modules/Events/Http/Controllers/web/WeeklyEventNController.php
+++ Modules/Events/Http/Controllers/web/WeeklyEventNController.php
@@ -143,7 +143,6 @@ class WeeklyEventNController extends MainController
         $lastStartDate = \Modules\Events\Entities\WeeklyStar::where("type", 'weekly_star')->max('start_date');
 
         $minStartDate = $lastStartDate ? \Carbon\Carbon::parse($lastStartDate)->addDay(8)->toDateString() : null;
-        dd($minStartDate, date("Y-m-d"));
         if ($minStartDate && $minStartDate < date("Y-m-d")) {
             $minStartDate = date("Y-m-d");
         }
