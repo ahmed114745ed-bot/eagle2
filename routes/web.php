efec80921690b57d4f51654318eb4b18fdb025b5
shami2024 <>
2026-03-12 13:55:06 +0200

    Refactor: Replace `UpdateSendCharismaToZigo` job dispatch with `dispatchRoomsRedis` for charisma updates.

--- routes/web.php
+++ routes/web.php
@@ -1760,17 +1760,31 @@ Route::get('make-seeders-for-permission', function () {
 
 
 
-
 Route::get('/queue-control/{queue}', function ($queue) {
 
     $check = shell_exec("ps aux | grep 'queue:work --queue=$queue' | grep -v grep");
 
     if ($check) {
-        Artisan::call('queue:restart');
-        return "Queue $queue restarted";
+        $output = [];
+        $returnVar = 0;
+        exec("php artisan queue:restart 2>&1", $output, $returnVar);
+        return response()->json([
+            'action' => 'restarted',
+            'queue' => $queue,
+            'return_code' => $returnVar,
+            'output' => $output
+        ]);
     } else {
-        shell_exec("php artisan queue:work --queue=$queue > /dev/null 2>&1 &");
-        return "Queue $queue started";
+        $output = [];
+        $returnVar = 0;
+        exec("php artisan queue:work --queue=$queue --tries=1 2>&1 &", $output, $returnVar);
+
+        return response()->json([
+            'action' => 'started',
+            'queue' => $queue,
+            'return_code' => $returnVar,
+            'output' => $output
+        ]);
     }
 
 });
