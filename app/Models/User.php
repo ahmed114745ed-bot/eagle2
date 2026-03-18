3cb0d4e9e660f85424c91833b786b2797185e1b2
makled98 <mkled.backend@utdsoftware.com>
2026-03-18 23:51:03 +0200

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,7 +47,7 @@ use Modules\UsersWallet\Entities\UserWallet;
  */
 class User extends Authenticatable
 {
-    use AchievementUser, ChatUserTrait, FollowTrait, HasApiTokens, HasFactory, MomentRelationshipTrait, Notifiable, PaymentGetWayTrait, SoftDeletes, SpecialId, TimestampsWithTimezone, UserTransferTrait, UserLevel;
+    use AchievementUser, ChatUserTrait, FollowTrait, HasApiTokens, HasFactory, MomentRelationshipTrait, Notifiable, PaymentGetWayTrait, SoftDeletes, SpecialId, TimestampsWithTimezone, UserTransferTrait, UserLevel,RealRelationshipTrait;
 
     /*
      * To enable and disable observer saving and updating methods
