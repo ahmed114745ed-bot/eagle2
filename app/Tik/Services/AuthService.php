28f5c348257420e9e0c7aae8c06322eccc76e55e
makled98 <mkled.backend@utdsoftware.com>
2026-03-25 14:31:44 +0200

    Update AuthService.php

--- app/Tik/Services/AuthService.php
+++ app/Tik/Services/AuthService.php
@@ -224,17 +224,7 @@ class AuthService
                 if ($countryId) {
                     $data['country_id'] = $countryId;
                 }
-                try {
-                    if (!empty($request['device_token']))  $this->devicesTokenHistory($request['device_token']);
-                } catch (\Exception $exception) {
-                    Log::warning('Device token limit reached............ on Google login', [
-                        'device_token' => $request['device_token'] ?? null,
-                        'email' => $request['email'] ?? null,
-                        'google_id' => $request['google_id'] ?? null,
-                        'error' => $exception->getMessage(),
-                    ]);
-                    return Common::apiResponse(false, $exception->getMessage(), [], 500);
-                }
+                if (!empty($request['device_token']))  $this->devicesTokenHistory($request['device_token']);
                 $user = $this->userRepository->create($data);
 
                 $is_new = true;
