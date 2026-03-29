450fb1eb5e9b074c381ae28f95fa971200674a6c
shami2024 <>
2026-03-29 14:00:48 +0200

    Update FormTemplateController.php

--- Modules/Form/Http/Controllers/FormTemplateController.php
+++ Modules/Form/Http/Controllers/FormTemplateController.php
@@ -59,6 +59,8 @@ class FormTemplateController extends Controller
 
     public function store(Request $request)
     {
+        \Log::info('Creating Form Template', ['request' => $request->all()]);
+
         $validated = $request->validate([
             'title' => 'required|array',
             'title.*' => 'required|string',
@@ -136,6 +138,8 @@ class FormTemplateController extends Controller
 
     public function edit($id, Content $content)
     {
+        \Log::info('Updating Form Template ID: ' . $id);
+    
         if (!Admin::user()->can('*')) {
             Permission::check('edit-' . $this->permission_name);
         }
