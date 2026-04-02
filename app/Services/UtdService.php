0b0eb71a9c6a2f4751dde107836bdadad2748ad9
Bola-Philip <77309023+Bola-Philip@users.noreply.github.com>
2026-04-02 11:03:29 +0200

    utd pay logs

--- app/Services/UtdService.php
+++ app/Services/UtdService.php
@@ -70,50 +70,50 @@ class UtdService
     {
         info($request);
         info('callback');
-        die();
-        $txnId = $request->input('TxnId');
-        $orderId = $request->input('OrderId');
-        $totalPrice = $request->input('TotalPrice');
-        $resultCode = $request->input('ResultCode');
-        $checksum = $request->input('Checksum');
-
-        // \Log::info('Codapay Callback Received', $request->all());
-
-        $secretKey = config('codapay.api_key');
-        $computedChecksum = md5($txnId . $secretKey . $orderId . $resultCode);
-
-        if ($checksum !== $computedChecksum) {
-            // \Log::warning('Codapay checksum failed', [
-            //     'expected' => $computedChecksum,
-            //     'received' => $checksum,
-            // ]);
-            return response()->json(['error' => 'Invalid checksum'], 403);
-        }
-
-        $coinLog = CoinLog::where('id', $orderId)->first();
-
-        if (!$coinLog) {
-            return response()->json([
-                'status' => 'ignored',
-                'trx' => $txnId,
-                'message' => "Failed",
-            ]);
-        }
-
-        // \Log::info('Codapay callback verified', [
-        //     'TxnId' => $txnId,
-        //     'OrderId' => $orderId,
-        //     'ResultCode' => $resultCode,
-        // ]);
-
-        if ($resultCode === "0") {
-            // Log::info("✅ Codapay Payment Success", compact('orderId', 'txnId'));
-            return $this->webhookPayment($orderId, method: 'codapay', newTrx: $txnId);
-        } else {
-            // Log::info("❌ Codapay Payment Failed", compact('orderId', 'txnId', 'resultCode'));
-            $coinLog->update(['status' => PaymentStatus::CANCELED, 'trx' => $txnId]);
-            return response()->json(['status' => false, 'trx' => $txnId, 'message' => 'Transaction declined.',]);
-        }
+//        die();
+//        $txnId = $request->input('TxnId');
+//        $orderId = $request->input('OrderId');
+//        $totalPrice = $request->input('TotalPrice');
+//        $resultCode = $request->input('ResultCode');
+//        $checksum = $request->input('Checksum');
+//
+//        // \Log::info('Codapay Callback Received', $request->all());
+//
+//        $secretKey = config('codapay.api_key');
+//        $computedChecksum = md5($txnId . $secretKey . $orderId . $resultCode);
+//
+//        if ($checksum !== $computedChecksum) {
+//            // \Log::warning('Codapay checksum failed', [
+//            //     'expected' => $computedChecksum,
+//            //     'received' => $checksum,
+//            // ]);
+//            return response()->json(['error' => 'Invalid checksum'], 403);
+//        }
+//
+//        $coinLog = CoinLog::where('id', $orderId)->first();
+//
+//        if (!$coinLog) {
+//            return response()->json([
+//                'status' => 'ignored',
+//                'trx' => $txnId,
+//                'message' => "Failed",
+//            ]);
+//        }
+//
+//        // \Log::info('Codapay callback verified', [
+//        //     'TxnId' => $txnId,
+//        //     'OrderId' => $orderId,
+//        //     'ResultCode' => $resultCode,
+//        // ]);
+//
+//        if ($resultCode === "0") {
+//            // Log::info("✅ Codapay Payment Success", compact('orderId', 'txnId'));
+//            return $this->webhookPayment($orderId, method: 'codapay', newTrx: $txnId);
+//        } else {
+//            // Log::info("❌ Codapay Payment Failed", compact('orderId', 'txnId', 'resultCode'));
+//            $coinLog->update(['status' => PaymentStatus::CANCELED, 'trx' => $txnId]);
+//            return response()->json(['status' => false, 'trx' => $txnId, 'message' => 'Transaction declined.',]);
+//        }
     }
 
     public function success(Request $request, $trx): JsonResponse
@@ -121,59 +121,59 @@ class UtdService
         info($trx);
         info($request);
         info('success');
-        die();
-        $coinLog = CoinLog::where('trx', $trx)->whereMethod('utd')->firstOrFail();
-
-        if (!$trx) {
-            return response()->json(['status' => 'error', 'message' => 'Missing transaction ID'], 400);
-        }
-
-        $url = $this->baseUrl . '/api/restful/v2.0/Payment/inquiryPaymentResult.json';
-        $body = [
-            'inquiryPaymentRequest' => [
-                'txnId' => $coinLog->trx,
-                'apiKey' => $this->apiKey,
-                'projectId' => $this->projectId,
-                'needStatusFinal' => true,
-            ],
-        ];
-
-        $response = Http::withHeaders([
-            'Content-Type' => 'application/json',
-        ])->post($url, $body);
-
-        $json = $response->json();
-
-        $paymentResult = $json['paymentResult'] ?? null;
-        $entries = $paymentResult['profile']['entry'] ?? [];
-
-        $statusValue = null;
-
-        foreach ($entries as $entry) {
-            if ($entry['key'] === 'status') {
-                $statusValue = strtolower($entry['value']);
-            }
-        }
-
-        switch ($statusValue) {
-            case 'success':
-                $status = true;
-                $message = 'Payment completed successfully!';
-                break;
-            case 'pending':
-                $status = true;
-                $message = 'pending';
-                break;
-            default:
-                $status = false;
-                $message = 'Payment failed or was cancelled.';
-                break;
-        }
-
-        return response()->json([
-            'status' => $status,
-            'trx' => $coinLog->trx,
-            'message' => $message,
-        ]);
+//        die();
+//        $coinLog = CoinLog::where('trx', $trx)->whereMethod('utd')->firstOrFail();
+//
+//        if (!$trx) {
+//            return response()->json(['status' => 'error', 'message' => 'Missing transaction ID'], 400);
+//        }
+//
+//        $url = $this->baseUrl . '/api/restful/v2.0/Payment/inquiryPaymentResult.json';
+//        $body = [
+//            'inquiryPaymentRequest' => [
+//                'txnId' => $coinLog->trx,
+//                'apiKey' => $this->apiKey,
+//                'projectId' => $this->projectId,
+//                'needStatusFinal' => true,
+//            ],
+//        ];
+//
+//        $response = Http::withHeaders([
+//            'Content-Type' => 'application/json',
+//        ])->post($url, $body);
+//
+//        $json = $response->json();
+//
+//        $paymentResult = $json['paymentResult'] ?? null;
+//        $entries = $paymentResult['profile']['entry'] ?? [];
+//
+//        $statusValue = null;
+//
+//        foreach ($entries as $entry) {
+//            if ($entry['key'] === 'status') {
+//                $statusValue = strtolower($entry['value']);
+//            }
+//        }
+//
+//        switch ($statusValue) {
+//            case 'success':
+//                $status = true;
+//                $message = 'Payment completed successfully!';
+//                break;
+//            case 'pending':
+//                $status = true;
+//                $message = 'pending';
+//                break;
+//            default:
+//                $status = false;
+//                $message = 'Payment failed or was cancelled.';
+//                break;
+//        }
+//
+//        return response()->json([
+//            'status' => $status,
+//            'trx' => $coinLog->trx,
+//            'message' => $message,
+//        ]);
     }
 }
