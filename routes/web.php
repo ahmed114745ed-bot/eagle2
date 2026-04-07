e81c76af6a8327cf953f9413144be915e5ca42b5
shami2024 <>
2026-04-07 09:34:21 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2499,7 +2499,7 @@ Route::get('/fix-charges-usd', function () {
 //})->middleware('local');
 
 Route::get('test-done', function () {
-   return 15;
+   return 16;
 });
 
 Route::get('clean-duplicates', [\App\Admin\Controllers\CustomController::class, 'cleanDuplicates'])->name('clean.duplicates');
