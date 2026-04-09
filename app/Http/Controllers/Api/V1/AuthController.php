91f3d884f2980e1dbc66d0b8fcc132f5aae236ba
makled98 <mkled.backend@utdsoftware.com>
2026-04-09 12:35:56 +0200

    Update AuthController.php

--- app/Http/Controllers/Api/V1/AuthController.php
+++ app/Http/Controllers/Api/V1/AuthController.php
@@ -44,7 +44,6 @@ class AuthController extends Controller
         
         // Check device account limit BEFORE registration
         $deviceToken = $request->input('device_token');
-        ]);
         
         if (!empty($deviceToken)) {
             try {
