3a11a3c06496caef704dcee0cffb74dc9b42ad85
shami2024 <>
2026-04-21 10:14:01 +0200

    Update UpdateUserWhenSendGift.php

--- app/Classes/Gifts/UpdateUserWhenSendGift.php
+++ app/Classes/Gifts/UpdateUserWhenSendGift.php
@@ -36,7 +36,7 @@ class UpdateUserWhenSendGift
             }
 
             $updateData = [
-                'salary_is_updated' => true,
+                'salary_is_updated' => 1,
                 'total_diamond_received' => DB::raw("total_diamond_received + {$totalCoins}"),
             ];
 
