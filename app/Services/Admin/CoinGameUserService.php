3746d3f6b04d78c5b5b1e5f82825ce02b6beee3b
shami2024 <>
2026-04-26 17:22:26 +0300

    feat: add debug logging for user and round IDs in coin game orders link

--- app/Services/Admin/CoinGameUserService.php
+++ app/Services/Admin/CoinGameUserService.php
@@ -764,6 +764,8 @@ class CoinGameUserService
         $grid->column('first_played', __('Start Date'))->display(fn($v) => $v)->sortable();
         $grid->column('last_played', __('End Date'))->display(fn($v) => $v)->sortable();
         $grid->column('orders', __('Orders'))->display(function () use ($userId) {
+            \Log::info("userId:serv " . $userId);
+            \Log::info("round_id:serv " . $this->round_id);
             $url = admin_url("coin-game-users/round-orders?user_id={$userId}&round_id={$this->round_id}");
             return "<a href='{$url}' class='btn btn-sm btn-primary'>
                     <i class='fa fa-eye'></i> " . __('Orders') . "
