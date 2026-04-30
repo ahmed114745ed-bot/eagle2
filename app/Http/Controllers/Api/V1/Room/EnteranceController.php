56f8998883da8972c6a614887e44abeb6dfc6283
shami2024 <>
2026-04-30 13:08:08 +0300

    Fix all SQL injection (parameter binding in SearchRepository, CalcsTrait, Ranking, AgencyController)

--- app/Http/Controllers/Api/V1/Room/EnteranceController.php
+++ app/Http/Controllers/Api/V1/Room/EnteranceController.php
@@ -31,6 +31,7 @@ use App\Tik\Services\EnteranceRoomServices;
 use Illuminate\Support\Facades\Validator;
 use Modules\Charizma\Http\Services\UserCharismaService;
 use Modules\CP\Entities\CpRoomHistory;
+use Illuminate\Support\Facades\Crypt;
 
 class EnteranceController extends Controller
 {
@@ -115,6 +116,38 @@ class EnteranceController extends Controller
                 'server_secret' => $zego_server_secret,
                 'app_id' => $zego_app_id,
                 'app_sign' => $app_sign,
+                'filter' => $zego_filter_enabled == 1 ? true : false,
+                'live_type' => $liveTypes[@$liveLibrary ?? 0]
+            ],
+            'library' => $libraries[$library],
+            'is_auto_preview' => $is_auto_preview == 1 ? true : false,
+
+
+        ];
+        return Common::apiResponse(1, '', $data);
+    }
+
+    public function libraryAgoraZegoV2()
+    {
+        $agora_app_id = Common::getConfig('app_id');
+        $zego_server_secret = Common::zegoData('zego_server_secret');
+        $zego_app_id = Common::zegoData('zego_app_id');
+        $app_sign = Common::zegoData('zego_app_sign');
+        $library = Common::getConfig('video_library');
+        $liveLibrary = (int) Common::getConfig('live_library');
+        $zego_filter_enabled = Common::getConfig('zego_filter_enabled');
+        $is_auto_preview = (int) Common::getConfig('is_auto_preview');
+
+
+        $libraries = ['agora', 'zego', 'tencent', 'utd zego'];
+        $liveTypes = ['RTC', 'CDN', 'L3'];
+
+        $data = [
+            'agora_app_id' => Crypt::encryptString($agora_app_id),
+            'zego' => [
+                'server_secret' => Crypt::encryptString($zego_server_secret),
+                'app_id' => Crypt::encryptString($zego_app_id),
+                'app_sign' => Crypt::encryptString($app_sign),
                 'filter' => $zego_filter_enabled == 1 ? true : false,
                 'live_type' => $liveTypes[@$liveLibrary ?? 0]
             ],
