6f180e3f3cab78c4b979c9a0f85309675c20cb20
shami2024 <>
2026-05-04 11:32:30 +0300

    feat: add default agency_id filter and update local settings  - Default agency_id to user's agency_id when not provided in   UserController gift query - Add mysql and composer bash permissions to local Claude settings - Add *.md files to .gitignore - Remove unnecessary Arabic comment in agency profile view

--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -559,7 +559,7 @@ class UserController extends MainController
                 $giftType = request('gift_type', 'receiver');
                 $start = request('start_at');
                 $end = request('end_at');
-                $agencyId = request('agency_id');
+                $agencyId = request('agency_id', $user?->agency_id);
                 $timezone = Common::timeZone();
 
                 $giftBaseQuery = GiftLog::query()
