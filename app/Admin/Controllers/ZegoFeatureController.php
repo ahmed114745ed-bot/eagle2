31924663766ffb86a2aecf3b80433c3aabb4b56f
shami2024 <>
2026-05-06 09:50:34 +0300

    rm logs

--- app/Admin/Controllers/ZegoFeatureController.php
+++ app/Admin/Controllers/ZegoFeatureController.php
@@ -39,7 +39,6 @@ class ZegoFeatureController extends MainController
     public function zegoKey(Request $request)
     {
         $keys = 'zego_feature';
-        \Log::info('ZegoFeatureController@zegoKey', ['request' => $request->all()]);
         $setting = Setting::where('key', $keys)->first();
         if ($setting) {
             if ($request->is_active == 1){
