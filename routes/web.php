304649a1d777ac75989fa0f1d1fa7329b9118c74
shami2024 <>
2026-05-18 15:57:11 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2972,7 +2972,6 @@ Route::get('/set-lucky-version-7', function () {
 Route::get('/fix-gift-prices/preview', function () {
     $affectedRecords = DB::table('gift_logs as gl')
         ->join('gifts as g', 'g.id', '=', 'gl.giftId')
-        ->where('gl.giftNum', '<>', 1)
         ->where('g.gift_category_id', 1)
         ->where('gl.created_at', '>=', '2026-05-01 00:00:00')
         ->whereRaw('gl.giftPrice != (gl.giftNum * gl.total)')
@@ -3004,7 +3003,6 @@ Route::get('/fix-gift-prices/execute', function () {
         // Get IDs of affected records
         $affectedIds = DB::table('gift_logs as gl')
             ->join('gifts as g', 'g.id', '=', 'gl.giftId')
-            ->where('gl.giftNum', '<>', 1)
             ->where('g.gift_category_id', 1)
             ->where('gl.created_at', '>=', '2026-05-01 00:00:00')
             ->whereRaw('gl.giftPrice != (gl.giftNum * gl.total)')
