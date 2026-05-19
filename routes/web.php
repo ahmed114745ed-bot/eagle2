61256148fef73be93276a1032f0bbdb7181e412a
shami2024 <>
2026-05-19 13:28:39 +0300

    feat: implement fix for monthly diamond discrepancies and add atomic upsert for race condition safety

--- routes/web.php
+++ routes/web.php
@@ -550,6 +550,7 @@ Route::get('/send-notification/{id}', function ($id) {
 });
 
  Route::get('/calculate-monthly-diamonds', [\App\Http\Controllers\DiamondController::class, 'calculateMonthlyDiamondReceived']);
+ Route::get('/fix-monthly-diamond-discrepancies', [\App\Http\Controllers\DiamondController::class, 'fixMonthlyDiamondDiscrepancies']);
 // Route::get('/calculate-salary' , [\App\Http\Controllers\DiamondController::class, 'calculateSalary']);
 // Route::get('/v2/calculate-salary', [\App\Http\Controllers\DiamondController::class, 'calculateSalaryV2']);
 // Route::get('monthly-diamond-receive', [\App\Http\Controllers\DiamondController::class, 'copyMonthlyDiamondReceive']);
